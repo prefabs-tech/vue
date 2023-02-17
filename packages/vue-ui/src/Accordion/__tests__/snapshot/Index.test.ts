2ef2465e12de8ed357decdc729c5a89ecd31189f
sachin <sachin.shilwal@dzangolab.com>
2023-02-17 15:46:26 +0545

    chore: add snapshot test

---
+++ packages/vue-ui/src/Accordion/__tests__/snapshot/Index.test.ts
@@ -0,0 +1,57 @@
+import { mount } from "@vue/test-utils";
+import { describe, expect, it } from "vitest";
+
+import Index from "../../Index.vue";
+import SubPane from "../../SubPane.vue";
+
+describe("Accordion", () => {
+  it("renders correctly with slot", () => {
+    const wrapper = mount(Index, {
+      global: {
+        components: { SubPane },
+      },
+      slots: {
+        default: `
+            <SubPane title="Subpane 1" icon="home.svg">
+            <p>Subpan 1 content</p>
+            </SubPane>
+            <SubPane title="Subpane 2">
+            <p>Subpane 2 content</p>
+            </SubPane>
+            <SubPane title="Subpane 3">
+            <p>Subpane 3 content</p>
+            </SubPane>`,
+      },
+    });
+
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  it("renders correctly with props", () => {
+    const wrapper = mount(Index, {
+      global: {
+        components: { SubPane },
+      },
+      props: {
+        activeIcon: "upchevron.svg",
+        direction: "horizontal",
+        inactiveIcon: "downchevron.svg",
+        defaultIndex: 1,
+      },
+      slots: {
+        default: `
+            <SubPane title="Subpane 1" icon="home.svg">
+            <p>Subpan 1 content</p>
+            </SubPane>
+            <SubPane title="Subpane 2">
+            <p>Subpane 2 content</p>
+            </SubPane>
+            <SubPane title="Subpane 3">
+            <p>Subpane 3 content</p>
+            </SubPane>`,
+      },
+    });
+
+    expect(wrapper.element).toMatchSnapshot();
+  });
+});
