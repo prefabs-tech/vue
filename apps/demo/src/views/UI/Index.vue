c0188d35307f46c021a31c60eed1b7c220c53ede
sachin <sachin.shilwal@dzangolab.com>
2023-02-24 11:03:56 +0545

    chore: add demo

--- apps/demo/src/views/UI/Index.vue
+++ apps/demo/src/views/UI/Index.vue
@@ -1,9 +1,19 @@
 <template>
-  <Page :title="$t('ui.title')"> </Page>
+  <Page :title="$t('ui.title')">
+    <h2>{{ $t("ui.subpane.title") }}</h2>
+
+    <SubPane>
+      <p>{{ $t("ui.subpane.content") }}</p>
+    </SubPane>
+  </Page>
 </template>
 
 <script lang="ts">
 export default {
   name: "UI",
 };
+</script>
+
+<script setup lang="ts">
+import { SubPane } from "@dzangolab/vue3-ui";
 </script>
