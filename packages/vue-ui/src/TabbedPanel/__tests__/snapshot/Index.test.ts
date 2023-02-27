e667b113c54316b772ffd9424c4cfbab85fefbe6
sachin <sachin.shilwal@dzangolab.com>
2023-02-27 16:44:02 +0545

    chore: snapshot test for basic tabbed panel

---
+++ packages/vue-ui/src/TabbedPanel/__tests__/snapshot/Index.test.ts
@@ -0,0 +1,26 @@
+import { mount } from "@vue/test-utils";
+import { describe, expect, it } from "vitest";
+
+import SubPane from "../../../components/SubPane.vue";
+import Index from "../../Index.vue";
+
+describe("TabbedPanel", () => {
+  it("matches snapshot", () => {
+    const wrapper = mount(Index, {
+      global: {
+        components: { SubPane },
+      },
+      slots: {
+        default: `
+            <SubPane title="Subpane 1" icon="home.svg">
+            <p>Tabbed Panel 1 content</p>
+            </SubPane>
+            <SubPane title="Subpane 2">
+            <p>Tabbed Panel 2 content</p>
+            </SubPane>`,
+      },
+    });
+
+    expect(wrapper.element).toMatchSnapshot();
+  });
+});
