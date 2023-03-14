aa383453e61d7d722599a486dc27001a70800bc9
Serwes Bhatta <serwes@dzangolab.com>
2023-03-14 15:39:26 +0545

    feat: create table by passing columns and rows as props

--- apps/demo/src/views/UI/EasyTable.vue
+++ apps/demo/src/views/UI/EasyTable.vue
@@ -1,5 +1,5 @@
 <template>
-  <EasyTable />
+  <EasyTable :columns="columns" :rows="rows" />
 </template>
 
 <script lang="ts">
@@ -10,4 +10,54 @@ export default {
 
 <script setup lang="ts">
 import { EasyTable } from "@dzangolab/vue3-easy-table";
+
+const columns = [
+  { text: "PLAYER", value: "player", sortable: true },
+  { text: "TEAM", value: "team" },
+  { text: "NUMBER", value: "number" },
+  { text: "POSITION", value: "position" },
+  { text: "HEIGHT", value: "indicator.height" },
+  { text: "WEIGHT (lbs)", value: "indicator.weight", sortable: true },
+  { text: "LAST ATTENDED", value: "lastAttended", width: 200 },
+  { text: "COUNTRY", value: "country" },
+];
+
+const rows = [
+  {
+    player: "Stephen Curry",
+    team: "GSW",
+    number: 30,
+    position: "G",
+    indicator: { height: "6-2", weight: 185 },
+    lastAttended: "Davidson",
+    country: "USA",
+  },
+  {
+    player: "Lebron James",
+    team: "LAL",
+    number: 6,
+    position: "F",
+    indicator: { height: "6-9", weight: 250 },
+    lastAttended: "St. Vincent-St. Mary HS (OH)",
+    country: "USA",
+  },
+  {
+    player: "Kevin Durant",
+    team: "BKN",
+    number: 7,
+    position: "F",
+    indicator: { height: "6-10", weight: 240 },
+    lastAttended: "Texas-Austin",
+    country: "USA",
+  },
+  {
+    player: "Giannis Antetokounmpo",
+    team: "MIL",
+    number: 34,
+    position: "F",
+    indicator: { height: "6-11", weight: 242 },
+    lastAttended: "Filathlitikos",
+    country: "Greece",
+  },
+];
 </script>
--- packages/vue-easy-table/src/components/EasyTable.vue
+++ packages/vue-easy-table/src/components/EasyTable.vue
@@ -2,8 +2,8 @@
   <div class="table">
     <EasyDataTable
       buttons-pagination
-      :headers="headers"
-      :items="items"
+      :headers="columns"
+      :items="rows"
       :hide-footer="true"
     />
   </div>
@@ -16,57 +16,22 @@ export default {
 </script>
 
 <script setup lang="ts">
+import { PropType, ref } from "vue";
 import EasyDataTable from "vue3-easy-data-table";
 
-import type { Header, Item } from "vue3-easy-data-table";
+import type { Column, Row } from "../types";
 
-const headers: Header[] = [
-  { text: "PLAYER", value: "player", sortable: true },
-  { text: "TEAM", value: "team" },
-  { text: "NUMBER", value: "number" },
-  { text: "POSITION", value: "position" },
-  { text: "HEIGHT", value: "indicator.height" },
-  { text: "WEIGHT (lbs)", value: "indicator.weight", sortable: true },
-  { text: "LAST ATTENDED", value: "lastAttended", width: 200 },
-  { text: "COUNTRY", value: "country" },
-];
+const props = defineProps({
+  columns: {
+    type: Array as PropType<Column[]>,
+    default: () => [],
+  },
+  rows: {
+    type: Array as PropType<Row[]>,
+    default: () => [],
+  },
+});
 
-const items: Item[] = [
-  {
-    player: "Stephen Curry",
-    team: "GSW",
-    number: 30,
-    position: "G",
-    indicator: { height: "6-2", weight: 185 },
-    lastAttended: "Davidson",
-    country: "USA",
-  },
-  {
-    player: "Lebron James",
-    team: "LAL",
-    number: 6,
-    position: "F",
-    indicator: { height: "6-9", weight: 250 },
-    lastAttended: "St. Vincent-St. Mary HS (OH)",
-    country: "USA",
-  },
-  {
-    player: "Kevin Durant",
-    team: "BKN",
-    number: 7,
-    position: "F",
-    indicator: { height: "6-10", weight: 240 },
-    lastAttended: "Texas-Austin",
-    country: "USA",
-  },
-  {
-    player: "Giannis Antetokounmpo",
-    team: "MIL",
-    number: 34,
-    position: "F",
-    indicator: { height: "6-11", weight: 242 },
-    lastAttended: "Filathlitikos",
-    country: "Greece",
-  },
-];
+const columns = ref(props.columns);
+const rows = ref(props.rows);
 </script>
