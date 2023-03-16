4b46b52aab1e771cb6823a714e695dacf517b79b
sachinshilwal <sachin.shilwal@dzangolab.com>
2023-03-16 10:02:11 +0545

    fix: fix test cases for Email and Password component

--- packages/vue-form/src/components/__test__/snapshot/Email.test.ts
+++ packages/vue-form/src/components/__test__/snapshot/Email.test.ts
@@ -1,12 +1,26 @@
+import i18nPlugin, { useLocaleStore } from "@dzangolab/vue3-i18n";
 import { mount, RouterLinkStub } from "@vue/test-utils";
 import { describe, expect, it } from "vitest";
 
 import Email from "../../Email.vue";
+import appConfig from "../config";
 
 describe("Email", () => {
+  const { setLocale } = useLocaleStore(appConfig.slug);
+
+  setLocale("en");
+
   it("matches snapshot", () => {
     const wrapper = mount(Email, {
       global: {
+        plugins: [
+          [
+            i18nPlugin,
+            {
+              config: appConfig,
+            },
+          ],
+        ],
         stubs: {
           RouterLink: RouterLinkStub,
         },
--- packages/vue-form/src/components/__test__/snapshot/Password.test.ts
+++ packages/vue-form/src/components/__test__/snapshot/Password.test.ts
@@ -1,12 +1,26 @@
+import i18nPlugin, { useLocaleStore } from "@dzangolab/vue3-i18n";
 import { mount, RouterLinkStub } from "@vue/test-utils";
 import { describe, expect, it } from "vitest";
 
 import Password from "../../Password.vue";
+import appConfig from "../config";
 
 describe("Password", () => {
+  const { setLocale } = useLocaleStore(appConfig.slug);
+
+  setLocale("en");
+
   it("matches snapshot", () => {
     const wrapper = mount(Password, {
       global: {
+        plugins: [
+          [
+            i18nPlugin,
+            {
+              config: appConfig,
+            },
+          ],
+        ],
         stubs: {
           RouterLink: RouterLinkStub,
         },
