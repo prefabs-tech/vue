b90b5303f2a8830212be0bb7da2fd65cd0c235cb
serwes12 <100108325+serwes12@users.noreply.github.com>
2023-03-17 14:11:11 +0545

    feat: create table by passing columns and rows as props (#205)

--- packages/vue-easy-table/src/components/EasyTable.vue
+++ packages/vue-easy-table/src/components/EasyTable.vue
@@ -2,9 +2,9 @@
   <div class="table">
     <EasyDataTable
       buttons-pagination
-      :headers="headers"
+      :headers="columns"
       :hide-footer="true"
-      :items="items"
+      :items="rows"
     />
   </div>
 </template>
@@ -16,51 +16,22 @@ export default {
 </script>
 
 <script setup lang="ts">
+import { PropType, reactive } from "vue";
 import EasyDataTable from "vue3-easy-data-table";
 
-import type { Header, Item } from "vue3-easy-data-table";
+import type { Column, Row } from "../types";
 
-const headers: Header[] = [
-  { sortable: true, text: "PLAYER", value: "name" },
-  { text: "COUNTRY", value: "country" },
-  { text: "LAST ATTENDED", value: "lastAttended", width: 200 },
-  { text: "NUMBER", value: "number" },
-  { text: "POSITION", value: "position" },
-  { text: "TEAM", value: "team" },
-];
+const props = defineProps({
+  columns: {
+    type: Array as PropType<Column[]>,
+    default: () => [],
+  },
+  rows: {
+    type: Array as PropType<Row[]>,
+    default: () => [],
+  },
+});
 
-const items: Item[] = [
-  {
-    name: "Stephen Curry",
-    country: "USA",
-    lastAttended: "Davidson",
-    number: 30,
-    position: "G",
-    team: "GSW",
-  },
-  {
-    name: "Lebron James",
-    country: "USA",
-    lastAttended: "St. Vincent-St. Mary HS (OH)",
-    number: 6,
-    position: "F",
-    team: "LAL",
-  },
-  {
-    name: "Kevin Durant",
-    country: "USA",
-    lastAttended: "Texas-Austin",
-    number: 7,
-    position: "F",
-    team: "BKN",
-  },
-  {
-    name: "Giannis Antetokounmpo",
-    country: "Greece",
-    lastAttended: "Filathlitikos",
-    number: 34,
-    position: "F",
-    team: "MIL",
-  },
-];
+const columns = reactive(props.columns);
+const rows = reactive(props.rows);
 </script>
---
+++ packages/vue-easy-table/src/types/index.ts
@@ -0,0 +1,6 @@
+import type { Header, Item } from "vue3-easy-data-table";
+
+type Column = Header;
+type Row = Item;
+
+export type { Column, Row };
