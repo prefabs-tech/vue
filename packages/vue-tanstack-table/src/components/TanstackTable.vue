cccec352c1409f98a2046f3099547389f0dacb88
Serwes Bhatta <serwes@dzangolab.com>
2023-03-23 16:19:26 +0545

    feat(vue-tanstack-table): add pagination to the table

--- packages/vue-tanstack-table/src/components/TanstackTable.vue
+++ packages/vue-tanstack-table/src/components/TanstackTable.vue
@@ -1,55 +1,108 @@
 <template>
-  <table :style="`width: ${table.getCenterTotalSize()}`">
-    <div v-if="props.enableToggle" class="toggle-wrap">
-      <button class="toggle" @click="toggleHeader">
-        <Icon icon="mingcute:menu-fill" class="toggle-icon" />
-      </button>
-    </div>
-    <thead :class="{ expand: expand }">
-      <tr v-for="headerGroup in table.getHeaderGroups()" :key="headerGroup.id">
-        <th
-          v-for="header in headerGroup.headers"
-          :key="header.id"
-          :colSpan="header.colSpan"
-          :style="`width: ${header.getSize()}px`"
+  <div class="table-wrapper">
+    <table :style="`width: ${table.getCenterTotalSize()}`">
+      <div v-if="props.enableToggle" class="toggle-wrap">
+        <button class="toggle" @click="toggleHeader">
+          <Icon icon="mingcute:menu-fill" class="toggle-icon" />
+        </button>
+      </div>
+      <thead :class="{ expand: expand }">
+        <tr
+          v-for="headerGroup in table.getHeaderGroups()"
+          :key="headerGroup.id"
         >
-          <div
-            :className="`resizer ${
-              header.column.getIsResizing() ? 'isResizing' : ''
-            }`"
-            :style="`
-              transform:
-                ${header.column.getIsResizing()}
-                  ? translateX(${
-                    table.getState().columnSizingInfo.deltaOffset
-                  }px)
-                  : ''`"
-            @mousedown="header.getResizeHandler()"
+          <th
+            v-for="header in headerGroup.headers"
+            :key="header.id"
+            :colSpan="header.colSpan"
+            :style="`width: ${header.getSize()}px`"
+          >
+            <div
+              :className="`resizer ${
+                header.column.getIsResizing() ? 'isResizing' : ''
+              }`"
+              :style="`
+                transform:
+                  ${header.column.getIsResizing()}
+                    ? translateX(${
+                      table.getState().columnSizingInfo.deltaOffset
+                    }px)
+                    : ''`"
+              @mousedown="header.getResizeHandler()"
+            >
+              <FlexRender
+                v-if="!header.isPlaceholder"
+                :props="header.getContext()"
+                :render="header.column.columnDef.header"
+              />
+            </div>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="row in table.getRowModel().rows" :key="row.id">
+          <td
+            v-for="cell in row.getVisibleCells()"
+            :key="cell.id"
+            :data-label="cell.column.id"
           >
             <FlexRender
-              v-if="!header.isPlaceholder"
-              :props="header.getContext()"
-              :render="header.column.columnDef.header"
+              :props="cell.getContext()"
+              :render="cell.column.columnDef.cell"
             />
-          </div>
-        </th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="row in table.getRowModel().rows" :key="row.id">
-        <td
-          v-for="cell in row.getVisibleCells()"
-          :key="cell.id"
-          :data-label="cell.column.id"
-        >
-          <FlexRender
-            :props="cell.getContext()"
-            :render="cell.column.columnDef.cell"
-          />
-        </td>
-      </tr>
-    </tbody>
-  </table>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <button
+      @click="table.setPageIndex(0)"
+      :disabled="!table.getCanPreviousPage()"
+    >
+      <Icon icon="material-symbols:keyboard-double-arrow-left-rounded" />
+    </button>
+    <button
+      @click="table.previousPage()"
+      :disabled="!table.getCanPreviousPage()"
+    >
+      <Icon icon="material-symbols:arrow-back-ios-new-rounded" />
+    </button>
+    <button @click="table.nextPage()" :disabled="!table.getCanNextPage()">
+      <Icon icon="material-symbols:arrow-forward-ios-rounded" />
+    </button>
+    <button
+      @click="table.setPageIndex(table.getPageCount() - 1)"
+      :disabled="!table.getCanNextPage()"
+    >
+      <Icon icon="material-symbols:keyboard-double-arrow-right-rounded" />
+    </button>
+    <span
+      >Page
+      <span class="page-number-indicators"
+        >{{ table.getState().pagination.pageIndex + 1 }} of
+        {{ table.getPageCount() }}</span
+      ></span
+    >
+    <span
+      >| Go to page:
+      <input
+        type="number"
+        :value="table.getState().pagination.pageIndex + 1"
+        :max="table.getPageCount()"
+        @input="onPageChange($event)"
+    /></span>
+    <select
+      :value="table.getState().pagination.pageSize"
+      @change="setPageSize($event)"
+    >
+      <option
+        v-for="pageSize in [10, 20, 30, 40, 50]"
+        :key="pageSize"
+        :value="pageSize"
+      >
+        Show {{ pageSize }}
+      </option>
+    </select>
+  </div>
 </template>
 
 <script lang="ts">
@@ -64,6 +117,8 @@ import {
   createColumnHelper,
   FlexRender,
   getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
   useVueTable,
 } from "@tanstack/vue-table";
 import { PropType, ref } from "vue";
@@ -110,11 +165,25 @@ const table = useVueTable({
   columnResizeMode: "onChange",
   data: props.rows,
   getCoreRowModel: getCoreRowModel(),
+  getFilteredRowModel: getFilteredRowModel(),
+  getPaginationRowModel: getPaginationRowModel(),
 });
 
 const expand = ref(false);
 
 const toggleHeader = () => {
   expand.value = !expand.value;
+};
+
+const onPageChange = (e: Event) => {
+  const input = e.target as HTMLInputElement;
+  const page = input.value ? Number(input.value) - 1 : 0;
+  table.setPageIndex(page);
+};
+
+const setPageSize = (e: Event) => {
+  const input = e.target as HTMLInputElement;
+  const pageSize = input.value ? Number(input.value) : 10;
+  table.setPageSize(pageSize);
 };
 </script>
