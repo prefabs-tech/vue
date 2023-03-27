a608d73ed4d360a228121382449112ce71cccd58
Deepak <deepak@dzangolab.com>
2023-03-27 17:31:52 +0545

    vue/tanstack-table: add table component (#229)

--- apps/demo/src/views/UI/Index.vue
+++ apps/demo/src/views/UI/Index.vue
@@ -1,5 +1,13 @@
 <template>
-  <Page :title="$t('ui.title')"> </Page>
+  <Page :title="$t('ui.title')">
+    <ul>
+      <li>
+        <router-link :to="{ name: 'table' }">
+          {{ $t("ui.pages.table") }}
+        </router-link>
+      </li>
+    </ul>
+  </Page>
 </template>
 
 <script lang="ts">
@@ -7,3 +15,5 @@ export default {
   name: "UI",
 };
 </script>
+
+<script setup lang="ts"></script>
