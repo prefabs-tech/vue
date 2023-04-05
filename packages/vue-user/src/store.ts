86132969c9945cbcf32a706e56193bafc55fe210
Sachin <34100453+sachinshilwal@users.noreply.github.com>
2023-04-05 16:23:43 +0545

    refactor: refactor getUser function and other logic to use localstorage (#237)

--- packages/vue-user/src/store.ts
+++ packages/vue-user/src/store.ts
@@ -16,11 +16,25 @@ import type {
   User,
 } from "./types";
 
+const USER_KEY = "user";
+
 const useUserStore = defineStore("user", () => {
   const user = ref<User | undefined>(undefined);
 
+  const getUser = (): User => {
+    if (user.value) {
+      return user.value;
+    }
+
+    const data = localStorage.getItem(USER_KEY);
+
+    return data ? JSON.parse(data) : undefined;
+  };
+
   const login = async (credentials: LoginCredentials) => {
-    user.value = await doLogin(credentials);
+    const response = await doLogin(credentials);
+
+    setUser(response);
   };
 
   const logout = async () => {
@@ -31,6 +45,8 @@ const useUserStore = defineStore("user", () => {
       document.cookie =
         "sFrontToken=; Max-Age=0; path=/; domain=" + location.hostname;
     });
+
+    localStorage.removeItem(USER_KEY);
   };
 
   const requestPasswordReset = async (
@@ -45,11 +61,20 @@ const useUserStore = defineStore("user", () => {
     return doResetPassword(payload);
   };
 
+  const setUser = (userData: User | undefined) => {
+    user.value = userData;
+
+    localStorage.setItem(USER_KEY, JSON.stringify(userData));
+  };
+
   const signup = async (credentials: LoginCredentials): Promise<void> => {
-    user.value = await doSignup(credentials);
+    const response = await doSignup(credentials);
+
+    setUser(response);
   };
 
   return {
+    getUser,
     login,
     logout,
     resetPassword,
