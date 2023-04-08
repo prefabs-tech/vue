921d7354d8e6845addbd66aa2db1b24ea4c81092
Olivier Pichon <op@dzango.com>
2023-04-08 18:58:32 +0200

    refact(ui): Refactor Accordion c omponent and accordion demo page

---
+++ apps/demo/src/views/UI/accordion/AllSection.vue
@@ -0,0 +1,34 @@
+<template>
+  <section>
+    <h2>{{ $t("ui.accordion.all") }}</h2>
+
+    <Accordion
+      :default-index="2"
+      :can-self-collapse="true"
+      active-icon="/downchevron.svg"
+      inactive-icon="/upchevron.svg"
+    >
+      <SubPane :title="$t('ui.accordion.contents.1.title')" icon="/home.svg">
+        <p>{{ $t("ui.accordion.contents.1.content") }}</p>
+      </SubPane>
+
+      <SubPane :title="$t('ui.accordion.contents.2.title')" icon="/home.svg">
+        <p>{{ $t("ui.accordion.contents.2.content") }}</p>
+      </SubPane>
+
+      <SubPane :title="$t('ui.accordion.contents.3.title')" icon="/home.svg">
+        <p>{{ $t("ui.accordion.contents.3.content") }}</p>
+      </SubPane>
+    </Accordion>
+  </section>
+</template>
+
+<script lang="ts">
+export default {
+  name: "AccordionPage",
+};
+</script>
+
+<script lang="ts" setup>
+import { Accordion, SubPane } from "@dzangolab/vue3-ui";
+</script>
---
+++ apps/demo/src/views/UI/accordion/Index.vue
@@ -0,0 +1,33 @@
+<template>
+  <Page :title="$t('ui.accordion.title')">
+    <BasicSection />
+    <DefaultIndexSection />
+    <SelfCollapseSection />
+    <DirectionSection />
+    <IconsSection />
+    <AllSection />
+    <SlotsSection />
+
+    <RouterLink :to="{ name: 'ui' }">
+      {{ $t("common.back") }}
+    </RouterLink>
+  </Page>
+</template>
+
+<script lang="ts">
+export default {
+  name: "AccordionPage",
+};
+</script>
+
+<script lang="ts" setup>
+import { Page } from "@dzangolab/vue3-ui";
+
+import AllSection from "./AllSection.vue";
+import BasicSection from "./BasicSection.vue";
+import DefaultIndexSection from "./DefaultIndexSection.vue";
+import DirectionSection from "./DirectionSection.vue";
+import IconsSection from "./IconsSection.vue";
+import SelfCollapseSection from "./SelfCollapseSection.vue";
+import SlotsSection from "./SlotsSection.vue";
+</script>
