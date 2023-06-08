a0739bccfe230e4a7e90ac1e356a9cf14fab162a
sachinshilwal <sachin.shilwal@dzangolab.com>
2023-06-08 14:27:52 +0545

    feat: implement basic sign in with google

---
+++ packages/vue-user/src/components/AuthGoogleCallback.vue
@@ -0,0 +1,39 @@
+<template>
+  <div v-if="loading">Loading ...</div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "AuthGoogleCallBack",
+};
+</script>
+
+<script lang="ts" setup>
+import { storeToRefs } from "pinia";
+import ThirdPartyEmailPassword from "supertokens-web-js/recipe/thirdpartyemailpassword";
+import { onMounted, ref } from "vue";
+import { useRouter } from "vue-router";
+
+import useUserStore from "../store";
+
+const loading = ref(false);
+const router = useRouter();
+
+const { user } = storeToRefs(useUserStore());
+
+onMounted(async () => {
+  loading.value = true;
+
+  const response = await ThirdPartyEmailPassword.thirdPartySignInAndUp({});
+
+  if (response.status !== "OK") {
+    return window.location.assign("/auth?error=signin");
+  }
+
+  user.value = response.user;
+
+  router.push({ name: "home" });
+
+  loading.value = false;
+});
+</script>
