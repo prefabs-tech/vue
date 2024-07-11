1711d3e04e150cd406696cda96156ef4d206c893
kabinthakuri <kabinkhand@gmail.com>
2024-07-11 11:45:34 +0545

    fix: resolve issue of throwing same error

--- packages/vue-user/src/supertokens/login.ts
+++ packages/vue-user/src/supertokens/login.ts
@@ -6,31 +6,34 @@ const login = async (
   credentials: LoginCredentials
 ): Promise<User | undefined> => {
   let user: User | undefined;
+  let response;
+
+  const data = {
+    formFields: [
+      {
+        id: "email",
+        value: credentials.email as string,
+      },
+      {
+        id: "password",
+        value: credentials.password as string,
+      },
+    ],
+  };
 
   try {
-    const response = await emailPasswordSignIn({
-      formFields: [
-        {
-          id: "email",
-          value: credentials.email as string,
-        },
-        {
-          id: "password",
-          value: credentials.password as string,
-        },
-      ],
-    });
+    response = await emailPasswordSignIn(data);
+  } catch (error) {
+    throw new Error("SOMETHING_WRONG");
+  }
 
-    if (response.status === "OK") {
-      user = response.user;
-    } else if (response.status === "WRONG_CREDENTIALS_ERROR") {
-      throw new Error("401");
-    } else {
-      throw new Error("SOMETHING_WRONG");
-    }
+  if (response.status === "OK") {
+    user = response.user;
 
     return user;
-  } catch (error) {
+  } else if (response.status === "WRONG_CREDENTIALS_ERROR") {
+    throw new Error("401");
+  } else {
     throw new Error("SOMETHING_WRONG");
   }
 };
