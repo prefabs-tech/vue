cf12083a427f2c7ddf916183e13dbbe2430d4f95
DeepakAryal <deepakaryal2050@gmail.com>
2024-08-08 07:05:47 +0000

    Deploying to gh-pages from @ dzangolab/vue@fc70fb7ccc33bd93e196fde1eb5634d387182863 🚀

--- assets/Index-617a1e2e.js
+++ assets/Index-617a1e2e.js
@@ -1,4 +1,4 @@
-import{o as c,b as v,G as h,H as f,a as i,d as m,c as d,w as o,y as e,f as n,i as q,u as p,I as _,t,J as b,r as $}from"./index-5109e171.js";import{_ as A}from"./_plugin-vue_export-helper-c27b6911.js";const S={name:"TestComponent"};const k=u=>(h("data-v-aeb7ff3f"),u=u(),f(),u),w={class:"test"},C=k(()=>i("p",null,"This is a test component",-1)),I=[C];function D(u,s,l,g,J,j){return c(),v("div",w,I)}const z=A(S,[["render",D],["__scopeId","data-v-aeb7ff3f"]]),N=i("div",{title:"Sagittis a curabitur"},[i("p",null,`
+import{o as c,y as v,N as h,O as q,a as i,d as m,c as d,w as o,K as e,B as n,E as f,u as p,P as _,z as t,Q as b,r as $}from"./index-bd69932c.js";import{_ as A}from"./_plugin-vue_export-helper-c27b6911.js";const S={name:"TestComponent"};const k=u=>(h("data-v-aeb7ff3f"),u=u(),q(),u),w={class:"test"},C=k(()=>i("p",null,"This is a test component",-1)),z=[C];function D(u,s,l,g,Q,j){return c(),v("div",w,z)}const I=A(S,[["render",D],["__scopeId","data-v-aeb7ff3f"]]),N=i("div",{title:"Sagittis a curabitur"},[i("p",null,`
         A viverra adipiscing consequat vel non potenti ac vel consectetur
         tristique a integer suspendisse condimentum. Id donec cum aliquet a orci
         nec arcu ullamcorper fusce sodales pulvinar id tristique risus curae
@@ -22,11 +22,11 @@ import{o as c,b as v,G as h,H as f,a as i,d as m,c as d,w as o,y as e,f as n,i a
         adipiscing eu hac nisi feugiat scelerisque elementum cum parturient
         maecenas a cum. Nisl a leo litora amet elementum nam leo adipiscing erat
         a mollis proin adipiscing adipiscing eros pretium tristique.
-      `)],-1),T=i("div",{title:"Diam a parturient sociosque"},[i("p",null,`
+      `)],-1),B=i("div",{title:"Diam a parturient sociosque"},[i("p",null,`
         Potenti lacus suscipit in ac adipiscing consectetur suspendisse dui
         aliquam egestas scelerisque porttitor nullam ad a elementum dignissim
         pulvinar.
-      `),e(),i("ul",{role:"list"},[i("li",null,"Vestibulum parturient"),e(),i("li",null,"Ullamcorper orci condimentum"),e(),i("li",null,"Sodales scelerisque ultrices")])],-1),V={title:"component"},y={name:"DemoAccordion"},a=m({...y,setup(u){return(s,l)=>(c(),d(p(_),null,{default:o(()=>[N,e(),P,e(),T,e(),i("div",V,[n(z)]),e(),q(s.$slots,"extra-pane")]),_:3}))}}),B=i("div",{icon:"/src/assets/svg/number-1.svg",title:"Sagittis a curabitur"},[i("p",null,`
+      `),e(),i("ul",{role:"list"},[i("li",null,"Vestibulum parturient"),e(),i("li",null,"Ullamcorper orci condimentum"),e(),i("li",null,"Sodales scelerisque ultrices")])],-1),T={title:"component"},V={name:"DemoAccordion"},a=m({...V,setup(u){return(s,l)=>(c(),d(p(_),null,{default:o(()=>[N,e(),P,e(),B,e(),i("div",T,[n(I)]),e(),f(s.$slots,"extra-pane")]),_:3}))}}),y=i("div",{icon:"/src/assets/svg/number-1.svg",title:"Sagittis a curabitur"},[i("p",null,`
         A viverra adipiscing consequat vel non potenti ac vel consectetur
         tristique a integer suspendisse condimentum. Id donec cum aliquet a orci
         nec arcu ullamcorper fusce sodales pulvinar id tristique risus curae
@@ -35,7 +35,7 @@ import{o as c,b as v,G as h,H as f,a as i,d as m,c as d,w as o,y as e,f as n,i a
         Adipiscing quis adipiscing interdum orci interdum eu tincidunt dictum
         erat amet a ullamcorper montes vestibulum consequat tempus feugiat
         inceptos suspendisse dis class a at hac ullamcorper.
-      `)],-1),L=i("div",{icon:"/src/assets/svg/number-2.svg",title:"Diam mus id per posuere vivamus"},[i("p",null,`
+      `)],-1),E=i("div",{icon:"/src/assets/svg/number-2.svg",title:"Diam mus id per posuere vivamus"},[i("p",null,`
         Libero ultrices vestibulum adipiscing enim venenatis lobortis ipsum a
         nam scelerisque condimentum sociis ligula parturient posuere enim a.
         Consectetur a parturient torquent natoque quisque dui eu erat adipiscing
@@ -50,12 +50,12 @@ import{o as c,b as v,G as h,H as f,a as i,d as m,c as d,w as o,y as e,f as n,i a
         adipiscing eu hac nisi feugiat scelerisque elementum cum parturient
         maecenas a cum. Nisl a leo litora amet elementum nam leo adipiscing erat
         a mollis proin adipiscing adipiscing eros pretium tristique.
-      `)],-1),U=i("div",{icon:"/src/assets/svg/number-3.svg",title:"Diam a parturient sociosque"},[i("p",null,`
+      `)],-1),L=i("div",{icon:"/src/assets/svg/number-3.svg",title:"Diam a parturient sociosque"},[i("p",null,`
         Potenti lacus suscipit in ac adipiscing consectetur suspendisse dui
         aliquam egestas scelerisque porttitor nullam ad a elementum dignissim
         pulvinar.
-      `),e(),i("ul",{role:"list"},[i("li",null,"Vestibulum parturient"),e(),i("li",null,"Ullamcorper orci condimentum"),e(),i("li",null,"Sodales scelerisque ultrices")])],-1),E={name:"DemoAccordion"},r=m({...E,setup(u){return(s,l)=>(c(),d(p(_),null,{default:o(()=>[B,e(),L,e(),U]),_:1}))}});const G=i("div",null,[i("p",null,`
+      `),e(),i("ul",{role:"list"},[i("li",null,"Vestibulum parturient"),e(),i("li",null,"Ullamcorper orci condimentum"),e(),i("li",null,"Sodales scelerisque ultrices")])],-1),U={name:"DemoAccordion"},r=m({...U,setup(u){return(s,l)=>(c(),d(p(_),null,{default:o(()=>[y,e(),E,e(),L]),_:1}))}});const K=i("div",null,[i("p",null,`
               Adipiscing condimentum elit a cum ullamcorper nisi rutrum aptent
               lacinia vestibulum adipiscing a a vestibulum quis malesuada
               ullamcorper praesent netus netus mus.
-            `)],-1),H={name:"AccordionPage"},M=m({...H,setup(u){return(s,l)=>{const g=$("router-link");return c(),d(p(b),{title:s.$t("ui.accordion.title")},{toolbar:o(()=>[n(g,{to:{name:"ui"},class:"back"},{default:o(()=>[e(t(s.$t("common.back")),1)]),_:1})]),default:o(()=>[e(),i("section",null,[i("h2",null,t(s.$t("ui.accordion.basic")),1),e(),n(a)]),e(),i("section",null,[i("h2",null,t(s.$t("ui.accordion.separate")),1),e(),n(a,{class:"separate"})]),e(),i("section",null,[i("h2",null,t(s.$t("ui.accordion.defaultIndex")),1),e(),n(a,{"default-index":1})]),e(),i("section",null,[i("h2",null,t(s.$t("ui.accordion.can-self-collapse")),1),e(),n(a,{"can-self-collapse":!0})]),e(),i("section",null,[i("h2",null,t(s.$t("ui.accordion.icons.toggle")),1),e(),n(a,{class:"show-toggle-icon"})]),e(),i("section",null,[i("h2",null,t(s.$t("ui.accordion.horizontal.basic")),1),e(),n(a,{direction:"horizontal"})]),e(),i("section",null,[i("h2",null,t(s.$t("ui.accordion.horizontal.separate")),1),e(),n(a,{direction:"horizontal",class:"separate"})]),e(),i("section",null,[i("h2",null,t(s.$t("ui.accordion.icons.css")),1),e(),n(a,{class:"with-icons"})]),e(),i("section",null,[i("h2",null,t(s.$t("ui.accordion.icons.slot")),1),e(),n(r)]),e(),i("section",null,[i("h2",null,t(s.$t("ui.accordion.icons.css")),1),e(),n(r,{class:"horizontal"})]),e(),i("section",null,[i("h2",null,t(s.$t("ui.accordion.icons.slot")),1),e(),n(r,{class:"horizontal show-toggle-icon"})]),e(),i("section",null,[i("h2",null,t(s.$t("ui.accordion.icons.toggle")),1),e(),n(a,{class:"show-toggle-icon custom-toggle-icons"})]),e(),i("section",null,[i("h2",null,t(s.$t("ui.accordion.slot-without-title")),1),e(),n(a,null,{"extra-pane":o(()=>[G]),_:1})])]),_:1},8,["title"])}}});export{M as default};
+            `)],-1),O={name:"AccordionPage"},H=m({...O,setup(u){return(s,l)=>{const g=$("router-link");return c(),d(p(b),{title:s.$t("ui.accordion.title")},{toolbar:o(()=>[n(g,{to:{name:"ui"},class:"back"},{default:o(()=>[e(t(s.$t("common.back")),1)]),_:1})]),default:o(()=>[e(),i("section",null,[i("h2",null,t(s.$t("ui.accordion.basic")),1),e(),n(a)]),e(),i("section",null,[i("h2",null,t(s.$t("ui.accordion.separate")),1),e(),n(a,{class:"separate"})]),e(),i("section",null,[i("h2",null,t(s.$t("ui.accordion.defaultIndex")),1),e(),n(a,{"default-index":1})]),e(),i("section",null,[i("h2",null,t(s.$t("ui.accordion.can-self-collapse")),1),e(),n(a,{"can-self-collapse":!0})]),e(),i("section",null,[i("h2",null,t(s.$t("ui.accordion.icons.toggle")),1),e(),n(a,{class:"show-toggle-icon"})]),e(),i("section",null,[i("h2",null,t(s.$t("ui.accordion.horizontal.basic")),1),e(),n(a,{direction:"horizontal"})]),e(),i("section",null,[i("h2",null,t(s.$t("ui.accordion.horizontal.separate")),1),e(),n(a,{direction:"horizontal",class:"separate"})]),e(),i("section",null,[i("h2",null,t(s.$t("ui.accordion.icons.css")),1),e(),n(a,{class:"with-icons"})]),e(),i("section",null,[i("h2",null,t(s.$t("ui.accordion.icons.slot")),1),e(),n(r)]),e(),i("section",null,[i("h2",null,t(s.$t("ui.accordion.icons.css")),1),e(),n(r,{class:"horizontal"})]),e(),i("section",null,[i("h2",null,t(s.$t("ui.accordion.icons.slot")),1),e(),n(r,{class:"horizontal show-toggle-icon"})]),e(),i("section",null,[i("h2",null,t(s.$t("ui.accordion.icons.toggle")),1),e(),n(a,{class:"show-toggle-icon custom-toggle-icons"})]),e(),i("section",null,[i("h2",null,t(s.$t("ui.accordion.slot-without-title")),1),e(),n(a,null,{"extra-pane":o(()=>[K]),_:1})])]),_:1},8,["title"])}}});export{H as default};
