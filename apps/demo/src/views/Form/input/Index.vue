7ed5b2f3d642e5ebd42cc8575700c7cb510d9076
kabinkhandthakuri <kabin@dzangolab.com>
2024-08-08 17:43:23 +0545

    refactor: add disabled and custom css input to demo

--- apps/demo/src/views/Form/input/Index.vue
+++ apps/demo/src/views/Form/input/Index.vue
@@ -84,6 +84,35 @@
     </section>
 
     <section>
+      <h3>{{ $t("form.label.disabledInput") }}</h3>
+
+      <!-- eslint-disable -->
+      <SshPre language="html-vue">
+        &lt;template&gt;
+          &lt;Input 
+            v-model="email"
+            label="Disabled input"
+            type="email"
+            disabled
+          /&gt;
+        &lt;/template&gt;
+
+        &lt;script setup lang="ts"&gt;
+        import { Input } from "@dzangolab/vue3-form";
+        &lt;/script&gt;
+      </SshPre>
+      <!-- eslint-enable -->
+
+      <Input
+        v-model="formData.disabled"
+        :label="$t('form.label.disabled')"
+        class="form-field"
+        type="email"
+        disabled
+      />
+    </section>
+
+    <section>
       <h3>{{ $t("form.label.inputWithValidation") }}</h3>
 
       <!-- eslint-disable -->
@@ -121,6 +150,54 @@
         />
       </Form>
     </section>
+
+    <section>
+      <h3>{{ $t("form.label.customStyleInput") }}</h3>
+
+      <!-- eslint-disable -->
+      <SshPre language="html-vue">
+        &lt;template&gt;
+          &lt;Input 
+            v-model="number"
+            class="custom-style-input"
+            label="Number"
+            placeholder="Enter a number"
+            type="number"
+          /&gt;
+        &lt;/template&gt;
+
+        &lt;script setup lang="ts"&gt;
+        import { Input } from "@dzangolab/vue3-form";
+        &lt;/script&gt;
+
+        &lt;style&gt;
+        .custom-style-input {
+          flex-direction: row;
+          align-items: center;
+        }
+
+        .custom-style-input input {
+          --_input-field-border-color: #007aff;
+          --_input-field-border-radius: 1em;
+          --_input-field-padding-h: 0.5em;
+          --_input-field-padding-v: 0.5em;
+        }
+
+        .custom-style-input input:focus {
+          box-shadow: 0 0 0 .25rem #004da08e;
+        }
+        &lt;/style&gt;
+      </SshPre>
+      <!-- eslint-enable -->
+
+      <Input
+        v-model="formData.numberInput"
+        :label="$t('form.label.number')"
+        :placeholder="$t('form.placeholder.number')"
+        class="form-field custom-style-input"
+        type="number"
+      />
+    </section>
   </Page>
 </template>
 
@@ -141,9 +218,29 @@ const { t } = useI18n();
 const inputSchema = z.string().min(3, { message: t("form.errors.input.min") });
 
 let formData = reactive({
+  disabled: ref("monorepo@gmail.com"),
   input: ref(),
   inputWithValidation: ref(),
   noLabelInput: ref(),
+  numberInput: ref(),
   passwordInput: ref(),
 });
 </script>
+
+<style lang="css">
+.custom-style-input {
+  flex-direction: row;
+  align-items: center;
+}
+
+.custom-style-input input {
+  --_input-field-border-color: var(--dz-primary-color);
+  --_input-field-border-radius: 1em;
+  --_input-field-padding-h: 0.5em;
+  --_input-field-padding-v: 0.5em;
+}
+
+.custom-style-input input:focus {
+  box-shadow: 0 0 0 0.25rem #004da08e;
+}
+</style>
