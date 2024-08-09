4d39e4d074d353c40e93124689fcb9fa48cf2b08
DeepakAryal <deepakaryal2050@gmail.com>
2024-08-09 07:39:35 +0000

    Deploying to gh-pages from @ dzangolab/vue@d7d90bdd1f266d0b4b77e23eed6e163fce6dc652 🚀

--- assets/Index-4fdc6409.js
+++ assets/Index-4fdc6409.js
@@ -1,4 +1,4 @@
-import{d as u,o as i,c as l,w as a,B as r,K as n,z as c,u as o,Q as m,a as e,r as d}from"./index-493322be.js";const p=e("h2",null,"Heading level 2",-1),h=e("section",null,[e("h3",null,"Section h3 heading"),n(),e("section",null,[e("h4",null,"Sub-section h4 heading"),n(),e("section",null,[e("h5",null,"Sub-section h5 heading"),n(),e("section",null,[e("h6",null,"Sub-section h6 heading"),n(),e("p",null,`
+import{d as u,o as i,c as l,w as a,b as r,e as n,t as c,u as o,J as m,a as e,r as d}from"./index-902006a8.js";const p=e("h2",null,"Heading level 2",-1),h=e("section",null,[e("h3",null,"Section h3 heading"),n(),e("section",null,[e("h4",null,"Sub-section h4 heading"),n(),e("section",null,[e("h5",null,"Sub-section h5 heading"),n(),e("section",null,[e("h6",null,"Sub-section h6 heading"),n(),e("p",null,`
               Vehicula curabitur ultrices a parturient elementum urna sed
               condimentum suspendisse sodales a litora a egestas.
             `),n(),e("p",null,`
@@ -26,4 +26,4 @@ import{d as u,o as i,c as l,w as a,B as r,K as n,z as c,u as o,Q as m,a as e,r a
         `)]),n(),e("p",null,`
         Vehicula curabitur ultrices a parturient elementum urna sed condimentum
         suspendisse sodales a litora a egestas.
-      `)],-1),g={name:"TypographyPage"},q=u({...g,setup(b){return(t,_)=>{const s=d("router-link");return i(),l(o(m),{title:t.$t("ui.typography.title")},{toolbar:a(()=>[r(s,{to:{name:"ui"},class:"back",type:"button"},{default:a(()=>[n(c(t.$t("common.back")),1)]),_:1})]),default:a(()=>[n(),p,n(),h]),_:1},8,["title"])}}});export{q as default};
+      `)],-1),b={name:"TypographyPage"},q=u({...b,setup(g){return(t,_)=>{const s=d("router-link");return i(),l(o(m),{title:t.$t("ui.typography.title")},{toolbar:a(()=>[r(s,{to:{name:"ui"},class:"back",type:"button"},{default:a(()=>[n(c(t.$t("common.back")),1)]),_:1})]),default:a(()=>[n(),p,n(),h]),_:1},8,["title"])}}});export{q as default};
