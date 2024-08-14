0e294f0a5a1d139d0f92bcaca38781603e409373
KabinKhandThakuri <kabin@dzangolab.com>
2024-08-14 11:02:35 +0545

    feat: add number input to demo app with source code (#314)

--- apps/demo/src/router/form.ts
+++ apps/demo/src/router/form.ts
@@ -1,5 +1,6 @@
 const Form = () => import("@/views/Form/Index.vue");
 const Input = () => import("@/views/Form/input/Index.vue");
+const NumberInput = () => import("@/views/Form/numberInput/Index.vue");
 const TextareaInput = () => import("@/views/Form/textareaInput/Index.vue");
 const TextInput = () => import("@/views/Form/textInput/Index.vue");
 
@@ -16,6 +17,11 @@ const routes = [
         component: Input,
         name: "input",
         path: "input",
+      },
+      {
+        component: NumberInput,
+        name: "numberInput",
+        path: "number-input",
       },
       {
         component: TextareaInput,
