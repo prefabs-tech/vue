6ff83ac82fa66514e456df6d24cd5605f04248c4
KabinKhandThakuri <KabinKhandThakuri@users.noreply.github.com>
2024-09-02 12:21:22 +0000

    Deploying to gh-pages from @ dzangolab/vue@ceec138acc6354df72ae559af5dfde2048c7fb1e 🚀

---
+++ assets/Index-827a0db5.js
@@ -0,0 +1,1 @@
+import{_ as o,o as n,c as a,r as s}from"./index-3bb8d7f9.js";const c={name:"Layout"};function r(e,l,p,_,i,u){const t=s("Page");return n(),a(t,{title:e.$t("layout.title")},null,8,["title"])}const m=o(c,[["render",r]]);export{m as default};
