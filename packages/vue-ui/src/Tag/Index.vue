0d6742cbcbc103f56b370e97760d8accef963392
jagrit <jagrit.gaire@dzangolab.com>
2024-09-04 15:47:18 +0545

    refactor: update props for tag package

---
+++ packages/vue-ui/src/Tag/Index.vue
@@ -0,0 +1,32 @@
+<template>
+  <span>
+    {{ label }}
+  </span>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Tag",
+};
+</script>
+
+<script setup lang="ts">
+defineProps({
+  className: {
+    default: "",
+    type: String,
+  },
+  color: {
+    default: "default",
+    type: String,
+  },
+  fullWidth: Boolean,
+  icon: String,
+  label: String,
+  rounded: Boolean,
+  style: {
+    default: () => ({}),
+    type: Object,
+  },
+});
+</script>
