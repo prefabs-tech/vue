216d65988a289c390a3d8435ec286c16ff533d0e
DeepakAryal <deepakaryal2050@gmail.com>
2024-09-05 10:55:49 +0000

    Deploying to gh-pages from @ dzangolab/vue@faec4c738b9af72640104f9497cc4274a8ba106e 🚀

--- assets/Index-aa5c976d.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,o as n,c as a,r as s}from"./index-1af78c85.js";const c={name:"Layout"};function r(e,l,p,_,i,u){const t=s("Page");return n(),a(t,{title:e.$t("layout.title")},null,8,["title"])}const m=o(c,[["render",r]]);export{m as default};
