62193c5e9c3e1b5a84981cc16104ddcaf9300679
jagrit <jagrit.gaire@dzangolab.com>
2024-09-10 15:23:29 +0545

    refactor: update locales and demo using I18n

--- apps/demo/src/views/UI/badge/Index.vue
+++ apps/demo/src/views/UI/badge/Index.vue
@@ -1,7 +1,7 @@
 <template>
   <Page :title="$t('ui.badge.title')" class="demo-badge">
     <section>
-      <h2>Basic</h2>
+      <h2>{{ $t("ui.badge.usage.basic") }}</h2>
 
       <div class="section-content">
         <!-- eslint-disable -->
@@ -16,12 +16,35 @@
         </SshPre>
         <!-- eslint-enable -->
 
-        <Badge label="Basic" />
+        <Badge :label="$t('ui.badge.label.basic')" />
       </div>
     </section>
 
     <section>
-      <h2>Severity</h2>
+      <h2>{{ $t("ui.badge.usage.withI18n") }}</h2>
+
+      <div class="section-content">
+        <!-- eslint-disable -->
+        <SshPre language="html-vue">
+          &lt;template&gt;
+            &lt;Badge :label=t("ui.badge.label.basic") /&gt; 
+          &lt;/template&gt;
+          
+          &lt;script setup lang="ts"&gt;
+            import { Badge } from "@dzangolab/vue3-ui";
+            import { useI18n } from "@dzangolab/vue3-i18n";
+
+            const { t } = useI18n();
+          &lt;/script&gt;
+        </SshPre>
+        <!-- eslint-enable -->
+
+        <Badge :label="$t('ui.badge.label.basic')" />
+      </div>
+    </section>
+
+    <section>
+      <h2>{{ $t("ui.badge.usage.severity") }}</h2>
 
       <div class="section-content">
         <!-- eslint-disable -->
@@ -42,18 +65,18 @@
         <!-- eslint-enable -->
 
         <div class="container">
-          <Badge label="Primary" severity="primary" />
-          <Badge label="Secondary" severity="secondary" />
-          <Badge label="Alternate" severity="alternate" />
-          <Badge label="Success" severity="success" />
-          <Badge label="Danger" severity="danger" />
-          <Badge label="Warning" severity="warning" />
+          <Badge :label="$t('ui.badge.label.primary')" severity="primary" />
+          <Badge :label="$t('ui.badge.label.secondary')" severity="secondary" />
+          <Badge :label="$t('ui.badge.label.alternate')" severity="alternate" />
+          <Badge :label="$t('ui.badge.label.success')" severity="success" />
+          <Badge :label="$t('ui.badge.label.danger')" severity="danger" />
+          <Badge :label="$t('ui.badge.label.warning')" severity="warning" />
         </div>
       </div>
     </section>
 
     <section>
-      <h2>Rounded</h2>
+      <h2>{{ $t("ui.badge.usage.rounded") }}</h2>
 
       <div class="section-content">
         <!-- eslint-disable -->
@@ -68,12 +91,12 @@
         </SshPre>
         <!-- eslint-enable -->
 
-        <Badge label="Rounded" rounded />
+        <Badge :label="$t('ui.badge.label.rounded')" rounded />
       </div>
     </section>
 
     <section>
-      <h2>Icon name</h2>
+      <h2>{{ $t("ui.badge.usage.iconName") }}</h2>
 
       <div class="section-content">
         <!-- eslint-disable -->
@@ -97,7 +120,7 @@
     </section>
 
     <section>
-      <h2>Icon with text</h2>
+      <h2>{{ $t("ui.badge.usage.iconWithText") }}</h2>
 
       <div class="section-content">
         <!-- eslint-disable -->
@@ -114,14 +137,17 @@
         <!-- eslint-enable -->
 
         <div class="container">
-          <Badge label="Locked" icon-left="pi pi-lock" />
-          <Badge label="Unlocked" icon-right="pi pi-lock-open" />
+          <Badge :label="$t('ui.badge.label.locked')" icon-left="pi pi-lock" />
+          <Badge
+            :label="$t('ui.badge.label.unLocked')"
+            icon-right="pi pi-lock-open"
+          />
         </div>
       </div>
     </section>
 
     <section>
-      <h2>Icon element</h2>
+      <h2>{{ $t("ui.badge.usage.iconElement") }}</h2>
 
       <div class="section-content">
         <!-- eslint-disable -->
@@ -147,13 +173,13 @@
         <!-- eslint-enable -->
 
         <div class="container">
-          <Badge label="Correct" icon-left="true">
+          <Badge :label="$t('ui.badge.label.correct')" icon-left="true">
             <template #iconLeft>
               <i class="pi pi-check" />
             </template>
           </Badge>
 
-          <Badge label="Wrong" icon-right="true">
+          <Badge :label="$t('ui.badge.label.wrong')" icon-right="true">
             <template #iconRight>
               <i class="pi pi-times" />
             </template>
@@ -163,7 +189,7 @@
     </section>
 
     <section>
-      <h2>Custom style</h2>
+      <h2>{{ $t("ui.badge.usage.customStyle") }}</h2>
 
       <div class="section-content">
         <!-- eslint-disable -->
@@ -181,14 +207,14 @@
               --_badge-color: #0dcaf0;
               --_badge-font-size: 1rem;
               --_badge-font-weight: 800;
-              --_badge-padding: 0.3rem 0.75rem;
+              --_badge-padding: 0.5rem 1rem;
               --_badge-text-color: black;
             }
           &lt;/style&gt;
         </SshPre>
         <!-- eslint-enable -->
 
-        <Badge label="Custom" class="custom-badge" />
+        <Badge :label="$t('ui.badge.label.custom')" class="custom-badge" />
       </div>
     </section>
   </Page>
