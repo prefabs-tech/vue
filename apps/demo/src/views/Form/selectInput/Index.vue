02b2f37ef81c1aa7ae5f48f245030afe444cc109
jagrit <jagrit.gaire@dzangolab.com>
2024-09-12 15:01:58 +0545

    refactor: wraped select element inside Form element for select component demo

--- apps/demo/src/views/Form/selectInput/Index.vue
+++ apps/demo/src/views/Form/selectInput/Index.vue
@@ -1,307 +1,326 @@
 <template>
-  <Page :title="$t('form.label.select')">
-    <section class="demo-section">
-      <h3>{{ $t("form.label.basicInput") }}</h3>
+  <Page :title="$t('form.label.select')" class="demo">
+    <section>
+      <h2>{{ $t("form.label.basicInput") }}</h2>
 
-      <!-- eslint-disable -->
-       <SshPre language="html-vue">
-        &lt;template&gt;
-          &lt;SelectInput
-            v-model="input"
+      <div class="section-content">
+        <!-- eslint-disable -->
+         <SshPre language="html-vue">
+          &lt;Form&gt;
+            &lt;SelectInput
+              v-model="input"
+              :options="options"
+              placeholder="Select a value"
+            /&gt;
+          &lt;/Form&gt;
+
+          &lt;script setup lang="ts"&gt;
+          import { SelectInput } from "@dzangolab/vue3-form";
+          import { ref } from "vue";
+
+          const options = ref([
+            { value: "FR", label: "French" },
+            { value: "DE", label: "German" },
+            { value: "BE", label: "Dutch" },
+            { value: "NP", label: "Nepali" },
+            { value: "HI", label: "Hindi" },
+          ]);
+          &lt;/script&gt;
+        </SshPre>
+        <!-- eslint-enable -->
+
+        <Form>
+          <SelectInput
+            v-model="formData.noLabelInput"
             :options="options"
-            placeholder="Select a value"
-          /&gt;
-        &lt;/template&gt;
-
-        &lt;script setup lang="ts"&gt;
-        import { SelectInput } from "@dzangolab/vue3-form";
-        import { ref } from "vue";
-
-        const options = ref([
-          { value: "FR", label: "FR" },
-          { value: "DE", label: "DE" },
-          { value: "BE", label: "BE" },
-          { value: "FE", label: "FE" },
-          { value: "RE", label: "RE" },
-        ]);
-        &lt;/script&gt;
-      </SshPre>
-      <!-- eslint-enable -->
-
-      <SelectInput
-        v-model="formData.noLabelInput"
-        :options="options"
-        :placeholder="$t('form.placeholder.select')"
-        class="form-field"
-      />
+            :placeholder="$t('form.placeholder.select')"
+          />
+        </Form>
+      </div>
     </section>
 
-    <section class="demo-section">
-      <h3>{{ $t("form.label.labelInput") }}</h3>
+    <section>
+      <h2>{{ $t("form.label.labelInput") }}</h2>
 
-      <!-- eslint-disable -->
-      <SshPre language="html-vue">
-        &lt;template&gt;
-          &lt;SelectInput 
-            v-model="input"
+      <div class="section-content">
+        <!-- eslint-disable -->
+        <SshPre language="html-vue">
+          &lt;Form&gt;
+            &lt;SelectInput 
+              v-model="input"
+              :options="options"
+              label="Select"
+              placeholder="Select a value"
+            /&gt;
+          &lt;/Form&gt;
+
+          &lt;script setup lang="ts"&gt;
+          import { SelectInput } from "@dzangolab/vue3-form";
+          import { ref } from "vue";
+
+          const options = ref([
+            { value: "FR", label: "French" },
+            { value: "DE", label: "German" },
+            { value: "BE", label: "Dutch" },
+            { value: "NP", label: "Nepali" },
+            { value: "HI", label: "Hindi" },
+          ]);
+          &lt;/script&gt;
+        </SshPre>
+        <!-- eslint-enable -->
+
+        <Form>
+          <SelectInput
+            v-model="formData.input"
+            :label="$t('form.label.select')"
             :options="options"
-            label="Select"
-            placeholder="Select a value"
-          /&gt;
-        &lt;/template&gt;
-
-        &lt;script setup lang="ts"&gt;
-        import { SelectInput } from "@dzangolab/vue3-form";
-        import { ref } from "vue";
-
-        const options = ref([
-          { value: "FR", label: "FR" },
-          { value: "DE", label: "DE" },
-          { value: "BE", label: "BE" },
-          { value: "FE", label: "FE" },
-          { value: "RE", label: "RE" },
-        ]);
-        &lt;/script&gt;
-      </SshPre>
-      <!-- eslint-enable -->
-
-      <SelectInput
-        v-model="formData.input"
-        :label="$t('form.label.select')"
-        :options="options"
-        :placeholder="$t('form.placeholder.select')"
-        class="form-field"
-      />
+            :placeholder="$t('form.placeholder.select')"
+          />
+        </Form>
+      </div>
     </section>
 
-    <section class="demo-section">
-      <h3>{{ $t("form.label.disabled") }}</h3>
+    <section>
+      <h2>{{ $t("form.label.disabled") }}</h2>
 
-      <!-- eslint-disable -->
-      <SshPre language="html-vue">
-        &lt;template&gt;
-          &lt;SelectInput 
-            v-model="disabled"
+      <div class="section-content">
+        <!-- eslint-disable -->
+        <SshPre language="html-vue">
+          &lt;Form&gt;
+            &lt;SelectInput 
+              v-model="disabled"
+              :options="options"
+              disabled
+              label="Disabled"
+            /&gt;
+          &lt;/Form&gt;
+
+          &lt;script setup lang="ts"&gt;
+          import { SelectInput } from "@dzangolab/vue3-form";
+          import { ref } from "vue";
+  
+          const disabled = ref("DE");
+
+          const options = ref([
+            { value: "FR", label: "French" },
+            { value: "DE", label: "German" },
+            { value: "BE", label: "Dutch" },
+            { value: "NP", label: "Nepali" },
+            { value: "HI", label: "Hindi" },
+          ]);
+          &lt;/script&gt;
+        </SshPre>
+        <!-- eslint-enable -->
+
+        <Form>
+          <SelectInput
+            v-model="formData.disabled"
+            :label="$t('form.label.disabled')"
             :options="options"
             disabled
-            label="Disabled"
-          /&gt;
-        &lt;/template&gt;
-
-        &lt;script setup lang="ts"&gt;
-        import { SelectInput } from "@dzangolab/vue3-form";
-        import { ref } from "vue";
-
-        const disabled = ref("DE");
-
-        const options = ref([
-          { value: "FR", label: "FR" },
-          { value: "DE", label: "DE" },
-          { value: "BE", label: "BE" },
-          { value: "FE", label: "FE" },
-          { value: "RE", label: "RE" },
-        ]);
-        &lt;/script&gt;
-      </SshPre>
-      <!-- eslint-enable -->
-
-      <SelectInput
-        v-model="formData.disabled"
-        :label="$t('form.label.disabled')"
-        :options="options"
-        class="form-field"
-        disabled
-      />
+          />
+        </Form>
+      </div>
     </section>
 
-    <section class="demo-section">
-      <h3>{{ $t("form.label.multiSelect") }}</h3>
+    <section>
+      <h2>{{ $t("form.label.multiSelect") }}</h2>
 
-      <!-- eslint-disable -->
-      <SshPre language="html-vue">
-        &lt;template&gt;
-          &lt;SelectInput 
-            v-model="input"
+      <div class="section-content">
+        <!-- eslint-disable -->
+        <SshPre language="html-vue">
+          &lt;Form&gt;
+            &lt;SelectInput 
+              v-model="input"
+              :options="options"
+              label="Select"
+              multiple
+              placeholder="Select multiple value"
+            /&gt;
+          &lt;/Form&gt;
+
+          &lt;script setup lang="ts"&gt;
+          import { SelectInput } from "@dzangolab/vue3-form";
+          import { ref } from "vue";
+
+          const options = ref([
+            { value: "FR", label: "French" },
+            { value: "DE", label: "German" },
+            { value: "BE", label: "Dutch" },
+            { value: "NP", label: "Nepali" },
+            { value: "HI", label: "Hindi" },
+          ]);
+          &lt;/script&gt;
+        </SshPre>
+        <!-- eslint-enable -->
+
+        <Form>
+          <SelectInput
+            v-model="formData.multiSelect"
+            :label="$t('form.label.select')"
             :options="options"
-            label="Select"
+            :placeholder="$t('form.placeholder.multiSelect')"
             multiple
-            placeholder="Select multiple value"
-          /&gt;
-        &lt;/template&gt;
-
-        &lt;script setup lang="ts"&gt;
-        import { SelectInput } from "@dzangolab/vue3-form";
-        import { ref } from "vue";
-
-        const options = ref([
-          { value: "FR", label: "FR" },
-          { value: "DE", label: "DE" },
-          { value: "BE", label: "BE" },
-          { value: "FE", label: "FE" },
-          { value: "RE", label: "RE" },
-        ]);
-        &lt;/script&gt;
-      </SshPre>
-      <!-- eslint-enable -->
-
-      <SelectInput
-        v-model="formData.multiSelect"
-        :label="$t('form.label.select')"
-        :options="options"
-        :placeholder="$t('form.placeholder.multiSelect')"
-        class="form-field"
-        multiple
-      />
+          />
+        </Form>
+      </div>
     </section>
 
-    <section class="demo-section">
-      <h3>{{ $t("form.label.disabledMultiSelect") }}</h3>
+    <section>
+      <h2>{{ $t("form.label.disabledMultiSelect") }}</h2>
 
-      <!-- eslint-disable -->
-      <SshPre language="html-vue">
-        &lt;template&gt;
-          &lt;SelectInput 
-            v-model="disabled"
+      <div class="section-content">
+        <!-- eslint-disable -->
+        <SshPre language="html-vue">
+          &lt;Form&gt;
+            &lt;SelectInput 
+              v-model="disabled"
+              :options="options"
+              disabled
+              label="Disabled"
+              multiple
+            /&gt;
+          &lt;/Form&gt;
+
+          &lt;script setup lang="ts"&gt;
+          import { SelectInput } from "@dzangolab/vue3-form";
+          import { ref } from "vue";
+
+          const disabled = ref(["BE", "DE"]);
+
+          const options = ref([
+            { value: "FR", label: "French" },
+            { value: "DE", label: "German" },
+            { value: "BE", label: "Dutch" },
+            { value: "NP", label: "Nepali" },
+            { value: "HI", label: "Hindi" },
+          ]);
+          &lt;/script&gt;
+        </SshPre>
+        <!-- eslint-enable -->
+
+        <Form>
+          <SelectInput
+            v-model="formData.disabledMultiSelect"
+            :label="$t('form.label.disabled')"
             :options="options"
             disabled
-            label="Disabled"
             multiple
-          /&gt;
-        &lt;/template&gt;
-
-        &lt;script setup lang="ts"&gt;
-        import { SelectInput } from "@dzangolab/vue3-form";
-        import { ref } from "vue";
-
-        const disabled = ref(["BE", "DE"]);
-
-        const options = ref([
-          { value: "FR", label: "FR" },
-          { value: "DE", label: "DE" },
-          { value: "BE", label: "BE" },
-          { value: "FE", label: "FE" },
-          { value: "RE", label: "RE" },
-        ]);
-        &lt;/script&gt;
-      </SshPre>
-      <!-- eslint-enable -->
-
-      <SelectInput
-        v-model="formData.disabledMultiSelect"
-        :label="$t('form.label.disabled')"
-        :options="options"
-        class="form-field"
-        disabled
-        multiple
-      />
+          />
+        </Form>
+      </div>
     </section>
 
-    <section class="demo-section">
-      <h3>{{ $t("form.label.inputWithValidation") }}</h3>
+    <section>
+      <h2>{{ $t("form.label.inputWithValidation") }}</h2>
 
-      <!-- eslint-disable -->
-      <SshPre language="html-vue">
-        &lt;template&gt;
-          &lt;SelectInput 
-            v-model="input"
+      <div class="section-content">
+        <!-- eslint-disable -->
+        <SshPre language="html-vue">
+          &lt;Form&gt;
+            &lt;SelectInput 
+              v-model="input"
+              :options="options"
+              :schema="inputSchema"
+              label="Select"
+              multiple
+              placeholder="Select multiple value"
+            /&gt;
+          &lt;/Form&gt;
+
+          &lt;script setup lang="ts"&gt;
+          import { SelectInput } from "@dzangolab/vue3-form";
+          import { ref } from "vue";
+
+          const inputSchema = z
+            .string({invalid_type_error: "Please select at least one valid option"})
+            .array()
+            .min(1, { message: "Please select at least one valid option" });
+
+          const options = ref([
+            { value: "FR", label: "French" },
+            { value: "DE", label: "German" },
+            { value: "BE", label: "Dutch" },
+            { value: "NP", label: "Nepali" },
+            { value: "HI", label: "Hindi" },
+          ]);
+          &lt;/script&gt;
+        </SshPre>
+        <!-- eslint-enable -->
+
+        <Form>
+          <SelectInput
+            v-model="formData.inputWithValidation"
+            :label="$t('form.label.select')"
             :options="options"
+            :placeholder="$t('form.placeholder.multiSelect')"
             :schema="inputSchema"
-            label="Select"
             multiple
-            placeholder="Select multiple value"
-          /&gt;
-        &lt;/template&gt;
-
-        &lt;script setup lang="ts"&gt;
-        import { SelectInput } from "@dzangolab/vue3-form";
-        import { ref } from "vue";
-
-        const inputSchema = z
-          .string({invalid_type_error: "Please select atleast one valid option"})
-          .array()
-          .min(1, { message: "Please select atleast one valid option" });
-
-        const options = ref([
-          { value: "FR", label: "FR" },
-          { value: "DE", label: "DE" },
-          { value: "BE", label: "BE" },
-          { value: "FE", label: "FE" },
-          { value: "RE", label: "RE" },
-        ]);
-        &lt;/script&gt;
-      </SshPre>
-      <!-- eslint-enable -->
-
-      <Form>
-        <SelectInput
-          v-model="formData.inputWithValidation"
-          :label="$t('form.label.select')"
-          :options="options"
-          :placeholder="$t('form.placeholder.multiSelect')"
-          :schema="inputSchema"
-          class="form-field"
-          multiple
-        />
-      </Form>
+          />
+        </Form>
+      </div>
     </section>
 
-    <section class="demo-section">
-      <h3>{{ $t("form.label.customStyleInput") }}</h3>
+    <section>
+      <h2>{{ $t("form.label.customStyleInput") }}</h2>
 
-      <!-- eslint-disable -->
-      <SshPre language="html-vue">
-        &lt;template&gt;
-          &lt;SelectInput 
-            v-model="input"
+      <div class="section-content">
+        <!-- eslint-disable -->
+        <SshPre language="html-vue">
+          &lt;Form&gt;
+            &lt;SelectInput 
+              v-model="input"
+              :options="options"
+              class="custom-style-select"
+              label="Select"
+              multiple
+              placeholder="Select multiple value"
+            /&gt;
+          &lt;/Form&gt;
+
+          &lt;script setup lang="ts"&gt;
+          import { SelectInput } from "@dzangolab/vue3-form";
+          import { ref } from "vue";
+
+          const options = ref([
+            { value: "FR", label: "French" },
+            { value: "DE", label: "German" },
+            { value: "BE", label: "Dutch" },
+            { value: "NP", label: "Nepali" },
+            { value: "HI", label: "Hindi" },
+          ]);
+          &lt;/script&gt;
+
+          &lt;style lang="css"&gt;
+          .custom-style-select .multiple-mode .selected-option {
+            --_multiselect-tag-border-radius: 1rem;
+            --_multiselect-tag-color: #007aff;
+            color: #fff;
+          }
+
+          .custom-style-select .multiselect-input {
+            --_multiselect-border-color: #007aff;
+          }
+
+          .custom-style-select .multiselect-option.selected {
+            --_multiselect-selected-bg-color: #22c55e;
+            --_multiselect-selected-color: #fff;
+          }
+        &lt;/style&gt;
+        </SshPre>
+        <!-- eslint-enable -->
+
+        <Form>
+          <SelectInput
+            v-model="formData.multiSelect"
+            :label="$t('form.label.select')"
             :options="options"
+            :placeholder="$t('form.placeholder.multiSelect')"
             class="custom-style-select"
-            label="Select"
             multiple
-            placeholder="Select multiple value"
-          /&gt;
-        &lt;/template&gt;
-
-        &lt;script setup lang="ts"&gt;
-        import { SelectInput } from "@dzangolab/vue3-form";
-        import { ref } from "vue";
-
-        const options = ref([
-          { value: "FR", label: "FR" },
-          { value: "DE", label: "DE" },
-          { value: "BE", label: "BE" },
-          { value: "FE", label: "FE" },
-          { value: "RE", label: "RE" },
-        ]);
-        &lt;/script&gt;
-
-        &lt;style lang="css"&gt;
-        .custom-style-select .multiple-mode .selected-option {
-          --_multiselect-tag-border-radius: 1rem;
-          --_multiselect-tag-color: #007aff;
-
-          color: #fff;
-        }
-
-        .custom-style-select .multiselect-input {
-          --_multiselect-border-color: #007aff;
-        }
-
-        .custom-style-select .multiselect-option.selected {
-          --_multiselect-selected-bg-color: #22c55e;
-          --_multiselect-selected-color: #fff;
-        }
-      &lt;/style&gt;
-      </SshPre>
-      <!-- eslint-enable -->
-
-      <SelectInput
-        v-model="formData.multiSelect"
-        :label="$t('form.label.select')"
-        :options="options"
-        :placeholder="$t('form.placeholder.multiSelect')"
-        class="form-field custom-style-select"
-        multiple
-      />
+          />
+        </Form>
+      </div>
     </section>
   </Page>
 </template>
@@ -336,11 +355,11 @@ let formData = reactive({
 });
 
 const options = ref([
-  { value: "FR", label: "FR" },
-  { value: "DE", label: "DE" },
-  { value: "BE", label: "BE" },
-  { value: "FE", label: "FE" },
-  { value: "RE", label: "RE" },
+  { value: "FR", label: "French" },
+  { value: "DE", label: "German" },
+  { value: "BE", label: "Dutch" },
+  { value: "NP", label: "Nepali" },
+  { value: "HI", label: "Hindi" },
 ]);
 </script>
 
