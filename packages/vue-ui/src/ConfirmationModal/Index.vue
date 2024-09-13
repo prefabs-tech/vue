754879fdda79b9827189351a41b84393c0791e0f
jagrit <jagrit.gaire@dzangolab.com>
2024-09-13 09:59:47 +0545

    refactor: update confirmation modal package

--- packages/vue-ui/src/ConfirmationModal/Index.vue
+++ packages/vue-ui/src/ConfirmationModal/Index.vue
@@ -1,4 +1,20 @@
-<template>Test</template>
+<template>
+  <Transition name="modal">
+    <div class="modal-wrapper">
+      <div class="modal-header">
+        <slot name="header"></slot>
+      </div>
+
+      <div class="modal-body">
+        <slot name="body"></slot>
+      </div>
+
+      <div class="modal-footer">
+        <slot name="footer"></slot>
+      </div>
+    </div>
+  </Transition>
+</template>
 
 <script lang="ts">
 export default {
