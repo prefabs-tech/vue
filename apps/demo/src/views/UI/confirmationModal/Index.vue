3ad9f83650ba293ca70870e3313b31c873273177
jagrit <jagrit.gaire@dzangolab.com>
2024-09-13 14:54:49 +0545

    refactor: update on cancel and on confirm operations

--- apps/demo/src/views/UI/confirmationModal/Index.vue
+++ apps/demo/src/views/UI/confirmationModal/Index.vue
@@ -1,9 +1,80 @@
 <template>
-  <Page :title="$t('ui.confirmationModal.title')" class="demo-modal">
-    <ConfirmationModal />
+  <Page :title="$t('ui.confirmationModal.title')" class="demo">
+    <section>
+      <h2>Simple confirmation modal</h2>
+
+      <div class="section-content">
+        <!-- eslint-disable -->
+        <SshPre language="html-vue">
+          &lt;template&gt;
+            &lt;div class="container"&gt;
+              &lt;ButtonElement
+                label="Confirm"
+                severity="success"
+                @click="showModal = true"
+              /&gt;
+              &lt;span&gt;{{ currentTime }}&lt;/span&gt;
+            &lt;/div&gt;
+
+            &lt;ConfirmationModal
+              v-show="showModal"
+              @on:close="showModal = false"
+              @on:confirm="onConfirm()"
+            /&gt;
+          &lt;/template&gt; 
+
+          &lt;script setup lang="ts"&gt;
+            import { ButtonElement, ConfirmationModal } from "@dzangolab/vue3-ui";
+            import { ref } from "vue";
+
+            const currentTime = ref(undefined as unknown as Date);
+            const showModal = ref(false);
+
+            function onConfirm() {
+              currentTime.value = new Date();
+              showModal.value = false;
+            }
+          &lt;/script&gt;
+        </SshPre>
+        <!-- eslint-enable -->
+
+        <div class="container">
+          <ButtonElement
+            label="Confirm"
+            severity="success"
+            @click="showModal = true"
+          />
+          <span>{{ currentTime }}</span>
+        </div>
+
+        <ConfirmationModal
+          v-show="showModal"
+          @on:close="showModal = false"
+          @on:confirm="onConfirm()"
+        />
+      </div>
+    </section>
   </Page>
 </template>
 
 <script setup lang="ts">
-import { ConfirmationModal } from "@dzangolab/vue3-ui";
+import { ButtonElement, ConfirmationModal } from "@dzangolab/vue3-ui";
+import { ref } from "vue";
+
+const currentTime = ref(undefined as unknown as Date);
+const showModal = ref(false);
+
+function onConfirm() {
+  currentTime.value = new Date();
+  showModal.value = false;
+}
 </script>
+
+<style lang="css" scoped>
+.demo .container {
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  flex-wrap: wrap;
+}
+</style>
--- packages/vue-ui/src/ConfirmationModal/Index.vue
+++ packages/vue-ui/src/ConfirmationModal/Index.vue
@@ -1,33 +1,72 @@
 <template>
-  <Transition name="modal">
-    <div class="modal">
-      <div class="modal-wrapper">
-        <div class="modal-container">
-          <div class="modal-header">
-            <slot name="header">
-              <h1>Hello world</h1>
-            </slot>
-          </div>
+  <div class="modal">
+    <div class="modal-wrapper">
+      <div class="modal-container">
+        <div class="modal-header">
+          <slot name="header" class="title">
+            <span>Confirmation required</span>
+          </slot>
 
-          <div class="modal-body">
-            <slot name="body">
-              <p>The content of the modal</p>
-            </slot>
-          </div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="1.5em"
+            height="1.5em"
+            viewBox="0 0 16 16"
+            @click="handleClose()"
+          >
+            <path
+              fill="currentColor"
+              d="M5.28 4.22a.75.75 0 0 0-1.06 1.06L6.94 8l-2.72 2.72a.75.75 0 1 0 1.06 1.06L8 9.06l2.72 2.72a.75.75 0 1 0 1.06-1.06L9.06 8l2.72-2.72a.75.75 0 0 0-1.06-1.06L8 6.94z"
+            />
+          </svg>
+        </div>
 
-          <div class="modal-footer">
-            <slot name="footer"></slot>
-          </div>
+        <div class="modal-body">
+          <slot name="body">
+            <p>Are you sure you want to proceed?</p>
+          </slot>
+        </div>
+
+        <div class="modal-footer">
+          <slot name="footer">
+            <ButtonElement
+              size="small"
+              label="Cancel"
+              severity="secondary"
+              @click="handleClose()"
+            />
+
+            <ButtonElement
+              size="small"
+              label="Ok"
+              severity="primary"
+              @click="handleConfirm()"
+            />
+          </slot>
         </div>
       </div>
     </div>
-  </Transition>
+  </div>
 </template>
 
 <script lang="ts">
 export default {
   name: "ConfirmationModal",
 };
+</script>
+
+<script setup lang="ts">
+import ButtonElement from "../Button/Index.vue";
+
+const emits = defineEmits(["on:confirm", "on:close"]);
+
+function handleConfirm() {
+  emits("on:confirm");
+}
+
+function handleClose() {
+  emits("on:close");
+}
 </script>
 
 <style lang="css">
@@ -40,18 +79,44 @@ export default {
   top: 0;
   transition: opacity 0.3s ease;
   width: 100%;
-  z-index: 999;
+  z-index: 1;
+}
+
+.modal .modal-body {
+  padding: 1em 0;
 }
 
 .modal .modal-container {
   background-color: #fff;
   box-shadow: 0 0.125rem 0.5rem rgba(0, 0, 0, 0.33);
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
   margin: 0 auto;
   overflow-x: hidden;
-  padding: 1.5rem;
+  padding: 1em;
   transition: all 0.3s ease;
   width: max-content;
   max-height: 80vh;
+}
+
+.modal .modal-footer {
+  display: flex;
+  gap: 1em;
+  margin-left: auto;
+}
+
+.modal .modal-header {
+  align-items: center;
+  display: flex;
+  font-size: 1.5em;
+  font-weight: 700;
+  gap: 1em;
+  justify-content: space-between;
+}
+
+.modal .modal-header svg {
+  cursor: pointer;
 }
 
 .modal .modal-wrapper {
