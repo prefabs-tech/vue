e71bba1160fa4f90878e8fd9e650906068ec5991
jagrit <jagrit.gaire@dzangolab.com>
2024-09-26 17:33:12 +0545

    refactor: add demo for using I18n

--- apps/demo/src/views/UI/confirmationModal/Index.vue
+++ apps/demo/src/views/UI/confirmationModal/Index.vue
@@ -136,8 +136,8 @@
               &lt;/template&gt;
 
               &lt;template #footer&gt;
-                &lt;ButtonElement :label="Cancel" severity="secondary" /&gt;
-                &lt;ButtonElement :label="Confirm" severity="danger" /&gt;
+                &lt;ButtonElement label="Cancel" severity="secondary" /&gt;
+                &lt;ButtonElement label="Confirm" severity="danger" /&gt;
               &lt;/template&gt;
             &lt;/ConfirmationModal&gt;
           &lt;/template&gt; 
@@ -148,6 +148,103 @@
 
             const currentTime = ref(undefined as unknown as Date);
             const showModal = ref(false);
+          &lt;/script&gt;
+        </SshPre>
+        <!-- eslint-enable -->
+
+        <div class="container">
+          <ButtonElement
+            :label="$t('ui.confirmationModal.buttonLabel.confirm')"
+            severity="success"
+            @click="showCustomContentModal = true"
+          />
+          <span>{{ currentTime }}</span>
+        </div>
+
+        <ConfirmationModal
+          v-show="showCustomContentModal"
+          @on:close="showCustomContentModal = false"
+          @on:confirm="showCustomContentModal = false"
+        >
+          <template #header>
+            <span>
+              {{ $t("ui.confirmationModal.headerContent.deleteUser") }}
+            </span>
+          </template>
+
+          <template #body>
+            <p>{{ $t("ui.confirmationModal.bodyContent.confirmDelete") }}</p>
+          </template>
+
+          <template #footer>
+            <ButtonElement
+              size="small"
+              :label="$t('ui.confirmationModal.buttonLabel.cancel')"
+              severity="secondary"
+            />
+
+            <ButtonElement
+              size="small"
+              :label="$t('ui.confirmationModal.buttonLabel.delete')"
+              severity="danger"
+            />
+          </template>
+        </ConfirmationModal>
+      </div>
+    </section>
+
+    <section>
+      <h2>{{ $t("ui.confirmationModal.usage.withI18n") }}</h2>
+
+      <div class="section-content">
+        <!-- eslint-disable -->
+        <SshPre language="html-vue">
+          &lt;template&gt;
+            &lt;div&gt;
+              &lt;ButtonElement
+                :label="t('ui.confirmationModal.buttonLabel.confirm')"
+                severity="success"
+                @click="showModal = true"
+              /&gt;
+              &lt;span&gt;{{ currentTime }}&lt;/span&gt;
+            &lt;/div&gt;
+
+            &lt;ConfirmationModal
+              v-show="showModal"
+              @on:close="showModal = false"
+              @on:confirm="showCustomContentModal = false"
+            &gt;
+              &lt;template #header&gt;
+                &lt;span&gt;
+                  &lbrace;&lbrace; t("ui.confirmationModal.headerContent.deleteUser") &rbrace;&rbrace;
+                &lt;/span&gt;
+              &lt;/template&gt;
+
+              &lt;template #body&gt;
+                &lt;p&gt;
+                  &lbrace;&lbrace; t("ui.confirmationModal.bodyContent.confirmDelete") &rbrace;&rbrace;
+                &lt;/p&gt;
+              &lt;/template&gt;
+
+              &lt;template #footer&gt;
+                &lt;ButtonElement
+                  :label="t('ui.confirmationModal.buttonLabel.cancel')" severity="secondary"
+                /&gt;
+                &lt;ButtonElement
+                  :label="t('ui.confirmationModal.buttonLabel.delete')" severity="danger"
+                /&gt;
+              &lt;/template&gt;
+            &lt;/ConfirmationModal&gt;
+          &lt;/template&gt;
+
+          &lt;script setup lang="ts"&gt;
+            import { ButtonElement, ConfirmationModal } from "@dzangolab/vue3-ui";
+            import { ref } from "vue";
+            import { useI18n } from "@dzangolab/vue3-i18n";
+
+            const currentTime = ref(undefined as unknown as Date);
+            const showModal = ref(false);
+            const { t } = useI18n();
           &lt;/script&gt;
         </SshPre>
         <!-- eslint-enable -->
