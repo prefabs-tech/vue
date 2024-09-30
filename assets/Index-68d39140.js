da591f76fc2b11b6415fcc030f5c11c60e1b6c1a
JagritGaire <68276478+JagritGaire@users.noreply.github.com>
2024-09-30 10:05:33 +0000

    Deploying to gh-pages from @ dzangolab/vue@4cf0547f916bf456c006ffe657605a28b99c71d0 🚀

--- assets/Index-68d39140.js
+++ assets/Index-68d39140.js
@@ -1,4 +1,4 @@
-import{d as O,e as d,o as L,c as z,w as i,a as o,t as r,f as e,g as l,h as s,a5 as u,Y as f,Z as v,a7 as c,r as B}from"./index-ca7167fa.js";const D={class:"section-content"},I={class:"container"},S={class:"section-content"},P={class:"container"},T={class:"section-content"},N={class:"container"},U={class:"section-content"},V={class:"container"},F={class:"section-content"},H={class:"container"},A={class:"section-content"},G={class:"container"},Y={class:"section-content"},Z={class:"container"},j=o("h2",null,"Custom style",-1),q={class:"section-content"},J={class:"container"},R=O({__name:"Index",setup(K){const b=d(void 0),p=d(!1),M=d(!1),w=d(!1),h=d(!1),g=d(!1),C=d(!1),$=d(!1),y=d(!1);function k(){b.value=new Date,y.value=!1}return(t,n)=>{const m=B("SshPre"),E=B("Page");return L(),z(E,{title:t.$t("ui.confirmationModal.title"),class:"demo"},{default:i(()=>[o("section",null,[o("h2",null,r(t.$t("ui.confirmationModal.usage.basic")),1),e(),o("div",D,[l(m,{language:"html-vue"},{default:i(()=>[e(`
+import{d as O,e as d,o as L,c as z,w as r,a as o,t as i,f as e,g as l,h as s,a5 as u,Y as f,Z as v,a7 as c,r as B}from"./index-0cbaa980.js";const D={class:"section-content"},I={class:"container"},S={class:"section-content"},P={class:"container"},T={class:"section-content"},N={class:"container"},U={class:"section-content"},V={class:"container"},F={class:"section-content"},H={class:"container"},A={class:"section-content"},G={class:"container"},Y={class:"section-content"},Z={class:"container"},j={class:"section-content"},q={class:"container"},Q=O({__name:"Index",setup(J){const b=d(void 0),p=d(!1),M=d(!1),w=d(!1),h=d(!1),g=d(!1),C=d(!1),$=d(!1),y=d(!1);function k(){b.value=new Date,y.value=!1}return(t,n)=>{const m=B("SshPre"),E=B("Page");return L(),z(E,{title:t.$t("ui.confirmationModal.title"),class:"demo"},{default:r(()=>[o("section",null,[o("h2",null,i(t.$t("ui.confirmationModal.usage.basic")),1),e(),o("div",D,[l(m,{language:"html-vue"},{default:r(()=>[e(`
           <template>
             <div>
               <ButtonElement
@@ -6,7 +6,7 @@ import{d as O,e as d,o as L,c as z,w as i,a as o,t as r,f as e,g as l,h as s,a5 
                 severity="success"
                 @click="showModal = true"
               />
-              <span>`+r(b.value)+`</span>
+              <span>`+i(b.value)+`</span>
             </div>
 
             <ConfirmationModal
@@ -28,7 +28,7 @@ import{d as O,e as d,o as L,c as z,w as i,a as o,t as r,f as e,g as l,h as s,a5 
               showModal.value = false;
             }
           <\/script>
-        `,1)]),_:1}),e(),o("div",I,[l(s(u),{label:t.$t("ui.confirmationModal.buttonLabel.confirm"),severity:"success",onClick:n[0]||(n[0]=a=>y.value=!0)},null,8,["label"]),e(),o("span",null,r(b.value),1)]),e(),f(l(s(c),{"onOn:close":n[1]||(n[1]=a=>y.value=!1),"onOn:confirm":n[2]||(n[2]=a=>k())},null,512),[[v,y.value]])])]),e(),o("section",null,[o("h2",null,r(t.$t("ui.confirmationModal.usage.withBorder")),1),e(),o("div",S,[l(m,{language:"html-vue"},{default:i(()=>[e(`
+        `,1)]),_:1}),e(),o("div",I,[l(s(u),{label:t.$t("ui.confirmationModal.buttonLabel.confirm"),severity:"success",onClick:n[0]||(n[0]=a=>y.value=!0)},null,8,["label"]),e(),o("span",null,i(b.value),1)]),e(),f(l(s(c),{"onOn:close":n[1]||(n[1]=a=>y.value=!1),"onOn:confirm":n[2]||(n[2]=a=>k())},null,512),[[v,y.value]])])]),e(),o("section",null,[o("h2",null,i(t.$t("ui.confirmationModal.usage.withBorder")),1),e(),o("div",S,[l(m,{language:"html-vue"},{default:r(()=>[e(`
           <template>
             <div>
               <ButtonElement
@@ -52,7 +52,7 @@ import{d as O,e as d,o as L,c as z,w as i,a as o,t as r,f as e,g as l,h as s,a5 
 
             const showModal = ref(false);
           <\/script>
-        `)]),_:1}),e(),o("div",P,[l(s(u),{label:t.$t("ui.confirmationModal.buttonLabel.confirm"),severity:"success",onClick:n[3]||(n[3]=a=>p.value=!0)},null,8,["label"])]),e(),f(l(s(c),{border:"","onOn:close":n[4]||(n[4]=a=>p.value=!1),"onOn:confirm":n[5]||(n[5]=a=>p.value=!1)},null,512),[[v,p.value]])])]),e(),o("section",null,[o("h2",null,r(t.$t("ui.confirmationModal.usage.disableHeader")),1),e(),o("div",T,[l(m,{language:"html-vue"},{default:i(()=>[e(`
+        `)]),_:1}),e(),o("div",P,[l(s(u),{label:t.$t("ui.confirmationModal.buttonLabel.confirm"),severity:"success",onClick:n[3]||(n[3]=a=>p.value=!0)},null,8,["label"])]),e(),f(l(s(c),{border:"","onOn:close":n[4]||(n[4]=a=>p.value=!1),"onOn:confirm":n[5]||(n[5]=a=>p.value=!1)},null,512),[[v,p.value]])])]),e(),o("section",null,[o("h2",null,i(t.$t("ui.confirmationModal.usage.disableHeader")),1),e(),o("div",T,[l(m,{language:"html-vue"},{default:r(()=>[e(`
           <template>
             <div>
               <ButtonElement
@@ -76,7 +76,7 @@ import{d as O,e as d,o as L,c as z,w as i,a as o,t as r,f as e,g as l,h as s,a5 
 
             const showModal = ref(false);
           <\/script>
-        `)]),_:1}),e(),o("div",N,[l(s(u),{label:t.$t("ui.confirmationModal.buttonLabel.confirm"),severity:"success",onClick:n[6]||(n[6]=a=>C.value=!0)},null,8,["label"])]),e(),f(l(s(c),{"disable-header":"","onOn:close":n[7]||(n[7]=a=>C.value=!1),"onOn:confirm":n[8]||(n[8]=a=>C.value=!1)},null,512),[[v,C.value]])])]),e(),o("section",null,[o("h2",null,r(t.$t("ui.confirmationModal.usage.disableBody")),1),e(),o("div",U,[l(m,{language:"html-vue"},{default:i(()=>[e(`
+        `)]),_:1}),e(),o("div",N,[l(s(u),{label:t.$t("ui.confirmationModal.buttonLabel.confirm"),severity:"success",onClick:n[6]||(n[6]=a=>C.value=!0)},null,8,["label"])]),e(),f(l(s(c),{"disable-header":"","onOn:close":n[7]||(n[7]=a=>C.value=!1),"onOn:confirm":n[8]||(n[8]=a=>C.value=!1)},null,512),[[v,C.value]])])]),e(),o("section",null,[o("h2",null,i(t.$t("ui.confirmationModal.usage.disableBody")),1),e(),o("div",U,[l(m,{language:"html-vue"},{default:r(()=>[e(`
           <template>
             <div>
               <ButtonElement
@@ -100,7 +100,7 @@ import{d as O,e as d,o as L,c as z,w as i,a as o,t as r,f as e,g as l,h as s,a5 
 
             const showModal = ref(false);
           <\/script>
-        `)]),_:1}),e(),o("div",V,[l(s(u),{label:t.$t("ui.confirmationModal.buttonLabel.confirm"),severity:"success",onClick:n[9]||(n[9]=a=>h.value=!0)},null,8,["label"])]),e(),f(l(s(c),{"disable-body":"","onOn:close":n[10]||(n[10]=a=>h.value=!1),"onOn:confirm":n[11]||(n[11]=a=>h.value=!1)},null,512),[[v,h.value]])])]),e(),o("section",null,[o("h2",null,r(t.$t("ui.confirmationModal.usage.disableFooter")),1),e(),o("div",F,[l(m,{language:"html-vue"},{default:i(()=>[e(`
+        `)]),_:1}),e(),o("div",V,[l(s(u),{label:t.$t("ui.confirmationModal.buttonLabel.confirm"),severity:"success",onClick:n[9]||(n[9]=a=>h.value=!0)},null,8,["label"])]),e(),f(l(s(c),{"disable-body":"","onOn:close":n[10]||(n[10]=a=>h.value=!1),"onOn:confirm":n[11]||(n[11]=a=>h.value=!1)},null,512),[[v,h.value]])])]),e(),o("section",null,[o("h2",null,i(t.$t("ui.confirmationModal.usage.disableFooter")),1),e(),o("div",F,[l(m,{language:"html-vue"},{default:r(()=>[e(`
           <template>
             <div>
               <ButtonElement
@@ -124,7 +124,7 @@ import{d as O,e as d,o as L,c as z,w as i,a as o,t as r,f as e,g as l,h as s,a5 
 
             const showModal = ref(false);
           <\/script>
-        `)]),_:1}),e(),o("div",H,[l(s(u),{label:t.$t("ui.confirmationModal.buttonLabel.confirm"),severity:"success",onClick:n[12]||(n[12]=a=>g.value=!0)},null,8,["label"])]),e(),f(l(s(c),{"disable-footer":"","onOn:close":n[13]||(n[13]=a=>g.value=!1),"onOn:confirm":n[14]||(n[14]=a=>g.value=!1)},null,512),[[v,g.value]])])]),e(),o("section",null,[o("h2",null,r(t.$t("ui.confirmationModal.usage.customContent")),1),e(),o("div",A,[l(m,{language:"html-vue"},{default:i(()=>[e(`
+        `)]),_:1}),e(),o("div",H,[l(s(u),{label:t.$t("ui.confirmationModal.buttonLabel.confirm"),severity:"success",onClick:n[12]||(n[12]=a=>g.value=!0)},null,8,["label"])]),e(),f(l(s(c),{"disable-footer":"","onOn:close":n[13]||(n[13]=a=>g.value=!1),"onOn:confirm":n[14]||(n[14]=a=>g.value=!1)},null,512),[[v,g.value]])])]),e(),o("section",null,[o("h2",null,i(t.$t("ui.confirmationModal.usage.customContent")),1),e(),o("div",A,[l(m,{language:"html-vue"},{default:r(()=>[e(`
           <template>
             <div>
               <ButtonElement
@@ -160,7 +160,7 @@ import{d as O,e as d,o as L,c as z,w as i,a as o,t as r,f as e,g as l,h as s,a5 
 
             const showModal = ref(false);
           <\/script>
-        `)]),_:1}),e(),o("div",G,[l(s(u),{label:t.$t("ui.confirmationModal.buttonLabel.confirm"),severity:"success",onClick:n[15]||(n[15]=a=>M.value=!0)},null,8,["label"]),e(),o("span",null,r(b.value),1)]),e(),f(l(s(c),{"onOn:close":n[16]||(n[16]=a=>M.value=!1),"onOn:confirm":n[17]||(n[17]=a=>M.value=!1)},{header:i(()=>[o("span",null,r(t.$t("ui.confirmationModal.headerContent.deleteUser")),1)]),body:i(()=>[o("p",null,r(t.$t("ui.confirmationModal.bodyContent.confirmDelete")),1)]),footer:i(()=>[l(s(u),{size:"small",label:t.$t("ui.confirmationModal.buttonLabel.cancel"),severity:"secondary"},null,8,["label"]),e(),l(s(u),{size:"small",label:t.$t("ui.confirmationModal.buttonLabel.delete"),severity:"danger"},null,8,["label"])]),_:1},512),[[v,M.value]])])]),e(),o("section",null,[o("h2",null,r(t.$t("ui.confirmationModal.usage.withI18n")),1),e(),o("div",Y,[l(m,{language:"html-vue"},{default:i(()=>[e(`
+        `)]),_:1}),e(),o("div",G,[l(s(u),{label:t.$t("ui.confirmationModal.buttonLabel.confirm"),severity:"success",onClick:n[15]||(n[15]=a=>M.value=!0)},null,8,["label"]),e(),o("span",null,i(b.value),1)]),e(),f(l(s(c),{"onOn:close":n[16]||(n[16]=a=>M.value=!1),"onOn:confirm":n[17]||(n[17]=a=>M.value=!1)},{header:r(()=>[o("span",null,i(t.$t("ui.confirmationModal.headerContent.deleteUser")),1)]),body:r(()=>[o("p",null,i(t.$t("ui.confirmationModal.bodyContent.confirmDelete")),1)]),footer:r(()=>[l(s(u),{size:"small",label:t.$t("ui.confirmationModal.buttonLabel.cancel"),severity:"secondary"},null,8,["label"]),e(),l(s(u),{size:"small",label:t.$t("ui.confirmationModal.buttonLabel.delete"),severity:"danger"},null,8,["label"])]),_:1},512),[[v,M.value]])])]),e(),o("section",null,[o("h2",null,i(t.$t("ui.confirmationModal.usage.withI18n")),1),e(),o("div",Y,[l(m,{language:"html-vue"},{default:r(()=>[e(`
           <template>
             <div>
               <ButtonElement
@@ -206,7 +206,7 @@ import{d as O,e as d,o as L,c as z,w as i,a as o,t as r,f as e,g as l,h as s,a5 
             const showModal = ref(false);
             const { t } = useI18n();
           <\/script>
-        `)]),_:1}),e(),o("div",Z,[l(s(u),{label:t.$t("ui.confirmationModal.buttonLabel.confirm"),severity:"success",onClick:n[18]||(n[18]=a=>$.value=!0)},null,8,["label"]),e(),o("span",null,r(b.value),1)]),e(),f(l(s(c),{"onOn:close":n[19]||(n[19]=a=>$.value=!1),"onOn:confirm":n[20]||(n[20]=a=>$.value=!1)},{header:i(()=>[o("span",null,r(t.$t("ui.confirmationModal.headerContent.deleteUser")),1)]),body:i(()=>[o("p",null,r(t.$t("ui.confirmationModal.bodyContent.confirmDelete")),1)]),footer:i(()=>[l(s(u),{size:"small",label:t.$t("ui.confirmationModal.buttonLabel.cancel"),severity:"secondary"},null,8,["label"]),e(),l(s(u),{size:"small",label:t.$t("ui.confirmationModal.buttonLabel.delete"),severity:"danger"},null,8,["label"])]),_:1},512),[[v,$.value]])])]),e(),o("section",null,[j,e(),o("div",q,[l(m,{language:"html-vue"},{default:i(()=>[e(`
+        `)]),_:1}),e(),o("div",Z,[l(s(u),{label:t.$t("ui.confirmationModal.buttonLabel.confirm"),severity:"success",onClick:n[18]||(n[18]=a=>$.value=!0)},null,8,["label"]),e(),o("span",null,i(b.value),1)]),e(),f(l(s(c),{"onOn:close":n[19]||(n[19]=a=>$.value=!1),"onOn:confirm":n[20]||(n[20]=a=>$.value=!1)},{header:r(()=>[o("span",null,i(t.$t("ui.confirmationModal.headerContent.deleteUser")),1)]),body:r(()=>[o("p",null,i(t.$t("ui.confirmationModal.bodyContent.confirmDelete")),1)]),footer:r(()=>[l(s(u),{size:"small",label:t.$t("ui.confirmationModal.buttonLabel.cancel"),severity:"secondary"},null,8,["label"]),e(),l(s(u),{size:"small",label:t.$t("ui.confirmationModal.buttonLabel.delete"),severity:"danger"},null,8,["label"])]),_:1},512),[[v,$.value]])])]),e(),o("section",null,[o("h2",null,i(t.$t("ui.confirmationModal.usage.customStyle")),1),e(),o("div",j,[l(m,{language:"html-vue"},{default:r(()=>[e(`
           <template>
             <div>
               <ButtonElement
@@ -241,4 +241,4 @@ import{d as O,e as d,o as L,c as z,w as i,a as o,t as r,f as e,g as l,h as s,a5 
               --_icon-color: #dee2e6;
             }
           </style>
-        `)]),_:1}),e(),o("div",J,[l(s(u),{label:t.$t("ui.confirmationModal.buttonLabel.confirm"),severity:"success",onClick:n[21]||(n[21]=a=>w.value=!0)},null,8,["label"])]),e(),f(l(s(c),{class:"custom-modal","onOn:close":n[22]||(n[22]=a=>w.value=!1),"onOn:confirm":n[23]||(n[23]=a=>w.value=!1)},null,512),[[v,w.value]])])])]),_:1},8,["title"])}}});export{R as default};
+        `)]),_:1}),e(),o("div",q,[l(s(u),{label:t.$t("ui.confirmationModal.buttonLabel.confirm"),severity:"success",onClick:n[21]||(n[21]=a=>w.value=!0)},null,8,["label"])]),e(),f(l(s(c),{class:"custom-modal","onOn:close":n[22]||(n[22]=a=>w.value=!1),"onOn:confirm":n[23]||(n[23]=a=>w.value=!1)},null,512),[[v,w.value]])])])]),_:1},8,["title"])}}});export{Q as default};
