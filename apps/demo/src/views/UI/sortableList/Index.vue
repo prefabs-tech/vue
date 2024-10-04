b647f6355be07f2288f5faacd05fa51f4e890324
jagrit <jagrit.gaire@dzangolab.com>
2024-10-04 12:27:15 +0545

    refactor: add demo code of sortable list

--- apps/demo/src/views/UI/sortableList/Index.vue
+++ apps/demo/src/views/UI/sortableList/Index.vue
@@ -1,9 +1,36 @@
 <template>
-  <Page :title="'Sortable list'" class="demo-message">
+  <Page :title="'Sortable list'" class="demo-sortable-list">
     <section>
       <h2>Basic</h2>
 
-      <SortableList :list="list" @on:drag="onDrag" />
+      <div class="section-content">
+        <!-- eslint-disable -->
+        <SshPre language="html-vue">
+          &lt;template&gt;
+            &lt;SortableList :list="list" @on:drag="onDrag" /&gt;
+          &lt;/template&gt;
+
+          &lt;script setup lang="ts"&gt; 
+            import { SortableList } from "@dzangolab/vue3-ui";
+            import { ref } from "vue";
+  
+            const list = ref([
+              {
+                id: 1,
+                data: "Item 1",
+              },
+              ...
+            ]);
+
+            const onDrag = (sortedList: []) => {
+              ...
+            };
+          &lt;/script&gt;
+        </SshPre>
+        <!-- eslint-enable -->
+
+        <SortableList :list="list" @on:drag="onDrag" />
+      </div>
     </section>
   </Page>
 </template>
@@ -39,7 +66,18 @@ const list = ref([
   },
 ]);
 
-const onDrag = (listData: []) => {
-  list.value = listData;
+const onDrag = (sortedList: []) => {
+  list.value = sortedList;
 };
 </script>
+
+<style>
+.demo-sortable-list section {
+  margin-bottom: 2.5em;
+}
+
+.demo-sortable-list .section-content {
+  display: grid;
+  gap: 1.5rem;
+}
+</style>
