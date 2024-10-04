6e9bfe21dcddd856dcabfe89a0c560bd49e05896
jagrit <jagrit.gaire@dzangolab.com>
2024-10-04 15:20:14 +0545

    test: add snapshot test for message banner

---
+++ packages/vue-ui/src/Message/__test__/snapshot/Message.test.ts
@@ -0,0 +1,21 @@
+import { mount } from "@vue/test-utils";
+import { describe, expect, it } from "vitest";
+
+import Message from "../../Index.vue";
+
+describe("Message", () => {
+  it("matches snapshot", () => {
+    const wrapper = mount(Message, {
+      props: {
+        enableClose: true,
+        message: "This is a very important message.",
+        icon: true,
+      },
+      slots: {
+        icon: "<i class='pi pi-bell' />",
+      },
+    });
+
+    expect(wrapper.element).toMatchSnapshot();
+  });
+});
