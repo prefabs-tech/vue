bec30c43125bf7fab8cad766d391517382d53868
Jagrit Gaire <68276478+JagritGaire@users.noreply.github.com>
2024-10-07 17:03:56 +0545

    Add test for loading page and message banner (#353)
    * test: add snapshot test for badge component
    
    * test: add snapshot test for button
    
    * test: add snapshot test for confirmation modal
    
    * test: add snapshot test for message banner
    
    * test: add snapshot test for loading page
    
    * test: update mount method of loading page

---
+++ packages/vue-ui/src/LoadingPage/__test__/snapshot/LoadingPage.test.ts
@@ -0,0 +1,12 @@
+import { mount } from "@vue/test-utils";
+import { describe, expect, it } from "vitest";
+
+import LoadingPage from "../../Index.vue";
+
+describe("LoadingPage", () => {
+  it("matches snapshot", () => {
+    const wrapper = mount(LoadingPage);
+
+    expect(wrapper.element).toMatchSnapshot();
+  });
+});
