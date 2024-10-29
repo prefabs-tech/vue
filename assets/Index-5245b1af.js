4437ec67a2165dbb54fa49763a05e8f783e5236a
DeepakAryal <deepakaryal2050@gmail.com>
2024-10-29 11:00:28 +0000

    Deploying to gh-pages from @ dzangolab/vue@88a9226f176d05d06fc20d8ade73faab0a6debf5 🚀

---
+++ assets/Index-5245b1af.js
@@ -0,0 +1,149 @@
+import{m as V,G as i,Q as d}from"./DzangolabVue3Form.es-7f4e4489.js";import{d as N,u as _,b as $,e as b,r as v,o as F,c as y,w as a,a as l,t as p,f as n,g as o,h as e}from"./index-3fcf3b87.js";const z={class:"section-content"},E={class:"section-content"},S={class:"section-content"},w={class:"section-content"},O={class:"section-content"},U={class:"section-content"},M={class:"section-content"},P={class:"section-content"},q={name:"NumberInputField"},L=N({...q,setup(W){const{t:f}=_(),g=V.coerce.number({invalid_type_error:f("form.errors.number.invalid")}).gte(1,f("form.errors.number.invalid")).lte(100,f("form.errors.number.invalid")),h={invalid:f("form.errors.number.invalid"),required:f("form.errors.number.required")},c={min:1,max:100,allow_leading_zeroes:!1};let r=$({customStyleInput:b(),customValidationInput:b(),disabled:b(100),input:b(),inputWithValidation:b(),noLabelInput:b(),textInput:b()});return(t,u)=>{const s=v("SshPre"),I=v("Page");return F(),y(I,{title:t.$t("form.label.number"),class:"demo"},{default:a(()=>[l("section",null,[l("h2",null,p(t.$t("form.label.basicInput")),1),n(),l("div",z,[o(s,{language:"html-vue"},{default:a(()=>[n(`
+          <Form>
+            <NumberInput 
+              v-model="input"
+              placeholder="Enter a number"
+            />
+          </Form>
+
+          <script setup lang="ts">
+          import { NumberInput } from "@dzangolab/vue3-form";
+          <\/script>
+        `)]),_:1}),n(),o(e(d),null,{default:a(()=>[o(e(i),{modelValue:e(r).noLabelInput,"onUpdate:modelValue":u[0]||(u[0]=m=>e(r).noLabelInput=m),placeholder:t.$t("form.placeholder.number")},null,8,["modelValue","placeholder"])]),_:1})])]),n(),l("section",null,[l("h2",null,p(t.$t("form.label.labelInput")),1),n(),l("div",E,[o(s,{language:"html-vue"},{default:a(()=>[n(`
+          <Form>
+            <NumberInput 
+              v-model="input"
+              label="Number"
+              placeholder="Enter a number"
+            />
+          </Form>
+
+          <script setup lang="ts">
+          import { NumberInput } from "@dzangolab/vue3-form";
+          <\/script>
+        `)]),_:1}),n(),o(e(d),null,{default:a(()=>[o(e(i),{modelValue:e(r).input,"onUpdate:modelValue":u[1]||(u[1]=m=>e(r).input=m),label:t.$t("form.label.number"),placeholder:t.$t("form.placeholder.number")},null,8,["modelValue","label","placeholder"])]),_:1})])]),n(),l("section",null,[l("h2",null,p(t.$t("form.label.disabled")),1),n(),l("div",S,[o(s,{language:"html-vue"},{default:a(()=>[n(`
+          <Form>
+            <NumberInput 
+              v-model="input"
+              label="Number"
+              disabled
+            />
+          </Form>
+
+          <script setup lang="ts">
+          import { NumberInput } from "@dzangolab/vue3-form";
+          <\/script>
+        `)]),_:1}),n(),o(e(d),null,{default:a(()=>[o(e(i),{modelValue:e(r).disabled,"onUpdate:modelValue":u[2]||(u[2]=m=>e(r).disabled=m),label:t.$t("form.label.number"),disabled:""},null,8,["modelValue","label"])]),_:1})])]),n(),l("section",null,[l("h2",null,p(t.$t("form.label.withI18n")),1),n(),l("div",w,[o(s,{language:"html-vue"},{default:a(()=>[n(`
+          <Form>
+            <NumberInput 
+              v-model="input"
+              :label="t('form.label.number')"
+              :placeholder="t('form.placeholder.number')"
+            />
+          </Form>
+
+          <script setup lang="ts">
+          import { NumberInput } from "@dzangolab/vue3-form";
+          import { useI18n } from "@dzangolab/vue3-i18n";
+
+          const { t } = useI18n();
+          <\/script>
+        `)]),_:1}),n(),o(e(d),null,{default:a(()=>[o(e(i),{modelValue:e(r).input,"onUpdate:modelValue":u[3]||(u[3]=m=>e(r).input=m),label:t.$t("form.label.number"),placeholder:t.$t("form.placeholder.number")},null,8,["modelValue","label","placeholder"])]),_:1})])]),n(),l("section",null,[l("h2",null,p(t.$t("form.label.inputWithValidation")),1),n(),l("div",O,[o(s,{language:"html-vue"},{default:a(()=>[n(`
+          <Form>
+            <NumberInput 
+              v-model="input"
+              :options="validationOptions"
+              label="Number"
+              placeholder="Enter a number"
+            />
+          </Form>
+
+          <script setup lang="ts">
+          import { Form, NumberInput } from "@dzangolab/vue3-form";
+          
+          import type { IsIntOptions } from "@dzangolab/vue3-form";
+  
+          const validationOptions = {
+            min: 1,
+            max: 100,
+            allow_leading_zeroes: false,
+          } as IsIntOptions;
+          <\/script>
+        `)]),_:1}),n(),o(e(d),null,{default:a(()=>[o(e(i),{modelValue:e(r).inputWithValidation,"onUpdate:modelValue":u[4]||(u[4]=m=>e(r).inputWithValidation=m),label:t.$t("form.label.number"),options:e(c),placeholder:t.$t("form.placeholder.number")},null,8,["modelValue","label","options","placeholder"])]),_:1})])]),n(),l("section",null,[l("h2",null,p(t.$t("form.label.customErrorMessageInput")),1),n(),l("div",U,[o(s,{language:"html-vue"},{default:a(()=>[n(`
+          <Form>
+            <NumberInput 
+              v-model="input"
+              :error-messages="validationMessages"
+              :options="validationOptions"
+              label="Number"
+              placeholder="Enter a number"
+            />
+          </Form>
+
+          <script setup lang="ts">
+          import { Form, NumberInput } from "@dzangolab/vue3-form";
+
+          import type { NumberErrorMessages, IsIntOptions } from "@dzangolab/vue3-form";
+
+          const validationMessages = {
+            invalid: "Please provide a number between 1 to 100",
+            required: "The field is required",
+          } as NumberErrorMessages;
+
+          const validationOptions = {
+            min: 1,
+            max: 100,
+            allow_leading_zeroes: false,
+          } as IsIntOptions;
+          <\/script>
+        `)]),_:1}),n(),o(e(d),null,{default:a(()=>[o(e(i),{modelValue:e(r).textInput,"onUpdate:modelValue":u[5]||(u[5]=m=>e(r).textInput=m),label:t.$t("form.label.number"),"error-messages":e(h),options:e(c),placeholder:t.$t("form.placeholder.number")},null,8,["modelValue","label","error-messages","options","placeholder"])]),_:1})])]),n(),l("section",null,[l("h2",null,p(t.$t("form.label.customValidationInput")),1),n(),l("div",M,[o(s,{language:"html-vue"},{default:a(()=>[n(`
+          <Form>
+            <NumberInput 
+              v-model="input"
+              :schema="inputSchema"
+              label="Number"
+              placeholder="Enter a number"
+              name="number"
+            />
+          </Form>
+
+          <script setup lang="ts">
+          import { Form, NumberInput } from "@dzangolab/vue3-form";
+          import { z } from "zod";
+
+          const invalidNumberError = "Please provide a number between 1 to 100";
+
+          const inputSchema =  z.coerce
+            .number({invalid_type_error: invalidNumberError})
+            .gte(1, invalidNumberError)
+            .lte(100, invalidNumberError);
+          <\/script>
+        `)]),_:1}),n(),o(e(d),null,{default:a(()=>[o(e(i),{modelValue:e(r).customValidationInput,"onUpdate:modelValue":u[6]||(u[6]=m=>e(r).customValidationInput=m),label:t.$t("form.label.number"),placeholder:t.$t("form.placeholder.number"),schema:e(g),name:"number"},null,8,["modelValue","label","placeholder","schema"])]),_:1})])]),n(),l("section",null,[l("h2",null,p(t.$t("form.label.customStyleInput")),1),n(),l("div",P,[o(s,{language:"html-vue"},{default:a(()=>[n(`
+          <Form>
+            <NumberInput 
+              v-model="input"
+              class="custom-style-input"
+              label="Number"
+              placeholder="Enter a number"
+            />
+          </Form>
+
+          <script setup lang="ts">
+          import { NumberInput } from "@dzangolab/vue3-form";
+          <\/script>
+
+          <style>
+          .custom-style-input input {
+            --_input-field-border-color: #007aff;
+            --_input-field-border-radius: 0.5em;
+            --_input-field-padding-h: 0.5em;
+            --_input-field-padding-v: 0.5em;
+
+            color: #007aff;
+          }
+
+          .custom-style-input input:focus {
+            box-shadow: 0 0 0 0.25rem #03449e9a;
+          }
+          </style>
+        `)]),_:1}),n(),o(e(d),null,{default:a(()=>[o(e(i),{modelValue:e(r).customStyleInput,"onUpdate:modelValue":u[7]||(u[7]=m=>e(r).customStyleInput=m),label:t.$t("form.label.number"),placeholder:t.$t("form.placeholder.number"),class:"custom-style-input"},null,8,["modelValue","label","placeholder"])]),_:1})])])]),_:1},8,["title"])}}});export{L as default};
---
+++ assets/Index-950e0ae7.js
@@ -0,0 +1,72 @@
+import{d as _,e as u,r as d,o as p,c as g,w as l,a as e,t as s,f as n,g as o,h as c,a8 as m,D as h,I as w,E as b}from"./index-3fcf3b87.js";const P={class:"section-content"},$={class:"email"},C={class:"label"},D={class:"value"},S={class:"name"},y={class:"label"},N={class:"value"},k={class:"section-content"},z=e("div",{class:"trigger"},"...",-1),I=_({__name:"Index",setup(B){const t=u([{disabled:!0,label:"Change password",value:"password"},{label:"Profile",value:"profile"}]),r=u(!1),v=a=>{a===t.value[1].value&&(r.value=!0)};return(a,V)=>{const i=d("SshPre"),f=d("Page");return p(),g(f,{title:a.$t("ui.dropdown.title"),class:"demo"},{default:l(()=>[e("section",null,[e("h2",null,s(a.$t("ui.dropdown.usage.basic")),1),n(),e("div",P,[o(i,{language:"html-vue"},{default:l(()=>[n(`
+          <template>
+            <Dropdown 
+              :menu="menu"
+              label="User"
+              @select="onSelect"
+            />
+
+            <template v-if="showProfile">
+              <div class="email">
+                <span class="label">
+                  Email
+                </span>
+                <span class="value">
+                  : monorepo@dzangolab.com
+                </span>
+              </div>
+              <div class="name">
+                <span class="label">
+                  Name
+                </span>
+                <span class="value">
+                  : Monorepo
+                </span>
+              </div>
+            </template>
+          </template>
+
+          <script setup lang="ts">
+          import { Dropdown } from "@dzangolab/vue3-ui";
+          import { ref } from "vue";
+
+          const menu = ref([
+            { disabled: true, label: "Change password", value: "password"  },
+            { label: "Profile", value: "profile" },
+          ]);
+
+          const showProfile = ref<boolean>(false);
+
+          const onSelect = (value: string | number) => {
+            if (value === menu.value[1].value) {
+              showProfile.value = true;
+            }
+          };
+          <\/script>
+        `)]),_:1}),n(),o(c(m),{label:a.$t("ui.dropdown.label.user"),menu:t.value,onSelect:v},null,8,["label","menu"]),n(),r.value?(p(),h(w,{key:0},[e("div",$,[e("span",C,s(a.$t("ui.dropdown.label.email")),1),n(),e("span",D,s(": "+a.$t("ui.dropdown.usage.email")),1)]),n(),e("div",S,[e("span",y,s(a.$t("ui.dropdown.label.name")),1),n(),e("span",N,s(": "+a.$t("ui.dropdown.usage.name")),1)])],64)):b("",!0)])]),n(),e("section",null,[e("h2",null,s(a.$t("ui.dropdown.usage.withSlot")),1),n(),e("div",k,[o(i,{language:"html-vue"},{default:l(()=>[n(`
+          <template>
+            <Dropdown 
+              :menu="menu"
+            >
+              <div class="trigger">...</div>
+            </Dropdown>
+          </template>
+
+          <script setup lang="ts">
+          import { Dropdown } from "@dzangolab/vue3-ui";
+          import { ref } from "vue";
+
+          const menu = ref([
+            { disabled: true, label: "Change password", value: "password"  },
+            { label: "Profile", value: "profile" },
+          ]);
+          <\/script>
+
+          <style lang="css">
+          .trigger {
+            font-size: 1.5rem;
+            font-weight: 600;
+            letter-spacing: 0.2rem;
+          }
+          </style>
+        `)]),_:1}),n(),o(c(m),{menu:t.value},{default:l(()=>[z]),_:1},8,["menu"])])])]),_:1},8,["title"])}}});export{I as default};
