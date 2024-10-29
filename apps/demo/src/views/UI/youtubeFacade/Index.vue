88a9226f176d05d06fc20d8ade73faab0a6debf5
KabinKhandThakuri <kabin@dzangolab.com>
2024-10-29 16:44:32 +0545

    Refactor stepper and youtube facade component and related demo (#369)

--- apps/demo/src/views/UI/youtubeFacade/Index.vue
+++ apps/demo/src/views/UI/youtubeFacade/Index.vue
@@ -37,7 +37,7 @@
           &lt;template&gt;
             &lt;YoutubeFacade
               alt="Vue basic"
-              aspect-ratio="16/9"
+              aspect-ratio="16/10"
               video-link="https://www.youtube.com/watch?v=F7PLPJqVotk"
               video-token="F7PLPJqVotk"
             /&gt;
@@ -51,7 +51,7 @@
 
         <YoutubeFacade
           :alt="$t('ui.youtubeFacade.alternativeMessage')"
-          aspect-ratio="16/9"
+          aspect-ratio="16/10"
           video-link="https://www.youtube.com/watch?v=F7PLPJqVotk"
           video-token="F7PLPJqVotk"
         />
@@ -67,7 +67,7 @@
           &lt;template&gt;
             &lt;YoutubeFacade
               alt="Vue basic"
-              aspect-ratio="9/3"
+              aspect-ratio="1.85/1"
               image-source="logo.png"
               video-link="https://www.youtube.com/watch?v=YrxBCBibVo0"
               video-token="YrxBCBibVo0"
@@ -82,49 +82,10 @@
 
         <YoutubeFacade
           :alt="$t('ui.youtubeFacade.alternativeMessage')"
-          aspect-ratio="9/3"
+          aspect-ratio="1.85/1"
           image-source="logo.png"
           video-link="https://www.youtube.com/watch?v=YrxBCBibVo0"
           video-token="YrxBCBibVo0"
-        />
-      </div>
-    </section>
-
-    <section>
-      <h2>{{ $t("ui.youtubeFacade.usage.customStyle") }}</h2>
-
-      <div class="section-content">
-        <!-- eslint-disable -->
-        <SshPre language="html-vue">
-          &lt;template&gt;
-            &lt;YoutubeFacade
-              alt="Vue basic"
-              aspect-ratio="16/9"
-              class="custom-youtube-facade"
-              video-link="https://www.youtube.com/watch?v=CYPZBK8zUik"
-              video-token="CYPZBK8zUik"
-            /&gt;
-          &lt;/template&gt;
-
-          &lt;script setup lang="ts"&gt;
-            import { YoutubeFacade } from "@dzangolab/vue3-ui";
-          &lt;/script&gt;
-
-          &lt;style lang="css"&gt;
-            .custom-youtube-facade {
-              --_margin: 0 auto;
-              --_width: 50%;
-            }
-          &lt;/style&gt;
-        </SshPre>
-        <!-- eslint-enable -->
-
-        <YoutubeFacade
-          :alt="$t('ui.youtubeFacade.alternativeMessage')"
-          aspect-ratio="16/9"
-          class="custom-youtube-facade"
-          video-link="https://www.youtube.com/watch?v=CYPZBK8zUik"
-          video-token="CYPZBK8zUik"
         />
       </div>
     </section>
