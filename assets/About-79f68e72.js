e25301a56778f44c8acf0d3ebfba30c698ad3446
DeepakAryal <deepakaryal2050@gmail.com>
2024-11-05 10:51:59 +0000

    Deploying to gh-pages from @ dzangolab/vue@2a73f8d870e495625109edbbb774ace6623f73a2 🚀

--- assets/About-79f68e72.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,r as n,o as a,c as s}from"./index-146b4921.js";const c={name:"About"};function r(t,p,_,l,u,i){const e=n("Page");return a(),s(e,{title:t.$t("about.title")},null,8,["title"])}const m=o(c,[["render",r]]);export{m as default};
