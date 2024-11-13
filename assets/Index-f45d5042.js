4c1ef56d2685b84272c4a96d333dbded1e7beed5
KabinKhandThakuri <kabin@dzangolab.com>
2024-11-13 12:23:51 +0000

    Deploying to gh-pages from @ dzangolab/vue@35e4cd9b8574c48b4dab246cb1ea672a924ced0f 🚀

---
+++ assets/Index-f45d5042.js
@@ -0,0 +1,30 @@
+import{d as _,u as b,e as u,r,o as c,c as f,w as a,f as l,g as e,t as o,a as n,h,ac as L,D as k,E as y}from"./index-282387c5.js";const S={class:"section-content"},D={key:0,class:"most-favourite"},C=_({__name:"Index",setup(B){const{t:s}=b(),d=u([{id:1,data:s("ui.sortableList.games.basketball")},{id:2,data:s("ui.sortableList.games.cricket")},{id:3,data:s("ui.sortableList.games.football")},{id:4,data:s("ui.sortableList.games.hockey")},{id:5,data:s("ui.sortableList.games.volleyball")}]),i=u(),m=t=>{i.value=t[0].data};return(t,F)=>{const v=r("router-link"),g=r("SshPre"),p=r("Page");return c(),f(p,{title:t.$t("ui.sortableList.title"),class:"demo-sortable-list"},{toolbar:a(()=>[l(v,{to:{name:"ui"},class:"back"},{default:a(()=>[e(o(t.$t("common.back")),1)]),_:1})]),default:a(()=>[e(),n("section",null,[n("h2",null,o(t.$t("ui.sortableList.usage.basic")),1),e(),n("div",S,[l(g,{language:"html-vue"},{default:a(()=>[e(`
+          <template>
+            <h3>Sort sports from most to least favorite</h3>
+
+            <SortableList :list="list" @on-drag="onDrag" />
+
+            <div v-if="mostFavourite" class="most-favourite">
+              {{ mostFavorite + " is your most favorite sport." }}
+            </div>
+          </template>
+
+          <script setup lang="ts"> 
+            import { SortableList } from "@dzangolab/vue3-ui";
+            import { ref } from "vue";
+  
+            const list = ref([
+              {
+                id: 1,
+                data: "Basketball",
+              },
+              ...
+            ]);
+
+            const mostFavourite = ref<string>();
+
+            const onDrag = (sortedList: [{ id: number, data: string }]) => {
+              mostFavourite.value = sortedList[0].data;
+            };
+          <\/script>
+        `)]),_:1}),e(),n("h3",null,o(t.$t("ui.sortableList.usage.sortSport")),1),e(),l(h(L),{list:d.value,onOnDrag:m},null,8,["list"]),e(),i.value?(c(),k("div",D,o(t.$t("ui.sortableList.games.favourite",{sport:i.value})),1)):y("",!0)])])]),_:1},8,["title"])}}});export{C as default};
