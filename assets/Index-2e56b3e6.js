52f0af9ccac48296a27bfc32e0f0aa44733c1d7b
DeepakAryal <deepakaryal2050@gmail.com>
2024-11-18 08:07:17 +0000

    Deploying to gh-pages from @ dzangolab/vue@8cba054664d26e0bc56b8bf4fa73aef18ebdf1f1 🚀

---
+++ assets/Index-2e56b3e6.js
@@ -0,0 +1,69 @@
+import{m as w,n as d,Z as p}from"./DzangolabVue3Form.es-264401b9.js";import{d as V,u as I,b as S,e as c,r as f,o as _,c as $,w as a,f as l,g as e,t as r,a as t,h as n}from"./index-a50c4f8e.js";const F={class:"section-content"},z={class:"section-content"},k={class:"section-content"},U={class:"section-content"},P={class:"section-content"},W={name:"SwitchInput"},L=V({...W,setup(B){const{t:b}=I(),h=w.coerce.boolean().refine(o=>o===!0,{message:b("form.errors.switch.invalid")});let s=S({disabled:c(!0),input:c(),inputWithValidation:c(!1),noLabelInput:c()});return(o,u)=>{const v=f("router-link"),m=f("SshPre"),g=f("Page");return _(),$(g,{title:o.$t("form.label.switch"),class:"demo"},{toolbar:a(()=>[l(v,{to:{name:"form"},class:"back"},{default:a(()=>[e(r(o.$t("common.back")),1)]),_:1})]),default:a(()=>[e(),t("section",null,[t("h2",null,r(o.$t("form.label.basicInput")),1),e(),t("div",F,[l(m,{language:"html-vue"},{default:a(()=>[e(`
+          <Form>
+            <SwitchInput
+              v-model="input"
+            />
+          </Form>
+
+          <script setup lang="ts">
+          import { SwitchInput } from "@dzangolab/vue3-form";
+          <\/script>
+        `)]),_:1}),e(),l(n(p),null,{default:a(()=>[l(n(d),{modelValue:n(s).noLabelInput,"onUpdate:modelValue":u[0]||(u[0]=i=>n(s).noLabelInput=i)},null,8,["modelValue"])]),_:1})])]),e(),t("section",null,[t("h2",null,r(o.$t("form.label.labelInput")),1),e(),t("div",z,[l(m,{language:"html-vue"},{default:a(()=>[e(`
+          <Form>
+            <SwitchInput 
+              v-model="input"
+              label="Switch"
+            />
+          </Form>
+
+          <script setup lang="ts">
+          import { SwitchInput } from "@dzangolab/vue3-form";
+          <\/script>
+        `)]),_:1}),e(),l(n(p),null,{default:a(()=>[l(n(d),{modelValue:n(s).input,"onUpdate:modelValue":u[1]||(u[1]=i=>n(s).input=i),label:o.$t("form.label.switch")},null,8,["modelValue","label"])]),_:1})])]),e(),t("section",null,[t("h2",null,r(o.$t("form.label.disabled")),1),e(),t("div",k,[l(m,{language:"html-vue"},{default:a(()=>[e(`
+          <Form>
+            <SwitchInput 
+              v-model="disabled"
+              disabled
+              label="Disabled"
+            />
+          </Form>
+
+          <script setup lang="ts">
+          import { SwitchInput } from "@dzangolab/vue3-form";
+          import { ref } from "vue";
+
+          const disabled = ref(true);
+          <\/script>
+        `)]),_:1}),e(),l(n(p),null,{default:a(()=>[l(n(d),{modelValue:n(s).disabled,"onUpdate:modelValue":u[2]||(u[2]=i=>n(s).disabled=i),label:o.$t("form.label.disabled"),disabled:""},null,8,["modelValue","label"])]),_:1})])]),e(),t("section",null,[t("h2",null,r(o.$t("form.label.withI18n")),1),e(),t("div",U,[l(m,{language:"html-vue"},{default:a(()=>[e(`
+          <Form>
+            <SwitchInput 
+              v-model="input"
+              :label="t('form.label.switch')"
+            />
+          </Form>
+
+          <script setup lang="ts">
+          import { SwitchInput } from "@dzangolab/vue3-form";
+          import { useI18n } from "@dzangolab/vue3-i18n";
+
+          const { t } = useI18n();
+          <\/script>
+        `)]),_:1}),e(),l(n(p),null,{default:a(()=>[l(n(d),{modelValue:n(s).input,"onUpdate:modelValue":u[3]||(u[3]=i=>n(s).input=i),label:o.$t("form.label.switch")},null,8,["modelValue","label"])]),_:1})])]),e(),t("section",null,[t("h2",null,r(o.$t("form.label.inputWithValidation")),1),e(),t("div",P,[l(m,{language:"html-vue"},{default:a(()=>[e(`
+          <Form>
+            <SwitchInput 
+              v-model="disabled"
+              :schema="inputSchema"
+              label="Switch"
+            />
+          </Form>
+
+          <script setup lang="ts">
+          import { SwitchInput } from "@dzangolab/vue3-form";
+
+          const inputSchema = z.coerce
+            .boolean()
+            .refine((value) => value === true, {
+              message: t("form.errors.switch.invalid"),
+            });
+          <\/script>
+        `)]),_:1}),e(),l(n(p),null,{default:a(()=>[l(n(d),{modelValue:n(s).inputWithValidation,"onUpdate:modelValue":u[4]||(u[4]=i=>n(s).inputWithValidation=i),label:o.$t("form.label.switch"),schema:n(h)},null,8,["modelValue","label","schema"])]),_:1})])])]),_:1},8,["title"])}}});export{L as default};
---
+++ assets/Index-ac257833.js
@@ -0,0 +1,221 @@
+import{m as V,X as u,Z as p}from"./DzangolabVue3Form.es-264401b9.js";import{d as P,u as F,b as $,e as f,r as b,o as w,c as y,w as l,f as n,g as e,t as s,a,h as t}from"./index-a50c4f8e.js";const z={class:"section-content"},_={class:"section-content"},B={class:"section-content"},U={class:"section-content"},M={class:"section-content"},O={class:"section-content"},I={class:"section-content"},A={class:"section-content"},R={class:"section-content"},Y={class:"section-content"},S={class:"section-content"},j={class:"section-content"},C={class:"section-content"},T={class:"section-content"},L={name:"DatePickerInput"},Z=P({...L,setup(N){const{t:g}=F(),v=V.coerce.date().max(new Date,{message:g("form.errors.datePicker.invalid")}),c=new Date,D=c;let r=$({appointmentDate:f(c),arrivalDate:f(),availableDatetime:f([c,new Date(D.setDate(D.getDate()+7))]),dateOfBirth:f(c),dateRange:f([c,new Date(D.setDate(D.getDate()+7))]),joinedDate:f(),month:f(),monthAndYear:f(),noLabelInput:f(),presentationDate:f(c),reportDate:f([c,new Date(D.setDate(D.getDate()+7))]),time:f(),year:f(c.getFullYear())});return(o,m)=>{const h=b("router-link"),d=b("SshPre"),k=b("Page");return w(),y(k,{title:o.$t("form.label.datePicker"),class:"demo"},{toolbar:l(()=>[n(h,{to:{name:"form"},class:"back"},{default:l(()=>[e(s(o.$t("common.back")),1)]),_:1})]),default:l(()=>[e(),a("section",null,[a("h2",null,s(o.$t("form.label.basicInput")),1),e(),a("div",z,[n(d,{language:"html-vue"},{default:l(()=>[e(`
+          <Form>
+            <DatePicker 
+              v-model="input"
+            />
+          </Form>
+
+          <script setup lang="ts">
+          import { DatePicker } from "@dzangolab/vue3-form";
+          <\/script>
+        `)]),_:1}),e(),n(t(p),null,{default:l(()=>[n(t(u),{modelValue:t(r).noLabelInput,"onUpdate:modelValue":m[0]||(m[0]=i=>t(r).noLabelInput=i)},null,8,["modelValue"])]),_:1})])]),e(),a("section",null,[a("h2",null,s(o.$t("form.label.labelInput")),1),e(),a("div",_,[n(d,{language:"html-vue"},{default:l(()=>[e(`
+          <Form>
+            <DatePicker 
+              v-model="arrivalDate"
+              label="Arrival date"
+            />
+          </Form>
+  
+          <script setup lang="ts">
+          import { DatePicker } from "@dzangolab/vue3-form";
+          <\/script>
+        `)]),_:1}),e(),n(t(p),null,{default:l(()=>[n(t(u),{modelValue:t(r).arrivalDate,"onUpdate:modelValue":m[1]||(m[1]=i=>t(r).arrivalDate=i),label:o.$t("form.label.arrivalDate")},null,8,["modelValue","label"])]),_:1})])]),e(),a("section",null,[a("h2",null,s(o.$t("form.label.withI18n")),1),e(),a("div",B,[n(d,{language:"html-vue"},{default:l(()=>[e(`
+          <Form>
+            <DatePicker 
+              v-model="dateOfBirth"
+              :label="t('form.label.dateOfBirth')"
+            />
+          </Form>
+  
+          <script setup lang="ts">
+          import { DatePicker } from "@dzangolab/vue3-form";
+          import { ref } from vue;
+          import { useI18n } from "@dzangolab/vue3-i18n";
+  
+          const dateOfBirth = ref(new Date());
+          const { t } = useI18n();
+          <\/script>
+        `)]),_:1}),e(),n(t(p),null,{default:l(()=>[n(t(u),{modelValue:t(r).dateOfBirth,"onUpdate:modelValue":m[2]||(m[2]=i=>t(r).dateOfBirth=i),label:o.$t("form.label.dateOfBirth")},null,8,["modelValue","label"])]),_:1})])]),e(),a("section",null,[a("h2",null,s(o.$t("form.label.disabled")),1),e(),a("div",U,[n(d,{language:"html-vue"},{default:l(()=>[e(`
+          <Form>
+            <DatePicker 
+              v-model="appointDate"
+              label="Appointment date"
+              disabled
+            />
+          </Form>
+  
+          <script setup lang="ts">
+          import { DatePicker } from "@dzangolab/vue3-form";
+          import { ref } from vue;
+  
+          const appointmentDate = ref(new Date());
+          <\/script>
+        `)]),_:1}),e(),n(t(p),null,{default:l(()=>[n(t(u),{modelValue:t(r).appointmentDate,"onUpdate:modelValue":m[3]||(m[3]=i=>t(r).appointmentDate=i),label:o.$t("form.label.appointmentDate"),disabled:""},null,8,["modelValue","label"])]),_:1})])]),e(),a("section",null,[a("h2",null,s(o.$t("form.label.disableWeekDays")),1),e(),a("div",M,[n(d,{language:"html-vue"},{default:l(()=>[e(`
+          <Form>
+            <DatePicker 
+              v-model="presentationDate"
+              :disabled-week-days="[6, 0]"
+              label="Presentation date"
+            />
+          </Form>
+  
+          <script setup lang="ts">
+          import { DatePicker } from "@dzangolab/vue3-form";
+          import { ref } from vue;
+  
+          const presentationDate = ref(new Date());
+          <\/script>
+        `)]),_:1}),e(),n(t(p),null,{default:l(()=>[n(t(u),{modelValue:t(r).presentationDate,"onUpdate:modelValue":m[4]||(m[4]=i=>t(r).presentationDate=i),"disabled-week-days":[6,0],label:o.$t("form.label.presentationDate")},null,8,["modelValue","label"])]),_:1})])]),e(),a("section",null,[a("h2",null,s(o.$t("form.label.customFormat")),1),e(),a("div",O,[n(d,{language:"html-vue"},{default:l(()=>[e(`
+          <Form>
+            <DatePicker 
+              v-model="dateOfBirth"
+              format="yyyy/MM/dd"
+              label="Date of birth"
+            />
+          </Form>
+  
+          <script setup lang="ts">
+          import { DatePicker } from "@dzangolab/vue3-form";
+          import { ref } from vue;
+  
+          const dateOfBirth = ref(new Date());
+          <\/script>
+        `)]),_:1}),e(),n(t(p),null,{default:l(()=>[n(t(u),{modelValue:t(r).dateOfBirth,"onUpdate:modelValue":m[5]||(m[5]=i=>t(r).dateOfBirth=i),format:"yyyy/MM/dd",label:o.$t("form.label.dateOfBirth")},null,8,["modelValue","label"])]),_:1})])]),e(),a("section",null,[a("h2",null,s(o.$t("form.label.dateRange")),1),e(),a("div",I,[n(d,{language:"html-vue"},{default:l(()=>[e(`
+          <Form>
+            <DatePicker 
+              v-model="dateRange"
+              label="Date range"
+              range
+            />
+          </Form>
+  
+          <script setup lang="ts">
+          import { DatePicker } from "@dzangolab/vue3-form";
+  
+          const startDate = new Date();
+          const endDate = startDate;
+  
+          const dateRange = ref([
+            startDate,
+            new Date(endDate.setDate(endDate.getDate() + 7)),
+          ]),
+          <\/script>
+        `)]),_:1}),e(),n(t(p),null,{default:l(()=>[n(t(u),{modelValue:t(r).dateRange,"onUpdate:modelValue":m[6]||(m[6]=i=>t(r).dateRange=i),label:o.$t("form.label.dateRange"),range:""},null,8,["modelValue","label"])]),_:1})])]),e(),a("section",null,[a("h2",null,s(o.$t("form.label.multiCalendars")),1),e(),a("div",A,[n(d,{language:"html-vue"},{default:l(()=>[e(`
+          <Form>
+            <DatePicker 
+              v-model="reportDate"
+              label="Report date"
+              multi-calendars
+              range
+            />
+          </Form>
+  
+          <script setup lang="ts">
+          import { DatePicker } from "@dzangolab/vue3-form";
+  
+          const startDate = new Date();
+          const endDate = startDate;
+  
+          const reportDate = ref([
+            startDate,
+            new Date(endDate.setDate(endDate.getDate() + 7)),
+          ]),
+          <\/script>
+        `)]),_:1}),e(),n(t(p),null,{default:l(()=>[n(t(u),{modelValue:t(r).reportDate,"onUpdate:modelValue":m[7]||(m[7]=i=>t(r).reportDate=i),label:o.$t("form.label.reportDate"),"multi-calendars":"",range:""},null,8,["modelValue","label"])]),_:1})])]),e(),a("section",null,[a("h2",null,s(o.$t("form.label.dateTimePicker")),1),e(),a("div",R,[n(d,{language:"html-vue"},{default:l(()=>[e(`
+          <Form>
+            <DatePicker 
+              v-model="availableDatetime"
+              label="Available datetime"
+              enable-time-picker
+              multi-calendars
+              range
+            />
+          </Form>
+  
+          <script setup lang="ts">
+          import { DatePicker } from "@dzangolab/vue3-form";
+  
+          const startDate = new Date();
+          const endDate = startDate;
+  
+          const availableDatetime = ref([
+            startDate,
+            new Date(endDate.setDate(endDate.getDate() + 7)),
+          ]),
+          <\/script>
+        `)]),_:1}),e(),n(t(p),null,{default:l(()=>[n(t(u),{modelValue:t(r).availableDatetime,"onUpdate:modelValue":m[8]||(m[8]=i=>t(r).availableDatetime=i),label:o.$t("form.label.availableDatetime"),"enable-time-picker":"","multi-calendars":"",range:""},null,8,["modelValue","label"])]),_:1})])]),e(),a("section",null,[a("h2",null,s(o.$t("form.label.monthPicker")),1),e(),a("div",Y,[n(d,{language:"html-vue"},{default:l(()=>[e(`
+          <Form>
+            <DatePicker 
+              v-model="month"
+              format="MMM"
+              label="Month"
+              month-picker
+            />
+          </Form>
+  
+          <script setup lang="ts">
+          import { DatePicker } from "@dzangolab/vue3-form";
+          import { ref } from vue;
+  
+          const month = ref();
+          <\/script>
+        `)]),_:1}),e(),n(t(p),null,{default:l(()=>[n(t(u),{modelValue:t(r).month,"onUpdate:modelValue":m[9]||(m[9]=i=>t(r).month=i),format:"MMM",label:o.$t("form.label.month"),"month-picker":""},null,8,["modelValue","label"])]),_:1})])]),e(),a("section",null,[a("h2",null,s(o.$t("form.label.monthAndYearPicker")),1),e(),a("div",S,[n(d,{language:"html-vue"},{default:l(()=>[e(`
+          <Form>
+            <DatePicker 
+              v-model="monthAndYear"
+              label="Month and year"
+              month-picker
+            />
+          </Form>
+  
+          <script setup lang="ts">
+          import { DatePicker } from "@dzangolab/vue3-form";
+          import { ref } from vue;
+  
+          const monthAndYear = ref();
+          <\/script>
+        `)]),_:1}),e(),n(t(p),null,{default:l(()=>[n(t(u),{modelValue:t(r).month,"onUpdate:modelValue":m[10]||(m[10]=i=>t(r).month=i),label:o.$t("form.label.monthAndYear"),"month-picker":""},null,8,["modelValue","label"])]),_:1})])]),e(),a("section",null,[a("h2",null,s(o.$t("form.label.yearPicker")),1),e(),a("div",j,[n(d,{language:"html-vue"},{default:l(()=>[e(`
+          <Form>
+            <DatePicker 
+              v-model="year"
+              label="Year"
+              year-picker
+            />
+          </Form>
+  
+          <script setup lang="ts">
+          import { DatePicker } from "@dzangolab/vue3-form";
+          import { ref } from "vue";
+  
+          const year = ref(new Date().getFullYear()),
+          <\/script>
+        `)]),_:1}),e(),n(t(p),null,{default:l(()=>[n(t(u),{modelValue:t(r).year,"onUpdate:modelValue":m[11]||(m[11]=i=>t(r).year=i),label:o.$t("form.label.year"),"year-picker":""},null,8,["modelValue","label"])]),_:1})])]),e(),a("section",null,[a("h2",null,s(o.$t("form.label.timePicker")),1),e(),a("div",C,[n(d,{language:"html-vue"},{default:l(()=>[e(`
+          <Form>
+            <DatePicker 
+              v-model="appointmentTime"
+              label="Appointment time"
+              time-picker
+            />
+          </Form>
+  
+          <script setup lang="ts">
+          import { DatePicker } from "@dzangolab/vue3-form";
+          <\/script>
+        `)]),_:1}),e(),n(t(p),null,{default:l(()=>[n(t(u),{modelValue:t(r).time,"onUpdate:modelValue":m[12]||(m[12]=i=>t(r).time=i),label:o.$t("form.label.appointmentTime"),"time-picker":""},null,8,["modelValue","label"])]),_:1})])]),e(),a("section",null,[a("h2",null,s(o.$t("form.label.inputWithValidation")),1),e(),a("div",T,[n(d,{language:"html-vue"},{default:l(()=>[e(`
+          <Form>
+            <DatePicker 
+              v-model="joinedDate"
+              :schema="inputSchema"
+              label="Joined date"
+            />
+          </Form>
+  
+          <script setup lang="ts">
+          import { DatePicker, Form } from "@dzangolab/vue3-form";
+          import { z } from "zod";
+  
+          const inputSchema = z.coerce
+            .date()
+            .max(new Date(), { message: "Please provide a present or past date" });
+          <\/script>
+        `)]),_:1}),e(),n(t(p),null,{default:l(()=>[n(t(u),{modelValue:t(r).joinedDate,"onUpdate:modelValue":m[13]||(m[13]=i=>t(r).joinedDate=i),label:o.$t("form.label.joinedDate"),schema:t(v)},null,8,["modelValue","label","schema"])]),_:1})])])]),_:1},8,["title"])}}});export{Z as default};
