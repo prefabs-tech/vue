12629becc1d514d66b05fc1601fb70c931b21a67
KabinKhandThakuri <kabin@dzangolab.com>
2024-11-20 12:19:51 +0000

    Deploying to gh-pages from @ dzangolab/vue@fc8942a0fdce721d8be0753904a3137709ba9ce7 🚀

---
+++ assets/Index-188e7379.js
@@ -0,0 +1,396 @@
+import{m as v,o as h,Z as p}from"./DzangolabVue3Form.es-66293d03.js";import{d as A,u as L,b as D,e as g,r as m,o as T,c as I,w as t,f as a,g as e,t as u,a as o,h as l}from"./index-d455476b.js";const V={class:"section-content"},k={class:"section-content"},S={class:"section-content"},$={class:"section-content"},P={class:"section-content"},C={class:"section-content"},q={name:"InputField"},O=A({...q,setup(F){const{t:b}=L(),w=v.string().min(3,{message:b("form.errors.input.min",{length:3})}),d=[{value:"Roslabel",label:"Roslabel - A fragrant flower often associated with romance"},{value:"Tulip",label:"Tulip - A bright, cup-shaped flower popular in spring"},{value:"Daisy",label:"Daisy - A simple, white flower with a yellow center"},{value:"Sunflower",label:"Sunflower - A tall plant with a large, yellow flower head"},{value:"Lily",label:"Lily - An elegant flower often used in bouquets"},{value:"Orchid",label:"Orchid - An exotic flower with a unique shape"},{value:"Marigold",label:"Marigold - A vibrant, orange or yellow flower"},{value:"Lavender",label:"Lavender - A fragrant flower known for its calming scent"},{value:"Peony",label:"Peony - A lush, full flower often used in weddings"},{value:"Chrysanthemum",label:"Chrysanthemum - A hardy flower with a variety of colors"},{value:"Daffodil",label:"Daffodil - A bright, yellow flower that blooms in spring"},{value:"Hydrangea",label:"Hydrangea - A large, cluster-like flower that changes color"}];let r=D({disabled:g("Lily"),input:g(),inputWithValidation:g(),noLabelInput:g()});return(n,i)=>{const c=m("router-link"),f=m("SshPre"),y=m("Page");return T(),I(y,{title:n.$t("form.label.typeahead"),class:"demo"},{toolbar:t(()=>[a(c,{to:{name:"form"},class:"back"},{default:t(()=>[e(u(n.$t("common.back")),1)]),_:1})]),default:t(()=>[e(),o("section",null,[o("h2",null,u(n.$t("form.label.basicInput")),1),e(),o("div",V,[a(f,{language:"html-vue"},{default:t(()=>[e(`
+          <Form>
+            <Typeahead
+              v-model="input"
+              :suggestions="suggestionItems"
+              placeholder="Enter Input"
+            />
+          </Form>
+
+          <script setup lang="ts">
+          import { Typeahead } from "@dzangolab/vue3-form";
+
+          const suggestionItems = [
+            {
+              value: "Roslabel",
+              label: "Roslabel - A fragrant flower often associated with romance",
+            },
+            { 
+              value: "Tulip",
+              label: "Tulip - A bright, cup-shaped flower popular in spring"
+            },
+            {
+              value: "Daisy",
+              label: "Daisy - A simple, white flower with a yellow center"
+            },
+            {
+              value: "Sunflower",
+              label: "Sunflower - A tall plant with a large, yellow flower head",
+            },
+            {
+              value: "Lily",
+              label: "Lily - An elegant flower often used in bouquets"
+            },
+            {
+              value: "Orchid",
+              label: "Orchid - An exotic flower with a unique shape"
+            },
+            {
+              value: "Marigold",
+              label: "Marigold - A vibrant, orange or yellow flower"
+            },
+            {
+              value: "Lavender",
+              label: "Lavender - A fragrant flower known for its calming scent"
+            },
+            {
+              value: "Peony",
+              label: "Peony - A lush, full flower often used in weddings"
+            },
+            {
+              value: "Chrysanthemum",
+              label: "Chrysanthemum - A hardy flower with a variety of colors"
+            },
+            {
+              value: "Daffodil",
+              label: "Daffodil - A bright, yellow flower that blooms in spring"
+            },
+            {
+              value: "Hydrangea",
+              label: "Hydrangea - A large, cluster-like flower that changes color",
+            },
+          ];
+          <\/script>
+        `)]),_:1}),e(),a(l(p),null,{default:t(()=>[a(l(h),{modelValue:l(r).noLabelInput,"onUpdate:modelValue":i[0]||(i[0]=s=>l(r).noLabelInput=s),placeholder:n.$t("form.placeholder.input"),suggestions:d},null,8,["modelValue","placeholder"])]),_:1})])]),e(),o("section",null,[o("h2",null,u(n.$t("form.label.labelInput")),1),e(),o("div",k,[a(f,{language:"html-vue"},{default:t(()=>[e(`
+          <Form>
+            <Typeahead
+              v-model="input"
+              :suggestions="suggestionItems"
+              label="Typeahead"
+              placeholder="Enter Input"
+            />
+          </Form>
+
+          <script setup lang="ts">
+          import { Typeahead } from "@dzangolab/vue3-form";
+
+          const suggestionItems = [
+            {
+              value: "Roslabel",
+              label: "Roslabel - A fragrant flower often associated with romance",
+            },
+            { 
+              value: "Tulip",
+              label: "Tulip - A bright, cup-shaped flower popular in spring"
+            },
+            {
+              value: "Daisy",
+              label: "Daisy - A simple, white flower with a yellow center"
+            },
+            {
+              value: "Sunflower",
+              label: "Sunflower - A tall plant with a large, yellow flower head",
+            },
+            {
+              value: "Lily",
+              label: "Lily - An elegant flower often used in bouquets"
+            },
+            {
+              value: "Orchid",
+              label: "Orchid - An exotic flower with a unique shape"
+            },
+            {
+              value: "Marigold",
+              label: "Marigold - A vibrant, orange or yellow flower"
+            },
+            {
+              value: "Lavender",
+              label: "Lavender - A fragrant flower known for its calming scent"
+            },
+            {
+              value: "Peony",
+              label: "Peony - A lush, full flower often used in weddings"
+            },
+            {
+              value: "Chrysanthemum",
+              label: "Chrysanthemum - A hardy flower with a variety of colors"
+            },
+            {
+              value: "Daffodil",
+              label: "Daffodil - A bright, yellow flower that blooms in spring"
+            },
+            {
+              value: "Hydrangea",
+              label: "Hydrangea - A large, cluster-like flower that changes color",
+            },
+          ];
+          <\/script>
+        `)]),_:1}),e(),a(l(p),null,{default:t(()=>[a(l(h),{modelValue:l(r).input,"onUpdate:modelValue":i[1]||(i[1]=s=>l(r).input=s),label:n.$t("form.label.typeahead"),placeholder:n.$t("form.placeholder.input"),suggestions:d},null,8,["modelValue","label","placeholder"])]),_:1})])]),e(),o("section",null,[o("h2",null,u(n.$t("form.label.disabled")),1),e(),o("div",S,[a(f,{language:"html-vue"},{default:t(()=>[e(`
+          <Form>
+            <Typeahead
+              v-model="input"
+              :suggestions="suggestionItems"
+              label="Disabled"
+              type="text"
+              disabled
+            />
+          </Form>
+
+          <script setup lang="ts">
+          import { Typeahead } from "@dzangolab/vue3-form";
+
+          const suggestionItems = [
+            {
+              value: "Roslabel",
+              label: "Roslabel - A fragrant flower often associated with romance",
+            },
+            { 
+              value: "Tulip",
+              label: "Tulip - A bright, cup-shaped flower popular in spring"
+            },
+            {
+              value: "Daisy",
+              label: "Daisy - A simple, white flower with a yellow center"
+            },
+            {
+              value: "Sunflower",
+              label: "Sunflower - A tall plant with a large, yellow flower head",
+            },
+            {
+              value: "Lily",
+              label: "Lily - An elegant flower often used in bouquets"
+            },
+            {
+              value: "Orchid",
+              label: "Orchid - An exotic flower with a unique shape"
+            },
+            {
+              value: "Marigold",
+              label: "Marigold - A vibrant, orange or yellow flower"
+            },
+            {
+              value: "Lavender",
+              label: "Lavender - A fragrant flower known for its calming scent"
+            },
+            {
+              value: "Peony",
+              label: "Peony - A lush, full flower often used in weddings"
+            },
+            {
+              value: "Chrysanthemum",
+              label: "Chrysanthemum - A hardy flower with a variety of colors"
+            },
+            {
+              value: "Daffodil",
+              label: "Daffodil - A bright, yellow flower that blooms in spring"
+            },
+            {
+              value: "Hydrangea",
+              label: "Hydrangea - A large, cluster-like flower that changes color",
+            },
+          ];
+          <\/script>
+        `)]),_:1}),e(),a(l(p),null,{default:t(()=>[a(l(h),{modelValue:l(r).disabled,"onUpdate:modelValue":i[2]||(i[2]=s=>l(r).disabled=s),label:n.$t("form.label.disabled"),suggestions:d,type:"text",disabled:""},null,8,["modelValue","label"])]),_:1})])]),e(),o("section",null,[o("h2",null,u(n.$t("form.label.withI18n")),1),e(),o("div",$,[a(f,{language:"html-vue"},{default:t(()=>[e(`
+          <Form>
+            <Typeahead
+              v-model="input"
+              :suggestions="suggestionItems"
+              :label="t('form.label.typeahead')"
+              :placeholder="t('form.placeholder.input')"
+            />
+          </Form>
+
+          <script setup lang="ts">
+          import { Typeahead } from "@dzangolab/vue3-form";
+          import { useI18n } from "@dzangolab/vue3-i18n";
+
+          const { t } = useI18n();
+
+          const suggestionItems = [
+            {
+              value: "Roslabel",
+              label: "Roslabel - A fragrant flower often associated with romance",
+            },
+            { 
+              value: "Tulip",
+              label: "Tulip - A bright, cup-shaped flower popular in spring"
+            },
+            {
+              value: "Daisy",
+              label: "Daisy - A simple, white flower with a yellow center"
+            },
+            {
+              value: "Sunflower",
+              label: "Sunflower - A tall plant with a large, yellow flower head",
+            },
+            {
+              value: "Lily",
+              label: "Lily - An elegant flower often used in bouquets"
+            },
+            {
+              value: "Orchid",
+              label: "Orchid - An exotic flower with a unique shape"
+            },
+            {
+              value: "Marigold",
+              label: "Marigold - A vibrant, orange or yellow flower"
+            },
+            {
+              value: "Lavender",
+              label: "Lavender - A fragrant flower known for its calming scent"
+            },
+            {
+              value: "Peony",
+              label: "Peony - A lush, full flower often used in weddings"
+            },
+            {
+              value: "Chrysanthemum",
+              label: "Chrysanthemum - A hardy flower with a variety of colors"
+            },
+            {
+              value: "Daffodil",
+              label: "Daffodil - A bright, yellow flower that blooms in spring"
+            },
+            {
+              value: "Hydrangea",
+              label: "Hydrangea - A large, cluster-like flower that changes color",
+            },
+          ];
+          <\/script>
+        `)]),_:1}),e(),a(l(p),null,{default:t(()=>[a(l(h),{modelValue:l(r).input,"onUpdate:modelValue":i[3]||(i[3]=s=>l(r).input=s),label:n.$t("form.label.typeahead"),placeholder:n.$t("form.placeholder.input"),suggestions:d},null,8,["modelValue","label","placeholder"])]),_:1})])]),e(),o("section",null,[o("h2",null,u(n.$t("form.label.debouncedTypeahead")),1),e(),o("div",P,[a(f,{language:"html-vue"},{default:t(()=>[e(`
+          <Form>
+            <Typeahead
+              v-model="input"
+              :debounce-time="1000"
+              :suggestions="suggestionItems"
+              label="Typeahead"
+              placeholder="Enter Input"
+            />
+          </Form>
+
+          <script setup lang="ts">
+          import { Typeahead } from "@dzangolab/vue3-form";
+
+          const suggestionItems = [
+            {
+              value: "Roslabel",
+              label: "Roslabel - A fragrant flower often associated with romance",
+            },
+            { 
+              value: "Tulip",
+              label: "Tulip - A bright, cup-shaped flower popular in spring"
+            },
+            {
+              value: "Daisy",
+              label: "Daisy - A simple, white flower with a yellow center"
+            },
+            {
+              value: "Sunflower",
+              label: "Sunflower - A tall plant with a large, yellow flower head",
+            },
+            {
+              value: "Lily",
+              label: "Lily - An elegant flower often used in bouquets"
+            },
+            {
+              value: "Orchid",
+              label: "Orchid - An exotic flower with a unique shape"
+            },
+            {
+              value: "Marigold",
+              label: "Marigold - A vibrant, orange or yellow flower"
+            },
+            {
+              value: "Lavender",
+              label: "Lavender - A fragrant flower known for its calming scent"
+            },
+            {
+              value: "Peony",
+              label: "Peony - A lush, full flower often used in weddings"
+            },
+            {
+              value: "Chrysanthemum",
+              label: "Chrysanthemum - A hardy flower with a variety of colors"
+            },
+            {
+              value: "Daffodil",
+              label: "Daffodil - A bright, yellow flower that blooms in spring"
+            },
+            {
+              value: "Hydrangea",
+              label: "Hydrangea - A large, cluster-like flower that changes color",
+            },
+          ];
+          <\/script>
+        `)]),_:1}),e(),a(l(p),null,{default:t(()=>[a(l(h),{modelValue:l(r).input,"onUpdate:modelValue":i[4]||(i[4]=s=>l(r).input=s),"debounce-time":1e3,label:n.$t("form.label.typeahead"),placeholder:n.$t("form.placeholder.input"),suggestions:d},null,8,["modelValue","label","placeholder"])]),_:1})])]),e(),o("section",null,[o("h2",null,u(n.$t("form.label.inputWithValidation")),1),e(),o("div",C,[a(f,{language:"html-vue"},{default:t(()=>[e(`
+          <Form>
+            <Typeahead
+              v-model="inputWithValidation"
+              :schema="inputSchema"
+              :suggestions="suggestionItems"
+              label="Input"
+              name="typeahead"
+              placeholder="Enter input"
+              type="text"
+            />
+          </Form>
+
+          <script setup lang="ts">
+          import { Form, Typeahead } from "@dzangolab/vue3-form";
+          import { z } from "zod";
+
+          const suggestionItems = [
+            {
+              value: "Roslabel",
+              label: "Roslabel - A fragrant flower often associated with romance",
+            },
+            { 
+              value: "Tulip",
+              label: "Tulip - A bright, cup-shaped flower popular in spring"
+            },
+            {
+              value: "Daisy",
+              label: "Daisy - A simple, white flower with a yellow center"
+            },
+            {
+              value: "Sunflower",
+              label: "Sunflower - A tall plant with a large, yellow flower head",
+            },
+            {
+              value: "Lily",
+              label: "Lily - An elegant flower often used in bouquets"
+            },
+            {
+              value: "Orchid",
+              label: "Orchid - An exotic flower with a unique shape"
+            },
+            {
+              value: "Marigold",
+              label: "Marigold - A vibrant, orange or yellow flower"
+            },
+            {
+              value: "Lavender",
+              label: "Lavender - A fragrant flower known for its calming scent"
+            },
+            {
+              value: "Peony",
+              label: "Peony - A lush, full flower often used in weddings"
+            },
+            {
+              value: "Chrysanthemum",
+              label: "Chrysanthemum - A hardy flower with a variety of colors"
+            },
+            {
+              value: "Daffodil",
+              label: "Daffodil - A bright, yellow flower that blooms in spring"
+            },
+            {
+              value: "Hydrangea",
+              label: "Hydrangea - A large, cluster-like flower that changes color",
+            },
+          ];
+
+          const inputSchema = z.string()
+            .min(3, { message: "Input must be at least 3 character(s)" });        
+          <\/script>
+        `)]),_:1}),e(),a(l(p),null,{default:t(()=>[a(l(h),{modelValue:l(r).inputWithValidation,"onUpdate:modelValue":i[5]||(i[5]=s=>l(r).inputWithValidation=s),label:n.$t("form.label.typeahead"),name:n.$t("form.label.typeahead"),placeholder:n.$t("form.placeholder.input"),schema:l(w),suggestions:d,type:"text"},null,8,["modelValue","label","name","placeholder","schema"])]),_:1})])])]),_:1},8,["title"])}}});export{O as default};
---
+++ assets/Index-5d38bf14.js
@@ -0,0 +1,118 @@
+import{d,r as u,o as g,c as m,w as o,f as l,g as t,t as i,a as n,h as s,a5 as h,ah as p}from"./index-d455476b.js";const v={class:"section-content"},_={class:"section-content"},f={class:"trigger"},T={class:"section-content"},b={class:"trigger"},$={class:"section-content"},y={class:"trigger"},k={class:"section-content"},z={class:"trigger"},B={class:"section-content"},C={class:"trigger"},P={class:"section-content"},w={class:"trigger"},D=d({__name:"Index",setup(N){return(e,S)=>{const c=u("router-link"),a=u("SshPre"),r=u("Page");return g(),m(r,{title:e.$t("ui.tooltip.title"),class:"demo"},{toolbar:o(()=>[l(c,{to:{name:"ui"},class:"back"},{default:o(()=>[t(i(e.$t("common.back")),1)]),_:1})]),default:o(()=>[t(),n("section",null,[n("h2",null,i(e.$t("ui.tooltip.usage.basic")),1),t(),n("div",v,[l(a,{language:"html-vue"},{default:o(()=>[t(`
+          <template>
+            <Tooltip>
+              <ButtonElement
+                :label="Tooltip"
+                severity="success"
+              />
+              <template #tooltip>
+                This is a tooltip
+              </template>
+            </Tooltip>
+          </template>
+
+          <script setup lang="ts">
+            import { ButtonElement, Tooltip } from "@dzangolab/vue3-ui";
+          <\/script>
+        `)]),_:1}),t(),l(s(p),null,{content:o(()=>[t(i(e.$t("ui.tooltip.content")),1)]),default:o(()=>[l(s(h),{label:e.$t("ui.tooltip.label.button"),severity:"success"},null,8,["label"]),t()]),_:1})])]),t(),n("section",null,[n("h2",null,i(e.$t("ui.tooltip.usage.top")),1),t(),n("div",_,[l(a,{language:"html-vue"},{default:o(()=>[t(`
+          <template>
+            <Tooltip position="top">
+              <div class="trigger">
+                Top
+              </div>
+              />
+              <template #tooltip>
+                This is a tooltip
+              </template>
+            </Tooltip>
+          </template>
+
+          <script setup lang="ts">
+          import { Tooltip } from "@dzangolab/vue3-ui";
+          <\/script>
+
+          <style lang="css">
+            .trigger {
+              border: 1px solid #000;
+              border-radius: 0.25rem;
+              padding: 0.5rem;
+              text-align: center;
+              width: 12rem
+            }
+          </style>
+        `)]),_:1}),t(),l(s(p),{position:"top"},{content:o(()=>[t(i(e.$t("ui.tooltip.content")),1)]),default:o(()=>[n("div",f,i(e.$t("ui.tooltip.usage.top")),1),t()]),_:1})])]),t(),n("section",null,[n("h2",null,i(e.$t("ui.tooltip.usage.bottom")),1),t(),n("div",T,[l(a,{language:"html-vue"},{default:o(()=>[t(`
+          <template>
+            <Tooltip position="bottom">
+              <div class="trigger">
+                Bottom
+              </div>
+              <template #tooltip>
+                This is a tooltip
+              </template>
+            </Tooltip>
+          </template>
+
+          <script setup lang="ts">
+          import { Tooltip } from "@dzangolab/vue3-ui";
+          <\/script>
+        `)]),_:1}),t(),l(s(p),{position:"bottom"},{content:o(()=>[t(i(e.$t("ui.tooltip.content")),1)]),default:o(()=>[n("div",b,i(e.$t("ui.tooltip.usage.bottom")),1),t()]),_:1})])]),t(),n("section",null,[n("h2",null,i(e.$t("ui.tooltip.usage.left")),1),t(),n("div",$,[l(a,{language:"html-vue"},{default:o(()=>[t(`
+          <template>
+            <Tooltip position="left">
+              <div class="trigger">
+                Left
+              </div>
+              <template #tooltip>
+                This is a tooltip
+              </template>
+            </Tooltip>
+          </template>
+
+          <script setup lang="ts">
+          import { Tooltip } from "@dzangolab/vue3-ui";
+          <\/script>
+        `)]),_:1}),t(),l(s(p),{position:"left"},{content:o(()=>[t(i(e.$t("ui.tooltip.content")),1)]),default:o(()=>[n("div",y,i(e.$t("ui.tooltip.usage.left")),1),t()]),_:1})])]),t(),n("section",null,[n("h2",null,i(e.$t("ui.tooltip.usage.right")),1),t(),n("div",k,[l(a,{language:"html-vue"},{default:o(()=>[t(`
+          <template>
+            <Tooltip position="right">
+              <div class="trigger">
+                Right
+              </div>
+              <template #tooltip>
+                This is a tooltip
+              </template>
+            </Tooltip>
+          </template>
+
+          <script setup lang="ts">
+          import { Tooltip } from "@dzangolab/vue3-ui";
+          <\/script>
+        `)]),_:1}),t(),l(s(p),{position:"right"},{content:o(()=>[t(i(e.$t("ui.tooltip.content")),1)]),default:o(()=>[n("div",z,i(e.$t("ui.tooltip.usage.right")),1),t()]),_:1})])]),t(),n("section",null,[n("h2",null,i(e.$t("ui.tooltip.usage.clickable")),1),t(),n("div",B,[l(a,{language:"html-vue"},{default:o(()=>[t(`
+          <template>
+            <Tooltip clickable>
+              <div class="trigger">
+                Clickable
+              </div>
+              <template #tooltip>
+                This is a tooltip
+              </template>
+            </Tooltip>
+          </template>
+
+          <script setup lang="ts">
+          import { Tooltip } from "@dzangolab/vue3-ui";
+          <\/script>
+        `)]),_:1}),t(),l(s(p),{clickable:""},{content:o(()=>[t(i(e.$t("ui.tooltip.content")),1)]),default:o(()=>[n("div",C,i(e.$t("ui.tooltip.label.button")),1),t()]),_:1})])]),t(),n("section",null,[n("h2",null,i(e.$t("ui.tooltip.usage.delay")),1),t(),n("div",P,[l(a,{language:"html-vue"},{default:o(()=>[t(`
+          <template>
+            <Tooltip :delay="1000">
+              <div class="trigger">
+                Delay
+              </div>
+              <template #tooltip>
+                This is a tooltip
+              </template>
+            </Tooltip>
+          </template>
+
+          <script setup lang="ts">
+          import { Tooltip } from "@dzangolab/vue3-ui";
+          <\/script>
+        `)]),_:1}),t(),l(s(p),{delay:1e3},{content:o(()=>[t(i(e.$t("ui.tooltip.content")),1)]),default:o(()=>[n("div",w,i(e.$t("ui.tooltip.label.delay")),1),t()]),_:1})])])]),_:1},8,["title"])}}});export{D as default};
