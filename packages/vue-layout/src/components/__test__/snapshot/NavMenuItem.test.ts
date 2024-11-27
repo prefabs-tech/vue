1e112cae431ac056837c0f9b2e322a79a55c4c75
KabinKhandThakuri <kabin@dzangolab.com>
2024-11-27 17:45:16 +0545

    Add snapshot test for sidebar, NavMenu and NavMenuItem (#402)
    * test: add snapshot test for sidebar
    
    * test: add snapshot tests for nam menu and nav menu item

---
+++ packages/vue-layout/src/components/__test__/snapshot/NavMenuItem.test.ts
@@ -0,0 +1,42 @@
+import { mount } from "@vue/test-utils";
+import { describe, it, expect } from "vitest";
+
+import type { VueWrapper } from "@vue/test-utils";
+
+import router from "@/__test__/router";
+import NavMenuItem from "@/components/NavMenuItem.vue";
+
+describe("NavMenuItem", () => {
+  const wrapper: VueWrapper = mount(NavMenuItem, {
+    global: {
+      mocks: {
+        showChildren: true,
+      },
+      plugins: [router],
+    },
+    props: {
+      item: {
+        name: "Menu1",
+        routeName: "menu-1",
+        shortName: "M1",
+        children: [
+          {
+            name: "child1",
+            routeName: "child-1",
+            shortName: "C1",
+          },
+          {
+            name: "child2",
+            routeName: "child-2",
+            shortName: "C2",
+          },
+        ],
+      },
+      sidebarActive: true,
+    },
+  });
+
+  it("matches snapshot ", () => {
+    expect(wrapper.element).toMatchSnapshot();
+  });
+});
