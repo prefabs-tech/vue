3ccac73a521b9579b422051e1122555c808eb5e0
Kabin <kabin@dzangolab.com>
2024-12-05 15:06:48 +0545

    chore: update constant in layout store

--- apps/demo/src/views/Layout/_store/layout-store.ts
+++ apps/demo/src/views/Layout/_store/layout-store.ts
@@ -5,6 +5,12 @@ import {
 } from "@dzangolab/vue3-user";
 import { ref, shallowRef } from "vue";
 
+import {
+  BASIC_LAYOUT,
+  SIDEBAR_HEADER_LAYOUT,
+  SIDEBAR_ONLY_LAYOUT,
+} from "../_constants/layout";
+
 const activeLayout = ref<string>("basic");
 const currentLayout = shallowRef(BasicLayout);
 const layoutKey = ref(0);
@@ -13,13 +19,13 @@ const switchLayout = (layout: string) => {
   activeLayout.value = layout;
 
   switch (layout) {
-    case "basic":
+    case BASIC_LAYOUT:
       currentLayout.value = BasicLayout;
       break;
-    case "sidebar_only":
+    case SIDEBAR_ONLY_LAYOUT:
       currentLayout.value = SidebarOnlyLayout;
       break;
-    case "sidebar_header":
+    case SIDEBAR_HEADER_LAYOUT:
       currentLayout.value = SidebarHeaderLayout;
       break;
     default:
