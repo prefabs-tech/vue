f95904154745db598bbc5b3931ad907f93540b8e
Kabin <kabin@dzangolab.com>
2024-12-11 18:02:20 +0545

    refactor: add card for each package demo in home page

--- apps/demo/src/views/Home.vue
+++ apps/demo/src/views/Home.vue
@@ -1,5 +1,19 @@
 <template>
-  <Page :title="$t('home.title')"> </Page>
+  <Page :title="$t('home.title')" class="home">
+    <div class="container">
+      <div
+        v-for="(menu, index) in packages"
+        :key="index + '-' + menu.name"
+        class="package"
+      >
+        <router-link :to="menu.route">
+          <Card :title="$t(`${menu.name}.title`)">
+            <div class="content">{{ menu.description }}</div>
+          </Card>
+        </router-link>
+      </div>
+    </div>
+  </Page>
 </template>
 
 <script lang="ts">
@@ -7,3 +21,34 @@ export default {
   name: "Home",
 };
 </script>
+
+<script setup lang="ts">
+import { Card } from "@dzangolab/vue3-ui";
+
+const packages = [
+  {
+    name: "ui",
+    route: "/ui",
+    description: "@dzangolab/vue3-ui components and utilities",
+  },
+  {
+    name: "form",
+    route: "/form",
+    description: "@dzangolab/vue3-form components and utilities",
+  },
+
+  {
+    name: "layout",
+    route: "/layout",
+    description: "@dzangolab/vue3-layout components and utilities",
+  },
+];
+</script>
+
+<style lang="css" scoped>
+.page.home .container {
+  display: grid;
+  gap: 1rem;
+  grid-template-columns: repeat(auto-fill, minmax(500px, 1fr));
+}
+</style>
