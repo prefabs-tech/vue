183f4d40b077f6b6e5829da00a3016bf952a383c
KabinKhandThakuri <kabin@dzangolab.com>
2024-12-12 11:24:42 +0545

    Add card for each package demo in home page (#424)
    * refactor: add card for each package demo in home page

--- apps/demo/src/views/Home.vue
+++ apps/demo/src/views/Home.vue
@@ -1,5 +1,15 @@
 <template>
-  <Page :title="$t('home.title')"> </Page>
+  <Page :title="$t('home.title')" class="home">
+    <div class="container">
+      <router-link
+        v-for="(menu, index) in packages"
+        :key="index + '-' + menu.name"
+        :to="menu.route"
+      >
+        <Card :title="$t(`${menu.name}.title`)">{{ menu.description }}</Card>
+      </router-link>
+    </div>
+  </Page>
 </template>
 
 <script lang="ts">
@@ -7,3 +17,38 @@ export default {
   name: "Home",
 };
 </script>
+
+<script setup lang="ts">
+import { Card } from "@dzangolab/vue3-ui";
+
+const packages = [
+  {
+    name: "ui",
+    route: "/ui",
+    description: "@dzangolab/vue3-ui components and utilities",
+  },
+  {
+    name: "form",
+    route: "/form",
+    description: "@dzangolab/vue3-form components and utilities",
+  },
+
+  {
+    name: "layout",
+    route: "/layout",
+    description: "@dzangolab/vue3-layout components and utilities",
+  },
+];
+</script>
+
+<style lang="css" scoped>
+.page.home .container {
+  display: grid;
+  gap: 1rem;
+  grid-template-columns: repeat(auto-fill, minmax(500px, 1fr));
+}
+
+.page.home .container .card:hover {
+  --_border-color: var(--dz-secondary-color);
+}
+</style>
