70391c0dbe2c541d2c3ebfda8236ff0fb82e7108
Kabin <kabin@dzangolab.com>
2024-12-18 17:35:27 +0545

    feat: add invitation modal component to vue user package

---
+++ packages/vue-user/src/components/invitation/InvitationModal.vue
@@ -0,0 +1,108 @@
+<template>
+  <Modal
+    :dismiss-on-click-out="dismissOnClickOut"
+    :show="show"
+    :title="title"
+    class="invitation-modal"
+    @on:close="handleClose"
+  >
+    <InvitationForm
+      :apps="apps"
+      :app-schema="appSchema"
+      :expires-at-schema="expiresAtSchema"
+      :expiry-mode="expiryMode"
+      :invitation-data="invitationData"
+      :roles="roles"
+      :role-schema="roleSchema"
+      :submit-label="submitLabel"
+      @submit="onSubmit"
+    />
+  </Modal>
+</template>
+
+<script lang="ts">
+export default {
+  name: "InvitationModal",
+};
+</script>
+
+<script setup lang="ts">
+import { Modal } from "@dzangolab/vue3-ui";
+import { z } from "zod";
+
+import InvitationForm from "./InvitationForm.vue";
+
+import type {
+  InvitationAppOption,
+  InvitationPayload,
+  InvitationRoleOption,
+} from "../../types";
+import type { PropType } from "vue";
+
+defineProps({
+  apps: {
+    default: () => [],
+    type: Array as PropType<Array<InvitationAppOption>>,
+  },
+  appSchema: {
+    default: () => {
+      return {};
+    },
+    required: false,
+    type: Object as PropType<z.ZodType<string | number | string[] | number[]>>,
+  },
+  dismissOnClickOut: {
+    default: true,
+    type: Boolean,
+  },
+  expiresAtSchema: {
+    default: () => {
+      return {};
+    },
+    required: false,
+    type: Object as PropType<z.ZodType<string | number | Date | object>>,
+  },
+  expiryMode: {
+    default: undefined,
+    type: String,
+    validator: (value: string) => ["calendar", "days"].includes(value),
+  },
+  invitationData: {
+    default: () => ({}) as InvitationPayload,
+    type: Object,
+  },
+  roles: {
+    default: () => [],
+    type: Array as PropType<Array<InvitationRoleOption>>,
+  },
+  roleSchema: {
+    default: () => {
+      return {};
+    },
+    required: false,
+    type: Object as PropType<z.ZodType<string | number | string[] | number[]>>,
+  },
+  show: {
+    default: false,
+    type: Boolean,
+  },
+  submitLabel: {
+    default: "",
+    type: String,
+  },
+  title: {
+    default: "",
+    type: String,
+  },
+});
+
+const emit = defineEmits(["on:close", "submit"]);
+
+const handleClose = () => {
+  emit("on:close");
+};
+
+const onSubmit = (data: InvitationPayload) => {
+  emit("submit", data);
+};
+</script>
