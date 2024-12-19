9f9482360bf993fb3c30387402c50441c3a3c539
Kabin <kabin@dzangolab.com>
2024-12-19 16:43:10 +0545

    feat: add terms and conditions component to vue user package

---
+++ packages/vue-user/src/components/TermsAndConditions.vue
@@ -0,0 +1,39 @@
+<template>
+  <div v-if="hasCheckbox" :class="`field ${name}`">
+    <div class="input-field-checkbox">
+      <input :id="name" :name="name" type="checkbox" />
+      <slot>
+        <label :for="name">{{ label }}</label>
+      </slot>
+    </div>
+  </div>
+  <p v-else :class="name" :aria-label="name">
+    <slot>
+      {{ label }}
+    </slot>
+  </p>
+</template>
+
+<script lang="ts">
+export default {
+  name: "TermsAndConditions",
+};
+</script>
+
+<script setup lang="ts">
+defineProps({
+  hasCheckbox: {
+    default: true,
+    type: Boolean,
+  },
+  label: {
+    default: undefined,
+    required: false,
+    type: String,
+  },
+  name: {
+    default: "terms-and-conditions",
+    type: String,
+  },
+});
+</script>
--- packages/vue-user/src/components/index.ts
+++ packages/vue-user/src/components/index.ts
@@ -6,4 +6,5 @@ export { default as PasswordResetForm } from "./PasswordResetForm.vue";
 export { default as PasswordResetRequestForm } from "./PasswordResetRequestForm.vue";
 export { default as SignInUpMenu } from "./SignInUpMenu.vue";
 export { default as SignupForm } from "./SignupForm.vue";
+export { default as TermsAndConditions } from "./TermsAndConditions.vue";
 export { default as UserMenu } from "./UserMenu.vue";
