a0c2e132689d98d8e0387a109224b2d14b6cf58b
Kabin <kabin@dzangolab.com>
2024-12-23 12:55:26 +0545

    refactor: set menu active on sub menu selected in main menu

--- packages/vue-layout/src/components/MainMenu.vue
+++ packages/vue-layout/src/components/MainMenu.vue
@@ -1,7 +1,7 @@
 <template>
   <ResponsiveMenu
     ref="mainMenu"
-    :active-route="String(route?.name) ?? 'home'"
+    :active-route="activeRoute ?? 'home'"
     :routes="routes"
     @change-route="$emit('close')"
   />
@@ -15,7 +15,8 @@ export default {
 
 <script setup lang="ts">
 import { ResponsiveMenu } from "@dzangolab/vue3-ui";
-import { useRoute } from "vue-router";
+import { computed } from "vue";
+import { useRoute, useRouter } from "vue-router";
 
 import type { PropType } from "vue";
 
@@ -23,7 +24,7 @@ defineEmits<{
   (e: "close"): void;
 }>();
 
-defineProps({
+const props = defineProps({
   routes: {
     required: true,
     type: Array as PropType<{ name: string; route: string }[]>,
@@ -31,4 +32,25 @@ defineProps({
 });
 
 const route = useRoute();
+const router = useRouter();
+
+const activeRoute = computed(() => {
+  let matchedRoute = props.routes?.find(
+    (routeData) => routeData.route === route?.name,
+  )?.route;
+
+  if (!matchedRoute) {
+    const match = route?.path.match(/^\/([^/]+)/);
+    if (match) {
+      matchedRoute = getRouteNameFromPath(`/${match[1]}`);
+    }
+  }
+
+  return matchedRoute;
+});
+
+const getRouteNameFromPath = (path: string) => {
+  const route = router.getRoutes().find((r) => r.path === path);
+  return String(route?.name);
+};
 </script>
