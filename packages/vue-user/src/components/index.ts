d448bb793ae5d262b97774555df59736561f800b
KabinKhandThakuri <kabin@dzangolab.com>
2025-01-14 15:12:24 +0545

    Add profile update form to user package (#454)

--- packages/vue-user/src/components/index.ts
+++ packages/vue-user/src/components/index.ts
@@ -6,6 +6,7 @@ export { default as InvitationModal } from "./invitation/InvitationModal.vue";
 export { default as LoginForm } from "./LoginForm.vue";
 export { default as PasswordResetForm } from "./PasswordResetForm.vue";
 export { default as PasswordResetRequestForm } from "./PasswordResetRequestForm.vue";
+export { default as ProfileForm } from "./profile/ProfileForm.vue";
 export { default as SignInUpMenu } from "./SignInUpMenu.vue";
 export { default as SignupForm } from "./SignupForm.vue";
 export { default as TermsAndConditions } from "./TermsAndConditions.vue";
