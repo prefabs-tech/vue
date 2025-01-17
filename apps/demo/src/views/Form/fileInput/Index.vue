4297e7395b00b5b5dc5a46b3721ee9d637dfa409
Kabin <kabin@dzangolab.com>
2025-01-17 09:56:36 +0545

    feat: add multiple and dropzone demo for file input

--- apps/demo/src/views/Form/fileInput/Index.vue
+++ apps/demo/src/views/Form/fileInput/Index.vue
@@ -45,6 +45,66 @@
         <!-- eslint-enable -->
       </div>
     </section>
+
+    <section>
+      <h2>{{ $t("form.label.dropzoneOptions") }}</h2>
+
+      <div class="section-content">
+        <FileInput
+          :dropzone-options="{
+            accept: ['image/jpeg', 'image/png'],
+            minSize: 1000,
+            maxSize: 1000000,
+          }"
+          enable-description
+          input-method="dropzone"
+          name="images"
+          show-error-message
+        />
+
+        <!-- eslint-disable -->
+        <SshPre language="html-vue">
+          &lt;template&gt;
+            &lt;FileInput input-method="dropzone" /&gt;
+          &lt;/template&gt;
+
+          &lt;script setup lang="ts"&gt;
+          import { FileInput } from "@dzangolab/vue3-form";
+          &lt;/script&gt;
+        </SshPre>
+        <!-- eslint-enable -->
+      </div>
+    </section>
+
+    <section>
+      <h2>{{ $t("form.label.multiple") }}</h2>
+
+      <div class="section-content">
+        <FileInput
+          :dropzone-options="{
+            accept: ['image/jpeg', 'image/png'],
+            maxFiles: 5,
+          }"
+          enable-description
+          input-method="dropzone"
+          multiple
+          name="files"
+          show-error-message
+        />
+
+        <!-- eslint-disable -->
+        <SshPre language="html-vue">
+          &lt;template&gt;
+            &lt;FileInput input-method="dropzone" /&gt;
+          &lt;/template&gt;
+
+          &lt;script setup lang="ts"&gt;
+          import { FileInput } from "@dzangolab/vue3-form";
+          &lt;/script&gt;
+        </SshPre>
+        <!-- eslint-enable -->
+      </div>
+    </section>
   </FormPage>
 </template>
 
