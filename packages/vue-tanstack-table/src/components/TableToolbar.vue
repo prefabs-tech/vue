bc4c3e90b589b7fbc09e34c198a61798f72f89fa
Kabin <kabin@dzangolab.com>
2025-01-20 20:52:41 +0545

    feat: add stub table toolbar component

---
+++ packages/vue-tanstack-table/src/components/TableToolbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="toolbar">
+    <ButtonElement
+      v-if="showResetButton"
+      :label="resetButtonLabel"
+      variant="outlined"
+      severity="secondary"
+      @click="$emit('on:reset')"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "TableToolbar",
+};
+</script>
+
+<script setup lang="ts">
+import { ButtonElement } from "@dzangolab/vue3-ui";
+
+defineProps({
+  resetButtonLabel: {
+    default: "Reset all",
+    type: String,
+  },
+  showResetButton: Boolean,
+});
+
+defineEmits(["on:reset"]);
+</script>
