6a9f019b6d1b51b154fbe6c26e20f04481e32dc5
KabinKhandThakuri <KabinKhandThakuri@users.noreply.github.com>
2025-01-23 11:55:19 +0000

    Deploying to gh-pages from @ dzangolab/vue@da847d1ec94f669482d0d0780a08a8a80e5e147d 🚀

---
+++ assets/Index-e2d4c404.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./UiPage.vue_vue_type_style_index_0_lang-630702af.js";import{d as o,o as a,c as n}from"./index-4dbfb8c7-0110dc13.js";const _={name:"UI"},l=o({..._,setup(r){return(t,s)=>(a(),n(e,{title:t.$t("ui.title")},null,8,["title"]))}});export{l as default};
