76e3c75da967d5efda88c68360d5f794c24c8607
Kabin <kabin@dzangolab.com>
2025-01-24 17:29:18 +0545

    feat: add sticky collapsible footer to layout component

---
+++ packages/vue-layout/src/components/StickyCollapsibleFooter.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer :class="`sticky-collapsible ${fixed ? 'fixed' : ''}`">
+    <slot></slot>
+  </footer>
+</template>
+
+<script lang="ts">
+export default {
+  name: "StickyCollapsibleFooter",
+};
+</script>
+
+<script setup lang="ts">
+defineProps({
+  fixed: {
+    default: true,
+    type: Boolean,
+  },
+});
+</script>
+
+<style lang="css">
+@import "@/assets/css/components/sticky-collapsible-footer.css";
+</style>
