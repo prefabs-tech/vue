2dde7342d9a97dcbe9836d7a3fb5a0bb9eb861ef
Kabin <kabin@dzangolab.com>
2025-01-28 13:05:51 +0545

    feat: update sticky footer possible demo

--- apps/demo/src/views/Layout/stickyCollapsibleFooter/Index.vue
+++ apps/demo/src/views/Layout/stickyCollapsibleFooter/Index.vue
@@ -4,13 +4,87 @@
     class="demo sticky-collapsible-footer"
   >
     <template #toolbar>
-      <router-link :to="{ name: 'form' }" class="back">
+      <router-link :to="{ name: 'layout' }" class="back">
         {{ $t("common.back") }}
       </router-link>
     </template>
 
     <section>
-      <h2>{{ $t("form.label.basicInput") }}</h2>
+      <h2>{{ $t("layout.label.footerFixed") }}</h2>
+
+      <div class="section-content">
+        <StickyCollapsibleFooter>
+          <Input
+            v-model="input"
+            :placeholder="$t('layout.placeholder.input')"
+          />
+
+          <ButtonElement
+            :label="$t('layout.label.submit')"
+            size="small"
+            @click="showAlert(input)"
+          />
+          <ButtonElement
+            :label="$t('layout.label.cancel')"
+            severity="secondary"
+            size="small"
+            variant="outlined"
+            @click="showAlert($t('layout.label.cancelled'))"
+          />
+        </StickyCollapsibleFooter>
+
+        <!-- eslint-disable -->
+        <SshPre language="html-vue">
+          &lt;template&gt;
+            &lt;StickyCollapsibleFooter&gt;
+              &lt;Input
+                v-model="input"
+                :placeholder="$t('layout.placeholder.input')"
+              /&gt;
+
+              &lt;ButtonElement
+                :label="$t('layout.label.submit')"
+                size="small"
+                @click="showAlert(input)"
+              /&gt;
+              &lt;ButtonElement
+                :label="$t('layout.label.cancel')"
+                severity="secondary"
+                size="small"
+                variant="outlined"
+                @click="showAlert($t('layout.label.cancelled'))"
+              /&gt;
+            &lt;/StickyCollapsibleFooter&gt;
+          &lt;/template&gt;
+
+          &lt;script setup lang="ts"&gt;
+          import { Input } from "@dzangolab/vue3-form";
+          import { StickyCollapsibleFooter } from "@dzangolab/vue3-layout";
+          import { ButtonElement } from "@dzangolab/vue3-ui";
+          import { ref } from "vue";
+
+          const input = ref();
+
+          const showAlert = (message: string) => {
+            if (message) {
+              alert(message);
+            }
+          };
+          &lt;/script&gt;
+
+          &lt;style lang="css"&gt;
+          .sticky-collapsible .content {
+            display: flex;
+            gap: 0.5rem;
+          }
+          &lt;/style&gt;
+        </SshPre>
+        <!-- eslint-enable -->
+      </div>
+    </section>
+
+    <section>
+      <h2>{{ $t("layout.label.footerAbsolute") }}</h2>
 
       <div class="section-content">
         <Card>
@@ -37,6 +111,12 @@
           import { StickyCollapsibleFooter } from "@dzangolab/vue3-layout";
           import { Card } from "@dzangolab/vue3-ui";
           &lt;/script&gt;
+
+          &lt;style lang="css"&gt;
+          .card {
+            position: relative;
+          }
+          &lt;/style&gt;
         </SshPre>
         <!-- eslint-enable -->
       </div>
@@ -51,13 +131,28 @@ export default {
 </script>
 
 <script setup lang="ts">
+import { Input } from "@dzangolab/vue3-form";
 import { StickyCollapsibleFooter } from "@dzangolab/vue3-layout";
-import { Card } from "@dzangolab/vue3-ui";
+import { ButtonElement, Card } from "@dzangolab/vue3-ui";
+import { ref } from "vue";
 
 import LayoutPage from "../LayoutPage.vue";
+
+const input = ref();
+
+const showAlert = (message: string) => {
+  if (message) {
+    alert(message);
+  }
+};
 </script>
 
 <style lang="css">
+.demo .section-content .sticky-collapsible .content {
+  display: flex;
+  gap: 0.5rem;
+}
+
 .sticky-collapsible-footer .card {
   position: relative;
   min-height: 15rem;
