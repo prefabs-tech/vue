add9b8d8fdb5e61f8ed71942bc457c8e7593896c
Kabin <kabin@dzangolab.com>
2025-01-29 14:51:34 +0545

    feat: add page with toolbar demo

--- apps/demo/src/views/UI/page/Index.vue
+++ apps/demo/src/views/UI/page/Index.vue
@@ -1,5 +1,5 @@
 <template>
-  <UiPage :title="$t('ui.page.title')" class="demo">
+  <UiPage class="demo">
     <template #toolbar>
       <router-link :to="{ name: 'ui' }" class="back">
         {{ $t("common.back") }}
@@ -7,15 +7,39 @@
     </template>
 
     <section>
-      <h2>{{ $t("ui.page.usage.basic") }}</h2>
-
       <div class="section-content">
-        <Page>Page content</Page>
+        <Page :title="$t('ui.page.usage.basic')">
+          {{ $t("ui.page.label.content") }}
+        </Page>
 
         <!-- eslint-disable -->
         <SshPre language="html-vue">
           &lt;template&gt;
-            &lt;Page&gt;Page content&lt;/Page&gt;
+            &lt;Page title="Basic"&gt;Page content&lt;/Page&gt;
+          &lt;/template&gt;
+
+          &lt;script setup lang="ts"&gt;
+          import { Page } from "@dzangolab/vue3-ui";
+          &lt;/script&gt;
+        </SshPre>
+        <!-- eslint-enable -->
+      </div>
+    </section>
+
+    <section>
+      <div class="section-content">
+        <Page :title="$t('ui.page.usage.toolbar')">
+          {{ $t("ui.page.label.content") }}
+
+          <template #toolbar>
+            <ButtonElement :label="$t('ui.page.label.edit')" />
+          </template>
+        </Page>
+
+        <!-- eslint-disable -->
+        <SshPre language="html-vue">
+          &lt;template&gt;
+            &lt;Page title="Basic"&gt;Page content&lt;/Page&gt;
           &lt;/template&gt;
 
           &lt;script setup lang="ts"&gt;
@@ -29,7 +53,13 @@
 </template>
 
 <script setup lang="ts">
-import { Page } from "@dzangolab/vue3-ui";
+import { ButtonElement, Page } from "@dzangolab/vue3-ui";
 
 import UiPage from "../UiPage.vue";
 </script>
+
+<style lang="css">
+.page .page-content {
+  height: 20vh;
+}
+</style>
