85ab50ffb89075825d779527f2e28b980a891c24
Kabin <kabin@dzangolab.com>
2025-01-30 11:40:40 +0545

    feat: add title tag prop to page with demo

--- apps/demo/src/views/UI/page/Index.vue
+++ apps/demo/src/views/UI/page/Index.vue
@@ -154,6 +154,65 @@
 
     <section>
       <div class="section-content">
+        <Page
+          :title="$t('ui.page.usage.titleTag')"
+          :title-tag="$t('ui.page.label.titleTag')"
+          title-element="h2"
+        >
+          {{ $t("ui.page.label.content") }}
+        </Page>
+
+        <!-- eslint-disable -->
+        <SshPre language="html-vue">
+          &lt;template&gt;
+            &lt;Page title="With title tag" title-tag="Heading tag"&gt;
+              Page content
+            &lt;/Page&gt;
+          &lt;/template&gt;
+
+          &lt;script setup lang="ts"&gt;
+          import { Page } from "@dzangolab/vue3-ui";
+          &lt;/script&gt;
+        </SshPre>
+        <!-- eslint-enable -->
+      </div>
+    </section>
+
+    <section>
+      <div class="section-content">
+        <Page :title="$t('ui.page.usage.titleTagSlot')" title-element="h2">
+          {{ $t("ui.page.label.content") }}
+
+          <template #titleTag>
+            <BadgeComponent
+              :label="$t('ui.page.label.titleTag')"
+              severity="success"
+            />
+          </template>
+        </Page>
+
+        <!-- eslint-disable -->
+        <SshPre language="html-vue">
+          &lt;template&gt;
+            &lt;Page title="With title tag slot"&gt;
+              Page content
+
+              &lt;template #titleTag&gt;
+                &lt;BadgeComponent label="Heading tag" severity="success" /&gt;
+              &lt;/template&gt;
+            &lt;/Page&gt;
+          &lt;/template&gt;
+
+          &lt;script setup lang="ts"&gt;
+          import { BadgeComponent, Page } from "@dzangolab/vue3-ui";
+          &lt;/script&gt;
+        </SshPre>
+        <!-- eslint-enable -->
+      </div>
+    </section>
+
+    <section>
+      <div class="section-content">
         <Page :title="$t('ui.page.usage.titleElement')" title-element="h3">
           {{ $t("ui.page.label.content") }}
         </Page>
--- packages/vue-ui/src/Page/Index.vue
+++ packages/vue-ui/src/Page/Index.vue
@@ -2,7 +2,15 @@
   <div class="page" :data-centered="centered">
     <div class="page-header">
       <div class="page-title-wrapper">
-        <component :is="titleElement" v-if="title">{{ title }}</component>
+        <component :is="titleElement" v-if="title" class="title">
+          {{ title }}
+
+          <slot name="titleTag">
+            <span v-if="titleTag" class="title-tag">
+              <BadgeComponent :label="titleTag" />
+            </span>
+          </slot>
+        </component>
 
         <slot name="subtitle">
           <small v-if="subTitle">{{ subTitle }}</small>
@@ -27,6 +35,8 @@ export default {
 </script>
 
 <script setup lang="ts">
+import BadgeComponent from "../Badge/Index.vue";
+
 import type { PropType } from "vue";
 
 defineProps({
@@ -46,6 +56,10 @@ defineProps({
     type: String,
     validator: (value: string) =>
       ["h1", "h2", "h3", "h4", "h5", "h6"].includes(value),
+  },
+  titleTag: {
+    default: undefined,
+    type: String,
   },
 });
 </script>
