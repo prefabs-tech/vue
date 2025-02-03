f9aa2948ed15c6b68c81cd5dddcd1b9b11497a99
ks-rohan <82809048+ks-rohan@users.noreply.github.com>
2025-02-03 09:54:36 +0000

    Deploying to gh-pages from @ dzangolab/vue@a090815c118fd197dc5f04ddc9fffdc6fcbd79d5 🚀

---
+++ assets/Index-804cb52b.js
@@ -0,0 +1,64 @@
+import{l as d,J as v}from"./DzangolabVue3Form.es-e9efd21c.js";import{d as g,a5 as w,p as V,j as p,J as c,o as I,c as S,w as u,K as a,W as e,L as m,f as l,D as t}from"./index-35228b09-5de2cbe6.js";import{m as _}from"./index-e4278714.js";import{_ as $}from"./FormPage.vue_vue_type_style_index_0_lang-a62ec854.js";const z={class:"section-content"},k={class:"section-content"},F={class:"section-content"},U={class:"section-content"},W={class:"section-content"},D={name:"SwitchInput"},P=g({...D,setup(L){const{t:f}=w(),b=_.coerce.boolean().refine(n=>n===!0,{message:f("form.errors.switch.invalid")});let o=V({disabled:p(!0),input:p(),inputWithValidation:p(!1),noLabelInput:p()});return(n,s)=>{const h=c("router-link"),r=c("SshPre");return I(),S($,{title:n.$t("form.label.switch"),class:"demo"},{toolbar:u(()=>[a(h,{to:{name:"form"},class:"back"},{default:u(()=>[e(m(n.$t("common.back")),1)]),_:1})]),default:u(()=>[e(),l("section",null,[l("h2",null,m(n.$t("form.label.basicInput")),1),e(),l("div",z,[a(t(d),{modelValue:t(o).noLabelInput,"onUpdate:modelValue":s[0]||(s[0]=i=>t(o).noLabelInput=i)},null,8,["modelValue"]),e(),a(r,{language:"html-vue"},{default:u(()=>[e(`
+          <template>
+            <SwitchInput v-model="input" />
+          </template>
+
+          <script setup lang="ts">
+          import { SwitchInput } from "@dzangolab/vue3-form";
+          <\/script>
+        `)]),_:1}),e()])]),e(),l("section",null,[l("h2",null,m(n.$t("form.label.labelInput")),1),e(),l("div",k,[a(t(d),{modelValue:t(o).input,"onUpdate:modelValue":s[1]||(s[1]=i=>t(o).input=i),label:n.$t("form.label.switch")},null,8,["modelValue","label"]),e(),a(r,{language:"html-vue"},{default:u(()=>[e(`
+          <Form>
+            <SwitchInput v-model="input" label="Switch" />
+          </Form>
+
+          <script setup lang="ts">
+          import { SwitchInput } from "@dzangolab/vue3-form";
+          <\/script>
+        `)]),_:1}),e()])]),e(),l("section",null,[l("h2",null,m(n.$t("form.label.disabled")),1),e(),l("div",F,[a(t(d),{modelValue:t(o).disabled,"onUpdate:modelValue":s[2]||(s[2]=i=>t(o).disabled=i),label:n.$t("form.label.disabled"),disabled:""},null,8,["modelValue","label"]),e(),a(r,{language:"html-vue"},{default:u(()=>[e(`
+          <template>
+            <SwitchInput 
+              v-model="disabled"
+              disabled
+              label="Disabled"
+            />
+          </template>
+
+          <script setup lang="ts">
+          import { SwitchInput } from "@dzangolab/vue3-form";
+          import { ref } from "vue";
+
+          const disabled = ref(true);
+          <\/script>
+        `)]),_:1}),e()])]),e(),l("section",null,[l("h2",null,m(n.$t("form.label.withI18n")),1),e(),l("div",U,[a(t(d),{modelValue:t(o).input,"onUpdate:modelValue":s[3]||(s[3]=i=>t(o).input=i),label:n.$t("form.label.switch")},null,8,["modelValue","label"]),e(),a(r,{language:"html-vue"},{default:u(()=>[e(`
+          <template>
+            <SwitchInput 
+              v-model="input"
+              :label="t('form.label.switch')"
+            />
+          </template>
+
+          <script setup lang="ts">
+          import { SwitchInput } from "@dzangolab/vue3-form";
+          import { useI18n } from "@dzangolab/vue3-i18n";
+
+          const { t } = useI18n();
+          <\/script>
+        `)]),_:1}),e()])]),e(),l("section",null,[l("h2",null,m(n.$t("form.label.inputWithValidation")),1),e(),l("div",W,[a(t(v),null,{default:u(()=>[a(t(d),{modelValue:t(o).inputWithValidation,"onUpdate:modelValue":s[4]||(s[4]=i=>t(o).inputWithValidation=i),label:n.$t("form.label.switch"),schema:t(b)},null,8,["modelValue","label","schema"])]),_:1}),e(),a(r,{language:"html-vue"},{default:u(()=>[e(`
+          <Form>
+            <SwitchInput 
+              v-model="disabled"
+              :schema="inputSchema"
+              label="Switch"
+            />
+          </Form>
+
+          <script setup lang="ts">
+          import { Form, SwitchInput } from "@dzangolab/vue3-form";
+
+          const inputSchema = z.coerce
+            .boolean()
+            .refine((value) => value === true, {
+              message: t("form.errors.switch.invalid"),
+            });
+          <\/script>
+        `)]),_:1}),e()])])]),_:1},8,["title"])}}});export{P as default};
---
+++ assets/Index-db1c0b7f.js
@@ -0,0 +1,86 @@
+import{d as p,J as u,o as b,c as m,w as s,K as a,W as e,L as o,f as l,D as t,at as i,ao as v,ap as h}from"./index-35228b09-5de2cbe6.js";import{_}from"./UiPage.vue_vue_type_style_index_0_lang-e3d60a97.js";import{_ as f}from"./index-e4278714.js";const r=c=>(v("data-v-c775f8af"),c=c(),h(),c),B={class:"section-content"},y={class:"section-content"},$={class:"section-content"},k={class:"section-content"},I={class:"container"},w={class:"section-content"},z={class:"container"},L={class:"section-content"},S={class:"container"},C={class:"section-content"},P={class:"container"},D={class:"section-content"},N={class:"container"},R=r(()=>l("i",{class:"pi pi-check"},null,-1)),U=r(()=>l("i",{class:"pi pi-times"},null,-1)),V=p({__name:"Index",setup(c){return(n,W)=>{const g=u("router-link"),d=u("SshPre");return b(),m(_,{title:n.$t("ui.badge.title"),class:"demo-badge"},{toolbar:s(()=>[a(g,{to:{name:"ui"},class:"back"},{default:s(()=>[e(o(n.$t("common.back")),1)]),_:1})]),default:s(()=>[e(),l("section",null,[l("h2",null,o(n.$t("ui.badge.usage.basic")),1),e(),l("div",B,[a(t(i),{label:n.$t("ui.badge.label.basic")},null,8,["label"]),e(),a(d,{language:"html-vue"},{default:s(()=>[e(`
+          <template>
+            <Badge label="Basic" /> 
+          </template>
+          
+          <script setup lang="ts">
+            import { Badge } from "@dzangolab/vue3-ui";
+          <\/script>
+        `)]),_:1}),e()])]),e(),l("section",null,[l("h2",null,o(n.$t("ui.badge.usage.withI18n")),1),e(),l("div",y,[a(t(i),{label:n.$t("ui.badge.label.addedIn")},null,8,["label"]),e(),a(d,{language:"html-vue"},{default:s(()=>[e(`
+          <template>
+            <Badge :label=t("ui.badge.label.addedIn") /> 
+          </template>
+          
+          <script setup lang="ts">
+            import { Badge } from "@dzangolab/vue3-ui";
+            import { useI18n } from "@dzangolab/vue3-i18n";
+
+            const { t } = useI18n();
+          <\/script>
+        `)]),_:1}),e()])]),e(),l("section",null,[l("h2",null,o(n.$t("ui.badge.usage.rounded")),1),e(),l("div",$,[a(t(i),{label:n.$t("ui.badge.label.rounded"),rounded:""},null,8,["label"]),e(),a(d,{language:"html-vue"},{default:s(()=>[e(`
+          <template>
+            <Badge label="Rounded" rounded /> 
+          </template>
+          
+          <script setup lang="ts">
+            import { Badge } from "@dzangolab/vue3-ui";
+          <\/script>
+        `)]),_:1}),e()])]),e(),l("section",null,[l("h2",null,o(n.$t("ui.badge.usage.severity")),1),e(),l("div",k,[l("div",I,[a(t(i),{label:n.$t("ui.badge.label.primary"),severity:"primary"},null,8,["label"]),e(),a(t(i),{label:n.$t("ui.badge.label.secondary"),severity:"secondary"},null,8,["label"]),e(),a(t(i),{label:n.$t("ui.badge.label.alternate"),severity:"alternate"},null,8,["label"]),e(),a(t(i),{label:n.$t("ui.badge.label.success"),severity:"success"},null,8,["label"]),e(),a(t(i),{label:n.$t("ui.badge.label.danger"),severity:"danger"},null,8,["label"]),e(),a(t(i),{label:n.$t("ui.badge.label.warning"),severity:"warning"},null,8,["label"])]),e(),a(d,{language:"html-vue"},{default:s(()=>[e(`
+          <template>
+            <Badge label="Primary" severity="primary" />
+            <Badge label="Secondary" severity="secondary" />
+            <Badge label="Alternate" severity="alternate" />
+            <Badge label="Success" severity="success" />
+            <Badge label="Danger" severity="danger" />
+            <Badge label="Warning" severity="warning" />
+          </template>
+          
+          <script setup lang="ts">
+            import { Badge } from "@dzangolab/vue3-ui";
+          <\/script>
+        `)]),_:1}),e()])]),e(),l("section",null,[l("h2",null,o(n.$t("ui.badge.usage.icon")),1),e(),l("div",w,[l("div",z,[a(t(i),{"icon-left":"pi pi-thumbs-up"}),e(),a(t(i),{"icon-right":"pi pi-thumbs-down"})]),e(),a(d,{language:"html-vue"},{default:s(()=>[e(`
+          <template>
+            <Badge icon-left="pi pi-thumbs-up" />
+            <Badge icon-right="pi pi-thumbs-down" />
+          </template>
+          
+          <script setup lang="ts">
+            import { Badge } from "@dzangolab/vue3-ui";
+          <\/script>
+        `)]),_:1}),e()])]),e(),l("section",null,[l("h2",null,o(n.$t("ui.badge.usage.iconPackage")),1),e(),l("div",L,[l("div",S,[a(t(i),{label:n.$t("ui.badge.label.locked"),"icon-left":"pi pi-lock"},null,8,["label"]),e(),a(t(i),{label:n.$t("ui.badge.label.unLocked"),"icon-right":"fa-solid fa-lock-open",severity:"success"},null,8,["label"])]),e(),a(d,{language:"html-vue"},{default:s(()=>[e(`
+          <template>
+            <Badge label="Locked" icon-left="pi pi-lock" />
+            <Badge label="Unlocked" icon-right="fa-solid fa-lock-open" severity="success />
+          </template>
+          
+          <script setup lang="ts">
+            import { Badge } from "@dzangolab/vue3-ui";
+          <\/script>
+        `)]),_:1}),e()])]),e(),l("section",null,[l("h2",null,o(n.$t("ui.badge.usage.iconAndLabel")),1),e(),l("div",C,[l("div",P,[a(t(i),{label:n.$t("ui.badge.label.locked"),"icon-left":"pi pi-lock"},null,8,["label"]),e(),a(t(i),{label:n.$t("ui.badge.label.unLocked"),"icon-right":"pi pi-lock-open",severity:"success"},null,8,["label"])]),e(),a(d,{language:"html-vue"},{default:s(()=>[e(`
+          <template>
+            <Badge label="Locked" icon-left="pi pi-lock" />
+            <Badge label="Unlocked" icon-right="pi pi-lock-open" severity="success />
+          </template>
+          
+          <script setup lang="ts">
+            import { Badge } from "@dzangolab/vue3-ui";
+          <\/script>
+        `)]),_:1}),e()])]),e(),l("section",null,[l("h2",null,o(n.$t("ui.badge.usage.iconElement")),1),e(),l("div",D,[l("div",N,[a(t(i),{label:n.$t("ui.badge.label.correct")},{iconLeft:s(()=>[R]),_:1},8,["label"]),e(),a(t(i),{label:n.$t("ui.badge.label.wrong"),severity:"danger"},{iconRight:s(()=>[U]),_:1},8,["label"])]),e(),a(d,{language:"html-vue"},{default:s(()=>[e(`
+          <template>
+            <Badge label="Correct">
+              <template #iconLeft>
+                <i class="pi pi-check" >
+              </template>
+            </Badge>
+
+            <Badge label="Wrong" severity="danger">
+              <template #iconRight>
+                <i class="pi pi-times" >
+              </template>
+            </Badge>
+          </template>
+          
+          <script setup lang="ts">
+            import { Badge } from "@dzangolab/vue3-ui";
+          <\/script>
+        `)]),_:1}),e()])])]),_:1},8,["title"])}}});const K=f(V,[["__scopeId","data-v-c775f8af"]]);export{K as default};
