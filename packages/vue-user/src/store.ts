bdfc9cffa7490ce34d6c8f08053cd66402fd7829
ks-rohan <rohan@dzangolab.com>
2025-02-13 11:15:52 +0545

    fix: apiBaseUrl type

--- packages/vue-user/src/store.ts
+++ packages/vue-user/src/store.ts
@@ -57,21 +57,21 @@ const useUserStore = defineStore("user", () => {
     );
   };
 
-  const login = async (credentials: LoginCredentials, apiBaseUrl?: string) => {
+  const login = async (credentials: LoginCredentials, apiBaseUrl: string) => {
     const selectedAuthProvider = auth();
 
     const response = await selectedAuthProvider.doLogin(
       credentials,
-      apiBaseUrl as string,
+      apiBaseUrl,
     );
 
     return response;
   };
 
-  const logout = async (apiBaseUrl?: string) => {
+  const logout = async (apiBaseUrl: string) => {
     const selectedAuthProvider = auth();
 
-    await selectedAuthProvider.doLogout(apiBaseUrl as string).then(() => {
+    await selectedAuthProvider.doLogout(apiBaseUrl).then(() => {
       user.value = undefined;
 
       // FIXME [SS 17 MARCH 2023]
