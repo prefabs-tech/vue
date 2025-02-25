07674ab380ad08a4df47be808c2e99516dd20bba
DeepakAryal <deepakaryal2050@gmail.com>
2025-02-25 08:02:55 +0000

    Deploying to gh-pages from @ dzangolab/vue@ae2e428a9494b92e960f27d8ac28caac96ccd9bf 🚀

--- assets/Index-BRpDmEBV.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./UiPage.vue_vue_type_style_index_0_lang-Sn8JrzpY.js";import{d as o,j as a,o as n}from"./index-CVjUtB44.js";const _={name:"UI"},l=o({..._,setup(r){return(t,s)=>(n(),a(e,{title:t.$t("ui.title")},null,8,["title"]))}});export{l as default};
