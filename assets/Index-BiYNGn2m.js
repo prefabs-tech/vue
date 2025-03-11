ac491efc589a0fd3d6d1cb81a6c3ae6f2bd63705
ks-rohan <82809048+ks-rohan@users.noreply.github.com>
2025-03-11 08:32:29 +0000

    Deploying to gh-pages from @ dzangolab/vue@414259ca59f463cd11fd85a5f516f4e6aa66a9df 🚀

---
+++ assets/Index-BiYNGn2m.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./UserPage.vue_vue_type_style_index_0_lang-7Y64nyRn.js";import{d as o,e as r,o as s}from"./index-XH3PV4Vq.js";const a={name:"User"},i=o({...a,setup(n){return(e,_)=>(s(),r(t,{title:e.$t("user.title")},null,8,["title"]))}});export{i as default};
--- assets/Index-TWo6bsPY.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./TablePage.vue_vue_type_style_index_0_lang-BUeH4cQ1.js";import{d as o,e as a,o as n}from"./index-C-Eehmak.js";const _={name:"TableDemo"},m=o({..._,setup(l){return(e,r)=>(n(),a(t,{title:e.$t("table.title")},null,8,["title"]))}});export{m as default};
