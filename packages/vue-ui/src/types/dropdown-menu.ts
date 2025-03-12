900aca54b4554bf791ca1ed28afb42877fcb602c
KabinKhandThakuri <kabin@dzangolab.com>
2025-03-12 15:38:24 +0545

    Update dropdown menu component to support icon, hide menu items and refactor css (#568)
    * feat: add stub table toolbar component
    
    * chore: add popup to table toolbar component
    
    * refactor: update sortable list to accept render item
    
    * feat: add sortable list component to table toolbar
    
    * feat: add reset and column action buttons with toolbar slot
    
    * chore: add reset button to demo
    
    * chore: update basic table demo code section
    
    * feat: add custom toolbar action demo
    
    * feat: add stub invitation table to user package
    
    * feat: add default columns to invitation table
    
    * feat: add invitation modal to invitation table
    
    * feat: add invitation table menu and demo to user section
    
    * chore: update demo initial sorting prop
    
    * chore: add constant for admin role
    
    * feat: add visible column prop for column visibility and order
    
    * refactor: update dropdown menu to support icon

--- packages/vue-ui/src/types/dropdown-menu.ts
+++ packages/vue-ui/src/types/dropdown-menu.ts
@@ -1,7 +1,11 @@
 interface DropdownMenu {
+  class?: string;
+  disabled?: boolean;
+  display?: boolean;
+  icon?: string;
+  key?: string;
   label?: string;
   value?: string | number;
-  disabled?: boolean;
 }
 
 export type { DropdownMenu };
