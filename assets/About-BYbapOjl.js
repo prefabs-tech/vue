a7b9b476aeb6976384c50a88f45f251eb81e14aa
DeepakAryal <deepakaryal2050@gmail.com>
2025-03-19 08:54:22 +0000

    Deploying to gh-pages from @ dzangolab/vue@e7e1ec337f9639159bb24b7cecd38254b16bb513 🚀

---
+++ assets/About-BYbapOjl.js
@@ -0,0 +1,1 @@
+import{_ as o,A as n,e as a,o as s}from"./index-Bt1mySq5.js";const c={name:"About"};function r(t,p,_,l,u,i){const e=n("Page");return s(),a(e,{title:t.$t("about.title")},null,8,["title"])}const m=o(c,[["render",r]]);export{m as default};
