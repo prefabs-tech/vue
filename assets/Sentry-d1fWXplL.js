f5face99e404bb5229e7f0c797a3851deb0cf87c
DeepakAryal <deepakaryal2050@gmail.com>
2025-03-21 04:26:13 +0000

    Deploying to gh-pages from @ dzangolab/vue@df8b0cf292e8aea292348e95222afcb79efc03bf 🚀

---
+++ assets/Sentry-d1fWXplL.js
@@ -0,0 +1,1 @@
+import{d as r,A as n,e as a,o as s,w as c,f as _}from"./index-C49kGRMu.js";const l={name:"About"},f=r({...l,setup(p){const e=()=>{throw new Error("Sentry error")};return(t,i)=>{const o=n("Page");return s(),a(o,{title:t.$t("sentry.title")},{default:c(()=>[_("button",{onClick:e},"Throw error")]),_:1},8,["title"])}}});export{f as default};
