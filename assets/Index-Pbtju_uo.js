5f19ecf8ff31c6a2c2ec3c05f716ada2b57e5860
DeepakAryal <deepakaryal2050@gmail.com>
2025-04-03 08:23:45 +0000

    Deploying to gh-pages from @ dzangolab/vue@3ea2087ace49cda9fe2f02275effefb59f7edd63 🚀

---
+++ assets/Index-Pbtju_uo.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./FormPage.vue_vue_type_style_index_0_lang-D-EusHnU.js";import{d as o,e as r,o as a}from"./index-I-jo6_za.js";const n={name:"Form"},l=o({...n,setup(_){return(t,s)=>(a(),r(e,{title:t.$t("form.title")},null,8,["title"]))}});export{l as default};
