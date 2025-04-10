857e2b4bcb6aafc7b4181bd2f03b97ac4dcbbf2c
DeepakAryal <deepakaryal2050@gmail.com>
2025-04-10 09:10:40 +0000

    Deploying to gh-pages from @ dzangolab/vue@1e9771aa9fc50c07195c86470cdb224396fad819 🚀

---
+++ assets/Index-TChVFgHS.js
@@ -0,0 +1,41 @@
+import{d as u,D as g,e as C,o as d,w as t,B as i,f as a,t as m,i as o,u as r,K as f,a as L,F as k,y as v,J as b,p as B}from"./index-Ds4pur8o.js";import{_ as $}from"./UiPage.vue_vue_type_style_index_0_lang-D0syEUMl.js";const y={class:"section-content"},N=u({__name:"Index",setup(_){const p=[{name:"Card 1",description:"Lorem ipsum dolor sit amet, consectetur adipiscing"},{name:"Card 2",description:"Lorem ipsum dolor sit amet, consectetur adipiscing"},{name:"Card 3",description:"Lorem ipsum dolor sit amet, consectetur adipiscing"},{name:"Card 4",description:"Lorem ipsum dolor sit amet, consectetur adipiscing"},{name:"Card 5",description:"Lorem ipsum dolor sit amet, consectetur adipiscing"}];return(n,e)=>{const c=g("SshPre");return d(),C($,{title:n.$t("ui.gridContainer.title"),class:"demo"},{toolbar:t(()=>[o(r(B),{label:n.$t("common.back"),"icon-left":"pi pi-chevron-left",size:"medium",variant:"textOnly",onClick:e[0]||(e[0]=s=>n.$router.push("/ui"))},null,8,["label"])]),default:t(()=>[e[4]||(e[4]=i()),a("section",null,[a("h2",null,m(n.$t("ui.gridContainer.usage.basic")),1),e[3]||(e[3]=i()),a("div",y,[o(r(f),null,{default:t(()=>[(d(),L(k,null,v(p,(s,l)=>o(r(b),{key:l+"-"+s.name,title:s.name},{default:t(()=>[i(m(s.description),1)]),_:2},1032,["title"])),64))]),_:1}),e[2]||(e[2]=i()),o(c,{language:"html-vue"},{default:t(()=>e[1]||(e[1]=[i(`
+          <template>
+            <GridContainer>
+              <Card
+                v-for="(menu, index) in packages"
+                :key="index + '-' + menu.name"
+                :title="menu.name"
+              >
+                {{ menu.description }}
+              </Card>
+            </GridContainer>
+          </template>
+
+          <script setup lang="ts">
+          import { Card, GridContainer } from "@dzangolab/vue3-ui";
+
+          const packages = [
+            {
+              name: "Card 1",
+              description: "Lorem ipsum dolor sit amet, consectetur adipiscing",
+            },
+            {
+              name: "Card 2",
+              description: "Lorem ipsum dolor sit amet, consectetur adipiscing",
+            },
+            {
+              name: "Card 3",
+              description: "Lorem ipsum dolor sit amet, consectetur adipiscing",
+            },
+
+            {
+              name: "Card 4",
+              description: "Lorem ipsum dolor sit amet, consectetur adipiscing",
+            },
+            {
+              name: "Card 5",
+              description: "Lorem ipsum dolor sit amet, consectetur adipiscing",
+            },
+          ];
+          <\/script>
+        `)])),_:1})])])]),_:1},8,["title"])}}});export{N as default};
---
+++ assets/Index-sxcsACWV.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./FormPage.vue_vue_type_style_index_0_lang-C022Jv2p.js";import{d as o,e as r,o as a}from"./index-Ds4pur8o.js";const n={name:"Form"},l=o({...n,setup(_){return(t,s)=>(a(),r(e,{title:t.$t("form.title")},null,8,["title"]))}});export{l as default};
