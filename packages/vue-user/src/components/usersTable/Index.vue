321b29669083c8909bea8c73c9bf4c0fab29866b
Anvesh Joshi <152248757+anveshdzangolab@users.noreply.github.com>
2025-04-11 13:48:02 +0545

    Add name column filter in users table (#603)
    * feat: add name column filter in users table
    
    * chore: fix coding standard

--- packages/vue-user/src/components/usersTable/Index.vue
+++ packages/vue-user/src/components/usersTable/Index.vue
@@ -157,8 +157,6 @@ const defaultColumns: TableColumnDefinition<UserType>[] = [
     header: t("user.table.defaultColumns.email"),
   },
   {
-    accessorKey: "name",
-    header: t("user.table.defaultColumns.name"),
     accessorFn: (original: UserType) => {
       return (
         (original.givenName ? original.givenName : "") +
@@ -166,7 +164,11 @@ const defaultColumns: TableColumnDefinition<UserType>[] = [
           (original.surname ? " " + original.surname : "") || "-"
       );
     },
+    accessorKey: "name",
     cell: ({ getValue }) => getValue(),
+    enableColumnFilter: true,
+    filterPlaceholder: "",
+    header: t("user.table.defaultColumns.name"),
   },
   {
     align: "center",
