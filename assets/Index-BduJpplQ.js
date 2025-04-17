026542d0aa437aab07eea1fe065e8bba193177e3
DeepakAryal <deepakaryal2050@gmail.com>
2025-04-17 05:45:59 +0000

    Deploying to gh-pages from @ dzangolab/vue@fbb2298a8f1d70152fb43e3f58243359926974c2 🚀

---
+++ assets/Index-BduJpplQ.js
@@ -0,0 +1,41 @@
+import{d as u,H as g,e as C,o as d,h as t,C as i,f as a,t as m,g as o,u as r,P as f,a as L,x as k,y as v,L as b,p as $}from"./index-B0WoFnxH.js";import{_ as x}from"./UiPage.vue_vue_type_style_index_0_lang-Diujfw7R.js";const y={class:"section-content"},G=u({__name:"Index",setup(_){const p=[{name:"Card 1",description:"Lorem ipsum dolor sit amet, consectetur adipiscing"},{name:"Card 2",description:"Lorem ipsum dolor sit amet, consectetur adipiscing"},{name:"Card 3",description:"Lorem ipsum dolor sit amet, consectetur adipiscing"},{name:"Card 4",description:"Lorem ipsum dolor sit amet, consectetur adipiscing"},{name:"Card 5",description:"Lorem ipsum dolor sit amet, consectetur adipiscing"}];return(n,e)=>{const c=g("SshPre");return d(),C(x,{title:n.$t("ui.gridContainer.title"),class:"demo"},{toolbar:t(()=>[o(r($),{label:n.$t("common.back"),"icon-left":"pi pi-chevron-left",size:"medium",variant:"textOnly",onClick:e[0]||(e[0]=s=>n.$router.push("/ui"))},null,8,["label"])]),default:t(()=>[e[4]||(e[4]=i()),a("section",null,[a("h2",null,m(n.$t("ui.gridContainer.usage.basic")),1),e[3]||(e[3]=i()),a("div",y,[o(r(f),null,{default:t(()=>[(d(),L(k,null,v(p,(s,l)=>o(r(b),{key:l+"-"+s.name,title:s.name},{default:t(()=>[i(m(s.description),1)]),_:2},1032,["title"])),64))]),_:1}),e[2]||(e[2]=i()),o(c,{language:"html-vue"},{default:t(()=>e[1]||(e[1]=[i(`
+          <template>
+            <GridContainer>
+              <Card
+                v-for="(menu, index) in packages"
+                :key="index + '-' + menu.name"
+                :title="menu.name"
+              >
+                {{ menu.description }}
+              </Card>
+            </GridContainer>
+          </template>
+
+          <script setup lang="ts">
+          import { Card, GridContainer } from "@dzangolab/vue3-ui";
+
+          const packages = [
+            {
+              name: "Card 1",
+              description: "Lorem ipsum dolor sit amet, consectetur adipiscing",
+            },
+            {
+              name: "Card 2",
+              description: "Lorem ipsum dolor sit amet, consectetur adipiscing",
+            },
+            {
+              name: "Card 3",
+              description: "Lorem ipsum dolor sit amet, consectetur adipiscing",
+            },
+
+            {
+              name: "Card 4",
+              description: "Lorem ipsum dolor sit amet, consectetur adipiscing",
+            },
+            {
+              name: "Card 5",
+              description: "Lorem ipsum dolor sit amet, consectetur adipiscing",
+            },
+          ];
+          <\/script>
+        `)])),_:1})])])]),_:1},8,["title"])}}});export{G as default};
---
+++ assets/Index-DuovpIZX.js
@@ -0,0 +1,154 @@
+import{B as C,F as z,z as V,q as F}from"./DzangolabVue3Form.es-Dz15471A.js";import{d as P,K as L,r,H as $,e as O,o as k,h as s,C as o,f as a,t as u,g as n,u as i,p as d,a5 as p,a as y,x as N,y as S}from"./index-B0WoFnxH.js";import{_ as I}from"./UiPage.vue_vue_type_style_index_0_lang-Diujfw7R.js";const A={class:"section-content"},T={class:"section-content"},q={class:"section-content"},U={class:"label"},H={class:"value"},K={class:"section-content"},h={class:"section-content"},j={role:"heading"},W=P({__name:"Index",setup(G){const f=L({email:r(),password:r()}),B=[{label:"Name",value:"Acer Nitro 5"},{label:"Category",value:"Laptop"},{label:"Color",value:"Black"}],w=r(!1),b=r(!1),g=r(!1),v=r(!1),M=r(!1);return(e,l)=>{const m=$("SshPre"),D=$("router-link");return k(),O(I,{title:e.$t("ui.modal.title"),class:"demo"},{toolbar:s(()=>[n(i(d),{label:e.$t("common.back"),"icon-left":"pi pi-chevron-left",size:"medium",variant:"textOnly",onClick:l[0]||(l[0]=t=>e.$router.push("/ui"))},null,8,["label"])]),default:s(()=>[l[39]||(l[39]=o()),a("section",null,[a("h2",null,u(e.$t("ui.modal.usage.basic")),1),l[17]||(l[17]=o()),a("div",A,[n(i(d),{label:e.$t("ui.modal.label.open"),onClick:l[1]||(l[1]=t=>g.value=!0)},null,8,["label"]),l[15]||(l[15]=o()),n(i(p),{show:g.value,"onOn:close":l[2]||(l[2]=t=>g.value=!1)},{default:s(()=>[o(u(e.$t("ui.modal.content")),1)]),_:1},8,["show"]),l[16]||(l[16]=o()),n(m,{language:"html-vue"},{default:s(()=>l[14]||(l[14]=[o(`
+          <template>
+            <ButtonElement
+              label="Open"
+              @click="showModal = true"
+            />
+
+            <Modal :show="showModal" @on:close="showModal = false" >
+              Lorem ipsum...
+            </Modal>
+          </template>
+
+          <script setup lang="ts">
+          import { ButtonElement, Modal } from "@dzangolab/vue3-ui";
+          import { ref } from "vue";
+
+          const showModal = ref(false);
+          <\/script>
+        `)])),_:1})])]),l[40]||(l[40]=o()),a("section",null,[a("h2",null,u(e.$t("ui.modal.usage.withTitle")),1),l[21]||(l[21]=o()),a("div",T,[n(i(d),{label:e.$t("ui.modal.label.show"),onClick:l[3]||(l[3]=t=>M.value=!0)},null,8,["label"]),l[19]||(l[19]=o()),n(i(p),{show:M.value,title:e.$t("ui.modal.header.productDetail"),"onOn:close":l[4]||(l[4]=t=>M.value=!1)},{default:s(()=>[o(u(e.$t("ui.modal.content")),1)]),_:1},8,["show","title"]),l[20]||(l[20]=o()),n(m,{language:"html-vue"},{default:s(()=>l[18]||(l[18]=[o(`
+          <template>
+            <ButtonElement
+              label="Show"
+              @click="showModal = true"
+            />
+
+            <Modal
+              :show="showModal"
+              title="Product detail"
+              @on:close="showModal = false"
+            >
+              Lorem ipsum...
+            </Modal>
+          </template>
+
+          <script setup lang="ts">
+          import { ButtonElement, Modal } from "@dzangolab/vue3-ui";
+          import { ref } from "vue";
+
+          const showModal = ref(false);
+          <\/script>
+        `)])),_:1})])]),l[41]||(l[41]=o()),a("section",null,[a("h2",null,u(e.$t("ui.modal.usage.disableDismiss")),1),l[26]||(l[26]=o()),a("div",q,[n(i(d),{label:e.$t("ui.modal.label.viewDetail"),onClick:l[5]||(l[5]=t=>w.value=!0)},null,8,["label"]),l[24]||(l[24]=o()),n(i(p),{"dismiss-on-click-out":!1,show:w.value,title:e.$t("ui.modal.header.productDetail"),"onOn:close":l[6]||(l[6]=t=>w.value=!1)},{default:s(()=>[(k(),y(N,null,S(B,(t,E)=>a("div",{key:E+"-"+t,class:"info"},[a("span",U,u(t.label),1),l[22]||(l[22]=o()),a("span",H,u(" : "+t.value),1)])),64))]),_:1},8,["show","title"]),l[25]||(l[25]=o()),n(m,{language:"html-vue"},{default:s(()=>l[23]||(l[23]=[o(`
+          <template>
+            <ButtonElement
+              label="View detail"
+              @click="showModal = true"
+            />
+
+            <Modal
+              :dismiss-on-click-out="false"
+              :show="showModal"
+              title="Product detail"
+              @on:close="showModal = false"
+            >
+              <div
+                v-for="(detail, index) in productDetail"
+                :key="index + '-' + detail"
+                class="info"
+              >
+                <span class="label">{{ detail.label }}</span>
+                <span class="value">{{ " : " + detail.value }}</span>
+              </div>
+            </Modal>
+          </template>
+
+          <script setup lang="ts">
+          import { ButtonElement, Modal } from "@dzangolab/vue3-ui";
+          import { ref } from "vue";
+
+          const productDetail = [
+            ...
+          ];
+
+          const showModal = ref(false);
+          <\/script>
+        `)])),_:1})])]),l[42]||(l[42]=o()),a("section",null,[a("h2",null,u(e.$t("ui.confirmationModal.usage.withI18n")),1),l[30]||(l[30]=o()),a("div",K,[n(i(d),{label:e.$t("ui.modal.label.show"),onClick:l[7]||(l[7]=t=>b.value=!0)},null,8,["label"]),l[28]||(l[28]=o()),n(i(p),{show:b.value,title:e.$t("ui.modal.header.productDetail"),"onOn:close":l[8]||(l[8]=t=>b.value=!1)},{default:s(()=>[o(u(e.$t("ui.modal.content")),1)]),_:1},8,["show","title"]),l[29]||(l[29]=o()),n(m,{language:"html-vue"},{default:s(()=>l[27]||(l[27]=[o(`
+          <template>
+            <ButtonElement
+              :label="t('ui.modal.label.show')"
+              @click="showModal = true"
+            />
+
+            <Modal
+              :show="showModal"
+              :title="t('ui.modal.header.productDetail')"
+              @on:close="showModal = false"
+            >
+              {{ t("ui.modal.content") }}
+            </Modal>
+          </template>
+
+          <script setup lang="ts">
+          import { useI18n } from "@dzangolab/vue3-i18n";
+          import { ButtonElement, Modal } from "@dzangolab/vue3-ui";
+          import { ref } from "vue";
+
+          const { t } = useI18n();
+
+          const showModal = ref(false);
+          <\/script>
+        `)])),_:1})])]),l[43]||(l[43]=o()),a("section",null,[a("h2",null,u(e.$t("ui.confirmationModal.usage.slots")),1),l[38]||(l[38]=o()),a("div",h,[n(i(d),{label:e.$t("ui.modal.label.login"),"icon-left":"pi pi-user",onClick:l[9]||(l[9]=t=>v.value=!0)},null,8,["label"]),l[36]||(l[36]=o()),n(i(p),{show:v.value,"onOn:close":l[13]||(l[13]=t=>v.value=!1)},{header:s(()=>[a("span",j,u(e.$t("ui.modal.label.login")),1)]),footer:s(()=>[n(D,{to:"/form/password-input"},{default:s(()=>[o(u(e.$t("ui.modal.label.forgotPassword")),1)]),_:1})]),default:s(()=>[l[33]||(l[33]=o()),n(i(C),null,{default:s(()=>[n(i(z),{modelValue:f.email,"onUpdate:modelValue":l[10]||(l[10]=t=>f.email=t),label:e.$t("form.label.email"),placeholder:e.$t("form.placeholder.email")},null,8,["modelValue","label","placeholder"]),l[31]||(l[31]=o()),n(i(V),{modelValue:f.password,"onUpdate:modelValue":l[11]||(l[11]=t=>f.password=t),label:e.$t("form.label.password"),placeholder:e.$t("form.placeholder.password")},null,8,["modelValue","label","placeholder"]),l[32]||(l[32]=o()),n(i(F),{"submit-label":e.$t("ui.modal.label.login"),alignment:"filled",class:"login-actions",onCancel:l[12]||(l[12]=t=>v.value=!1)},null,8,["submit-label"])]),_:1}),l[34]||(l[34]=o())]),_:1},8,["show"]),l[37]||(l[37]=o()),n(m,{language:"html-vue"},{default:s(()=>l[35]||(l[35]=[o(`
+          <template>
+            <ButtonElement
+              icon-left="pi pi-user"
+              label="Login"
+              @click="showModal = true"
+            />
+
+            <Modal :show="showModal" @on:close="showModal = false" >
+              <template #header>
+                <span role="heading">Login</span>
+              </template>
+
+              <Form>
+                <Email
+                  v-model="formData.email"
+                  label="Email"
+                  placeholder="Enter a email"
+                />
+
+                <Password
+                  v-model="formData.password"
+                  label="Password"
+                  placeholder="Enter a password"
+                />
+
+                <FormActions
+                  alignment="filled"
+                  class="login-actions"
+                  submit-label="Login"
+                  @cancel="showSlotModal = false"
+                />
+              </Form>
+
+              <template #footer>
+                <router-link to="/form/password-input">
+                  Forgot password?
+                </router-link>
+              </template>
+            </Modal>
+          </template>
+
+          <script setup lang="ts">
+          import { Email, Form, FormActions, Password } from "@dzangolab/vue3-form";
+          import { ButtonElement, Modal } from "@dzangolab/vue3-ui";
+          import { reactive, ref } from "vue";
+
+          const formData = reactive({
+            email: ref(),
+            password: ref(),
+          });
+
+          const showModal = ref(false);
+          <\/script>
+        `)])),_:1})])])]),_:1},8,["title"])}}});export{W as default};
