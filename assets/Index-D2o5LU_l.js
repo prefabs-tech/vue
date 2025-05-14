952a7c4560c3b1527b64123b7ff46c6ae38d9401
DeepakAryal <deepakaryal2050@gmail.com>
2025-05-14 09:58:48 +0000

    Deploying to gh-pages from @ dzangolab/vue@9eaf526536a7580ccecd26e837080b686c9cb00c 🚀

---
+++ assets/Index-D2o5LU_l.js
@@ -0,0 +1,40 @@
+import{d,K as p,f as b,o as m,i as u,D as e,g as n,t as l,h as a,u as i,a9 as r,v}from"./index-Dncpa1QU.js";import{_ as g}from"./UiPage.vue_vue_type_style_index_0_lang-CK4fi2ux.js";const w={class:"section-content"},k={class:"section-content"},c={class:"section-content"},y=d({__name:"Index",setup(B){return(o,t)=>{const s=p("SshPre");return m(),b(g,{title:o.$t("ui.youtubeFacade.title"),class:"demo-youtube-facade"},{toolbar:u(()=>[a(i(v),{label:o.$t("common.back"),"icon-left":"pi pi-chevron-left",size:"medium",variant:"textOnly",onClick:t[0]||(t[0]=f=>o.$router.push("/ui"))},null,8,["label"])]),default:u(()=>[t[10]||(t[10]=e()),n("section",null,[n("h2",null,l(o.$t("ui.youtubeFacade.usage.basic")),1),t[3]||(t[3]=e()),n("div",w,[a(i(r),{alt:o.$t("ui.youtubeFacade.alternativeMessage"),"video-link":"https://www.youtube.com/watch?v=YrxBCBibVo0","video-token":"YrxBCBibVo0"},null,8,["alt"]),t[2]||(t[2]=e()),a(s,{language:"html-vue"},{default:u(()=>t[1]||(t[1]=[e(`
+          <template>
+            <YoutubeFacade
+              alt="Vue basic"
+              video-link="https://www.youtube.com/watch?v=YrxBCBibVo0"
+              video-token="YrxBCBibVo0"
+            />
+          </template>
+
+          <script setup lang="ts">
+            import { YoutubeFacade } from "@dzangolab/vue3-ui";
+          <\/script>
+        `)])),_:1})])]),t[11]||(t[11]=e()),n("section",null,[n("h2",null,l(o.$t("ui.youtubeFacade.usage.aspectRatio")),1),t[6]||(t[6]=e()),n("div",k,[a(i(r),{alt:o.$t("ui.youtubeFacade.alternativeMessage"),"aspect-ratio":"16/10","video-link":"https://www.youtube.com/watch?v=F7PLPJqVotk","video-token":"F7PLPJqVotk"},null,8,["alt"]),t[5]||(t[5]=e()),a(s,{language:"html-vue"},{default:u(()=>t[4]||(t[4]=[e(`
+          <template>
+            <YoutubeFacade
+              alt="Vue basic"
+              aspect-ratio="16/10"
+              video-link="https://www.youtube.com/watch?v=F7PLPJqVotk"
+              video-token="F7PLPJqVotk"
+            />
+          </template>
+
+          <script setup lang="ts">
+            import { YoutubeFacade } from "@dzangolab/vue3-ui";
+          <\/script>
+        `)])),_:1})])]),t[12]||(t[12]=e()),n("section",null,[n("h2",null,l(o.$t("ui.youtubeFacade.usage.customThumbnail")),1),t[9]||(t[9]=e()),n("div",c,[a(i(r),{alt:o.$t("ui.youtubeFacade.alternativeMessage"),"aspect-ratio":"1.95/1","image-source":"logo.png","video-link":"https://www.youtube.com/watch?v=YrxBCBibVo0","video-token":"YrxBCBibVo0"},null,8,["alt"]),t[8]||(t[8]=e()),a(s,{language:"html-vue"},{default:u(()=>t[7]||(t[7]=[e(`
+          <template>
+            <YoutubeFacade
+              alt="Vue basic"
+              aspect-ratio="1.95/1"
+              image-source="logo.png"
+              video-link="https://www.youtube.com/watch?v=YrxBCBibVo0"
+              video-token="YrxBCBibVo0"
+            />
+          </template>
+
+          <script setup lang="ts">
+            import { YoutubeFacade } from "@dzangolab/vue3-ui";
+          <\/script>
+        `)])),_:1})])])]),_:1},8,["title"])}}});export{y as default};
