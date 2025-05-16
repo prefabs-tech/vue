e0db87389aa78de4907ce24a0f7c0b13191cfa9f
Kabin <kabin@dzangolab.com>
2025-05-16 12:10:30 +0545

    chore: sort variable alphabetically

--- packages/vue-tanstack-table/src/components/TableHeader.vue
+++ packages/vue-tanstack-table/src/components/TableHeader.vue
@@ -177,11 +177,10 @@ const getFormattedDateRange = (dateRange: Date[]) => {
 
   return (dateRange as Date[])
     .map((date, index) => {
-      const year = date.getFullYear();
-      const month = String(date.getMonth() + 1).padStart(2, "0");
       const day = String(date.getDate()).padStart(2, "0");
-
+      const month = String(date.getMonth() + 1).padStart(2, "0");
       const time = index === 0 ? "00:00:00" : "23:59:59";
+      const year = date.getFullYear();
 
       return `${year}-${month}-${day} ${time}`;
     })
