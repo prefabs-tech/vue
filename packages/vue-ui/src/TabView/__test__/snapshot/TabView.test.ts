c4f0f6b1ccbe4374440b9ddb13589972c0ac324e
Kabin <kabin@dzangolab.com>
2025-05-20 13:21:46 +0545

    test: add snapshot test for tab view component

---
+++ packages/vue-ui/src/TabView/__test__/snapshot/TabView.test.ts
@@ -0,0 +1,35 @@
+import { mount } from "@vue/test-utils";
+import { describe, expect, it } from "vitest";
+
+import TabView from "../../Index.vue";
+
+describe("TabView", () => {
+  it("matches snapshot", () => {
+    const wrapper = mount(TabView, {
+      props: {
+        activeKey: "tab1",
+        tabs: [
+          {
+            key: "tab1",
+            label: "Tab1"
+          },
+          {
+            key: "tab2",
+            label: "Tab2"
+          },
+          {
+            key: "tab3",
+            label: "Tab3"
+          },
+          {
+            key: "tab4",
+            label: "Tab4"
+          },
+        ],
+        visibleTabs: [ "tab1", "tab2", "tab3", "tab4" ],
+      },
+    });
+
+    expect(wrapper.element).toMatchSnapshot();
+  });
+});
