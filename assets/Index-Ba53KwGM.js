ada11442ee670c69fc17cffcdc16b500bcf3dacf
premsgr <32644681+premsgr@users.noreply.github.com>
2025-06-04 07:09:04 +0000

    Deploying to gh-pages from @ dzangolab/vue@b8d8d4866f14c7c49453d7ad5953865015114665 🚀

--- assets/Index-Ba53KwGM.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./FormPage.vue_vue_type_style_index_0_lang-l1avFXRS.js";import{d as o,f as r,o as a}from"./index-CHigbUgk.js";const n={name:"Form"},f=o({...n,setup(_){return(t,s)=>(a(),r(e,{title:t.$t("form.title")},null,8,["title"]))}});export{f as default};
