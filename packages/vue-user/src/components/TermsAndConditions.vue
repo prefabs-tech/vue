34bc418b83083f8e1679a6167c55068424df83f1
Kabin <kabin@dzangolab.com>
2025-06-05 13:22:28 +0545

    refactor: update terms and condition component

--- packages/vue-user/src/components/TermsAndConditions.vue
+++ packages/vue-user/src/components/TermsAndConditions.vue
@@ -3,17 +3,25 @@
     <div class="input-field-checkbox">
       <Checkbox :name="name" :label="label" @update:model-value="onChange" />
 
-      <slot></slot>
+      <slot>
+        <template v-if="!label">
+          <TermsText />
+        </template>
+      </slot>
     </div>
   </div>
   <p v-else :class="name" :aria-label="name">
     <slot>
-      {{ label }}
+      <template v-if="label">{{ label }}</template>
+      <template v-else>
+        <TermsText />
+      </template>
     </slot>
   </p>
 </template>
 
 <script lang="ts">
+// eslint-disable-next-line vue/one-component-per-file
 export default {
   name: "TermsAndConditions",
 };
@@ -21,8 +29,13 @@ export default {
 
 <script setup lang="ts">
 import { Checkbox } from "@dzangolab/vue3-form";
+import { useI18n } from "@dzangolab/vue3-i18n";
+import { defineComponent, h } from "vue";
+import { RouterLink } from "vue-router";
 
-defineProps({
+import { useTranslations } from "../index";
+
+const props = defineProps({
   hasCheckbox: {
     default: true,
     type: Boolean,
@@ -36,9 +49,32 @@ defineProps({
     default: "terms-and-conditions",
     type: String,
   },
+  route: {
+    default: "",
+    type: String,
+  },
 });
 
 const emit = defineEmits(["update:check"]);
+
+const messages = useTranslations();
+
+const { t } = useI18n({ messages });
+
+// eslint-disable-next-line vue/one-component-per-file
+const TermsText = defineComponent({
+  name: "TermsText",
+  setup() {
+    return () =>
+      h("div", { class: "terms-and-conditions" }, [
+        t("user.signup.form.termsAndConditions.prefix") + " ",
+        h(RouterLink, { to: props.route }, () =>
+          t("user.signup.form.termsAndConditions.infix"),
+        ),
+        " " + t("user.signup.form.termsAndConditions.suffix"),
+      ]);
+  },
+});
 
 const onChange = (value: boolean) => {
   emit("update:check", value);
@@ -56,5 +92,17 @@ const onChange = (value: boolean) => {
 
 .input-field-checkbox input {
   width: fit-content;
+}
+
+.signup .terms-and-conditions a {
+  color: var(--dz-primary-color, #1c4786);
+  text-decoration: underline;
+}
+
+.signup .terms-and-conditions input[type="checkbox"],
+.signup .terms-and-conditions input[type="checkbox"]:focus-visible {
+  box-shadow: none;
+  cursor: pointer;
+  height: fit-content;
 }
 </style>
