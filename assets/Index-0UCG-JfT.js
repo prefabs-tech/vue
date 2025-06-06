018983526d7611713257c254fb677f302300d066
premsgr <32644681+premsgr@users.noreply.github.com>
2025-06-06 08:23:33 +0000

    Deploying to gh-pages from @ dzangolab/vue@9e73b8d712a15a2b3e5222f4f8a03693e4c1bfba 🚀

---
+++ assets/Index-0UCG-JfT.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./LayoutPage.vue_vue_type_style_index_0_lang-BLLSjIRA.js";import{d as o,f as a,o as n}from"./index-CKc01FpU.js";const _={name:"LayoutDemo"},m=o({..._,setup(r){return(t,s)=>(n(),a(e,{title:t.$t("layout.title")},null,8,["title"]))}});export{m as default};
