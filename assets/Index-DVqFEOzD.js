d0cc91ab48de79743b627cd132bf56e2b5f42665
premsgr <32644681+premsgr@users.noreply.github.com>
2025-06-11 05:00:56 +0000

    Deploying to gh-pages from @ dzangolab/vue@8d7289ac99bcea4a7a38bdbd92278d3633876bc8 🚀

---
+++ assets/Index-DVqFEOzD.js
@@ -0,0 +1,87 @@
+import{x as p,q as g}from"./DzangolabVue3Form.es-Bt7fBSdd.js";import{d as v,L as I,M as V,N as $,r as d,K as w,f as z,o as y,i as m,E as e,g as o,t as i,h as a,u as n,x as U}from"./index-dUn2P5Kw.js";import{_ as E}from"./FormPage.vue_vue_type_style_index_0_lang-CIflVNFS.js";const S={class:"section-content"},T={class:"section-content"},W={class:"section-content"},k={class:"section-content"},C={class:"section-content"},F={class:"section-content"},L={name:"InputField"},D=v({...L,setup(N){const{t:f}=I(),b=V.string().min(3,{message:f("form.errors.input.min",{length:3})});let s=$({disabled:d("monorepo@gmail.com"),input:d(),inputWithValidation:d(),noLabelInput:d(),passwordInput:d()});return(t,l)=>{const u=w("SshPre");return y(),z(E,{title:t.$t("form.label.input"),class:"demo"},{toolbar:m(()=>[a(n(U),{label:t.$t("common.back"),"icon-left":"pi pi-chevron-left",size:"medium",variant:"textOnly",onClick:l[0]||(l[0]=r=>t.$router.push("/form"))},null,8,["label"])]),default:m(()=>[l[25]||(l[25]=e()),o("section",null,[o("h2",null,i(t.$t("form.label.basicInput")),1),l[9]||(l[9]=e()),o("div",S,[a(n(p),{modelValue:n(s).noLabelInput,"onUpdate:modelValue":l[1]||(l[1]=r=>n(s).noLabelInput=r),placeholder:t.$t("form.placeholder.username"),type:"text"},null,8,["modelValue","placeholder"]),l[8]||(l[8]=e()),a(u,{language:"html-vue"},{default:m(()=>l[7]||(l[7]=[e(`
+          <template>
+            <Input 
+              v-model="username"
+              placeholder="Enter username"
+              type="text"
+            />
+          </template>
+
+          <script setup lang="ts">
+          import { Input } from "@dzangolab/vue3-form";
+          <\/script>
+        `)])),_:1})])]),l[26]||(l[26]=e()),o("section",null,[o("h2",null,i(t.$t("form.label.labelInput")),1),l[12]||(l[12]=e()),o("div",T,[a(n(p),{modelValue:n(s).input,"onUpdate:modelValue":l[2]||(l[2]=r=>n(s).input=r),label:t.$t("form.label.username"),placeholder:t.$t("form.placeholder.username")},null,8,["modelValue","label","placeholder"]),l[11]||(l[11]=e()),a(u,{language:"html-vue"},{default:m(()=>l[10]||(l[10]=[e(`
+          <template>
+            <Input 
+              v-model="username"
+              label="Username"
+              placeholder="Enter username"
+            />
+          </template>
+
+          <script setup lang="ts">
+          import { Input } from "@dzangolab/vue3-form";
+          import { ref } from "vue";
+  
+          const username = ref(null as string);
+          <\/script>
+        `)])),_:1})])]),l[27]||(l[27]=e()),o("section",null,[o("h2",null,i(t.$t("form.label.inputWithType")),1),l[15]||(l[15]=e()),o("div",W,[a(n(p),{modelValue:n(s).passwordInput,"onUpdate:modelValue":l[3]||(l[3]=r=>n(s).passwordInput=r),label:t.$t("form.label.password"),placeholder:t.$t("form.placeholder.password"),type:"password"},null,8,["modelValue","label","placeholder"]),l[14]||(l[14]=e()),a(u,{language:"html-vue"},{default:m(()=>l[13]||(l[13]=[e(`
+          <template>
+            <Input 
+              v-model="password"
+              label="Password"
+              placeholder="Enter password"
+              type="password"
+            />
+          </template>
+
+          <script setup lang="ts">
+          import { Input } from "@dzangolab/vue3-form";
+          <\/script>
+        `)])),_:1})])]),l[28]||(l[28]=e()),o("section",null,[o("h2",null,i(t.$t("form.label.disabled")),1),l[18]||(l[18]=e()),o("div",k,[a(n(p),{modelValue:n(s).disabled,"onUpdate:modelValue":l[4]||(l[4]=r=>n(s).disabled=r),label:t.$t("form.label.email"),type:"email",disabled:""},null,8,["modelValue","label"]),l[17]||(l[17]=e()),a(u,{language:"html-vue"},{default:m(()=>l[16]||(l[16]=[e(`
+          <template>
+            <Input 
+              v-model="email"
+              label="Email"
+              type="email"
+              disabled
+            />
+          </template>
+
+          <script setup lang="ts">
+          import { Input } from "@dzangolab/vue3-form";
+          <\/script>
+        `)])),_:1})])]),l[29]||(l[29]=e()),o("section",null,[o("h2",null,i(t.$t("form.label.withI18n")),1),l[21]||(l[21]=e()),o("div",C,[a(n(p),{modelValue:n(s).input,"onUpdate:modelValue":l[5]||(l[5]=r=>n(s).input=r),label:t.$t("form.label.username"),placeholder:t.$t("form.placeholder.username")},null,8,["modelValue","label","placeholder"]),l[20]||(l[20]=e()),a(u,{language:"html-vue"},{default:m(()=>l[19]||(l[19]=[e(`
+          <template>
+            <Input 
+              v-model="username"
+              :label="t('form.label.username')"
+              :placeholder="t('form.placeholder.username')"
+            />
+          </template>
+
+          <script setup lang="ts">
+          import { Input } from "@dzangolab/vue3-form";
+          import { useI18n } from "@dzangolab/vue3-i18n";
+
+          const { t } = useI18n();
+          <\/script>
+        `)])),_:1})])]),l[30]||(l[30]=e()),o("section",null,[o("h2",null,i(t.$t("form.label.inputWithValidation")),1),l[24]||(l[24]=e()),o("div",F,[a(n(g),null,{default:m(()=>[a(n(p),{modelValue:n(s).inputWithValidation,"onUpdate:modelValue":l[6]||(l[6]=r=>n(s).inputWithValidation=r),label:t.$t("form.label.title"),placeholder:t.$t("form.placeholder.title"),schema:n(b),name:"title",type:"text"},null,8,["modelValue","label","placeholder","schema"])]),_:1}),l[23]||(l[23]=e()),a(u,{language:"html-vue"},{default:m(()=>l[22]||(l[22]=[e(`
+          <Form>
+            <Input 
+              v-model="courseTitle"
+              :schema="inputSchema"
+              label="Title"
+              placeholder="Enter course title"
+              type="text"
+            />
+          </Form>
+
+          <script setup lang="ts">
+          import { Form, Input } from "@dzangolab/vue3-form";
+          import { z } from "zod";
+
+          const inputSchema = z.string()
+            .min(3, { message: "Title must be at least 3 character(s)" });        
+          <\/script>
+        `)])),_:1})])])]),_:1},8,["title"])}}});export{D as default};
---
+++ assets/Index-VU85ZrWO.js
@@ -0,0 +1,310 @@
+import{j as C}from"./DzangolabVue3TanstackTable.es-DjOhvQkW.js";import{d as O,r as d,K as M,f as T,o as g,i as r,E as t,g as i,t as s,h as l,u as a,a8 as c,x as m,a as j,F as U,A as F,b as K,I as D}from"./index-dUn2P5Kw.js";import{_ as q}from"./UiPage.vue_vue_type_style_index_0_lang-CT6mOZFx.js";import"./DzangolabVue3Form.es-Bt7fBSdd.js";const G={class:"section-content"},H={class:"section-content"},J={class:"section-content"},Q={class:"section-content"},W={class:"section-content"},X={class:"section-content"},Y={class:"tab-button-group"},Z={class:"section-content"},x={class:"section-content"},_={class:"section-content"},ee={class:"section-content"},te={class:"section-content"},ie={class:"section-content"},ne={class:"section-content"},be=O({__name:"Index",setup(le){const k=d("1"),V=d(),h=d(),v=d("1"),S=d(),f=d(!1),y=d(!1),p=d(["1"]),P=[{accessorKey:"name",header:"Name"},{accessorKey:"description",header:"Description"}],$=[{description:"Triggers before tab actually change",id:1,name:"beforeTabChange"},{description:"Triggers before tab actually close",id:2,name:"beforeTabClose"},{description:"Triggers on active key change",id:3,name:"update:activeKey"},{description:"Triggers on visible tabs change",id:4,name:"update:visibleTabs"}],R=[{accessorKey:"prop",header:"Property"},{accessorKey:"type",header:"Type"},{accessorKey:"default",header:"Default"},{accessorKey:"description",header:"Description"}],z=[{default:"-",description:"Active key of TabView.",id:1,prop:"activeKey",type:"string"},{default:"-",description:"Id of tab to save the state. Should provide 'id' in case of tab state persistence.",id:2,prop:"id",type:"string"},{default:"false",description:"Enable event emitting before tab actually change.",id:3,prop:"interceptTabChange",type:"boolean"},{default:"false",description:"Enable event emitting before tab actually change.",id:4,prop:"interceptTabClose",type:"boolean"},{default:"true",description:"If true, tab state is saved either in localStorage or sessionStorage.",id:5,prop:"persistState",type:"boolean"},{default:"localStorage",description:"Storage to save tab state.",id:6,prop:"persistStateStorage",type:'"localStorage" | "sessionStorage"'},{default:"top",description:"Position of the tab panel header relative to its content.",id:7,prop:"position",type:'"top" | "left" | "bottom" | "right"'},{default:"-",description:"Array of tab object.",id:8,prop:"tabs",type:"Tab[]"},{default:"-",description:"Array of visible tabs.",id:9,prop:"visibleTabs",type:"string[]"}],A=[{accessorKey:"name",header:"Name"},{accessorKey:"description",header:"Description"}],I=[{key:"description",label:"Description"},{key:"review",label:"Reviews"},{key:"specifications",label:"Specifications"},{key:"pricing",label:"Pricing"},{key:"installation",label:"Installation"},{key:"certifications",label:"Certifications"}],u=[{children:"Description",key:"1",label:"Description"},{children:"Reviews",closable:!0,key:"2",label:"Reviews"},{children:"Specifications",closable:!0,key:"3",label:"Specifications"},{children:"Pricing",closable:!0,key:"4",label:"Pricing"},{children:"Installation Instructions",key:"5",label:"Installation"},{children:"Certifications",key:"6",label:"Certifications"}],w=n=>{p.value.includes(n)||(p.value=[...p.value,n]),k.value=n},B=n=>{y.value=!0,S.value=n},E=n=>{f.value=!0,V.value=n},N=()=>{v.value=S.value||v.value,y.value=!1},L=()=>{var n;(n=h.value)==null||n.closeTab(V.value),f.value=!1};return(n,e)=>{const b=M("SshPre");return g(),T(q,{title:n.$t("ui.tabView.title"),class:"tab-view"},{toolbar:r(()=>[l(a(m),{label:n.$t("common.back"),"icon-left":"pi pi-chevron-left",size:"medium",variant:"textOnly",onClick:e[0]||(e[0]=o=>n.$router.push("/ui"))},null,8,["label"])]),default:r(()=>[e[47]||(e[47]=t()),i("section",null,[i("h2",null,s(n.$t("ui.tabView.usage.basic")),1),e[11]||(e[11]=t()),i("div",G,[l(a(c),{id:"tab-view",tabs:u,"active-key":"1"}),e[10]||(e[10]=t()),l(b,{language:"html-vue"},{default:r(()=>e[9]||(e[9]=[t(`
+          <template>
+            <TabView id="tab-view" :tabs="tabs" active-key="1" />
+          </template>
+
+          <script setup lang="ts">
+          import { TabView } from "@dzangolab/vue3-ui";
+
+          const tabs = [
+            { children: "Description", key: "1", label: "Description" },
+            { children: "Reviews", closable: true, key: "2", label: "Reviews" },
+            {
+              children: "Specifications",
+              closable: true,
+              key: "3",
+              label: "Specifications",
+            },
+            { children: "Pricing", closable: true, key: "4", label: "Pricing",  },
+            { children: "Installation Instructions", key: "5", label: "Installation",  },
+            { children: "Certifications", key: "6", label: "Certifications" },
+          ];
+          <\/script>
+        `)])),_:1})])]),e[48]||(e[48]=t()),i("section",null,[i("h2",null,s(n.$t("ui.tabView.usage.disableTabState.title")),1),e[15]||(e[15]=t()),i("div",H,[i("p",null,s(n.$t("ui.tabView.usage.disableTabState.subtitle")),1),e[13]||(e[13]=t()),l(a(c),{"persist-state":!1,tabs:u,"visible-tabs":["1","5","6"],"active-key":"1"}),e[14]||(e[14]=t()),l(b,{language:"html-vue"},{default:r(()=>e[12]||(e[12]=[t(`
+          <template>
+            <TabView
+              :persist-state="false"
+              :tabs="tabs"    
+              :visible-tabs="['1', '5', '6']"
+              active-key="1"
+            />
+          </template>
+
+          <script setup lang="ts">
+          import { TabView } from "@dzangolab/vue3-ui";
+
+          const tabs = [
+            { children: "Description", key: "1", label: "Description" },
+            { children: "Reviews", closable: true, key: "2", label: "Reviews" },
+            {
+              children: "Specifications",
+              closable: true,
+              key: "3",
+              label: "Specifications",
+            },
+            { children: "Pricing", closable: true, key: "4", label: "Pricing",  },
+            { children: "Installation Instructions", key: "5", label: "Installation",  },
+            { children: "Certifications", key: "6", label: "Certifications" },
+          ];
+          <\/script>
+        `)])),_:1})])]),e[49]||(e[49]=t()),i("section",null,[i("h2",null,s(n.$t("ui.tabView.usage.positionBottom")),1),e[18]||(e[18]=t()),i("div",J,[l(a(c),{id:"tab-view-bottom",tabs:u,"visible-tabs":["1","5","6"],"active-key":"1",position:"bottom"}),e[17]||(e[17]=t()),l(b,{language:"html-vue"},{default:r(()=>e[16]||(e[16]=[t(`
+          <template>
+            <TabView
+              id="tab-view-bottom"
+              :tabs="tabs"    
+              :visible-tabs="['1', '5', '6']"
+              active-key="1"
+              position="bottom"
+            />
+          </template>
+
+          <script setup lang="ts">
+          import { TabView } from "@dzangolab/vue3-ui";
+
+          const tabs = [
+            { children: "Description", key: "1", label: "Description" },
+            { children: "Reviews", closable: true, key: "2", label: "Reviews" },
+            {
+              children: "Specifications",
+              closable: true,
+              key: "3",
+              label: "Specifications",
+            },
+            { children: "Pricing", closable: true, key: "4", label: "Pricing",  },
+            { children: "Installation Instructions", key: "5", label: "Installation",  },
+            { children: "Certifications", key: "6", label: "Certifications" },
+          ];
+          <\/script>
+        `)])),_:1})])]),e[50]||(e[50]=t()),i("section",null,[i("h2",null,s(n.$t("ui.tabView.usage.positionLeft")),1),e[21]||(e[21]=t()),i("div",Q,[l(a(c),{id:"tab-view-left",tabs:u,"visible-tabs":["1","5","6"],"active-key":"1",position:"left"}),e[20]||(e[20]=t()),l(b,{language:"html-vue"},{default:r(()=>e[19]||(e[19]=[t(`
+          <template>
+            <TabView
+              id="tab-view-left"
+              :tabs="tabs"    
+              :visible-tabs="['1', '5', '6']"
+              active-key="1"
+              position="left"
+            />
+          </template>
+
+          <script setup lang="ts">
+          import { TabView } from "@dzangolab/vue3-ui";
+
+          const tabs = [
+            { children: "Description", key: "1", label: "Description" },
+            { children: "Reviews", closable: true, key: "2", label: "Reviews" },
+            {
+              children: "Specifications",
+              closable: true,
+              key: "3",
+              label: "Specifications",
+            },
+            { children: "Pricing", closable: true, key: "4", label: "Pricing",  },
+            { children: "Installation Instructions", key: "5", label: "Installation",  },
+            { children: "Certifications", key: "6", label: "Certifications" },
+          ];
+          <\/script>
+        `)])),_:1})])]),e[51]||(e[51]=t()),i("section",null,[i("h2",null,s(n.$t("ui.tabView.usage.positionRight")),1),e[24]||(e[24]=t()),i("div",W,[l(a(c),{id:"tab-view-right",tabs:u,"visible-tabs":["1","5","6"],"active-key":"1",position:"right"}),e[23]||(e[23]=t()),l(b,{language:"html-vue"},{default:r(()=>e[22]||(e[22]=[t(`
+          <template>
+            <TabView
+              id="tab-view-right"
+              :tabs="tabs"    
+              :visible-tabs="['1', '5', '6']"
+              active-key="1"
+              position="right"
+            />
+          </template>
+
+          <script setup lang="ts">
+          import { TabView } from "@dzangolab/vue3-ui";
+
+          const tabs = [
+            { children: "Description", key: "1", label: "Description" },
+            { children: "Reviews", closable: true, key: "2", label: "Reviews" },
+            {
+              children: "Specifications",
+              closable: true,
+              key: "3",
+              label: "Specifications",
+            },
+            { children: "Pricing", closable: true, key: "4", label: "Pricing",  },
+            { children: "Installation Instructions", key: "5", label: "Installation",  },
+            { children: "Certifications", key: "6", label: "Certifications" },
+          ];
+          <\/script>
+        `)])),_:1})])]),e[52]||(e[52]=t()),i("section",null,[i("h2",null,s(n.$t("ui.tabView.usage.closableTab")),1),e[30]||(e[30]=t()),i("div",X,[i("div",Y,[l(a(m),{label:"Add specifications tab",onClick:e[1]||(e[1]=o=>w("3"))}),e[25]||(e[25]=t()),l(a(m),{label:"Add reviews tab",onClick:e[2]||(e[2]=o=>w("2"))}),e[26]||(e[26]=t()),l(a(m),{label:"Add pricing tab",onClick:e[3]||(e[3]=o=>w("4"))})]),e[28]||(e[28]=t()),l(a(c),{id:"tab-view-closable","active-key":k.value,"onUpdate:activeKey":e[4]||(e[4]=o=>k.value=o),"visible-tabs":p.value,"onUpdate:visibleTabs":e[5]||(e[5]=o=>p.value=o),tabs:u},null,8,["active-key","visible-tabs"]),e[29]||(e[29]=t()),l(b,{language:"html-vue"},{default:r(()=>e[27]||(e[27]=[t(`
+          <template>
+            <div class="tab-button-group">
+              <ButtonElement label="Add specifications tab" @click="addTab('3')" />
+              <ButtonElement label="Add reviews tab" @click="addTab('2')" />
+              <ButtonElement label="Add pricing tab" @click="addTab('4')" />
+            </div>
+            <TabView
+              id="tab-view-closable"
+              v-model:active-key="activeKey"
+              v-model:visible-tabs="VisibleTabs"
+              :tabs="tabs"    
+            />
+          </template>
+
+          <script setup lang="ts">
+          import { TabView } from "@dzangolab/vue3-ui";
+          import { ref } from "vue";
+
+          const activeKey = ref<string>("1");
+          const visibleTabs = ref<string[]>(["1"]);
+
+          const tabs = [
+            { children: "Description", key: "1", label: "Description" },
+            { children: "Reviews", closable: true, key: "2", label: "Reviews" },
+            {
+              children: "Specifications",
+              closable: true,
+              key: "3",
+              label: "Specifications",
+            },
+            { children: "Pricing", closable: true, key: "4", label: "Pricing",  },
+            { children: "Installation Instructions", key: "5", label: "Installation",  },
+            { children: "Certifications", key: "6", label: "Certifications" },
+          ];
+
+          const addTab = (key: string) => {
+            if (!visibleTabs.value.includes(key)) {
+              visibleTabs.value = [...visibleTabs.value, key];
+            }
+
+            activeKey.value = key;
+          };
+          <\/script>
+        `)])),_:1})])]),e[53]||(e[53]=t()),i("section",null,[i("h2",null,s(n.$t("ui.tabView.usage.withSlot")),1),e[33]||(e[33]=t()),i("div",Z,[l(a(c),{id:"tab-view-slot",tabs:I,"visible-tabs":["description","installation","certifications"],"active-key":"description"},{default:r(()=>[(g(),j(U,null,F(I,o=>i("p",{key:o.key},s(o.label),1)),64))]),_:1}),e[32]||(e[32]=t()),l(b,{language:"html-vue"},{default:r(()=>e[31]||(e[31]=[t(`
+          <template>
+            <TabView
+              id="tab-view-slot"
+              :tabs="tabList"    
+              :visible-tabs="['description', 'installation', 'certifications']"
+              active-key="['description']"
+            >
+              <p v-for="tab in tabList" :key="tab.key">{{ tab.label }}</p>
+            </TabView>
+          </template>
+
+          <script setup lang="ts">
+          import { TabView } from "@dzangolab/vue3-ui";
+
+          const tabList = [
+            { key: "description", label: "Description" },
+            { key: "review", label: "Reviews" },
+            { key: "specifications", label: "Specifications" },
+            { key: "pricing", label: "Pricing" },
+            { key: "installation", label: "Installation" },
+            { key: "certifications", label: "Certifications" },
+          ];
+          <\/script>
+        `)])),_:1})])]),e[54]||(e[54]=t()),i("section",null,[i("h2",null,s(n.$t("ui.tabView.usage.interceptTabChange")),1),e[37]||(e[37]=t()),i("div",x,[l(a(c),{id:"tab-view-interception","active-key":v.value,"onUpdate:activeKey":e[6]||(e[6]=o=>v.value=o),tabs:u,"visible-tabs":["1","5","6"],"intercept-tab-change":"",onBeforeTabChange:B},null,8,["active-key"]),e[35]||(e[35]=t()),y.value?(g(),T(a(D),{key:0,"onOn:close":e[7]||(e[7]=o=>y.value=!1),"onOn:confirm":N})):K("",!0),e[36]||(e[36]=t()),l(b,{language:"html-vue"},{default:r(()=>e[34]||(e[34]=[t(`
+          <template>
+            <TabView
+              id="tab-view-interception"
+              v-model:active-key="activeKey"
+              :tabs="tabs"
+              :visible-tabs="['1', '5', '6']"
+              intercept-tab-change
+              @before-tab-change="beforeTabChange"
+            />
+
+            <ConfirmationModal
+              v-if="showConfirmation"
+              @on:close="showConfirmation = false"
+              @on:confirm="onConfirmChange"
+            />
+          </template>
+
+          <script setup lang="ts">
+          import { ConfirmationModal, TabView } from "@dzangolab/vue3-ui";
+          import { ref } from "vue";
+
+          const activeKey = ref<string>("1");
+          const nextTabKey = ref<string>();
+          const showConfirmation = ref<boolean>(false);
+
+          const tabs = [
+            { children: "Description", key: "1", label: "Description" },
+            { children: "Reviews", closable: true, key: "2", label: "Reviews" },
+            {
+              children: "Specifications",
+              closable: true,
+              key: "3",
+              label: "Specifications",
+            },
+            { children: "Pricing", closable: true, key: "4", label: "Pricing",  },
+            { children: "Installation Instructions", key: "5", label: "Installation",  },
+            { children: "Certifications", key: "6", label: "Certifications" },
+          ];
+
+          const beforeTabChange = (key: string) => {
+            showConfirmation.value = true;
+            nextTabKey.value = key;
+          };
+
+          const onConfirmChange = () => {
+            activeKey.value = nextTabKey.value || activeKey.value;
+            showConfirmation.value = false;
+          };
+          <\/script>
+        `)])),_:1})])]),e[55]||(e[55]=t()),i("section",null,[i("h2",null,s(n.$t("ui.tabView.usage.interceptTabClose")),1),e[41]||(e[41]=t()),i("div",_,[l(a(c),{id:"tab-view-interception-close",ref_key:"dzangolabVueTabView",ref:h,tabs:u,"active-key":"1","intercept-tab-close":"",onBeforeTabClose:E},null,512),e[39]||(e[39]=t()),f.value?(g(),T(a(D),{key:0,"onOn:close":e[8]||(e[8]=o=>f.value=!1),"onOn:confirm":L})):K("",!0),e[40]||(e[40]=t()),l(b,{language:"html-vue"},{default:r(()=>e[38]||(e[38]=[t(`
+          <template>
+            <TabView
+              id="tab-view-interception-close"
+              ref="dzangolabVueTabView"
+              :tabs="tabs"
+              active-key="1"
+              intercept-tab-close
+              @before-tab-change="beforeTabClose"
+            />
+
+            <ConfirmationModal
+              v-if="showConfirmation"
+              @on:close="showConfirmation = false"
+              @on:confirm="onConfirmChange"
+            />
+          </template>
+
+          <script setup lang="ts">
+          import { ConfirmationModal, TabView } from "@dzangolab/vue3-ui";
+          import { ref } from "vue";
+
+          const closingKey = ref<string>();
+          const dzangolabVueTabView = ref();
+          const showConfirmation = ref<boolean>(false);
+
+          const tabs = [
+            { children: "Description", key: "1", label: "Description" },
+            { children: "Reviews", closable: true, key: "2", label: "Reviews" },
+            {
+              children: "Specifications",
+              closable: true,
+              key: "3",
+              label: "Specifications",
+            },
+            { children: "Pricing", closable: true, key: "4", label: "Pricing",  },
+            { children: "Installation Instructions", key: "5", label: "Installation",  },
+            { children: "Certifications", key: "6", label: "Certifications" },
+          ];
+
+          const beforeTabClose = (key: string) => {
+            showConfirmation.value = true;
+            closingKey.value = key;
+          };
+
+          const onConfirm = () => {
+            dzangolabVueTabView.value?.closeTab(closingKey.value);
+            showConfirmation.value = false;
+          };
+          <\/script>
+        `)])),_:1})])]),e[56]||(e[56]=t()),i("section",null,[i("h2",null,s(n.$t("common.properties",{value:"TabViewProperties"})),1),e[42]||(e[42]=t()),i("div",ee,[l(a(C),{"columns-data":R,data:z,paginated:!1})])]),e[57]||(e[57]=t()),i("section",null,[i("h2",null,s(n.$t("common.slots")),1),e[43]||(e[43]=t()),i("div",te,[l(a(C),{"columns-data":A,data:[{description:"Content for the tab, matched using the key attribute on each element",id:1,name:"default"}],paginated:!1})])]),e[58]||(e[58]=t()),i("section",null,[i("h2",null,s(n.$t("common.events")),1),e[44]||(e[44]=t()),i("div",ie,[l(a(C),{"columns-data":P,data:$,paginated:!1})])]),e[59]||(e[59]=t()),i("section",null,[i("h2",null,s(n.$t("common.type")),1),e[46]||(e[46]=t()),i("div",ne,[l(b,{language:"html-vue"},{default:r(()=>e[45]||(e[45]=[t(`
+            type Tab = {
+              children?: VNode | string;
+              closable?: boolean;
+              icon?: string;
+              key: string;
+              label: string;
+            };
+          `)])),_:1})])])]),_:1},8,["title"])}}});export{be as default};
