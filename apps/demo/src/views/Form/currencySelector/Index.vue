eb2b36692180d1f72edb78b2c06cc6d8c7c2bad0
KabinKhandThakuri <kabin@dzangolab.com>
2025-06-13 17:44:43 +0545

    Add possible demo for currency picker component (#670)
    * feat: add currency selector component to form component
    
    * feat: add basic demo for currency selector
    
    * chore: update short name to code
    
    * chore: add  space to code section
    
    * chore: update form data
    
    * feat: add support to search currency with label code and symbol
    
    * chore: sort search props
    
    * feat: add multiselect currency selector demo
    
    * feat: add select option label order demo
    
    * feat: add select input option demo to currency picker
    
    * feat: add currency selector custom validation demo
    
    * chore: sort props alphabetically for demo
    
    * test: add currency selector snapshot test

--- apps/demo/src/views/Form/currencySelector/Index.vue
+++ apps/demo/src/views/Form/currencySelector/Index.vue
@@ -41,7 +41,7 @@
           const options = [
             { code: "AUD", label: "Australian Dollar", symbol: "$", value: "AUD" },
             { code: "GBP", label: "British Pound", symbol: "£", value: "GBP" },
-            { code: "EUR", label: "Euro", symbol: "€", value: "EUR" },
+            { code: "EUR", disabled: true, label: "Euro", symbol: "€", value: "EUR" },
             { code: "JPY", label: "Japanese Yen", symbol: "¥", value: "JPY" },
             { code: "USD", label: "US Dollar", symbol: "$", value: "USD" },
           ] as CurrencyOption[];
@@ -59,7 +59,6 @@
           v-model="formData.selectWithSearch"
           :options="options"
           :placeholder="$t('form.placeholder.currency')"
-          :search-placeholder="$t('form.placeholder.search')"
           enable-search
         />
 
@@ -71,7 +70,6 @@
               :options="options"
               enable-search
               placeholder="Select a currency"
-              search-placeholder="Select..."
             /&gt;
           &lt;/template&gt;
 
@@ -86,7 +84,219 @@
           const options = [
             { code: "AUD", label: "Australian Dollar", symbol: "$", value: "AUD" },
             { code: "GBP", label: "British Pound", symbol: "£", value: "GBP" },
-            { code: "EUR", label: "Euro", symbol: "€", value: "EUR" },
+            { code: "EUR", disabled: true, label: "Euro", symbol: "€", value: "EUR" },
+            { code: "JPY", label: "Japanese Yen", symbol: "¥", value: "JPY" },
+            { code: "USD", label: "US Dollar", symbol: "$", value: "USD" },
+          ] as CurrencyOption[];
+          &lt;/script&gt;
+        </SshPre>
+        <!-- eslint-enable -->
+      </div>
+    </section>
+
+    <section>
+      <h2>{{ $t("form.label.selectLabelOrder") }}</h2>
+
+      <div class="section-content">
+        <CurrencySelector
+          v-model="formData.selectWithOrder"
+          :option-label-order="['symbol', 'label', 'code']"
+          :options="options"
+          :placeholder="$t('form.placeholder.currency')"
+          enable-search
+        />
+
+        <!-- eslint-disable -->
+        <SshPre language="html-vue">
+          &lt;template&gt;
+            &lt;CurrencySelector
+              v-model="input"
+              :option-label-order="['symbol', 'label', 'code']"
+              :options="options"
+              enable-search
+              placeholder="Select a currency"
+            /&gt;
+          &lt;/template&gt;
+
+          &lt;script setup lang="ts"&gt;
+          import { CurrencySelector } from "@dzangolab/vue3-form";
+          import { ref } from "vue";
+
+          import type { CurrencyOption } from "@dzangolab/vue3-form";
+
+          const input = ref();
+
+          const options = [
+            { code: "AUD", label: "Australian Dollar", symbol: "$", value: "AUD" },
+            { code: "GBP", label: "British Pound", symbol: "£", value: "GBP" },
+            { code: "EUR", disabled: true, label: "Euro", symbol: "€", value: "EUR" },
+            { code: "JPY", label: "Japanese Yen", symbol: "¥", value: "JPY" },
+            { code: "USD", label: "US Dollar", symbol: "$", value: "USD" },
+          ] as CurrencyOption[];
+          &lt;/script&gt;
+        </SshPre>
+        <!-- eslint-enable -->
+      </div>
+    </section>
+
+    <section>
+      <h2>{{ $t("form.label.multiselect") }}</h2>
+
+      <div class="section-content">
+        <CurrencySelector
+          v-model="formData.multiselect"
+          :options="options"
+          :placeholder="$t('form.placeholder.currency')"
+          enable-search
+          multiple
+        />
+
+        <!-- eslint-disable -->
+        <SshPre language="html-vue">
+          &lt;template&gt;
+            &lt;CurrencySelector
+              v-model="input"
+              :options="options"
+              enable-search
+              multiple
+              placeholder="Select a currency"
+            /&gt;
+          &lt;/template&gt;
+
+          &lt;script setup lang="ts"&gt;
+          import { CurrencySelector } from "@dzangolab/vue3-form";
+          import { ref } from "vue";
+
+          import type { CurrencyOption } from "@dzangolab/vue3-form";
+
+          const input = ref();
+
+          const options = [
+            { code: "AUD", label: "Australian Dollar", symbol: "$", value: "AUD" },
+            { code: "GBP", label: "British Pound", symbol: "£", value: "GBP" },
+            { code: "EUR", disabled: true, label: "Euro", symbol: "€", value: "EUR" },
+            { code: "JPY", label: "Japanese Yen", symbol: "¥", value: "JPY" },
+            { code: "USD", label: "US Dollar", symbol: "$", value: "USD" },
+          ] as CurrencyOption[];
+          &lt;/script&gt;
+        </SshPre>
+        <!-- eslint-enable -->
+      </div>
+    </section>
+
+    <section>
+      <h2>{{ $t("form.label.withSelectOptions") }}</h2>
+
+      <div class="section-content">
+        <Form>
+          <CurrencySelector
+            v-model="formData.selectOptionsInput"
+            :options="options"
+            :placeholder="$t('form.placeholder.currency')"
+            :selection-options="{
+              hasSortedOptions: false,
+              maxSelection: 3,
+              minSelection: 2,
+              searchPlaceholder: $t('form.placeholder.search'),
+              showRemoveSelection: true,
+            }"
+            enable-search
+            multiple
+          />
+        </Form>
+
+        <!-- eslint-disable -->
+        <SshPre language="html-vue">
+          &lt;template&gt;
+            &lt;Form&gt;
+              &lt;CurrencySelector
+                v-model="input"
+                :options="options"
+                :selection-options="{
+                  hasSortedOptions: false,
+                  maxSelection: 3,
+                  minSelection: 2,
+                  searchPlaceholder: $t('form.placeholder.search'),
+                  showRemoveSelection: true,
+                }"
+                enable-search
+                multiple
+                placeholder="Select a currency"
+              /&gt;
+            &lt;/Form&gt;
+          &lt;/template&gt;
+
+          &lt;script setup lang="ts"&gt;
+          import { CurrencySelector, Form } from "@dzangolab/vue3-form";
+          import { ref } from "vue";
+
+          import type { CurrencyOption } from "@dzangolab/vue3-form";
+
+          const input = ref();
+
+          const options = [
+            { code: "AUD", label: "Australian Dollar", symbol: "$", value: "AUD" },
+            { code: "GBP", label: "British Pound", symbol: "£", value: "GBP" },
+            { code: "EUR", disabled: true, label: "Euro", symbol: "€", value: "EUR" },
+            { code: "JPY", label: "Japanese Yen", symbol: "¥", value: "JPY" },
+            { code: "USD", label: "US Dollar", symbol: "$", value: "USD" },
+          ] as CurrencyOption[];
+          &lt;/script&gt;
+        </SshPre>
+        <!-- eslint-enable -->
+      </div>
+    </section>
+
+    <section>
+      <h2>{{ $t("form.label.customValidationInput") }}</h2>
+
+      <div class="section-content">
+        <Form>
+          <CurrencySelector
+            v-model="formData.validationInput"
+            :options="options"
+            :placeholder="$t('form.placeholder.currency')"
+            :schema="inputSchema"
+            enable-search
+            multiple
+          />
+        </Form>
+
+        <!-- eslint-disable -->
+        <SshPre language="html-vue">
+          &lt;template&gt;
+            &lt;Form&gt;
+              &lt;CurrencySelector
+                v-model="input"
+                :options="options"
+                :schema="inputSchema"
+                enable-search
+                multiple
+                placeholder="Select a currency"
+              /&gt;
+            &lt;/Form&gt;
+          &lt;/template&gt;
+
+          &lt;script setup lang="ts"&gt;
+          import { CurrencySelector, Form } from "@dzangolab/vue3-form";
+          import { ref } from "vue";
+          import { z } from "zod";
+
+          import type { CurrencyOption } from "@dzangolab/vue3-form";
+
+          const input = ref();
+
+          const inputSchema = z.preprocess(
+            (value) => (value === null || value === undefined ? [] : value),
+            z
+              .array(z.string())
+              .min(1, { message: "Please select at least one valid option" })
+          );
+
+          const options = [
+            { code: "AUD", label: "Australian Dollar", symbol: "$", value: "AUD" },
+            { code: "GBP", label: "British Pound", symbol: "£", value: "GBP" },
+            { code: "EUR", disabled: true, label: "Euro", symbol: "€", value: "EUR" },
             { code: "JPY", label: "Japanese Yen", symbol: "¥", value: "JPY" },
             { code: "USD", label: "US Dollar", symbol: "$", value: "USD" },
           ] as CurrencyOption[];
@@ -105,23 +315,38 @@ export default {
 </script>
 
 <script setup lang="ts">
-import { CurrencySelector } from "@dzangolab/vue3-form";
+import { CurrencySelector, Form } from "@dzangolab/vue3-form";
+import { useI18n } from "@dzangolab/vue3-i18n";
 import { ButtonElement } from "@dzangolab/vue3-ui";
 import { reactive } from "vue";
+import { z } from "zod";
 
 import FormPage from "../FormPage.vue";
 
 import type { CurrencyOption } from "@dzangolab/vue3-form";
 
+const { t } = useI18n();
+
 const formData = reactive({
   basic: undefined,
+  multiselect: undefined,
+  selectOptionsInput: undefined,
+  selectWithOrder: undefined,
   selectWithSearch: undefined,
+  validationInput: undefined,
 });
+
+const inputSchema = z.preprocess(
+  (value) => (value === null || value === undefined ? [] : value),
+  z
+    .array(z.string())
+    .min(1, { message: t("form.errors.currencySelector.invalid") }),
+);
 
 const options = [
   { code: "AUD", label: "Australian Dollar", symbol: "$", value: "AUD" },
   { code: "GBP", label: "British Pound", symbol: "£", value: "GBP" },
-  { code: "EUR", label: "Euro", symbol: "€", value: "EUR" },
+  { code: "EUR", disabled: true, label: "Euro", symbol: "€", value: "EUR" },
   { code: "JPY", label: "Japanese Yen", symbol: "¥", value: "JPY" },
   { code: "USD", label: "US Dollar", symbol: "$", value: "USD" },
 ] as CurrencyOption[];
--- packages/vue-form/src/components/Select.vue
+++ packages/vue-form/src/components/Select.vue
@@ -66,6 +66,7 @@
         v-if="enableSearch || enableCustomSearch"
         v-model="searchInput"
         :placeholder="searchPlaceholder"
+        class="multiselect-search"
         @update:model-value="$emit('update:searchInput', $event)"
       />
 
@@ -237,8 +238,12 @@ const sortedOptions = computed(() => {
   return filteredOptions.value;
 });
 
-const getSelectedOption = (value: number | string | boolean) =>
-  options.value?.find((option) => option.value === value);
+const getSelectedOption = (value: number | string | boolean) => {
+  return (
+    options.value?.find((option) => option.value === value) ||
+    selectedOptions.value.find((option) => option.value === value)
+  );
+};
 
 const isAllSelected = (options: SelectOption[]): boolean => {
   if (selectedOptions.value.length != activeOptions.value.length) {
@@ -250,10 +255,11 @@ const isAllSelected = (options: SelectOption[]): boolean => {
   );
 };
 
-const isSelected = (option: SelectOption): boolean =>
-  selectedOptions.value.some(
+const isSelected = (option: SelectOption): boolean => {
+  return selectedOptions.value.some(
     (selectedOption) => selectedOption.value === option.value,
   );
+};
 
 const nextIndex = (startIndex: number): number => {
   const total = sortedOptions.value.length;
