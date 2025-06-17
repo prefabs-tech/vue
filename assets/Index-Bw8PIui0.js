2d756684ff58985ffe807504c8b0e5d9135f2e15
premsgr <32644681+premsgr@users.noreply.github.com>
2025-06-17 04:19:03 +0000

    Deploying to gh-pages from @ dzangolab/vue@ab00735975e033db34a083cea19ef4cf8570aa3b 🚀

---
+++ assets/Index-Bw8PIui0.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./TablePage.vue_vue_type_style_index_0_lang-izphQfj0.js";import{d as o,f as a,o as n}from"./index-ozhbLx6f.js";const _={name:"TableDemo"},m=o({..._,setup(l){return(e,r)=>(n(),a(t,{title:e.$t("table.title")},null,8,["title"]))}});export{m as default};
--- assets/Index-DpUJst10.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./UserPage.vue_vue_type_style_index_0_lang-CVDQ730i.js";import{d as o,f as r,o as s}from"./index-CN0dyNvH.js";const a={name:"User"},f=o({...a,setup(n){return(e,_)=>(s(),r(t,{title:e.$t("user.title")},null,8,["title"]))}});export{f as default};
