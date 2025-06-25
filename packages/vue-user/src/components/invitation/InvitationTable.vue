4b5facd8fcb59ebe57975aec0c916d71dc0afb85
KabinKhandThakuri <kabin@dzangolab.com>
2025-06-25 14:57:09 +0545

    Update app  column to show app name instead of id for invitations table (#685)
    * refactor: update form action for confirm cancel left to right alignment
    
    * chore: sort css classes order
    
    * refactor: update invitation table to show app name instead of id
    
    * chore: update app name map logic

--- packages/vue-user/src/components/invitation/InvitationTable.vue
+++ packages/vue-user/src/components/invitation/InvitationTable.vue
@@ -154,7 +154,7 @@ const defaultColumns: TableColumnDefinition<Invitation>[] = [
     align: "center",
     accessorKey: "app",
     header: t("user.invitation.table.defaultColumns.app"),
-    cell: ({ row }) => row.original.appId || "—",
+    cell: ({ row }) => appNameMap.value?.get(row.original.appId) || "-",
   },
   {
     accessorKey: "role",
@@ -267,6 +267,12 @@ const actionMenuData = computed(() => [
     requireConfirmationModal: true,
   },
 ]);
+
+const appNameMap = computed(() => {
+  const apps = props.apps ?? [];
+
+  return new Map(apps.map((app) => [app.id, app.name]));
+});
 
 const mergedColumns = computed(() => [
   ...defaultColumns.map((defaultColumn) => {
