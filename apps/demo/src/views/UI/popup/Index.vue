7666067334a67da35c48aee6cf3040e5f00ed424
KabinKhandThakuri <kabin@dzangolab.com>
2025-07-03 13:28:05 +0545

    Update popup demo with props, events and slots table (#697)
    * refactor: update popup demo with props event and slot table
    
    * chore: sort data alphabetically
    
    * refactor: update documentation and related css

--- apps/demo/src/views/UI/popup/Index.vue
+++ apps/demo/src/views/UI/popup/Index.vue
@@ -1,5 +1,9 @@
 <template>
-  <UiPage :title="$t('ui.popup.title')" class="demo">
+  <UiPage
+    :sub-title="$t('ui.popup.subtitle')"
+    :title="$t('ui.popup.title')"
+    class="demo"
+  >
     <template #toolbar>
       <ButtonElement
         :label="$t('common.back')"
@@ -89,11 +93,14 @@
     </section>
 
     <section>
-      <h2>{{ $t("ui.tooltip.usage.top") }}</h2>
+      <h2>{{ $t("ui.popup.usage.top") }}</h2>
 
       <div class="section-content">
         <Popup position="top">
-          <ButtonElement :label="$t('ui.popup.usage.top')" severity="success" />
+          <ButtonElement
+            :label="$t('ui.popup.label.show')"
+            severity="success"
+          />
           <template #content>
             {{ $t("ui.popup.content") }}
           </template>
@@ -104,7 +111,7 @@
           &lt;template&gt;
             &lt;Popup position="top"&gt;
               &lt;ButtonElement
-                :label="Top"
+                :label="Show"
                 severity="success"
               /&gt;
               &lt;template #content&gt;
@@ -122,12 +129,12 @@
     </section>
 
     <section>
-      <h2>{{ $t("ui.tooltip.usage.bottom") }}</h2>
+      <h2>{{ $t("ui.popup.usage.bottom") }}</h2>
 
       <div class="section-content">
         <Popup position="bottom">
           <ButtonElement
-            :label="$t('ui.popup.usage.bottom')"
+            :label="$t('ui.popup.label.show')"
             severity="success"
           />
           <template #content>
@@ -140,7 +147,7 @@
           &lt;template&gt;
             &lt;Popup position="bottom"&gt;
               &lt;ButtonElement
-                :label="Bottom"
+                :label="Show"
                 severity="success"
               /&gt;
               &lt;template #content&gt;
@@ -158,12 +165,12 @@
     </section>
 
     <section>
-      <h2>{{ $t("ui.tooltip.usage.left") }}</h2>
+      <h2>{{ $t("ui.popup.usage.left") }}</h2>
 
       <div class="section-content">
         <Popup position="left">
           <ButtonElement
-            :label="$t('ui.popup.usage.left')"
+            :label="$t('ui.popup.label.show')"
             severity="success"
           />
           <template #content>
@@ -176,7 +183,7 @@
           &lt;template&gt;
             &lt;Popup position="left"&gt;
               &lt;ButtonElement
-                :label="Left"
+                :label="Show"
                 severity="success"
               /&gt;
               &lt;template #content&gt;
@@ -194,12 +201,12 @@
     </section>
 
     <section>
-      <h2>{{ $t("ui.tooltip.usage.right") }}</h2>
+      <h2>{{ $t("ui.popup.usage.right") }}</h2>
 
       <div class="section-content">
         <Popup position="right">
           <ButtonElement
-            :label="$t('ui.popup.usage.right')"
+            :label="$t('ui.popup.label.show')"
             severity="success"
           />
           <template #content>
@@ -212,7 +219,7 @@
           &lt;template&gt;
             &lt;Popup position="right"&gt;
               &lt;ButtonElement
-                :label="Right"
+                :label="Show"
                 severity="success"
               /&gt;
               &lt;template #content&gt;
@@ -228,11 +235,61 @@
         <!-- eslint-enable -->
       </div>
     </section>
+
+    <ComponentDocumentation
+      :events-data="eventsData"
+      :props-data="propsData"
+      :props-table-title="$t('common.properties', { value: 'PopupProperties' })"
+      :slots-data="slotsData"
+    />
   </UiPage>
 </template>
 
 <script setup lang="ts">
 import { ButtonElement, Popup } from "@dzangolab/vue3-ui";
 
+import ComponentDocumentation from "../../../components/ComponentDocumentation.vue";
 import UiPage from "../UiPage.vue";
+
+const eventsData = [
+  {
+    description: "Emitted when the user clicks outside the popup.",
+    name: "onClickOutside",
+    payload: "-",
+  },
+];
+
+const propsData = [
+  {
+    default: `"popup"`,
+    description: "Aria label for accessibility, used on the trigger element.",
+    prop: "ariaLabel",
+    type: "String",
+  },
+  {
+    default: `10`,
+    description:
+      "Spacing in pixels between the trigger element and popup content.",
+    prop: "offset",
+    type: "Number",
+  },
+  {
+    default: `-`,
+    description:
+      "Supported position of the popup (`top`, `bottom`, `left`, or `right`).",
+    prop: "position",
+    type: "String",
+  },
+];
+
+const slotsData = [
+  {
+    description: "Content to be shown in the popup.",
+    name: "content",
+  },
+  {
+    description: "The trigger element that opens the popup.",
+    name: "default",
+  },
+];
 </script>
