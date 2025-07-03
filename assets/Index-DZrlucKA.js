84ee7e95dafb5e9f511b39323180d8ab0008abf1
DeepakAryal <deepakaryal2050@gmail.com>
2025-07-03 11:39:00 +0000

    Deploying to gh-pages from @ dzangolab/vue@938314d0cfca957f5e6b8cb8d29132755c51ef23 🚀

---
+++ assets/Index-DZrlucKA.js
@@ -0,0 +1,203 @@
+import{k as d,S as b}from"./DzangolabVue3Form.es-ByH_DYOt.js";import{d as y,J as P,M as D,L as c,I as R,e as U,o as S,i as u,E as l,f as o,t as m,g as r,u as s,x as g}from"./index-pg879in7.js";import{c as A}from"./data-uYwV_J55.js";import{_ as $}from"./FormPage.vue_vue_type_style_index_0_lang-CcX4MMZ5.js";const C={class:"section-content"},k={class:"section-content"},Z={class:"section-content"},O={class:"section-content"},V={class:"section-content"},B={class:"section-content"},N={name:"CurrencyPickerDemo"},G=y({...N,setup(E){const{t:v}=P(),t=D({basic:void 0,extensivePicker:void 0,multiselect:void 0,selectOptionsInput:void 0,selectWithOrder:void 0,validationInput:void 0}),f=c.preprocess(n=>n??[],c.array(c.string()).min(1,{message:v("form.errors.currencyPicker.invalid")})),p=[{code:"AUD",label:"Australian Dollar",symbol:"$",value:"AUD"},{code:"GBP",label:"British Pound",symbol:"£",value:"GBP"},{code:"EUR",disabled:!0,label:"Euro",symbol:"€",value:"EUR"},{code:"JPY",label:"Japanese Yen",symbol:"¥",value:"JPY"},{code:"USD",label:"US Dollar",symbol:"$",value:"USD"},{code:"DZD",label:"Algerian dinar",value:"DZD"},{code:"ZAR",label:"South African Rand",value:"ZAR"},{code:"NPR",label:"Nepalese rupee",symbol:"Rs",value:"NPR"}];return(n,e)=>{const i=R("SshPre");return S(),U($,{title:n.$t("form.label.currencyPicker"),class:"demo"},{toolbar:u(()=>[r(s(g),{label:n.$t("common.back"),"icon-left":"pi pi-chevron-left",size:"medium",variant:"textOnly",onClick:e[0]||(e[0]=a=>n.$router.push("/form"))},null,8,["label"])]),default:u(()=>[e[25]||(e[25]=l()),o("section",null,[o("h2",null,m(n.$t("form.label.basicInput")),1),e[9]||(e[9]=l()),o("div",C,[r(s(d),{modelValue:t.basic,"onUpdate:modelValue":e[1]||(e[1]=a=>t.basic=a),options:p,placeholder:n.$t("form.placeholder.currency")},null,8,["modelValue","placeholder"]),e[8]||(e[8]=l()),r(i,{language:"html-vue"},{default:u(()=>e[7]||(e[7]=[l(`
+          <template>
+            <CurrencyPicker
+              v-model="input"
+              :options="options"
+              placeholder="Select a currency"
+            />
+          </template>
+
+          <script setup lang="ts">
+          import { CurrencyPicker } from "@dzangolab/vue3-form";
+          import { ref } from "vue";
+
+          import type { CurrencyOption } from "@dzangolab/vue3-form";
+
+          const input = ref();
+
+          const options = [
+            { code: "AUD", label: "Australian Dollar", symbol: "$", value: "AUD" },
+            { code: "GBP", label: "British Pound", symbol: "£", value: "GBP" },
+            { code: "EUR", disabled: true, label: "Euro", symbol: "€", value: "EUR" },
+            { code: "JPY", label: "Japanese Yen", symbol: "¥", value: "JPY" },
+            { code: "USD", label: "US Dollar", symbol: "$", value: "USD" },
+            { code: "DZD", label: "Algerian dinar", value: "DZD" },
+            {
+              code: "ZAR",
+              label: "South African Rand",
+              value: "ZAR",
+            },
+            { code: "NPR", label: "Nepalese rupee", symbol: "Rs", value: "NPR" },
+          ] as CurrencyOption[];
+          <\/script>
+        `)])),_:1})])]),e[26]||(e[26]=l()),o("section",null,[o("h2",null,m(n.$t("form.label.selectLabelOrder")),1),e[12]||(e[12]=l()),o("div",k,[r(s(d),{modelValue:t.selectWithOrder,"onUpdate:modelValue":e[2]||(e[2]=a=>t.selectWithOrder=a),"option-label-order":["symbol","label","code"],options:p,placeholder:n.$t("form.placeholder.currency")},null,8,["modelValue","placeholder"]),e[11]||(e[11]=l()),r(i,{language:"html-vue"},{default:u(()=>e[10]||(e[10]=[l(`
+          <template>
+            <CurrencyPicker
+              v-model="input"
+              :option-label-order="['symbol', 'label', 'code']"
+              :options="options"
+              placeholder="Select a currency"
+            />
+          </template>
+
+          <script setup lang="ts">
+          import { CurrencyPicker } from "@dzangolab/vue3-form";
+          import { ref } from "vue";
+
+          import type { CurrencyOption } from "@dzangolab/vue3-form";
+
+          const input = ref();
+
+          const options = [
+            { code: "AUD", label: "Australian Dollar", symbol: "$", value: "AUD" },
+            { code: "GBP", label: "British Pound", symbol: "£", value: "GBP" },
+            { code: "EUR", disabled: true, label: "Euro", symbol: "€", value: "EUR" },
+            { code: "JPY", label: "Japanese Yen", symbol: "¥", value: "JPY" },
+            { code: "USD", label: "US Dollar", symbol: "$", value: "USD" },
+            { code: "DZD", label: "Algerian dinar", value: "DZD" },
+            {
+              code: "ZAR",
+              label: "South African Rand",
+              value: "ZAR",
+            },
+            { code: "NPR", label: "Nepalese rupee", symbol: "Rs", value: "NPR" },
+          ] as CurrencyOption[];
+          <\/script>
+        `)])),_:1})])]),e[27]||(e[27]=l()),o("section",null,[o("h2",null,m(n.$t("form.label.currencyPickerExtensive")),1),e[15]||(e[15]=l()),o("div",Z,[r(s(d),{modelValue:t.extensivePicker,"onUpdate:modelValue":e[3]||(e[3]=a=>t.extensivePicker=a),options:s(A),placeholder:n.$t("form.placeholder.currency")},null,8,["modelValue","options","placeholder"]),e[14]||(e[14]=l()),r(i,{language:"html-vue"},{default:u(()=>e[13]||(e[13]=[l(`
+          <template>
+            <CurrencyPicker
+              v-model="input"
+              :options="currencies"
+              placeholder="Select a currency"
+            />
+          </template>
+
+          <script setup lang="ts">
+          import { CurrencyPicker } from "@dzangolab/vue3-form";
+          import { ref } from "vue";
+
+          import { currencies } from "../data";
+
+          import type { CurrencyOption } from "@dzangolab/vue3-form";
+
+          const input = ref();
+          <\/script>
+        `)])),_:1})])]),e[28]||(e[28]=l()),o("section",null,[o("h2",null,m(n.$t("form.label.multiselect")),1),e[18]||(e[18]=l()),o("div",O,[r(s(d),{modelValue:t.multiselect,"onUpdate:modelValue":e[4]||(e[4]=a=>t.multiselect=a),options:p,placeholder:n.$t("form.placeholder.currency"),multiple:""},null,8,["modelValue","placeholder"]),e[17]||(e[17]=l()),r(i,{language:"html-vue"},{default:u(()=>e[16]||(e[16]=[l(`
+          <template>
+            <CurrencyPicker
+              v-model="input"
+              :options="options"
+              multiple
+              placeholder="Select a currency"
+            />
+          </template>
+
+          <script setup lang="ts">
+          import { CurrencyPicker } from "@dzangolab/vue3-form";
+          import { ref } from "vue";
+
+          import type { CurrencyOption } from "@dzangolab/vue3-form";
+
+          const input = ref();
+
+          const options = [
+            { code: "AUD", label: "Australian Dollar", symbol: "$", value: "AUD" },
+            { code: "GBP", label: "British Pound", symbol: "£", value: "GBP" },
+            { code: "EUR", disabled: true, label: "Euro", symbol: "€", value: "EUR" },
+            { code: "JPY", label: "Japanese Yen", symbol: "¥", value: "JPY" },
+            { code: "USD", label: "US Dollar", symbol: "$", value: "USD" },
+            { code: "DZD", label: "Algerian dinar", value: "DZD" },
+            {
+              code: "ZAR",
+              label: "South African Rand",
+              value: "ZAR",
+            },
+            { code: "NPR", label: "Nepalese rupee", symbol: "Rs", value: "NPR" },
+            ] as CurrencyOption[];
+          <\/script>
+        `)])),_:1})])]),e[29]||(e[29]=l()),o("section",null,[o("h2",null,m(n.$t("form.label.withSelectOptions")),1),e[21]||(e[21]=l()),o("div",V,[r(s(b),null,{default:u(()=>[r(s(d),{modelValue:t.selectOptionsInput,"onUpdate:modelValue":e[5]||(e[5]=a=>t.selectOptionsInput=a),options:p,placeholder:n.$t("form.placeholder.currency"),"selection-options":{hasSortedOptions:!1,maxSelection:3,minSelection:2,showRemoveSelection:!0},multiple:""},null,8,["modelValue","placeholder"])]),_:1}),e[20]||(e[20]=l()),r(i,{language:"html-vue"},{default:u(()=>e[19]||(e[19]=[l(`
+          <template>
+            <Form>
+              <CurrencyPicker
+                v-model="input"
+                :options="options"
+                :selection-options="{
+                  hasSortedOptions: false,
+                  maxSelection: 3,
+                  minSelection: 2,
+                  showRemoveSelection: true,
+                }"
+                multiple
+                placeholder="Select a currency"
+              />
+            </Form>
+          </template>
+
+          <script setup lang="ts">
+          import { CurrencyPicker, Form } from "@dzangolab/vue3-form";
+          import { ref } from "vue";
+
+          import type { CurrencyOption } from "@dzangolab/vue3-form";
+
+          const input = ref();
+
+          const options = [
+            { code: "AUD", label: "Australian Dollar", symbol: "$", value: "AUD" },
+            { code: "GBP", label: "British Pound", symbol: "£", value: "GBP" },
+            { code: "EUR", disabled: true, label: "Euro", symbol: "€", value: "EUR" },
+            { code: "JPY", label: "Japanese Yen", symbol: "¥", value: "JPY" },
+            { code: "USD", label: "US Dollar", symbol: "$", value: "USD" },
+            { code: "DZD", label: "Algerian dinar", value: "DZD" },
+            {
+              code: "ZAR",
+              label: "South African Rand",
+              value: "ZAR",
+            },
+            { code: "NPR", label: "Nepalese rupee", symbol: "Rs", value: "NPR" },
+          ] as CurrencyOption[];
+          <\/script>
+        `)])),_:1})])]),e[30]||(e[30]=l()),o("section",null,[o("h2",null,m(n.$t("form.label.customValidationInput")),1),e[24]||(e[24]=l()),o("div",B,[r(s(b),null,{default:u(()=>[r(s(d),{modelValue:t.validationInput,"onUpdate:modelValue":e[6]||(e[6]=a=>t.validationInput=a),options:p,placeholder:n.$t("form.placeholder.currency"),schema:s(f),multiple:""},null,8,["modelValue","placeholder","schema"])]),_:1}),e[23]||(e[23]=l()),r(i,{language:"html-vue"},{default:u(()=>e[22]||(e[22]=[l(`
+          <template>
+            <Form>
+              <CurrencyPicker
+                v-model="input"
+                :options="options"
+                :schema="inputSchema"
+                multiple
+                placeholder="Select a currency"
+              />
+            </Form>
+          </template>
+
+          <script setup lang="ts">
+          import { CurrencyPicker, Form } from "@dzangolab/vue3-form";
+          import { ref } from "vue";
+          import { z } from "zod";
+
+          import type { CurrencyOption } from "@dzangolab/vue3-form";
+
+          const input = ref();
+
+          const inputSchema = z.preprocess(
+            (value) => (value === null || value === undefined ? [] : value),
+            z
+              .array(z.string())
+              .min(1, { message: "Please select at least one valid option" })
+          );
+
+          const options = [
+            { code: "AUD", label: "Australian Dollar", symbol: "$", value: "AUD" },
+            { code: "GBP", label: "British Pound", symbol: "£", value: "GBP" },
+            { code: "EUR", disabled: true, label: "Euro", symbol: "€", value: "EUR" },
+            { code: "JPY", label: "Japanese Yen", symbol: "¥", value: "JPY" },
+            { code: "USD", label: "US Dollar", symbol: "$", value: "USD" },
+            { code: "DZD", label: "Algerian dinar", value: "DZD" },
+            {
+              code: "ZAR",
+              label: "South African Rand",
+              value: "ZAR",
+            },
+            { code: "NPR", label: "Nepalese rupee", symbol: "Rs", value: "NPR" },
+          ] as CurrencyOption[];
+          <\/script>
+        `)])),_:1})])])]),_:1},8,["title"])}}});export{G as default};
