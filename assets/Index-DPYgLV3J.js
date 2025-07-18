738b83d2d662df89c6411dece8cc2071cfb541e4
DeepakAryal <deepakaryal2050@gmail.com>
2025-07-18 12:07:53 +0000

    Deploying to gh-pages from @ dzangolab/vue@824cbcb7d9945777ce261e881541d27ca4e979af 🚀

--- assets/Index-DPYgLV3J.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./UserPage.vue_vue_type_style_index_0_lang-D36tPu29.js";import{d as o,e as r,o as s}from"./index-BfHf9_CU.js";const a={name:"User"},i=o({...a,setup(n){return(e,_)=>(s(),r(t,{title:e.$t("user.title")},null,8,["title"]))}});export{i as default};
