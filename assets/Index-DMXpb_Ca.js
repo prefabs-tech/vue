3551168698457dc99473ccc562d1c2c2c34dd245
DeepakAryal <deepakaryal2050@gmail.com>
2025-08-04 07:50:31 +0000

    Deploying to gh-pages from @ prefabs-tech/vue@df44853e3731b132838d32d6f6a0e611c6e79b1d 🚀

--- assets/Index-DMXpb_Ca.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./LayoutPage.vue_vue_type_style_index_0_lang-D7HkxV6M.js";import{d as o,e as a,o as n}from"./index-DR6bHbim.js";const _={name:"LayoutDemo"},m=o({..._,setup(r){return(t,s)=>(n(),a(e,{title:t.$t("layout.title")},null,8,["title"]))}});export{m as default};
