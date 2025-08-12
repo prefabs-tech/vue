ae8024f375bbc74a209c5f416f33f2f9d0874084
premsgr <32644681+premsgr@users.noreply.github.com>
2025-08-12 05:36:16 +0000

    Deploying to gh-pages from @ prefabs-tech/vue@d0d8995c9b8556f618dd5f39aaf244510ae0119d 🚀

---
+++ assets/Index-B6QILEb3.js
@@ -0,0 +1,41 @@
+import{d as g,H as C,e as f,o as m,j as s,B as t,f as r,h as n,t as d,u as a,P as L,a as k,F as b,y as v,N as $,v as y}from"./index-Bv0414Gs.js";import{_ as B}from"./ComponentDocumentation.vue_vue_type_script_setup_true_lang-B4k_v8r4.js";import{_}from"./UiPage.vue_vue_type_style_index_0_lang-DNdHPWS4.js";import"./PrefabsTechVue3TanstackTable.es-B3zZpVTw.js";import"./PrefabsTechVue3Form.es-Bt4TCb5D.js";const N={class:"section-content"},F=g({__name:"Index",setup(x){const l=[{name:"Card 1",description:"Lorem ipsum dolor sit amet, consectetur adipiscing"},{name:"Card 2",description:"Lorem ipsum dolor sit amet, consectetur adipiscing"},{name:"Card 3",description:"Lorem ipsum dolor sit amet, consectetur adipiscing"},{name:"Card 4",description:"Lorem ipsum dolor sit amet, consectetur adipiscing"},{name:"Card 5",description:"Lorem ipsum dolor sit amet, consectetur adipiscing"}],p=[{name:"default",description:"Default slot to render grid items inside the container."}];return(i,e)=>{const u=C("SshPre");return m(),f(_,{"sub-title":i.$t("ui.gridContainer.subtitle"),title:i.$t("ui.gridContainer.title"),class:"demo"},{toolbar:s(()=>[n(a(y),{label:i.$t("common.back"),"icon-left":"pi pi-chevron-left",size:"medium",variant:"textOnly",onClick:e[0]||(e[0]=o=>i.$router.push("/ui"))},null,8,["label"])]),default:s(()=>[e[4]||(e[4]=t()),r("section",null,[r("h2",null,d(i.$t("ui.gridContainer.usage.basic")),1),e[3]||(e[3]=t()),r("div",N,[n(a(L),null,{default:s(()=>[(m(),k(b,null,v(l,(o,c)=>n(a($),{key:c+"-"+o.name,title:o.name},{default:s(()=>[t(d(o.description),1)]),_:2},1032,["title"])),64))]),_:1}),e[2]||(e[2]=t()),n(u,{language:"html-vue"},{default:s(()=>e[1]||(e[1]=[t(`
+          <template>
+            <GridContainer>
+              <Card
+                v-for="(menu, index) in packages"
+                :key="index + '-' + menu.name"
+                :title="menu.name"
+              >
+                {{ menu.description }}
+              </Card>
+            </GridContainer>
+          </template>
+
+          <script setup lang="ts">
+          import { Card, GridContainer } from "@prefabs.tech/vue3-ui";
+
+          const packages = [
+            {
+              name: "Card 1",
+              description: "Lorem ipsum dolor sit amet, consectetur adipiscing",
+            },
+            {
+              name: "Card 2",
+              description: "Lorem ipsum dolor sit amet, consectetur adipiscing",
+            },
+            {
+              name: "Card 3",
+              description: "Lorem ipsum dolor sit amet, consectetur adipiscing",
+            },
+
+            {
+              name: "Card 4",
+              description: "Lorem ipsum dolor sit amet, consectetur adipiscing",
+            },
+            {
+              name: "Card 5",
+              description: "Lorem ipsum dolor sit amet, consectetur adipiscing",
+            },
+          ];
+          <\/script>
+        `)])),_:1})])]),e[5]||(e[5]=t()),n(B,{"slots-data":p})]),_:1},8,["sub-title","title"])}}});export{F as default};
---
+++ assets/Index-COhgy2gn.js
@@ -0,0 +1,121 @@
+import{T as i,$ as f}from"./PrefabsTechVue3Form.es-Bt4TCb5D.js";import{d as V,E as N,I as $,L as E,r as p,H as F,e as O,o as z,j as m,B as n,f as o,t as d,h as r,u as l,v as w}from"./index-Bv0414Gs.js";import{_ as U}from"./FormPage.vue_vue_type_style_index_0_lang-CKGhBJSI.js";const y={class:"section-content"},M={class:"section-content"},S={class:"section-content"},h={class:"section-content"},k={class:"section-content"},q={class:"section-content"},B={class:"section-content"},C={name:"NumberInputField"},D=V({...C,setup(L){const{t:b}=N(),I=$.coerce.number({invalid_type_error:b("form.errors.number.invalid")}).gte(1,b("form.errors.number.invalid")).lte(100,b("form.errors.number.invalid")),g={invalid:b("form.errors.number.invalid"),required:b("form.errors.number.required")},v={min:1,max:100,allow_leading_zeroes:!1};let u=E({customValidationInput:p(),disabled:p(100),i18nInput:p(),input:p(),inputWithValidation:p(),noLabelInput:p(),textInput:p()});return(t,e)=>{const s=F("SshPre");return z(),O(U,{title:t.$t("form.label.number"),class:"demo"},{toolbar:m(()=>[r(l(w),{label:t.$t("common.back"),"icon-left":"pi pi-chevron-left",size:"medium",variant:"textOnly",onClick:e[0]||(e[0]=a=>t.$router.push("/form"))},null,8,["label"])]),default:m(()=>[e[29]||(e[29]=n()),o("section",null,[o("h2",null,d(t.$t("form.label.basicInput")),1),e[10]||(e[10]=n()),o("div",y,[r(l(i),{modelValue:l(u).noLabelInput,"onUpdate:modelValue":e[1]||(e[1]=a=>l(u).noLabelInput=a),placeholder:t.$t("form.placeholder.number")},null,8,["modelValue","placeholder"]),e[9]||(e[9]=n()),r(s,{language:"html-vue"},{default:m(()=>e[8]||(e[8]=[n(`
+          <template>
+            <NumberInput 
+              v-model="input"
+              placeholder="Enter a number"
+            />
+          </template>
+
+          <script setup lang="ts">
+          import { NumberInput } from "@prefabs.tech/vue3-form";
+          <\/script>
+        `)])),_:1})])]),e[30]||(e[30]=n()),o("section",null,[o("h2",null,d(t.$t("form.label.labelInput")),1),e[13]||(e[13]=n()),o("div",M,[r(l(i),{modelValue:l(u).input,"onUpdate:modelValue":e[2]||(e[2]=a=>l(u).input=a),label:t.$t("form.label.number"),placeholder:t.$t("form.placeholder.number")},null,8,["modelValue","label","placeholder"]),e[12]||(e[12]=n()),r(s,{language:"html-vue"},{default:m(()=>e[11]||(e[11]=[n(`
+          <template>
+            <NumberInput 
+              v-model="input"
+              label="Number"
+              placeholder="Enter a number"
+            />
+          </template>
+
+          <script setup lang="ts">
+          import { NumberInput } from "@prefabs.tech/vue3-form";
+          <\/script>
+        `)])),_:1})])]),e[31]||(e[31]=n()),o("section",null,[o("h2",null,d(t.$t("form.label.disabled")),1),e[16]||(e[16]=n()),o("div",S,[r(l(i),{modelValue:l(u).disabled,"onUpdate:modelValue":e[3]||(e[3]=a=>l(u).disabled=a),label:t.$t("form.label.number"),disabled:""},null,8,["modelValue","label"]),e[15]||(e[15]=n()),r(s,{language:"html-vue"},{default:m(()=>e[14]||(e[14]=[n(`
+          <template>
+            <NumberInput 
+              v-model="input"
+              label="Number"
+              disabled
+            />
+          </template>
+
+          <script setup lang="ts">
+          import { NumberInput } from "@prefabs.tech/vue3-form";
+          <\/script>
+        `)])),_:1})])]),e[32]||(e[32]=n()),o("section",null,[o("h2",null,d(t.$t("form.label.withI18n")),1),e[19]||(e[19]=n()),o("div",h,[r(l(i),{modelValue:l(u).i18nInput,"onUpdate:modelValue":e[4]||(e[4]=a=>l(u).i18nInput=a),label:t.$t("form.label.number"),placeholder:t.$t("form.placeholder.number")},null,8,["modelValue","label","placeholder"]),e[18]||(e[18]=n()),r(s,{language:"html-vue"},{default:m(()=>e[17]||(e[17]=[n(`
+          <template>
+            <NumberInput 
+              v-model="input"
+              :label="t('form.label.number')"
+              :placeholder="t('form.placeholder.number')"
+            />
+          </template>
+
+          <script setup lang="ts">
+          import { NumberInput } from "@prefabs.tech/vue3-form";
+          import { useI18n } from "@prefabs.tech/vue3-i18n";
+
+          const { t } = useI18n();
+          <\/script>
+        `)])),_:1})])]),e[33]||(e[33]=n()),o("section",null,[o("h2",null,d(t.$t("form.label.inputWithValidation")),1),e[22]||(e[22]=n()),o("div",k,[r(l(f),null,{default:m(()=>[r(l(i),{modelValue:l(u).inputWithValidation,"onUpdate:modelValue":e[5]||(e[5]=a=>l(u).inputWithValidation=a),label:t.$t("form.label.number"),options:v,placeholder:t.$t("form.placeholder.number")},null,8,["modelValue","label","placeholder"])]),_:1}),e[21]||(e[21]=n()),r(s,{language:"html-vue"},{default:m(()=>e[20]||(e[20]=[n(`
+          <Form>
+            <NumberInput 
+              v-model="input"
+              :options="validationOptions"
+              label="Number"
+              placeholder="Enter a number"
+            />
+          </Form>
+
+          <script setup lang="ts">
+          import { Form, NumberInput } from "@prefabs.tech/vue3-form";
+          
+          import type { IsIntOptions } from "@prefabs.tech/vue3-form";
+  
+          const validationOptions = {
+            min: 1,
+            max: 100,
+            allow_leading_zeroes: false,
+          } as IsIntOptions;
+          <\/script>
+        `)])),_:1})])]),e[34]||(e[34]=n()),o("section",null,[o("h2",null,d(t.$t("form.label.customErrorMessageInput")),1),e[25]||(e[25]=n()),o("div",q,[r(l(f),null,{default:m(()=>[r(l(i),{modelValue:l(u).textInput,"onUpdate:modelValue":e[6]||(e[6]=a=>l(u).textInput=a),label:t.$t("form.label.number"),"error-messages":g,options:v,placeholder:t.$t("form.placeholder.number")},null,8,["modelValue","label","placeholder"])]),_:1}),e[24]||(e[24]=n()),r(s,{language:"html-vue"},{default:m(()=>e[23]||(e[23]=[n(`
+          <Form>
+            <NumberInput 
+              v-model="input"
+              :error-messages="validationMessages"
+              :options="validationOptions"
+              label="Number"
+              placeholder="Enter a number"
+            />
+          </Form>
+
+          <script setup lang="ts">
+          import { Form, NumberInput } from "@prefabs.tech/vue3-form";
+
+          import type { NumberErrorMessages, IsIntOptions } from "@prefabs.tech/vue3-form";
+
+          const validationMessages = {
+            invalid: "Please provide a number between 1 to 100",
+            required: "The field is required",
+          } as NumberErrorMessages;
+
+          const validationOptions = {
+            min: 1,
+            max: 100,
+            allow_leading_zeroes: false,
+          } as IsIntOptions;
+          <\/script>
+        `)])),_:1})])]),e[35]||(e[35]=n()),o("section",null,[o("h2",null,d(t.$t("form.label.customValidationInput")),1),e[28]||(e[28]=n()),o("div",B,[r(l(f),null,{default:m(()=>[r(l(i),{modelValue:l(u).customValidationInput,"onUpdate:modelValue":e[7]||(e[7]=a=>l(u).customValidationInput=a),label:t.$t("form.label.number"),placeholder:t.$t("form.placeholder.number"),schema:l(I),name:"number"},null,8,["modelValue","label","placeholder","schema"])]),_:1}),e[27]||(e[27]=n()),r(s,{language:"html-vue"},{default:m(()=>e[26]||(e[26]=[n(`
+          <Form>
+            <NumberInput 
+              v-model="input"
+              :schema="inputSchema"
+              label="Number"
+              placeholder="Enter a number"
+              name="number"
+            />
+          </Form>
+
+          <script setup lang="ts">
+          import { Form, NumberInput } from "@prefabs.tech/vue3-form";
+          import { z } from "zod";
+
+          const invalidNumberError = "Please provide a number between 1 to 100";
+
+          const inputSchema =  z.coerce
+            .number({invalid_type_error: invalidNumberError})
+            .gte(1, invalidNumberError)
+            .lte(100, invalidNumberError);
+          <\/script>
+        `)])),_:1})])])]),_:1},8,["title"])}}});export{D as default};
