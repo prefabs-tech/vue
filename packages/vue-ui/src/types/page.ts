8cfc914ab551a428588b45353f6a91769a8e81ca
KabinKhandThakuri <kabin@dzangolab.com>
2025-08-13 15:32:29 +0545

    refactor(ui/page):  add toolbar actions menu  property  to page component and show action list  dropdown on mobile view (#760)
    * refactor(ui/page): update page toolbar to support action menu items
    
    * style(ui/page): update page header css to flex
    
    * feat(ui/page): add action menu toolbar demo to page component demo
    
    * refactor(ui/page): update page component documentation translation
    
    * chore: sort translation alphabetically

---
+++ packages/vue-ui/src/types/page.ts
@@ -0,0 +1,7 @@
+import type { DropdownMenu } from "./dropdown-menu";
+import type { ExtractPropTypes } from "vue";
+import ButtonElement from "../Button/Index.vue";
+
+type ButtonElementProps = ExtractPropTypes<(typeof ButtonElement)["__props"]>;
+
+export interface ToolbarActionMenu extends DropdownMenu, ButtonElementProps {}
