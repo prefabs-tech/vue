daaf4ddf7edffbbfb405e55b0b4e871ce48b1d47
premsgr <32644681+premsgr@users.noreply.github.com>
2025-08-13 11:21:31 +0000

    Deploying to gh-pages from @ prefabs-tech/vue@a25b6fc37cfa5ceab25c4f0b00f172c3afa17678 🚀

--- assets/Index-Dg9j5_oY.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./UserPage.vue_vue_type_style_index_0_lang-C2nDIY6f.js";import{d as o,e as r,o as s}from"./index-1ySJLuIO.js";const a={name:"User"},i=o({...a,setup(n){return(e,_)=>(s(),r(t,{title:e.$t("user.title")},null,8,["title"]))}});export{i as default};
