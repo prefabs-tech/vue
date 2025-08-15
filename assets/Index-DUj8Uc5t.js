aec4fb0a015f5ac3e61c9043bea3923bbb04647c
rameshlohala <rameslohala@gmail.com>
2025-08-15 07:35:41 +0000

    Deploying to gh-pages from @ prefabs-tech/vue@73418690ed0ddb52e61b8dd12637c91c4c8b1756 🚀

---
+++ assets/Index-DUj8Uc5t.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./FormPage.vue_vue_type_style_index_0_lang-Dw-u2W8p.js";import{d as o,e as r,o as a}from"./index-bCT4rut6.js";const n={name:"Form"},l=o({...n,setup(_){return(t,s)=>(a(),r(e,{title:t.$t("form.title")},null,8,["title"]))}});export{l as default};
