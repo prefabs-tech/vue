d3ec1abda9cadb31f3675d09a95cf9c9fd04c95e
rameshlohala <rameslohala@gmail.com>
2025-08-22 10:37:51 +0000

    Deploying to gh-pages from @ prefabs-tech/vue@dce58341837323065f02c3e653df1c8647b7b8cd 🚀

--- assets/Index-C2gvTbJ9.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./LayoutPage.vue_vue_type_style_index_0_lang-CTZb1WyI.js";import{d as o,e as a,o as n}from"./index-B2kpz5D5.js";const _={name:"LayoutDemo"},m=o({..._,setup(r){return(t,s)=>(n(),a(e,{title:t.$t("layout.title")},null,8,["title"]))}});export{m as default};
