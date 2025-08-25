ea62593799c6e0dd9839c4f34421b1c75c10aebc
premsgr <32644681+premsgr@users.noreply.github.com>
2025-08-25 09:02:46 +0000

    Deploying to gh-pages from @ prefabs-tech/vue@29e52dca0d9b3ef220710e007e11b3f7c5ed64a2 🚀

--- assets/Index-DFuEKMyR.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./UiPage.vue_vue_type_style_index_0_lang-C5WL_JBx.js";import{d as o,e as a,o as n}from"./index-BtKzF4F8.js";const _={name:"UI"},l=o({..._,setup(r){return(t,s)=>(n(),a(e,{title:t.$t("ui.title")},null,8,["title"]))}});export{l as default};
---
+++ assets/Index-DSJorQWk.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./LayoutPage.vue_vue_type_style_index_0_lang-DfIe5KrI.js";import{d as o,e as a,o as n}from"./index-CT2_3BjD.js";const _={name:"LayoutDemo"},m=o({..._,setup(r){return(t,s)=>(n(),a(e,{title:t.$t("layout.title")},null,8,["title"]))}});export{m as default};
