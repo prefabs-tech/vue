95bcf420baa3596eb7f37b2580d6150322e01093
ks-rohan <82809048+ks-rohan@users.noreply.github.com>
2025-08-26 03:57:33 +0000

    Deploying to gh-pages from @ prefabs-tech/vue@bad8786b1d5852dad0580df6407c7b293a5318c8 🚀

--- assets/Index-DzGduCFj.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./UiPage.vue_vue_type_style_index_0_lang-DNfbiUxA.js";import{d as o,e as a,o as n}from"./index-CT2_3BjD.js";const _={name:"UI"},l=o({..._,setup(r){return(t,s)=>(n(),a(e,{title:t.$t("ui.title")},null,8,["title"]))}});export{l as default};
