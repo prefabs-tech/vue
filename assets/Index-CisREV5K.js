d1ff33de77c3a547b24647b5733090797a1d5d4a
DeepakAryal <deepakaryal2050@gmail.com>
2025-09-01 09:30:43 +0000

    Deploying to gh-pages from @ prefabs-tech/vue@96b13bb70fc9516b1f602da8a4942e0c08ff0611 🚀

---
+++ assets/Index-CisREV5K.js
@@ -0,0 +1,83 @@
+import{E as m,O as v}from"./PrefabsTechVue3Form.es-Cfb7Ra_o.js";import{d as w,I as V,L as I,M as g,r as d,J as S,e as $,o as h,j as u,C as t,f as o,t as p,h as s,u as e,v as L}from"./index-qbZHo0y6.js";import{_ as O}from"./FormPage.vue_vue_type_style_index_0_lang-BPtojiSk.js";const F={class:"section-content"},y={class:"section-content"},U={class:"section-content"},k={class:"section-content"},C={class:"section-content"},N={class:"section-content"},W={name:"SwitchInput"},P=w({...W,setup(z){const{t:f}=V(),b=I.coerce.boolean().refine(n=>n===!0,{message:f("form.errors.switch.invalid")});let i=g({disabled:d(!0),input:d(),inputWithValidation:d(!1),noLabelInput:d(),onOffLabel:d()});return(n,l)=>{const r=S("SshPre");return h(),$(O,{title:n.$t("form.label.switch"),class:"demo"},{toolbar:u(()=>[s(e(L),{label:n.$t("common.back"),"icon-left":"pi pi-chevron-left",size:"medium",variant:"textOnly",onClick:l[0]||(l[0]=a=>n.$router.push("/form"))},null,8,["label"])]),default:u(()=>[l[25]||(l[25]=t()),o("section",null,[o("h2",null,p(n.$t("form.label.basicInput")),1),l[9]||(l[9]=t()),o("div",F,[s(e(m),{modelValue:e(i).noLabelInput,"onUpdate:modelValue":l[1]||(l[1]=a=>e(i).noLabelInput=a)},null,8,["modelValue"]),l[8]||(l[8]=t()),s(r,{language:"html-vue"},{default:u(()=>l[7]||(l[7]=[t(`
+          <template>
+            <SwitchInput v-model="input" />
+          </template>
+
+          <script setup lang="ts">
+          import { SwitchInput } from "@prefabs.tech/vue3-form";
+          <\/script>
+        `)])),_:1})])]),l[26]||(l[26]=t()),o("section",null,[o("h2",null,p(n.$t("form.label.labelInput")),1),l[12]||(l[12]=t()),o("div",y,[s(e(m),{modelValue:e(i).input,"onUpdate:modelValue":l[2]||(l[2]=a=>e(i).input=a),label:n.$t("form.label.switch")},null,8,["modelValue","label"]),l[11]||(l[11]=t()),s(r,{language:"html-vue"},{default:u(()=>l[10]||(l[10]=[t(`
+          <Form>
+            <SwitchInput v-model="input" label="Switch" />
+          </Form>
+
+          <script setup lang="ts">
+          import { SwitchInput } from "@prefabs.tech/vue3-form";
+          <\/script>
+        `)])),_:1})])]),l[27]||(l[27]=t()),o("section",null,[o("h2",null,p(n.$t("form.label.disabled")),1),l[15]||(l[15]=t()),o("div",U,[s(e(m),{modelValue:e(i).disabled,"onUpdate:modelValue":l[3]||(l[3]=a=>e(i).disabled=a),label:n.$t("form.label.disabled"),disabled:""},null,8,["modelValue","label"]),l[14]||(l[14]=t()),s(r,{language:"html-vue"},{default:u(()=>l[13]||(l[13]=[t(`
+          <template>
+            <SwitchInput 
+              v-model="disabled"
+              disabled
+              label="Disabled"
+            />
+          </template>
+
+          <script setup lang="ts">
+          import { SwitchInput } from "@prefabs.tech/vue3-form";
+          import { ref } from "vue";
+
+          const disabled = ref(true);
+          <\/script>
+        `)])),_:1})])]),l[28]||(l[28]=t()),o("section",null,[o("h2",null,p(n.$t("form.label.withOnOffLabel")),1),l[18]||(l[18]=t()),o("div",k,[s(e(m),{modelValue:e(i).onOffLabel,"onUpdate:modelValue":l[4]||(l[4]=a=>e(i).onOffLabel=a),label:n.$t("form.label.switch"),"off-label":n.$t("form.label.no"),"on-label":n.$t("form.label.yes")},null,8,["modelValue","label","off-label","on-label"]),l[17]||(l[17]=t()),s(r,{language:"html-vue"},{default:u(()=>l[16]||(l[16]=[t(`
+          <Form>
+            <SwitchInput
+              v-model="input"
+              :label="$t('form.label.switch')"
+              off-label="No"
+              on-label="Yes"
+            />
+          </Form>
+
+          <script setup lang="ts">
+          import { SwitchInput } from "@prefabs.tech/vue3-form";
+          <\/script>
+
+          <style>
+            .switch input[type=checkbox] {
+              --_width: 5rem;
+            }
+          </style>
+        `)])),_:1})])]),l[29]||(l[29]=t()),o("section",null,[o("h2",null,p(n.$t("form.label.withI18n")),1),l[21]||(l[21]=t()),o("div",C,[s(e(m),{modelValue:e(i).input,"onUpdate:modelValue":l[5]||(l[5]=a=>e(i).input=a),label:n.$t("form.label.switch")},null,8,["modelValue","label"]),l[20]||(l[20]=t()),s(r,{language:"html-vue"},{default:u(()=>l[19]||(l[19]=[t(`
+          <template>
+            <SwitchInput 
+              v-model="input"
+              :label="t('form.label.switch')"
+            />
+          </template>
+
+          <script setup lang="ts">
+          import { SwitchInput } from "@prefabs.tech/vue3-form";
+          import { useI18n } from "@prefabs.tech/vue3-i18n";
+
+          const { t } = useI18n();
+          <\/script>
+        `)])),_:1})])]),l[30]||(l[30]=t()),o("section",null,[o("h2",null,p(n.$t("form.label.inputWithValidation")),1),l[24]||(l[24]=t()),o("div",N,[s(e(v),null,{default:u(()=>[s(e(m),{modelValue:e(i).inputWithValidation,"onUpdate:modelValue":l[6]||(l[6]=a=>e(i).inputWithValidation=a),label:n.$t("form.label.switch"),schema:e(b)},null,8,["modelValue","label","schema"])]),_:1}),l[23]||(l[23]=t()),s(r,{language:"html-vue"},{default:u(()=>l[22]||(l[22]=[t(`
+          <Form>
+            <SwitchInput 
+              v-model="disabled"
+              :schema="inputSchema"
+              label="Switch"
+            />
+          </Form>
+
+          <script setup lang="ts">
+          import { Form, SwitchInput } from "@prefabs.tech/vue3-form";
+
+          const inputSchema = z.coerce
+            .boolean()
+            .refine((value) => value === true, {
+              message: t("form.errors.switch.invalid"),
+            });
+          <\/script>
+        `)])),_:1})])])]),_:1},8,["title"])}}});export{P as default};
---
+++ assets/Index-DlBRLDJY.js
@@ -0,0 +1,153 @@
+import{C as m,O as w}from"./PrefabsTechVue3Form.es-Cfb7Ra_o.js";import{d as y,I,L as h,M as S,r as f,J as D,e as O,o as M,j as i,C as o,f as t,h as l,t as u,u as s,A as U,v as q}from"./index-qbZHo0y6.js";import{_ as E}from"./ComponentDocumentation.vue_vue_type_script_setup_true_lang-CfEHKesE.js";import{_ as F}from"./FormPage.vue_vue_type_style_index_0_lang-BPtojiSk.js";import"./PrefabsTechVue3TanstackTable.es-CpAXdbhR.js";const L={class:"section-content"},k={class:"section-content"},z={class:"section-content"},C={class:"section-content"},W={class:"section-content"},T={class:"section-content"},A={class:"section-content"},B={class:"section-content"},N={name:"PasswordInputField"},Q=y({...N,setup(Z){const{t:a}=I(),g=[{description:a("form.documentation.eventDescription.input.modelValue"),name:"update:modelValue",payload:"string | null | undefined"}],v=h.string({invalid_type_error:a("form.errors.password.invalid"),required_error:a("form.errors.input.required")}).min(8,{message:a("form.errors.password.min",{length:8})}).regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$/,a("form.errors.password.invalid")),P=[{default:"false",description:a("form.documentation.propsDescription.input.disabled"),prop:"disabled",type:"Boolean"},{default:'{ required: "A password is required", weak: "This password is too weak" }',description:a("form.documentation.propsDescription.input.errorMessages"),prop:"errorMessages",type:"PasswordErrorMessages"},{default:"null",description:a("form.documentation.propsDescription.input.label"),prop:"label",type:"string | null | undefined"},{default:"null",description:a("form.documentation.propsDescription.input.modelValue"),prop:"modelValue",type:"string | null | undefined"},{default:'"password"',description:a("form.documentation.propsDescription.input.name"),prop:"name",type:"String"},{default:"-",description:a("form.documentation.propsDescription.input.options"),prop:"options",type:"StrongPasswordOptions"},{default:"-",description:a("form.documentation.propsDescription.input.placeholder"),prop:"placeholder",type:"String"},{default:"-",description:a("form.documentation.propsDescription.input.schema"),prop:"schema",type:"z.ZodType<string | number | object>"}],V=[{description:a("form.documentation.slotDescription.password.icon"),name:"icon",props:"{ showPassword: boolean }"}],$={required:a("form.errors.input.required"),weak:a("form.errors.password.invalid")},b={minLength:10,minLowercase:1,minUppercase:1,minSymbols:1};let p=S({customValidationInput:f(),disabled:f("P@ssw0rd"),input:f(),inputWithValidation:f(),noLabelInput:f(),passwordInput:f()});return(n,e)=>{const d=D("SshPre");return M(),O(F,{subtitle:s(a)("form.subtitle.password"),title:n.$t("form.label.password"),class:"demo"},{toolbar:i(()=>[l(s(q),{label:n.$t("common.back"),"icon-left":"pi pi-chevron-left",size:"medium",variant:"textOnly",onClick:e[0]||(e[0]=r=>n.$router.push("/form"))},null,8,["label"])]),default:i(()=>[e[34]||(e[34]=o()),t("section",null,[t("h2",null,u(n.$t("form.label.basicInput")),1),e[11]||(e[11]=o()),t("div",L,[l(s(m),{modelValue:s(p).noLabelInput,"onUpdate:modelValue":e[1]||(e[1]=r=>s(p).noLabelInput=r),placeholder:n.$t("form.placeholder.password")},null,8,["modelValue","placeholder"]),e[10]||(e[10]=o()),l(d,{language:"html-vue"},{default:i(()=>e[9]||(e[9]=[o(`
+          <template>
+            <Password 
+              v-model="input"
+              placeholder="Enter a password"
+            />
+          </template>
+
+          <script setup lang="ts">
+          import { Password } from "@prefabs.tech/vue3-form";
+          <\/script>
+        `)])),_:1})])]),e[35]||(e[35]=o()),t("section",null,[t("h2",null,u(n.$t("form.label.labelInput")),1),e[14]||(e[14]=o()),t("div",k,[l(s(m),{modelValue:s(p).input,"onUpdate:modelValue":e[2]||(e[2]=r=>s(p).input=r),label:n.$t("form.label.password"),placeholder:n.$t("form.placeholder.password")},null,8,["modelValue","label","placeholder"]),e[13]||(e[13]=o()),l(d,{language:"html-vue"},{default:i(()=>e[12]||(e[12]=[o(`
+          <template>
+            <Password 
+              v-model="input"
+              label="Password"
+              placeholder="Enter a password"
+            />
+          </template>
+
+          <script setup lang="ts">
+          import { Password } from "@prefabs.tech/vue3-form";
+          <\/script>
+        `)])),_:1})])]),e[36]||(e[36]=o()),t("section",null,[t("h2",null,u(n.$t("form.label.disabled")),1),e[17]||(e[17]=o()),t("div",z,[l(s(m),{modelValue:s(p).disabled,"onUpdate:modelValue":e[3]||(e[3]=r=>s(p).disabled=r),label:n.$t("form.label.password"),disabled:""},null,8,["modelValue","label"]),e[16]||(e[16]=o()),l(d,{language:"html-vue"},{default:i(()=>e[15]||(e[15]=[o(`
+          <template>
+            <Password 
+              v-model="input"
+              label="Password"
+              disabled
+            />
+          </template>
+
+          <script setup lang="ts">
+          import { Password } from "@prefabs.tech/vue3-form";
+          import { ref } from vue;
+  
+          const input = ref("P@ssw0rd");
+          <\/script>
+        `)])),_:1})])]),e[37]||(e[37]=o()),t("section",null,[t("h2",null,u(n.$t("form.label.withI18n")),1),e[20]||(e[20]=o()),t("div",C,[l(s(m),{modelValue:s(p).input,"onUpdate:modelValue":e[4]||(e[4]=r=>s(p).input=r),label:n.$t("form.label.password"),placeholder:n.$t("form.placeholder.password")},null,8,["modelValue","label","placeholder"]),e[19]||(e[19]=o()),l(d,{language:"html-vue"},{default:i(()=>e[18]||(e[18]=[o(`
+          <template>
+            <Password 
+              v-model="input"
+              :label="t('form.label.password')"
+              :placeholder="t('form.placeholder.password')"
+            />
+          </template>
+
+          <script setup lang="ts">
+          import { Password } from "@prefabs.tech/vue3-form";
+          import { useI18n } from "@prefabs.tech/vue3-i18n";
+
+          const { t } = useI18n();
+          <\/script>
+        `)])),_:1})])]),e[38]||(e[38]=o()),t("section",null,[t("h2",null,u(n.$t("form.label.iconSlot")),1),e[23]||(e[23]=o()),t("div",W,[l(s(m),{modelValue:s(p).input,"onUpdate:modelValue":e[5]||(e[5]=r=>s(p).input=r),label:n.$t("form.label.password"),placeholder:n.$t("form.placeholder.password")},{icon:i(({showPassword:r})=>[t("i",{class:U(`pi ${r?"pi-eye":"pi-eye-slash"}`)},null,2)]),_:1},8,["modelValue","label","placeholder"]),e[22]||(e[22]=o()),l(d,{language:"html-vue"},{default:i(()=>e[21]||(e[21]=[o(`
+          <template>
+            <Password 
+              v-model="input"
+              :label="t('form.label.password')"
+              :placeholder="t('form.placeholder.password')"
+            >
+              <template #icon="{ showPassword }">
+                <i :class="\`pi \${ showPassword ? 'pi-eye' : 'pi-eye-slash'}\`" />
+              </template>
+            <Password>
+          </template>
+
+          <script setup lang="ts">
+          import { Password } from "@prefabs.tech/vue3-form";
+          import { useI18n } from "@prefabs.tech/vue3-i18n";
+
+          const { t } = useI18n();
+          <\/script>
+        `)])),_:1})])]),e[39]||(e[39]=o()),t("section",null,[t("h2",null,u(n.$t("form.label.inputWithValidation")),1),e[26]||(e[26]=o()),t("div",T,[l(s(w),null,{default:i(()=>[l(s(m),{modelValue:s(p).inputWithValidation,"onUpdate:modelValue":e[6]||(e[6]=r=>s(p).inputWithValidation=r),label:n.$t("form.label.password"),options:b,placeholder:n.$t("form.placeholder.password")},null,8,["modelValue","label","placeholder"])]),_:1}),e[25]||(e[25]=o()),l(d,{language:"html-vue"},{default:i(()=>e[24]||(e[24]=[o(`
+          <Form>
+            <Password 
+              v-model="input"
+              :options="validationOptions"
+              label="Password"
+              placeholder="Enter a password"
+            />
+          </Form>
+
+          <script setup lang="ts">
+          import { Form, Password } from "@prefabs.tech/vue3-form";
+          
+          import type { StrongPasswordOptions } from "@prefabs.tech/vue3-form";
+
+          const validationOptions = {
+            minLength: 10,
+            minLowercase: 1,
+            minUppercase: 1,
+            minSymbols: 1,
+          } as StrongPasswordOptions;
+          <\/script>
+        `)])),_:1})])]),e[40]||(e[40]=o()),t("section",null,[t("h2",null,u(n.$t("form.label.customErrorMessageInput")),1),e[29]||(e[29]=o()),t("div",A,[l(s(w),null,{default:i(()=>[l(s(m),{modelValue:s(p).passwordInput,"onUpdate:modelValue":e[7]||(e[7]=r=>s(p).passwordInput=r),label:n.$t("form.label.password"),"error-messages":$,options:b,placeholder:n.$t("form.placeholder.password")},null,8,["modelValue","label","placeholder"])]),_:1}),e[28]||(e[28]=o()),l(d,{language:"html-vue"},{default:i(()=>e[27]||(e[27]=[o(`
+          <Form>
+            <Password 
+              v-model="input"
+              :error-messages="validationMessages"
+              :options="validationOptions"
+              label="Password"
+              placeholder="Enter a password"
+            />
+          </Form>
+
+          <script setup lang="ts">
+          import { Form, Password } from "@prefabs.tech/vue3-form";
+          import type { PasswordErrorMessages, StrongPasswordOptions } from "@prefabs.tech/vue3-form";
+
+          const validationMessages = {
+            required: "The field should not be empty",
+            weak: "Please provide a strong password",
+          } as PasswordErrorMessages;
+
+          const validationOptions = {
+            minLength: 10,
+            minLowercase: 1,
+            minUppercase: 1,
+            minSymbols: 1,
+          } as StrongPasswordOptions;
+          <\/script>
+        `)])),_:1})])]),e[41]||(e[41]=o()),t("section",null,[t("h2",null,u(n.$t("form.label.customValidationInput")),1),e[32]||(e[32]=o()),t("div",B,[l(s(w),null,{default:i(()=>[l(s(m),{modelValue:s(p).customValidationInput,"onUpdate:modelValue":e[8]||(e[8]=r=>s(p).customValidationInput=r),label:n.$t("form.label.password"),placeholder:n.$t("form.placeholder.password"),schema:s(v),name:"custom_input"},null,8,["modelValue","label","placeholder","schema"])]),_:1}),e[31]||(e[31]=o()),l(d,{language:"html-vue"},{default:i(()=>e[30]||(e[30]=[o(`
+          <Form>
+            <Password 
+              v-model="input"
+              :schema="inputSchema"
+              label="Password"
+              placeholder="Enter a password"
+              name="input"
+            />
+          </Form>
+  
+          <script setup lang="ts">
+          import { Form, Password } from "@prefabs.tech/vue3-form";
+          import { z } from "zod";
+  
+          const inputSchema = z
+            .string({
+              invalid_type_error: "Please provide a strong password",
+              required_error: "The field is required"
+            })
+            .min(8, { message: "Input must be at least 8 character(s)" })
+            .regex(
+              /^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[\\W_]).+$/,
+              "Please provide a strong password"
+            );
+          <\/script>
+        `)])),_:1})])]),e[42]||(e[42]=o()),l(E,{"events-data":g,"props-data":P,"props-table-title":n.$t("common.properties",{value:"PasswordProperties"}),"slots-data":V},null,8,["props-table-title"]),e[43]||(e[43]=o()),l(d,{language:"html-vue"},{default:i(()=>e[33]||(e[33]=[o(`
+      interface PasswordErrorMessages {
+        required?: string;
+        weak?: string;
+      }
+    `)])),_:1})]),_:1},8,["subtitle","title"])}}});export{Q as default};
