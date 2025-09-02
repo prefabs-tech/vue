477989ea0c5d1e8b7fc885d3688a5932ca1dcf77
DeepakAryal <deepakaryal2050@gmail.com>
2025-09-02 12:00:06 +0000

    Deploying to gh-pages from @ prefabs-tech/vue@c977536695d7b3b1ad58f2b1577c9b343813d62e 🚀

--- assets/Index-CsdzvsRa.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./UiPage.vue_vue_type_style_index_0_lang-C12RI7xm.js";import{d as o,e as a,o as n}from"./index-BOw3R0Gy.js";const _={name:"UI"},l=o({..._,setup(r){return(t,s)=>(n(),a(e,{title:t.$t("ui.title")},null,8,["title"]))}});export{l as default};
