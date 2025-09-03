37bb83d497382f91d5d6d419d54ca235671a1c8c
DeepakAryal <deepakaryal2050@gmail.com>
2025-09-03 09:22:48 +0000

    Deploying to gh-pages from @ prefabs-tech/vue@82c2b0e31193fc91a64d8fe7a520f5faba9ab3d4 🚀

--- assets/Index-CC57PL_L.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./FormPage.vue_vue_type_style_index_0_lang-9df8pLFh.js";import{d as o,e as r,o as a}from"./index-DhI5jERR.js";const n={name:"Form"},l=o({...n,setup(_){return(t,s)=>(a(),r(e,{title:t.$t("form.title")},null,8,["title"]))}});export{l as default};
---
+++ assets/Index-gxlejByh.js
@@ -0,0 +1,41 @@
+import{d as C,J as g,e as f,o as m,j as s,C as t,f as r,h as n,t as d,u as a,Q as L,a as k,F as b,z as v,O as $,v as _}from"./index-VOKHa5hN.js";import{_ as y}from"./ComponentDocumentation.vue_vue_type_script_setup_true_lang-BeJ-vFO0.js";import{_ as B}from"./UiPage.vue_vue_type_style_index_0_lang-C1iJ1EO0.js";import"./PrefabsTechVue3TanstackTable.es-CKsUW9jk.js";import"./PrefabsTechVue3Form.es-qh8wDnMJ.js";const x={class:"section-content"},F=C({__name:"Index",setup(D){const l=[{name:"Card 1",description:"Lorem ipsum dolor sit amet, consectetur adipiscing"},{name:"Card 2",description:"Lorem ipsum dolor sit amet, consectetur adipiscing"},{name:"Card 3",description:"Lorem ipsum dolor sit amet, consectetur adipiscing"},{name:"Card 4",description:"Lorem ipsum dolor sit amet, consectetur adipiscing"},{name:"Card 5",description:"Lorem ipsum dolor sit amet, consectetur adipiscing"}],p=[{name:"default",description:"Default slot to render grid items inside the container."}];return(i,e)=>{const u=g("SshPre");return m(),f(B,{"sub-title":i.$t("ui.gridContainer.subtitle"),title:i.$t("ui.gridContainer.title"),class:"demo"},{toolbar:s(()=>[n(a(_),{label:i.$t("common.back"),"icon-left":"pi pi-chevron-left",size:"medium",variant:"textOnly",onClick:e[0]||(e[0]=o=>i.$router.push("/ui"))},null,8,["label"])]),default:s(()=>[e[4]||(e[4]=t()),r("section",null,[r("h2",null,d(i.$t("ui.gridContainer.usage.basic")),1),e[3]||(e[3]=t()),r("div",x,[n(a(L),null,{default:s(()=>[(m(),k(b,null,v(l,(o,c)=>n(a($),{key:c+"-"+o.name,title:o.name},{default:s(()=>[t(d(o.description),1)]),_:2},1032,["title"])),64))]),_:1}),e[2]||(e[2]=t()),n(u,{language:"html-vue"},{default:s(()=>e[1]||(e[1]=[t(`
+          <template>
+            <GridContainer>
+              <Card
+                v-for="(menu, index) in packages"
+                :key="index + '-' + menu.name"
+                :title="menu.name"
+              >
+                {{ menu.description }}
+              </Card>
+            </GridContainer>
+          </template>
+
+          <script setup lang="ts">
+          import { Card, GridContainer } from "@prefabs.tech/vue3-ui";
+
+          const packages = [
+            {
+              name: "Card 1",
+              description: "Lorem ipsum dolor sit amet, consectetur adipiscing",
+            },
+            {
+              name: "Card 2",
+              description: "Lorem ipsum dolor sit amet, consectetur adipiscing",
+            },
+            {
+              name: "Card 3",
+              description: "Lorem ipsum dolor sit amet, consectetur adipiscing",
+            },
+
+            {
+              name: "Card 4",
+              description: "Lorem ipsum dolor sit amet, consectetur adipiscing",
+            },
+            {
+              name: "Card 5",
+              description: "Lorem ipsum dolor sit amet, consectetur adipiscing",
+            },
+          ];
+          <\/script>
+        `)])),_:1})])]),e[5]||(e[5]=t()),n(y,{"slots-data":p})]),_:1},8,["sub-title","title"])}}});export{F as default};
