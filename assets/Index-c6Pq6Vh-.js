e34b3a788f67c7072072d5a51218126be0423f0e
DeepakAryal <deepakaryal2050@gmail.com>
2025-09-10 07:38:19 +0000

    Deploying to gh-pages from @ prefabs-tech/vue@e35b17312ef0351bd1c4c7a4865365bdbfa6f86a 🚀

---
+++ assets/Index-c6Pq6Vh-.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./FormPage.vue_vue_type_style_index_0_lang-DdoAoczb.js";import{d as o,e as r,o as a}from"./index-ByOAbTJU.js";const n={name:"Form"},l=o({...n,setup(_){return(t,s)=>(a(),r(e,{title:t.$t("form.title")},null,8,["title"]))}});export{l as default};
