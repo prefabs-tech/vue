9241aa024247d8c24bda96611057b0d21d1c04e0
DeepakAryal <deepakaryal2050@gmail.com>
2025-09-19 09:08:38 +0000

    Deploying to gh-pages from @ prefabs-tech/vue@8cc9317893bf5ef1f84d192285e5090cb66530b5 🚀

--- assets/Index-B2XJLlCQ.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./UserPage.vue_vue_type_style_index_0_lang-ifFT6eTc.js";import{d as o,e as r,o as s}from"./index-Ds2msYCw.js";const a={name:"User"},i=o({...a,setup(n){return(e,_)=>(s(),r(t,{title:e.$t("user.title")},null,8,["title"]))}});export{i as default};
---
+++ assets/Index-CQdQ5nWy.js
@@ -0,0 +1,69 @@
+import{d as g,J as f,e as w,o as b,j as a,C as e,f as i,h as o,t as n,u as r,a6 as l,v as c}from"./index-BNFnUDdE.js";import{_ as y}from"./ComponentDocumentation.vue_vue_type_script_setup_true_lang-KeGqZsO_.js";import{_ as h}from"./UiPage.vue_vue_type_style_index_0_lang-SQPidsC3.js";import"./PrefabsTechVue3TanstackTable.es-CCmcQvvp.js";import"./PrefabsTechVue3Form.es-tpySeI4B.js";const $={class:"section-content"},D={class:"content-wrapper"},C={class:"section-content"},k={class:"content-wrapper vertical"},d="Programming isn’t about what you know; it’s about what you can figure out.",u="Code is read much more often than it is written.",m="The most dangerous phrase in the language is, ‘We’ve always done it this way.'",N=g({__name:"Index",setup(x){const v=[{default:'"horizontal"',description:"Sets the direction of the divider.",prop:"orientation",type:'"horizontal" | "vertical"'}];return(s,t)=>{const p=f("SshPre");return b(),w(h,{"sub-title":s.$t("ui.divider.subtitle"),title:s.$t("ui.divider.title"),class:"demo"},{toolbar:a(()=>[o(r(c),{label:s.$t("common.back"),"icon-left":"pi pi-chevron-left",size:"medium",variant:"textOnly",onClick:t[0]||(t[0]=z=>s.$router.push("/ui"))},null,8,["label"])]),default:a(()=>[t[15]||(t[15]=e()),i("section",null,[i("h2",null,n(s.$t("ui.divider.usage.horizontal")),1),t[7]||(t[7]=e()),i("div",$,[i("div",D,[i("p",{class:"content-item"},n(d)),t[1]||(t[1]=e()),o(r(l)),t[2]||(t[2]=e()),i("p",{class:"content-item"},n(u)),t[3]||(t[3]=e()),o(r(l),{orientation:"horizontal"}),t[4]||(t[4]=e()),i("p",{class:"content-item"},n(m))]),t[6]||(t[6]=e()),o(p,{language:"html-vue"},{default:a(()=>t[5]||(t[5]=[e(`
+          <template>
+            <div class="content-wrapper">
+              <p class="content-item">
+                Programming isn’t about what you know; it’s about what you can figure out.
+              </p>
+              <Divider />
+              <p class="content-item">
+                Code is read much more often than it is written.
+              </p>
+              <Divider orientation="horizontal" />
+              <p class="content-item">
+                The most dangerous phrase in the language is, ‘We’ve always done it this way.'
+              </p>
+            </div>
+          </template>
+
+          <script setup lang="ts">
+          import { Divider } from "@prefabs.tech/vue3-ui";
+          <\/script>
+
+          <style lang="css">
+          .content-wrapper {
+            border: 1px solid #b5b5b5;
+            border-radius: 0.3rem;
+            display: flex;
+            flex-direction: column;
+            padding: 1rem 0.75rem;
+          }
+          </style>
+        `)])),_:1})])]),t[16]||(t[16]=e()),i("section",null,[i("h2",null,n(s.$t("ui.divider.usage.vertical")),1),t[14]||(t[14]=e()),i("div",C,[i("div",k,[i("p",{class:"content-item"},n(d)),t[8]||(t[8]=e()),o(r(l),{orientation:"vertical"}),t[9]||(t[9]=e()),i("p",{class:"content-item"},n(u)),t[10]||(t[10]=e()),o(r(l),{orientation:"vertical"}),t[11]||(t[11]=e()),i("p",{class:"content-item"},n(m))]),t[13]||(t[13]=e()),o(p,{language:"html-vue"},{default:a(()=>t[12]||(t[12]=[e(`
+          <template>
+            <div class="content-wrapper vertical">
+              <p class="content-item">
+                Programming isn’t about what you know; it’s about what you can figure out.
+              </p>
+              <Divider orientation="vertical" />
+              <p class="content-item">
+                Code is read much more often than it is written.
+              </p>
+              <Divider orientation="vertical" />
+              <p class="content-item">
+                The most dangerous phrase in the language is, ‘We’ve always done it this way.'
+              </p>
+            </div>
+          </template>
+
+          <script setup lang="ts">
+          import { Divider } from "@prefabs.tech/vue3-ui";
+          <\/script>
+
+          <style lang="css">
+          .content-wrapper {
+            border: 1px solid #b5b5b5;
+            border-radius: 0.3rem;
+            display: flex;
+            flex-direction: column;
+            padding: 1rem 0.75rem;
+          }
+
+          .content-wrapper > .content-item {
+            width: 100%;
+          }
+
+          .content-wrapper.vertical {
+            flex-direction: row;
+          }
+          </style>
+        `)])),_:1})])]),t[17]||(t[17]=e()),o(y,{"props-data":v,"props-table-title":s.$t("common.properties",{value:"DividerProperties"})},null,8,["props-table-title"])]),_:1},8,["sub-title","title"])}}});export{N as default};
