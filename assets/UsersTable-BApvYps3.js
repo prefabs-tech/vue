40e63faa263e13c4e0d04dab30cd4ec859c5ddec
DeepakAryal <deepakaryal2050@gmail.com>
2025-10-08 09:04:40 +0000

    Deploying to gh-pages from @ prefabs-tech/vue@7844edec70e4cdf1ec06c3fcbf7413d19ee93d1d 🚀

---
+++ assets/UsersTable-BApvYps3.js
@@ -0,0 +1,25 @@
+import{d as u,J as m,e as d,o as p,j as l,C as t,f as i,t as b,h as a,u as o,a2 as f,v}from"./index-Bq7_1nMf.js";import{b as c}from"./data-CPcWa_8W.js";import{_ as C}from"./UserPage.vue_vue_type_style_index_0_lang-Cxf8WhXF.js";const g={class:"section-content"},N=u({__name:"UsersTable",setup(U){const n=["email","name","roles","signedUpAt","disabled"];return(s,e)=>{const r=m("SshPre");return p(),d(C,{title:s.$t("user.label.usersTable"),class:"demo-users-table"},{toolbar:l(()=>[a(o(v),{label:s.$t("common.back"),"icon-left":"pi pi-chevron-left",size:"medium",variant:"textOnly",onClick:e[0]||(e[0]=$=>s.$router.push("/user"))},null,8,["label"])]),default:l(()=>[e[4]||(e[4]=t()),i("section",null,[i("h2",null,b(s.$t("user.label.basic")),1),e[3]||(e[3]=t()),i("div",g,[a(o(f),{id:"users-table","initial-sorting":[{id:"email",desc:!1}],"invitation-modal-title":s.$t("user.label.inviteUser"),users:o(c),"visible-columns":n},null,8,["invitation-modal-title","users"]),e[2]||(e[2]=t()),a(r,{language:"html-vue"},{default:l(()=>e[1]||(e[1]=[t(`
+          <template>
+            <UsersTable
+              id="users-table"
+              :initial-sorting="[{ id: 'email', desc: false }]"
+              :users="users"
+              :visible-columns="visibleColumns"
+              invitation-modal-title="Invite user"
+            />
+          </template>
+
+          <script setup lang="ts">
+          import { UsersTable } from "@prefabs.tech/vue3-user";
+
+          import { allUsers } from "./data";
+
+          const visibleColumns = [
+            "email",
+            "name",
+            "roles",
+            "signedUpAt",
+            "disabled",
+          ];
+          <\/script>
+        `)])),_:1})])])]),_:1},8,["title"])}}});export{N as default};
