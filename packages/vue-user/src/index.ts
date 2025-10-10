22f320fb92710ebab424348c93968b68c30c454a
kabin thakuri <kabin@dzangolab.com>
2025-10-10 11:58:52 +0545

    fix(user): update route guard and email verification status in user store

--- packages/vue-user/src/index.ts
+++ packages/vue-user/src/index.ts
@@ -6,6 +6,7 @@ import { inject } from "vue";
 
 import client from "./api/axios";
 import initAuthProvider from "./auth-provider";
+import { ERROR_UNAUTHORIZED } from "./constant";
 import messages from "./locales/messages.json";
 import updateRouter from "./router";
 import userStore from "./store";
@@ -27,6 +28,17 @@ const plugin: Plugin = {
 
     if (options?.config?.user?.features?.authProvider !== "laravel-passport") {
       initSupertokens(options.config);
+    }
+
+    if (options?.config?.user?.features?.signUp?.emailVerification) {
+      const { getVerificationStatus, logout } = userStore();
+
+      getVerificationStatus()
+      .catch((error) => {
+        if (error.status === ERROR_UNAUTHORIZED) {
+          logout();
+        }
+      });
     }
 
     initAuthProvider(options.config);
