ff5bfc727b669606e9660fb55f1972eeb03ea020
Anusha <anusha.chaudhary@dzangolab.com>
2025-12-08 14:40:25 +0545

    feat: add countries list

--- packages/vue-ui/src/FormWidgets/country-picker/CountryPicker.vue
+++ packages/vue-ui/src/FormWidgets/country-picker/CountryPicker.vue
@@ -1,3 +1,41 @@
 <template>
-  <p>Here this is component CountryPicker</p>
+  <div class="country-picker">
+    <select v-model="selectedCountry" class="form-select" :name="name">
+      <option
+        v-for="country in countries"
+        :key="country.code"
+        :value="country.code"
+      >
+        {{ country.i18n[locale] }}
+      </option>
+    </select>
+  </div>
 </template>
+<script setup>
+import { ref } from "vue";
+
+import countriesData from "./countries.json";
+
+const props = defineProps({
+  name: {
+    type: String,
+    default: "country",
+  },
+  locale: {
+    type: String,
+    default: "en",
+  },
+  value: {
+    type: [String, Number],
+    default: "NP",
+  },
+});
+const selectedCountry = ref(props.value);
+const countries = ref(countriesData);
+</script>
+
+<style>
+.country-picker {
+  width: 100%;
+}
+</style>
