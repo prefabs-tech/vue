2c876fe39480c40f6d32bfbf43f4ebb4721105eb
DeepakAryal <deepakaryal2050@gmail.com>
2025-12-09 10:34:26 +0000

    Deploying to gh-pages from @ prefabs-tech/vue@c35260b5b94622eb61be22ac8404133c193e1211 🚀

---
+++ assets/Index-DCIxU0kY.js
@@ -0,0 +1,22 @@
+import{d as m,r as g,L as f,e as v,o as b,j as i,C as n,f as l,h as o,t as c,u as s,v as r,W as $}from"./index-STrZF6Wn.js";import{_ as k}from"./ComponentDocumentation.vue_vue_type_script_setup_true_lang-Dl_YUsvn.js";import{_ as C}from"./UiPage.vue_vue_type_style_index_0_lang-DcWhYAEI.js";import"./PrefabsTechVue3TanstackTable.es-BN3dqrFI.js";import"./PrefabsTechVue3Form.es-DV1FRZxy.js";const L={class:"section-content"},B={class:"container"},E=m({__name:"Index",setup(P){const a=g(!1),u=[{default:"false",description:"Controls the visibility of the loading overlay.",prop:"loading",type:"Boolean"}],p=()=>{a.value=!0,setTimeout(()=>{a.value=!1},5e3)};return(e,t)=>{const d=f("SshPre");return b(),v(C,{"sub-title":e.$t("ui.loading.subtitle"),title:e.$t("ui.loading.title"),class:"demo-loading"},{toolbar:i(()=>[o(s(r),{label:e.$t("common.back"),"icon-left":"pi pi-chevron-left",size:"medium",variant:"textOnly",onClick:t[0]||(t[0]=y=>e.$router.push("/ui"))},null,8,["label"])]),default:i(()=>[t[5]||(t[5]=n()),l("section",null,[l("h2",null,c(e.$t("ui.loading.usage.basic")),1),t[4]||(t[4]=n()),l("div",L,[l("div",B,[o(s(r),{label:e.$t("ui.loading.buttonLabel.clickMe"),onClick:p},null,8,["label"]),t[1]||(t[1]=n()),o(s($),{loading:a.value},null,8,["loading"])]),t[3]||(t[3]=n()),o(d,{language:"html-vue"},{default:i(()=>t[2]||(t[2]=[n(`
+          <template>
+            <ButtonElement label="Click me" @click="startLoading" />
+
+            <LoadingPage :loading="loading" />
+          </template>
+          
+          <script setup lang="ts"> 
+            import { ButtonElement, LoadingPage } from "@prefabs.tech/vue3-ui";
+            import { ref } from "vue";
+
+            const loading = ref(false);
+
+            const startLoading = () => {
+              loading.value = true;
+
+              setTimeout(() => {
+                loading.value = false;
+              }, 5000);
+            };
+          <\/script>
+        `)])),_:1})])]),t[6]||(t[6]=n()),o(k,{"props-data":u,"props-table-title":e.$t("common.properties",{value:"LoadingPageProperties"})},null,8,["props-table-title"])]),_:1},8,["sub-title","title"])}}});export{E as default};
