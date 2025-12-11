a2f7ca602747291bae46d1d0cc206a11969637ff
DeepakAryal <deepakaryal2050@gmail.com>
2025-12-11 10:03:10 +0000

    Deploying to gh-pages from @ prefabs-tech/vue@0dc8ee489c1784a6b16218a977417386f883314f 🚀

---
+++ assets/Index-DOoaXvE7.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./UserPage.vue_vue_type_style_index_0_lang-D6BtSVmC.js";import{d as o,e as r,o as s}from"./index-CCRQ-N6v.js";const a={name:"User"},i=o({...a,setup(n){return(e,_)=>(s(),r(t,{title:e.$t("user.title")},null,8,["title"]))}});export{i as default};
