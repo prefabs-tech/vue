0bf7346d7ecbef90fbfd75df5931d95e045352f7
KabinKhandThakuri <kabin@dzangolab.com>
2025-12-30 12:15:24 +0000

    Deploying to gh-pages from @ prefabs-tech/vue@b2bf9625a958daa8d4c18961c3ade8c1f459309a 🚀

---
+++ assets/Index-BMIjvTtY.js
@@ -0,0 +1,162 @@
+import{j as p}from"./PrefabsTechVue3Form.es-f5lc6FXr.js";import{o as C}from"./PrefabsTechVue3TanstackTable.es-BeMiy9dy.js";import{d as y,I as P,M as I,J as b,e as N,o as B,i as c,f as n,B as t,t as a,h as s,u}from"./index-CLlYJ0SI.js";import{_ as R}from"./FormPage.vue_vue_type_style_index_0_lang-RXL3u3bN.js";const G="Afghanistan",T="Îles Åland",L="Albanie",D="Algérie",V="Samoa américaines",U="Andorre",K="Angola",E="Anguilla",k="Antarctique",F="Antigua-et-Barbuda",h="Argentine",H="Arménie",Z="Aruba",O="Autriche",q="Azerbaïdjan",J="Bahamas",Y="Bahreïn",W="Bangladesh",Q="Barbade",w="Biélorussie",z="Belize",j="Bénin",X="Bermudes",x="Bhoutan",_="Bolivie",oo="Pays-Bas caribéens",to="Bosnie-Herzégovine",no="Botswana",eo="Île Bouvet",so="Brésil",lo="Territoire britannique de l'océan Indien",io="Brunei",ro="Bulgarie",ao="Burkina Faso",co="Burundi",uo="Cambodge",po="Cameroun",mo="Canada",fo="Cap-Vert",Co="Îles Caïmans",So="République centrafricaine",go="Tchad",vo="Chili",Ao="Chine",$o="Île Christmas",Mo="Îles Cocos",yo="Colombie",Po="Comores",Io="Congo",bo="République démocratique du Congo",No="Îles Cook",Bo="Costa Rica",Ro="Côte d’Ivoire",Go="Croatie",To="Curaçao",Lo="Chypre",Do="République tchèque",Vo="Danemark",Uo="Djibouti",Ko="Dominique",Eo="République dominicaine",ko="Équateur",Fo="Égypte",ho="Salvador",Ho="Guinée équatoriale",Zo="Érythrée",Oo="Estonie",qo="Eswatini",Jo="Éthiopie",Yo="Îles Malouines",Wo="Îles Féroé",Qo="Fidji",wo="Finlande",zo="France",jo="Guyane française",Xo="Polynésie française",xo="Terres australes françaises",_o="Gabon",ot="Gambie",tt="Géorgie",nt="Allemagne",et="Ghana",st="Gibraltar",lt="Grèce",it="Groenland",rt="Grenade",at="Guadeloupe",ct="Guam",ut="Guatemala",dt="Guernesey",pt="Guinée",mt="Guinée-Bissau",ft="Guyana",Ct="Haïti",St="Îles Heard-et-MacDonald",gt="Vatican",vt="Honduras",At="Hong Kong",$t="Hongrie",Mt="Islande",yt="Inde",Pt="Indonésie",It="Iran",bt="Irak",Nt="Irlande",Bt="Île de Man",Rt="Israël",Gt="Italie",Tt="Jamaïque",Lt="Japon",Dt="Jersey",Vt="Jordanie",Ut="Kazakhstan",Kt="Kenya",Et="Kiribati",kt="Corée du Nord",Ft="Corée du Sud",ht="Koweït",Ht="Kirghizistan",Zt="Laos",Ot="Lettonie",qt="Liban",Jt="Lesotho",Yt="Libéria",Wt="Libye",Qt="Liechtenstein",wt="Lituanie",zt="Luxembourg",jt="Macao",Xt="Madagascar",xt="Malawi",_t="Malaisie",on="Maldives",tn="Mali",nn="Malte",en="Îles Marshall",sn="Martinique",ln="Mauritanie",rn="Maurice",an="Mayotte",cn="Mexique",un="Micronésie",dn="Moldavie",pn="Monaco",mn="Mongolie",fn="Monténégro",Cn="Montserrat",Sn="Maroc",gn="Mozambique",vn="Myanmar",An="Namibie",$n="Nauru",Mn="Pays-Bas",yn="Nouvelle-Calédonie",Pn="Nouvelle-Zélande",In="Nicaragua",bn="Niger",Nn="Nigéria",Bn="Niue",Rn="Île Norfolk",Gn="Macédoine du Nord",Tn="Îles Mariannes du Nord",Ln="Norvège",Dn="Oman",Vn="Pakistan",Un="Palaos",Kn="Palestine",En="Panama",kn="Papouasie-Nouvelle-Guinée",Fn="Paraguay",hn="Pérou",Hn="Philippines",Zn="Îles Pitcairn",On="Pologne",qn="Portugal",Jn="Porto Rico",Yn="Qatar",Wn="La Réunion",Qn="Roumanie",wn="Russie",zn="Rwanda",jn="Saint-Barthélemy",Xn="Sainte-Hélène",xn="Saint-Christophe-et-Niévès",_n="Sainte-Lucie",oe="Saint-Martin",te="Saint-Pierre-et-Miquelon",ne="Saint-Vincent-et-les-Grenadines",ee="Samoa",se="Saint-Marin",le="Sao Tomé-et-Principe",ie="Arabie saoudite",re="Sénégal",ae="Serbie",ce="Seychelles",ue="Sierra Leone",de="Singapour",pe="Saint-Martin (partie néerlandaise)",me="Slovaquie",fe="Slovénie",Ce="Îles Salomon",Se="Somalie",ge="Afrique du Sud",ve="Soudan du Sud",Ae="Espagne",$e="Sri Lanka",Me="Soudan",ye="Suriname",Pe="Svalbard et Jan Mayen",Ie="Suède",be="Suisse",Ne="Syrie",Be="Taïwan",Re="Tadjikistan",Ge="Tanzanie",Te="Thaïlande",Le="Timor oriental",De="Togo",Ve="Tokelau",Ue="Tonga",Ke="Trinité-et-Tobago",Ee="Tunisie",ke="Turquie",Fe="Turkménistan",he="Îles Turques-et-Caïques",He="Tuvalu",Ze="Ouganda",Oe="Ukraine",qe="Émirats arabes unis",Je="Royaume-Uni",Ye="États-Unis",We="Uruguay",Qe="Ouzbékistan",we="Vanuatu",ze="Venezuela",je="Viêt Nam",Xe="Wallis-et-Futuna",xe="Sahara occidental",_e="Yémen",os="Zambie",ts="Zimbabwe",ns={AF:G,AX:T,AL:L,DZ:D,AS:V,AD:U,AO:K,AI:E,AQ:k,AG:F,AR:h,AM:H,AW:Z,AT:O,AZ:q,BS:J,BH:Y,BD:W,BB:Q,BY:w,BZ:z,BJ:j,BM:X,BT:x,BO:_,BQ:oo,BA:to,BW:no,BV:eo,BR:so,IO:lo,BN:io,BG:ro,BF:ao,BI:co,KH:uo,CM:po,CA:mo,CV:fo,KY:Co,CF:So,TD:go,CL:vo,CN:Ao,CX:$o,CC:Mo,CO:yo,KM:Po,CG:Io,CD:bo,CK:No,CR:Bo,CI:Ro,HR:Go,CW:To,CY:Lo,CZ:Do,DK:Vo,DJ:Uo,DM:Ko,DO:Eo,EC:ko,EG:Fo,SV:ho,GQ:Ho,ER:Zo,EE:Oo,SZ:qo,ET:Jo,FK:Yo,FO:Wo,FJ:Qo,FI:wo,FR:zo,GF:jo,PF:Xo,TF:xo,GA:_o,GM:ot,GE:tt,DE:nt,GH:et,GI:st,GR:lt,GL:it,GD:rt,GP:at,GU:ct,GT:ut,GG:dt,GN:pt,GW:mt,GY:ft,HT:Ct,HM:St,VA:gt,HN:vt,HK:At,HU:$t,IS:Mt,IN:yt,ID:Pt,IR:It,IQ:bt,IE:Nt,IM:Bt,IL:Rt,IT:Gt,JM:Tt,JP:Lt,JE:Dt,JO:Vt,KZ:Ut,KE:Kt,KI:Et,KP:kt,KR:Ft,KW:ht,KG:Ht,LA:Zt,LV:Ot,LB:qt,LS:Jt,LR:Yt,LY:Wt,LI:Qt,LT:wt,LU:zt,MO:jt,MG:Xt,MW:xt,MY:_t,MV:on,ML:tn,MT:nn,MH:en,MQ:sn,MR:ln,MU:rn,YT:an,MX:cn,FM:un,MD:dn,MC:pn,MN:mn,ME:fn,MS:Cn,MA:Sn,MZ:gn,MM:vn,NA:An,NR:$n,NL:Mn,NC:yn,NZ:Pn,NI:In,NE:bn,NG:Nn,NU:Bn,NF:Rn,MK:Gn,MP:Tn,NO:Ln,OM:Dn,PK:Vn,PW:Un,PS:Kn,PA:En,PG:kn,PY:Fn,PE:hn,PH:Hn,PN:Zn,PL:On,PT:qn,PR:Jn,QA:Yn,RE:Wn,RO:Qn,RU:wn,RW:zn,BL:jn,SH:Xn,KN:xn,LC:_n,MF:oe,PM:te,VC:ne,WS:ee,SM:se,ST:le,SA:ie,SN:re,RS:ae,SC:ce,SL:ue,SG:de,SX:pe,SK:me,SI:fe,SB:Ce,SO:Se,ZA:ge,SS:ve,ES:Ae,LK:$e,SD:Me,SR:ye,SJ:Pe,SE:Ie,CH:be,SY:Ne,TW:Be,TJ:Re,TZ:Ge,TH:Te,TL:Le,TG:De,TK:Ve,TO:Ue,TT:Ke,TN:Ee,TR:ke,TM:Fe,TC:he,TV:He,UG:Ze,UA:Oe,AE:qe,GB:Je,US:Ye,UY:We,UZ:Qe,VU:we,VE:ze,VN:je,WF:Xe,EH:xe,YE:_e,ZM:os,ZW:ts},es="อัฟกานิสถาน",ss="แอลเบเนีย",ls="แอลจีเรีย",is="อาร์เจนตินา",rs="อาร์เมเนีย",as="ออสเตรเลีย",cs="ออสเตรีย",us="อาเซอร์ไบจาน",ds="บังกลาเทศ",ps="เบลเยียม",ms="บราซิล",fs="บัลแกเรีย",Cs="แคนาดา",Ss="ชิลี",gs="จีน",vs="โคลอมเบีย",As="โครเอเชีย",$s="เช็ก",Ms="เดนมาร์ก",ys="อียิปต์",Ps="ฟินแลนด์",Is="ฝรั่งเศส",bs="เยอรมนี",Ns="กรีซ",Bs="ฮ่องกง",Rs="ฮังการี",Gs="อินเดีย",Ts="อินโดนีเซีย",Ls="อิหร่าน",Ds="ไอร์แลนด์",Vs="อิสราเอล",Us="อิตาลี",Ks="ญี่ปุ่น",Es="เคนยา",ks="เกาหลีใต้",Fs="มาเลเซีย",hs="เม็กซิโก",Hs="เนเธอร์แลนด์",Zs="นิวซีแลนด์",Os="นอร์เวย์",qs="ปากีสถาน",Js="ฟิลิปปินส์",Ys="โปแลนด์",Ws="โปรตุเกส",Qs="รัสเซีย",ws="ซาอุดีอาระเบีย",zs="สิงคโปร์",js="สเปน",Xs="สวีเดน",xs={AF:es,AL:ss,DZ:ls,AR:is,AM:rs,AU:as,AT:cs,AZ:us,BD:ds,BE:ps,BR:ms,BG:fs,CA:Cs,CL:Ss,CN:gs,CO:vs,HR:As,CZ:$s,DK:Ms,EG:ys,FI:Ps,FR:Is,DE:bs,GR:Ns,HK:Bs,HU:Rs,IN:Gs,ID:Ts,IR:Ls,IE:Ds,IL:Vs,IT:Us,JP:Ks,KE:Es,KR:ks,MY:Fs,MX:hs,NL:Hs,NZ:Zs,NO:Os,PK:qs,PH:Js,PL:Ys,PT:Ws,RU:Qs,SA:ws,SG:zs,ES:js,SE:Xs},_s={class:"section-content"},ol={class:"section-content"},tl={class:"section-content"},nl={class:"section-content"},el={class:"section-content"},sl={class:"section-content"},ll={class:"section-content"},il={class:"section-content"},rl={class:"section-content"},ml=y({__name:"Index",setup(al){const{t:r}=P(),m={fr:ns,th:xs},S=[{accessorKey:"prop",header:"Property"},{accessorKey:"type",header:"Type"},{accessorKey:"default",header:"Default"},{accessorKey:"description",header:"Description"}],g=[{default:"[]",description:r("form.documentation.propsDescription.select.exclude"),id:1,prop:"exclude",type:"String[]"},{default:"en",description:r("form.documentation.propsDescription.select.fallbackLocale"),id:2,prop:"fallbackLocale",type:"String"},{default:"[]",description:r("form.documentation.propsDescription.select.favorites"),id:3,prop:"favorites",type:"String[]"},{default:"true",description:r("form.documentation.propsDescription.select.hasSortedOption"),id:4,prop:"has-sorted-options",type:"Boolean"},{default:"[]",description:r("form.documentation.propsDescription.select.include"),id:5,prop:"include",type:"String[]"},{default:"true",description:r("form.documentation.propsDescription.select.includeFavorites"),id:6,prop:"includeFavorites",type:"Boolean"},{default:"{ en: defaultEnCatalogue }",description:r("form.documentation.propsDescription.select.i18n"),id:7,prop:"i18n",type:"Record<string, Record<string, string>>"},{default:"en",description:r("form.documentation.propsDescription.select.locale"),id:8,prop:"locale",type:"String"},{default:"-",description:r("form.documentation.propsDescription.input.modelValue"),id:9,prop:"modelValue",type:"String | Number | Array<String|Number> | undefined"},{default:"false",description:r("form.documentation.propsDescription.select.multiple"),id:10,prop:"multiple",type:"Boolean"},{default:"-",description:r("form.documentation.propsDescription.input.placeholder"),id:11,prop:"placeholder",type:"String"}],v=[{accessorKey:"name",header:"Name"},{accessorKey:"payload",header:"Payload"},{accessorKey:"description",header:"Description"}],A=[{description:r("form.documentation.eventDescription.input.modelValue"),name:"update:modelValue",payload:"string | number | (string | number)[] | undefined"}],l=I({basic:void 0,custom:[],customLocale:[],excludedCountries:[],includedCountries:[],multiselect:[]}),$=["US","AU","QA","IR","CI","CA"],f=["US","AU","QA","IR","CI","CA","NP","IN"],M=["US","CA","FR","AU","NP"];return(e,o)=>{const d=b("SshPre");return B(),N(R,{subtitle:e.$t("form.subtitle.countryPicker"),title:e.$t("form.label.countryPicker")},{default:c(()=>[n("section",null,[n("h2",null,a(e.$t("form.label.basicInput")),1),o[10]||(o[10]=t()),n("div",_s,[s(u(p),{modelValue:l.basic,"onUpdate:modelValue":o[0]||(o[0]=i=>l.basic=i),placeholder:e.$t("form.placeholder.country")},null,8,["modelValue","placeholder"]),o[9]||(o[9]=t()),s(d,{language:"html-vue"},{default:c(()=>o[8]||(o[8]=[t(`
+         <template>
+            <CountryPicker
+              v-model="input"
+              placeholder="$t('form.placeholder.country')"
+            />
+          </template>
+                  
+          <script setup lang="ts">
+          import { CountryPicker } from '@prefabs.tech/vue3-form';
+          import { ref } from 'vue';
+                  
+          const input = ref();
+          <\/script>
+        `)])),_:1})])]),o[36]||(o[36]=t()),n("section",null,[n("h2",null,a(e.$t("form.label.multiselect")),1),o[13]||(o[13]=t()),n("div",ol,[s(u(p),{modelValue:l.multiselect,"onUpdate:modelValue":o[1]||(o[1]=i=>l.multiselect=i),placeholder:e.$t("form.placeholder.countries"),multiple:""},null,8,["modelValue","placeholder"]),o[12]||(o[12]=t()),s(d,{language:"html-vue"},{default:c(()=>o[11]||(o[11]=[t(`
+          <template>
+            <CountryPicker
+              v-model="input"
+              multiple
+              placeholder="$t('form.placeholder.countries')"
+            />
+          </template>
+
+          <script setup lang="ts">
+          import { CountryPicker } from "@prefabs.tech/vue3-form";
+          import { ref } from "vue";
+
+          const input = ref();
+          <\/script>
+        `)])),_:1})])]),o[37]||(o[37]=t()),n("section",null,[n("h2",null,a(e.$t("form.label.customLocale")),1),o[16]||(o[16]=t()),n("div",tl,[s(u(p),{modelValue:l.customLocale,"onUpdate:modelValue":o[2]||(o[2]=i=>l.customLocale=i),i18n:m,locale:e.$i18n.locale,placeholder:e.$t("form.placeholder.countries"),multiple:""},null,8,["modelValue","locale","placeholder"]),o[15]||(o[15]=t()),s(d,{language:"html-vue"},{default:c(()=>o[14]||(o[14]=[t(`
+          <template>
+              <CountryPicker
+                v-model="input"
+                :i18n="countryI18n"
+                :locale="$i18n.locale"
+                multiple
+                placeholder="$t('form.placeholder.countries')"
+              />
+          </template>
+
+          <script setup lang="ts">
+          import { ref } from 'vue';
+          import fr from "./fr.json";
+
+          const countryI18n = {
+            fr,
+          };
+          const input = ref();
+          <\/script>
+        `)])),_:1})])]),o[38]||(o[38]=t()),n("section",null,[n("h2",null,a(e.$t("form.label.fallbackLocale")),1),o[19]||(o[19]=t()),n("div",nl,[s(u(p),{modelValue:l.fallbackDemo,"onUpdate:modelValue":o[3]||(o[3]=i=>l.fallbackDemo=i),i18n:m,locale:e.$i18n.locale,"fallback-locale":"th",placeholder:e.$t("form.placeholder.countries"),multiple:""},null,8,["modelValue","locale","placeholder"]),o[18]||(o[18]=t()),s(d,{language:"html-vue"},{default:c(()=>o[17]||(o[17]=[t(`
+      <template>
+        <CountryPicker
+          v-model="input"
+          :i18n="countryI18n"
+          :locale="$i18n.locale"
+          fallback-locale="th"
+          multiple
+          placeholder="$t('form.placeholder.countries')"
+        />
+      </template>
+
+      <script setup lang="ts">
+      import { ref } from 'vue';
+      import fr from "./fr.json";
+      import th from "./th.json";
+
+      const countryI18n = {
+        fr,
+        th,
+      };
+      const input = ref();
+     
+      <\/script>
+    `)])),_:1})])]),o[39]||(o[39]=t()),n("section",null,[n("h2",null,a(e.$t("form.label.include")),1),o[22]||(o[22]=t()),n("div",el,[s(u(p),{modelValue:l.includedCountries,"onUpdate:modelValue":o[4]||(o[4]=i=>l.includedCountries=i),include:M,placeholder:e.$t("form.placeholder.countries"),multiple:""},null,8,["modelValue","placeholder"]),o[21]||(o[21]=t()),s(d,{language:"html-vue"},{default:c(()=>o[20]||(o[20]=[t(`
+          <template>
+            <CountryPicker
+              v-model="input"
+              :include="includedCountries"
+              multiple
+              placeholder="$t('form.placeholder.countries')"
+            />
+          </template>
+
+          <script setup lang="ts">
+          import { CountryPicker } from "@prefabs.tech/vue3-form";
+          import { ref } from "vue";
+
+          const includedCountries = ['US', 'CA', 'FR', 'AU','NP'];
+          const input = ref();
+          <\/script>
+        `)])),_:1})])]),o[40]||(o[40]=t()),n("section",null,[n("h2",null,a(e.$t("form.label.exclude")),1),o[25]||(o[25]=t()),n("div",sl,[s(u(p),{modelValue:l.excludedCountries,"onUpdate:modelValue":o[5]||(o[5]=i=>l.excludedCountries=i),exclude:$,placeholder:e.$t("form.placeholder.countries"),multiple:""},null,8,["modelValue","placeholder"]),o[24]||(o[24]=t()),s(d,{language:"html-vue"},{default:c(()=>o[23]||(o[23]=[t(`
+          <template>
+            <CountryPicker
+              v-model="input"
+              :exclude="excludedCountries"
+              multiple
+              placeholder="$t('form.placeholder.countries')"
+            />
+          </template>
+
+          <script setup lang="ts">
+          import { CountryPicker } from "@prefabs.tech/vue3-form";
+          import { ref } from "vue";
+          
+          const excludedCountries = ["US", "AU", "QA", "IR", "CI", "CA"];
+          const input = ref();
+          <\/script>
+        `)])),_:1})])]),o[41]||(o[41]=t()),n("section",null,[n("h2",null,a(e.$t("form.label.favorites")),1),o[28]||(o[28]=t()),n("div",ll,[s(u(p),{modelValue:l.favorites,"onUpdate:modelValue":o[6]||(o[6]=i=>l.favorites=i),favorites:f,"has-sorted-options":!1,placeholder:e.$t("form.placeholder.countries"),multiple:""},null,8,["modelValue","placeholder"]),o[27]||(o[27]=t()),s(d,{language:"html-vue"},{default:c(()=>o[26]||(o[26]=[t(`
+          <template>
+              <CountryPicker
+                v-model="input"
+                :favorites="favoritesCountries"
+                :has-sorted-options="false"
+                placeholder="$t('form.placeholder.countries')"
+              />
+          </template>
+
+          <script setup lang="ts">
+          import { ref } from 'vue';
+          
+          const favoritesCountries = ["US", "AU", "QA", "IR", "CI", "CA", "NP", "IN"];
+          const input = ref();
+          <\/script>
+        `)])),_:1})])]),o[42]||(o[42]=t()),n("section",null,[n("h2",null,a(e.$t("form.label.includeFavorites")),1),o[31]||(o[31]=t()),n("div",il,[s(u(p),{modelValue:l.favorites,"onUpdate:modelValue":o[7]||(o[7]=i=>l.favorites=i),favorites:f,"has-sorted-options":!1,"include-favorites":!1,placeholder:e.$t("form.placeholder.countries"),multiple:""},null,8,["modelValue","placeholder"]),o[30]||(o[30]=t()),s(d,{language:"html-vue"},{default:c(()=>o[29]||(o[29]=[t(`
+          <template>
+              <CountryPicker
+                v-model="input"
+                :favorites="favoritesCountries"
+                :has-sorted-options="false"
+                multiple
+                placeholder="$t('form.placeholder.countries')"
+              />
+          </template>
+
+          <script setup lang="ts">
+          import { ref } from 'vue';
+          
+          const favoritesCountries = ["US", "AU", "QA", "IR", "CI", "CA", "NP", "IN"];
+          const input = ref();
+          <\/script>
+        `)])),_:1})])]),o[43]||(o[43]=t()),n("section",null,[n("h2",null,a(e.$t("common.properties",{value:"CountryPickerProperties"})),1),o[32]||(o[32]=t()),s(u(C),{"columns-data":S,data:g,paginated:!1,"persist-state":!1})]),o[44]||(o[44]=t()),n("section",null,[n("h2",null,a(e.$t("common.events")),1),o[33]||(o[33]=t()),s(u(C),{"columns-data":v,data:A,paginated:!1,"persist-state":!1})]),o[45]||(o[45]=t()),n("section",null,[n("h2",null,a(e.$t("common.type")),1),o[35]||(o[35]=t()),n("div",rl,[s(d,{language:"typescript"},{default:c(()=>o[34]||(o[34]=[t(`
+            type TranslationCatalogue = Record<string, string>;
+
+            type I18nConfig = Record<string, TranslationCatalogue>;
+
+            interface CountryOption {
+             code: string;
+             label?: string;
+            }
+
+            // Example usage:
+            const example: I18nConfig = {
+              en: { 
+                US: "USA",
+                FR: "France"
+              },
+              fr: { 
+                US: "États-Unis",
+                FR: "France"
+              }
+              };
+          `)])),_:1})])])]),_:1},8,["subtitle","title"])}}});export{ml as default};
