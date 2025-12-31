f526b25f8779165abaff3e9a970104f51f02ccde
Anusha <anusha.chaudhary@dzangolab.com>
2025-12-31 17:19:46 +0545

    chore: refactor code lines

--- packages/vue-form/src/components/Country.vue
+++ packages/vue-form/src/components/Country.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :data-country-code="countryCode" class="country">
+  <div :data-country-code="countryCode">
     {{ countryLabel }}
   </div>
 </template>
@@ -9,10 +9,24 @@ import { computed } from "vue";
 
 import englishData from "./country-picker/en.json";
 
+type I18nConfigData = Record<string, Record<string, string>>;
+
 const props = defineProps({
   code: {
-    type: String,
     required: true,
+    type: String,
+  },
+  fallbackLocale: {
+    default: "en",
+    type: String,
+  },
+  i18n: {
+    default: () => ({}),
+    type: Object as () => I18nConfigData,
+  },
+  locale: {
+    default: "en",
+    type: String,
   },
 });
 
@@ -21,6 +35,11 @@ const countryCode = computed(() => props.code.trim().toUpperCase());
 const countryLabel = computed(() => {
   const code = countryCode.value;
 
-  return englishData[code as keyof typeof englishData] || "-";
+  return (
+    props.i18n?.[props.locale]?.[code] ||
+    props.i18n?.[props.fallbackLocale]?.[code] ||
+    englishData[code as keyof typeof englishData] ||
+    "-"
+  );
 });
 </script>
