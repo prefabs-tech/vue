f88edd712a3209d1640dc6847061a094b5babb79
KabinKhandThakuri <kabin@dzangolab.com>
2026-01-08 12:15:57 +0000

    Deploying to gh-pages from @ prefabs-tech/vue@bad3e1d75e81b945c732ea330b8253bda64a12e9 🚀

--- assets/Index-CttdWkZi.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./LayoutPage.vue_vue_type_style_index_0_lang-eQfNOc6e.js";import{d as o,e as a,o as n}from"./index-BD1JkeeG.js";const _={name:"LayoutDemo"},m=o({..._,setup(r){return(t,s)=>(n(),a(e,{title:t.$t("layout.title")},null,8,["title"]))}});export{m as default};
