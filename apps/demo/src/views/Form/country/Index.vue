33336b6e64c92ac6241cb1b3e895db8ca1d70409
Anusha Chuadhhary <84500422+onusha45@users.noreply.github.com>
2026-01-14 10:39:16 +0545

    refactor: enhance Country component flag display (#915)
    * feat: add locale and fallback support to country display
    
    * feat: add flag support
    
    * chore(docs): add data properties table
    
    * fix: updated country display props
    
    * chore(docs): add type in country display data properties
    
    * chore: fix dta properties table
    
    * chore: np json added and indentication fixed
    
    * style: add style to country
    
    * fix: fix style and intendication
    
    * style: add style for flag
    
    * refactor: remove unused lines of code
    
    * fix: resolve merge conflict
    
    * docs: add flag visibility in demo
    
    * fix: rename variable name
    
    * fix: resolve merge conflict
    
    * feat: add locales support
    
    * chore: removed unused line of codes
    
    * chore: remove unsed line of codes
    
    * chore: remove unsed line of codes
    
    * chore(docs): update data table properties
    
    * chore: rename country classs name
    
    * chore: remove the used line of codes
    
    * chore: fix typescript import
    
    * chore: remove unused line of code
    
    * refactor(country-picker): rename englishData to defaultEnglishTranslation
    
    * chore(docs): fix indentication
    
    * chore: remove unused lines of code
    
    * chore(docs): fix items alphabetically in in np.json
    
    * fix: lint fix
    
    * chore: refactor country display
    
    * chore(sort): reorder items alphabetically
    
    * chore(sort): reorder items alphabetically
    
    * chore: remove unused lines of code
    
    * chore: remove unused lines of code
    
    * chore(sort): align items alphabetically
    
    * chore(sort): align items alphabetically
    
    * style: refactor country display style
    
    * refactor: add flag to country display
    
    * chore(sort): sort items alphabetically
    
    * chore(docs): update data properties and slots table
    
    * chore: fix indentication
    
    * chore(style): refactor style classed name

--- apps/demo/src/views/Form/country/Index.vue
+++ apps/demo/src/views/Form/country/Index.vue
@@ -21,12 +21,366 @@
             <!-- eslint-enable -->
       </div>
     </section>
-    >
+    <section>
+      <h2>{{ $t("form.label.customLocale") }}</h2>
+      <div>
+        <Country :locales="locales" :locale="selectedLocale" code="NP" />
+        <!-- eslint-disable -->
+            <SshPre language="html-vue">
+              &lt;template&gt;
+                &lt;Country :locales="locales" :locale="selectedLocale" code="NP" /&gt;
+              &lt;/template&gt;
+                      
+              &lt;script setup lang="ts"&gt;
+              import { Country } from '@prefabs.tech/vue3-form';
+              import fr from "./fr.json";
+
+              const { locale } = useI18n();
+
+              const selectedLocale = "np";
+              
+              const locales = {
+                fr,
+              };
+              &lt;/script&gt;
+            </SshPre>    
+            <!-- eslint-enable -->
+      </div>
+    </section>
+    <section>
+      <h2>{{ $t("form.label.localesSupport") }}</h2>
+      <div>
+        <Country :locale="locale" :locales="locales" code="JP" />
+        <!-- eslint-disable -->
+            <SshPre language="html-vue">
+              &lt;template&gt;
+                &lt;Country :locale="locale" :locales="locales" code="JP" /&gt;
+              &lt;/template&gt;
+                      
+              &lt;script setup lang="ts"&gt;
+              import { Country } from '@prefabs.tech/vue3-form';
+              import fr from "./fr.json";
+
+              const { locale } = useI18n();
+              
+              const locales = {
+                fr,
+              };
+              &lt;/script&gt;
+            </SshPre>    
+            <!-- eslint-enable -->
+      </div>
+    </section>
+    <section>
+      <h2>{{ $t("form.label.fallbackLocale") }}</h2>
+      <div>
+        <Country
+          :locale="locale"
+          :locales="locales"
+          code="CN"
+          fallback-locale="np"
+        />
+        <!-- eslint-disable -->
+            <SshPre language="html-vue">
+              &lt;template&gt;
+                &lt;Country 
+                  :locale="locale"
+                  :locales="locales"
+                  code="CN"
+                  fallback-locale="np" 
+                /&gt;
+              &lt;/template&gt;
+                      
+              &lt;script setup lang="ts"&gt;
+              import { Country } from '@prefabs.tech/vue3-form';
+              import { useI18n } from "vue-i18n";
+              import fr from "./fr.json";
+              import np from "./np.json";
+              
+              const { locale } = useI18n();
+
+              const locales = {
+                fr,
+                np,
+              };
+              &lt;/script&gt;
+            </SshPre>    
+            <!-- eslint-enable -->
+      </div>
+    </section>
+    <section>
+      <h2>{{ $t("form.label.flagVisibility") }}</h2>
+      <div>
+        <Country code="FR" :show-flag="false" />
+        <!-- eslint-disable -->
+            <SshPre language="html-vue">
+              &lt;template&gt;
+                &lt;Country code="FR" :show-flag="false" /&gt;
+              &lt;/template&gt;
+                      
+              &lt;script setup lang="ts"&gt;
+              import { Country } from '@prefabs.tech/vue3-form';
+           
+              &lt;/script&gt;
+            </SshPre>    
+            <!-- eslint-enable -->
+      </div>
+    </section>
+    <section>
+      <h2>{{ $t("form.label.flagsStyle") }}</h2>
+      <div>
+        <h3>{{ $t("form.label.rectangle") }}</h3>
+        <Country code="IS" />
+        <!-- eslint-disable -->
+            <SshPre language="html-vue">
+              &lt;template&gt;
+                &lt;Country code="IS"&gt; 
+              &lt;/template&gt;
+
+              &lt;script setup lang="ts"&gt;
+              import { Country } from '@prefabs.tech/vue3-form';
+           
+              &lt;/script&gt;       
+            </SshPre>    
+            <!-- eslint-enable -->
+      </div>
+      <div>
+        <h3>{{ $t("form.label.square") }}</h3>
+        <Country code="IS" flags-style="square" />
+        <!-- eslint-disable -->
+            <SshPre language="html-vue">
+              &lt;template&gt;
+                &lt;Country code="IS" flags-style="square"&gt; 
+              &lt;/template&gt;
+
+              &lt;script setup lang="ts"&gt;
+              import { Country } from '@prefabs.tech/vue3-form';
+           
+              &lt;/script&gt;          
+            </SshPre>    
+            <!-- eslint-enable -->
+      </div>
+      <div>
+        <h3>{{ $t("form.label.circle") }}</h3>
+        <Country code="IS" flags-style="circle" />
+        <!-- eslint-disable -->
+            <SshPre language="html-vue">
+              &lt;template&gt;
+                &lt;Country code="IS" flags-style="circle"&gt; 
+              &lt;/template&gt;   
+
+              &lt;script setup lang="ts"&gt;
+              import { Country } from '@prefabs.tech/vue3-form';
+           
+              &lt;/script&gt;       
+            </SshPre>    
+            <!-- eslint-enable -->
+      </div>
+    </section>
+    <section>
+      <h2>{{ $t("form.label.flagsPosition") }}</h2>
+      <div>
+        <h3>{{ $t("form.label.left") }}</h3>
+        <Country code="IS" />
+        <!-- eslint-disable -->
+            <SshPre language="html-vue">
+              &lt;template&gt;
+                &lt;Country code="IS"&gt; 
+              &lt;/template&gt;     
+
+              &lt;script setup lang="ts"&gt;
+              import { Country } from '@prefabs.tech/vue3-form';
+           
+              &lt;/script&gt;  
+            </SshPre>    
+            <!-- eslint-enable -->
+      </div>
+      <div>
+        <h3>{{ $t("form.label.right") }}</h3>
+        <Country code="IS" flags-position="right" />
+        <!-- eslint-disable -->
+            <SshPre language="html-vue">
+              &lt;template&gt;
+                &lt;Country code="IS" flags-position="right"&gt; 
+              &lt;/template&gt;
+
+              &lt;script setup lang="ts"&gt;
+              import { Country } from '@prefabs.tech/vue3-form';
+           
+              &lt;/script&gt;  
+            </SshPre>    
+            <!-- eslint-enable -->
+      </div>
+      <div>
+        <h3>{{ $t("form.label.rightEdge") }}</h3>
+        <Country code="IS" flags-position="right-edge" />
+        <!-- eslint-disable -->
+            <SshPre language="html-vue">
+              &lt;template&gt;
+                &lt;Country code="IS" flags-position="right-edge"&gt; 
+              &lt;/template&gt;
+              
+              &lt;script setup lang="ts"&gt;
+              import { Country } from '@prefabs.tech/vue3-form';
+           
+              &lt;/script&gt;  
+            </SshPre>    
+            <!-- eslint-enable -->
+      </div>
+    </section>
+    <section>
+      <h2>{{ $t("form.label.unknownCountryCode") }}</h2>
+      <div>
+        <span class="unknown-country">Country:</span>
+        <Country
+          :locale="locale"
+          :locales="locales"
+          code="WW"
+          fallback-locale="th"
+        />
+
+        <!-- eslint-disable -->
+            <SshPre language="html-vue">
+              &lt;template&gt;
+                &lt;Country 
+                :locale="locale" 
+                :locales="locales" 
+                code="WW" 
+                fallback-locale="th" 
+                /&gt;
+              &lt;/template&gt;
+                      
+              &lt;script setup lang="ts"&gt;
+              import { Country } from '@prefabs.tech/vue3-form';
+              import { useI18n } from "vue-i18n";
+              import fr from "./fr.json";
+              import np from "./np.json";
+
+              const { locale } = useI18n();
+
+              const locales = {
+                fr,
+                th,
+              };
+              &lt;/script&gt;
+            </SshPre>    
+            <!-- eslint-enable -->
+      </div>
+    </section>
+    <ComponentDocumentation
+      :props-data="propsData"
+      :props-table-title="
+        $t('common.properties', { value: 'CountryProperties' })
+      "
+      :slots-data="slotsData"
+    />
+    <section>
+      <h2>{{ $t("common.type") }}</h2>
+      <div class="section-content">
+        <!-- eslint-disable -->
+         <SshPre language="typescript">
+            interface CountryProperties {
+             code: string;
+             fallbackLocale?: string;  
+             locale?: string;          
+             locales?: I18nData;         
+             showFlag?: boolean;            
+            }
+          </SshPre>
+      </div>
+    </section>
   </FormPage>
 </template>
 
 <script setup lang="ts">
 import { Country } from "@prefabs.tech/vue3-form";
+import { useI18n } from "vue-i18n";
 
+import ComponentDocumentation from "../../../components/ComponentDocumentation.vue";
+import fr from "../countryPicker/fr.json";
+import np from "../countryPicker/np.json";
 import FormPage from "../FormPage.vue";
+
+const { t } = useI18n();
+
+const { locale } = useI18n();
+
+const locales = {
+  fr,
+  np,
+};
+
+const selectedLocale = "np";
+
+const propsData = [
+  {
+    default: "-",
+    description: t("form.documentation.propsDescription.select.countryCode"),
+    id: 1,
+    prop: "code",
+    type: "String",
+  },
+  {
+    default: "en",
+    description: t("form.documentation.propsDescription.select.fallbackLocale"),
+    id: 2,
+    prop: "fallbackLocale",
+    type: "String",
+  },
+  {
+    default: "-",
+    description: t("form.documentation.propsDescription.select.flagsPath"),
+    id: 3,
+    prop: "flagsPath",
+    type: "(code: string) => string",
+  },
+  {
+    default: "left",
+    description: t("form.documentation.propsDescription.select.flagsPosition"),
+    id: 4,
+    prop: "flagsPosition",
+    type: "left | right | right-edge",
+  },
+  {
+    default: "rectangular",
+    description: t("form.documentation.propsDescription.select.flagsStyle"),
+    id: 5,
+    prop: "flagsStyle",
+    type: "circle | rectangular | square",
+  },
+  {
+    default: "en",
+    description: t("form.documentation.propsDescription.select.locale"),
+    id: 6,
+    prop: "locale",
+    type: "String",
+  },
+  {
+    default: "{ en: defaultEnCatalogue }",
+    description: t("form.documentation.propsDescription.select.locales"),
+    id: 7,
+    prop: "locales",
+    type: "Record<string, Record<string, string>>",
+  },
+  {
+    default: "true",
+    description: t("form.documentation.propsDescription.select.showFlag"),
+    id: 8,
+    prop: "showFlag",
+    type: "Boolean",
+  },
+];
+
+const slotsData = [
+  {
+    description: t("form.documentation.slotDescription.country.code"),
+    name: "default",
+    props: "{ code: string }",
+  },
+  {
+    description: t("form.documentation.slotDescription.country.label"),
+    name: "default",
+    props: "{ label: string }",
+  },
+];
 </script>
--- packages/vue-form/src/components/Country.vue
+++ packages/vue-form/src/components/Country.vue
@@ -1,13 +1,26 @@
 <template>
-  <div :data-country-code="countryCode">
-    {{ countryLabel }}
+  <div :data-country-code="countryCode" class="country-display">
+    <slot :code="countryCode" :label="countryLabel">
+      <div class="country-content">
+        <template v-if="showFlag">
+          <img
+            v-if="flagsPath"
+            :alt="countryLabel"
+            :class="getFlagClass()"
+            :src="flagsPath(countryCode)"
+          />
+          <span v-else :class="getFlagClass(countryCode)" />
+        </template>
+        <span class="country-label">{{ countryLabel }}</span>
+      </div>
+    </slot>
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue";
+import { computed, type PropType } from "vue";
 
-import englishData from "./country-picker/en.json";
+import { getFallbackTranslation } from "../utils/CountryPicker";
 
 type I18nConfigData = Record<string, Record<string, string>>;
 
@@ -20,26 +33,66 @@ const props = defineProps({
     default: "en",
     type: String,
   },
-  i18n: {
-    default: () => ({}),
-    type: Object as () => I18nConfigData,
+  flagsPath: {
+    default: undefined,
+    type: Function as PropType<(code: string) => string>,
+  },
+  flagsPosition: {
+    default: "left",
+    type: String as PropType<"left" | "right" | "right-edge">,
+    validator: (value: string) =>
+      ["left", "right", "right-edge"].includes(value),
+  },
+  flagsStyle: {
+    default: "rectangular",
+    type: String as PropType<"circle" | "rectangular" | "square">,
+    validator: (value: string) =>
+      ["circle", "rectangular", "square"].includes(value),
   },
   locale: {
     default: "en",
     type: String,
   },
+  locales: {
+    default: () => ({}),
+    type: Object as () => I18nConfigData,
+  },
+  showFlag: {
+    type: Boolean,
+    default: true,
+  },
 });
 
 const countryCode = computed(() => props.code.trim().toUpperCase());
 
+const fallbackTranslation = getFallbackTranslation(
+  props.fallbackLocale,
+  props.locales,
+);
+
 const countryLabel = computed(() => {
   const code = countryCode.value;
+  if (!code) {
+    return "";
+  }
 
   return (
-    props.i18n?.[props.locale]?.[code] ||
-    props.i18n?.[props.fallbackLocale]?.[code] ||
-    englishData[code as keyof typeof englishData] ||
-    "-"
+    props.locales?.[props.locale]?.[code] || fallbackTranslation?.[code] || code
   );
 });
+
+const getFlagClass = (code?: string) =>
+  [
+    "flag-icon",
+    code && `flag-icon-${code.trim().toLowerCase()}`,
+    props.flagsPosition === "right" && "flag-icon-right",
+    props.flagsPosition === "right-edge" && "flag-icon-right-edge",
+    props.flagsStyle === "circle" && "flag-icon-rounded",
+    props.flagsStyle === "square" && "flag-icon-squared",
+  ]
+    .filter(Boolean)
+    .join(" ");
 </script>
+<style lang="css">
+@import "../assets/css/country.css";
+</style>
