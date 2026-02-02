b256731263c3826f1c88e6b6e52561fa1df6732f
kabin thakuri <kabin@dzangolab.com>
2026-02-02 17:14:34 +0545

    feat(config): update ESLint configuration and dependencies

--- packages/vue-config/eslint.config.js
+++ packages/vue-config/eslint.config.js
@@ -1,20 +1,5 @@
-import path from "path";
-import { fileURLToPath } from "url";
-
-import { FlatCompat } from "@eslint/eslintrc";
-import js from "@eslint/js";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
+import vueConfig from "@prefabs.tech/eslint-config/vue.js";
 
 export default [
-  {
-    ignores: ["dist/**", "node_modules/**"],
-  },
-  ...compat.extends("@prefabs.tech/eslint-config/vue"),
+  ...vueConfig,
 ];
