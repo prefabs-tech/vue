ac8ff5f859c87e2db719d1031f82d94ea16f8899
kabin thakuri <kabin@dzangolab.com>
2026-02-06 09:59:00 +0545

    chore: remove import ignore rule for vite config

--- packages/vue-user/eslint.config.js
+++ packages/vue-user/eslint.config.js
@@ -2,15 +2,4 @@ import vueConfig from "@prefabs.tech/eslint-config/vue.js";
 
 export default [
   ...vueConfig,
-  {
-    files: ["vite.config.*"],
-    settings: {
-      "import/ignore": [
-        "@vitejs/plugin-vue",
-        "@vitejs/plugin-vue-jsx",
-        "vite",
-        "rollup",
-      ],
-    },
-  },
 ];
