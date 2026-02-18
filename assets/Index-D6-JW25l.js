d2ec7232cafe2683ffd34653bf70fb394ae22abf
KabinKhandThakuri <kabin@dzangolab.com>
2026-02-18 11:39:25 +0000

    Deploying to gh-pages from @ prefabs-tech/vue@1c384a878e9457e3aa2933a7ed18cd224896160d 🚀

---
+++ assets/Index-D6-JW25l.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./FormPage.vue_vue_type_style_index_0_lang-B9MiltuR.js";import{d as o,o as r,e as a}from"./index-xehVrwF-.js";const n={name:"FormView"},i=o({...n,setup(_){return(e,s)=>(r(),a(t,{title:e.$t("form.title")},null,8,["title"]))}});export{i as default};
