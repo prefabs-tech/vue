7273a45efe5d9b635e65f9141a0c65e1010150e6
KabinKhandThakuri <kabin@dzangolab.com>
2026-02-23 07:05:57 +0000

    Deploying to gh-pages from @ prefabs-tech/vue@0463d1809940cd3a11b93becf78ed69660aadf30 🚀

---
+++ assets/Index-DUoN04uG.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./TablePage.vue_vue_type_style_index_0_lang-Dc81b_Y-.js";import{d as o,o as a,e as n}from"./index-He64vdhC.js";const _={name:"TableDemo"},m=o({..._,setup(l){return(e,r)=>(a(),n(t,{title:e.$t("table.title")},null,8,["title"]))}});export{m as default};
