d5700a9addbdbb40967629e49b7e60d752b7b875
KabinKhandThakuri <KabinKhandThakuri@users.noreply.github.com>
2026-04-02 03:42:42 +0000

    Deploying to gh-pages from @ prefabs-tech/vue@0c6816caa49f5d9163bc7787dc245b32a447cf15 🚀

---
+++ assets/Index-CjmEvxX3.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./TablePage.vue_vue_type_style_index_0_lang-lpVVQwHG.js";import{d as a,o,h as n}from"./index-zQfi5wTG.js";const _={name:"TableView"},i=a({..._,setup(l){return(e,r)=>(o(),n(t,{title:e.$t("table.title")},null,8,["title"]))}});export{i as default};
---
+++ assets/Index-Dzcf1lRs.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./FormPage.vue_vue_type_style_index_0_lang-CLZhxIQH.js";import{d as o,o as r,h as a}from"./index-zQfi5wTG.js";const n={name:"FormView"},i=o({...n,setup(_){return(t,s)=>(r(),a(e,{title:t.$t("form.title")},null,8,["title"]))}});export{i as default};
