8523ff2178df2c767ff40773f428ae01eb61cdd7
kabin thakuri <kabin@dzangolab.com>
2026-05-14 14:27:33 +0545

    feat: initialize stub Vue3 Stripe package

---
+++ packages/vue-stripe/src/index.ts
@@ -0,0 +1,1 @@
+export { default as StripeProvider } from "./components/StripeProvider.vue";
---
+++ packages/vue-stripe/vite.config.ts
@@ -0,0 +1,50 @@
+import vue from "@vitejs/plugin-vue";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { defineConfig, loadEnv } from "vite";
+
+import { dependencies, peerDependencies } from "./package.json";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return {
+    build: {
+      lib: {
+        entry: resolve(dirname(fileURLToPath(import.meta.url)), "src/index.ts"),
+        fileName: (format) => `PrefabsTechVue3Stripe.${format}.js`,
+        name: "PrefabsTechVue3Stripe",
+      },
+      rollupOptions: {
+        external: [
+          ...Object.keys(peerDependencies),
+          ...Object.keys(dependencies),
+        ],
+        output: {
+          exports: "named",
+          globals: {
+            "@prefabs.tech/vue3-config": "PrefabsTechVue3Config",
+            "@prefabs.tech/vue3-i18n": "PrefabsTechVue3I18n",
+            "@prefabs.tech/vue3-ui": "PrefabsTechVue3UI",
+            vue: "Vue",
+          },
+        },
+      },
+      target: "esnext",
+    },
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        "@/": new URL("src/", import.meta.url).pathname,
+      },
+    },
+    test: {
+      coverage: {
+        provider: "istanbul",
+        reporter: ["text", "json", "html"],
+      },
+      environment: "jsdom",
+    },
+  };
+});
